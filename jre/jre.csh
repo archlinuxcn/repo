cd7a3ed6e78750dd3d41cca098aa171e279286e4
Phoenix Nemo <mrphoenixlzx@gmail.com>
2013-07-23 12:03:57 +0800

    removed jre

--- jre/jre.csh
+++
@@ -1,3 +0,0 @@
-setenv J2REDIR /opt/java/jre
-setenv PATH ${PATH}:/opt/java/jre/bin
-setenv JAVA_HOME /opt/java/
--- jre/jre.sh
+++
@@ -1,3 +0,0 @@
-export J2REDIR=/opt/java/jre
-export PATH=$PATH:/opt/java/jre/bin
-export JAVA_HOME=${JAVA_HOME:-/opt/java/jre}
