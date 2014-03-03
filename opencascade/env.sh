bd95ea01a69a332881b3092e7379976610eac042
Yichao Yu <yyc1992@gmail.com>
2014-03-03 14:57:48 -0500

    [opencascade] update

--- opencascade/env.sh
+++ opencascade/env.sh
@@ -1,15 +1,12 @@
 #!/bin/sh -f
 
-export OS_NAME=`uname`
-
 export PATH="$PATH:$CASROOT/bin"
-export LD_LIBRARY_PATH="$LD_LIBRARY_PATH:$CASROOT/lib"
+export LD_LIBRARY_PATH="${LD_LIBRARY_PATH:+$LD_LIBRARY_PATH:}$CASROOT/lib"
 
 export CSF_LANGUAGE=us
 export MMGT_CLEAR=1
 export CSF_EXCEPTION_PROMPT=1
 
-export CSF_MDTVFontDirectory="$CASROOT"/src/FontMFT
 export CSF_SHMessage="$CASROOT"/src/SHMessage
 export CSF_MDTVTexturesDirectory="$CASROOT"/src/Textures
 export CSF_XSMessage="$CASROOT"/src/XSMessage
@@ -22,15 +19,4 @@ export CSF_UnitsDefinition="$CASROOT"/src/UnitsAPI/Units.dat
 export CSF_IGESDefaults="$CASROOT"/src/XSTEPResource
 export CSF_STEPDefaults="$CASROOT"/src/XSTEPResource
 export CSF_XmlOcafResource="$CASROOT"/src/XmlOcafResource
-
 export CSF_GraphicShr="$CASROOT"/lib/libTKOpenGl.so
-
-export TCLHOME=/usr
-export TCLLIBPATH="$TCLHOME"/lib
-export ITK_LIBRARY="$TCLLIBPATH"
-export ITCL_LIBRARY="$TCLLIBPATH"
-if [ -n "TIX_LIBRARY" ]; then
-    export TIX_LIBRARY="";
-fi
-export TK_LIBRARY="$TCLLIBPATH"
-export TCL_LIBRARY="$TCLLIBPATH"
