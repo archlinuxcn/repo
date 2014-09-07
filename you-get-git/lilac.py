0119b5b045008f7570cc9c6b123fccc5021b8b06
lilydjwg <lilydjwg@gmail.com>
2014-09-07 16:41:13 +0800

    update and new lilac.py

---
+++ you-get-git/lilac.py
@@ -0,0 +1,13 @@
+from lilaclib import *
+
+build_prefix = 'extra-x86_64'
+
+def pre_build():
+  run_cmd(['makepkg', '-o'])
+  output = run_cmd(["git", "status", "-s", "PKGBUILD"]).strip()
+  if not output:
+    raise RuntimeError('no update available. something goes wrong?')
+
+def post_build():
+  git_add_files('PKGBUILD')
+  git_commit()
