af55b0fc6bda7a1c7541d994704cad495d70385e
lilydjwg <lilydjwg@gmail.com>
2014-09-08 16:54:42 +0800

    mitmproxy-git: add git to makedepends

--- mitmproxy-git/lilac.py
+++ mitmproxy-git/lilac.py
@@ -1,11 +1,21 @@
 #!/usr/bin/env python3
 
+import fileinput
+
 from lilaclib import *
 
 build_prefix = 'extra-x86_64'
 depends = ['python2-netlib-git']
-pre_build = aur_pre_build
 post_build = aur_post_build
+
+def pre_build():
+  aur_pre_build()
+  with fileinput.input(files=('PKGBUILD',), inplace=True) as f:
+    for line in f:
+      line = line.rstrip('\n')
+      if line.startswith('source='):
+        line = line + "\nmakedepends=('git')"
+      print(line)
 
 if __name__ == '__main__':
   lilac_build()
