8c395f56e92d890ac4133fe5ba11aa41c44335e2
lilydjwg <lilydjwg@gmail.com>
2014-09-10 21:43:43 +0800

    add aufs3-util-lily-git

---
+++ aufs3-util-lily-git/lilac.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python3
+
+import fileinput
+
+from lilaclib import *
+
+build_prefix = 'extra-x86_64'
+depends = ['python2-netlib-git']
+post_build = aur_post_build
+
+patch = '''\
+  lilyver=$(pacman -Q linux-lily | awk '{print $2}')
+  mkdir -p libau/linux
+  cp "/lib/modules/${lilyver}-lily/build/include/uapi/linux/aufs_type.h" libau/linux
+  sed -i 's/__user//g' libau/linux/aufs_type.h
+  mkdir -p fhsm/libau/linux
+  cp libau/linux/aufs_type.h fhsm/libau/linux
+  sed -i 's/-lrt -L. -lfhsm -L.. -lautil/-L. -lfhsm -L.. -lautil -lrt/' fhsm/Makefile
+
+'''
+
+def pre_build():
+  aur_pre_build()
+  with fileinput.input(files=('PKGBUILD',), inplace=True) as f:
+    for line in f:
+      line = line.rstrip('\n')
+      if line.strip() == 'make':
+        line = patch + line
+      elif line.startswith('pkgname='):
+        line = 'pkgname=aufs3-util-lily-git'
+      elif line.startswith('makedepends'):
+        line = "makedepends=('linux-lily' 'git')"
+      print(line)
+
+if __name__ == '__main__':
+  lilac_build()
+
