7a65293def5b18b5f07a96836a20b756c923810c
lilydjwg <lilydjwg@gmail.com>
2014-09-12 16:43:39 +0800

    fix

--- vim-lily/lilac.py
+++ vim-lily/lilac.py
@@ -19,15 +19,18 @@ atexit.register(bye_git_daemon)
 
 def pre_build():
   global p
+
   if not os.path.isdir('vim'):
     os.mkdir('vim')
     run_cmd(["git", "clone", "-b", "all", "git@github.com:lilydjwg/vim.git"], use_pty=True)
     with at_dir('vim'):
       run_cmd(["git", "remote", "add", "upstream", "git@github.com:vim-jp/vim.git"])
+
   with at_dir('vim'):
     run_cmd(["git", "reset", "--hard"])
     run_cmd(["git", "fetch", "upstream"], use_pty=True)
     run_cmd(["git", "merge", "--no-edit", "upstream/master"])
+
   cmd = ["git", "daemon", "--export-all", "--reuseaddr", "--base-path=.", "--listen=127.0.0.1", "--", "./vim"]
   p = subprocess.Popen(cmd)
   if p.poll():
