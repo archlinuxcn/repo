c799c02f34ac99887b022122d8d6cbd4e3952d82
lilydjwg <lilydjwg@gmail.com>
2014-09-12 16:04:17 +0800

    fix

--- vim-lily/lilac.py
+++ vim-lily/lilac.py
@@ -9,17 +9,17 @@ build_prefix = 'extra-x86_64'
 def pre_build():
   if not os.path.isdir('vim'):
     os.mkdir('vim')
-    run_cmd(["git", "clone", "-b", "all", "git@github.com:lilydjwg/vim.git"])
+    run_cmd(["git", "clone", "-b", "all", "git@github.com:lilydjwg/vim.git"], use_pty=True)
     with at_dir('vim'):
       run_cmd(["git", "remote", "add", "upstream", "git@github.com:vim-jp/vim.git"])
   with at_dir('vim'):
     run_cmd(["git", "reset", "--hard"])
-    run_cmd(["git", "fetch", "uptream"])
-    run_cmd(["git", "merge", "uptream/master"])
+    run_cmd(["git", "fetch", "upstream"], use_pty=True)
+    run_cmd(["git", "merge", "--no-edit", "uptream/master"])
 
 def post_build():
   with at_dir('vim'):
-    run_cmd(["git", "push", "origin", "all"])
+    run_cmd(["git", "push", "origin", "all"], use_pty=True)
 
 if __name__ == '__main__':
   single_main()
