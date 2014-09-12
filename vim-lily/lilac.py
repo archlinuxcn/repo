d1edd0a1f17130c4d2a89aebbf220335e2343411
lilydjwg <lilydjwg@gmail.com>
2014-09-12 15:55:25 +0800

    vim-lily: fixes

--- vim-lily/lilac.py
+++ vim-lily/lilac.py
@@ -6,9 +6,5 @@ from lilaclib import *
 
 build_prefix = 'extra-x86_64'
 
-def post_build():
-  os.chdir('vim')
-  run_cmd(["git", "push", "git@github.com:lilydjwg/vim.git", "all"])
-
 if __name__ == '__main__':
   single_main()
