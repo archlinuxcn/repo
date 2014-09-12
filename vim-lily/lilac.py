71ad664b88b6b09258f207daacc4b35a4d23cbee
lilydjwg <lilydjwg@gmail.com>
2014-09-12 15:23:28 +0800

    vim-lily: can't use ssh for git in build env

--- vim-lily/lilac.py
+++ vim-lily/lilac.py
@@ -8,7 +8,7 @@ build_prefix = 'extra-x86_64'
 
 def post_build():
   os.chdir('vim')
-  run_cmd(["git", "push"])
+  run_cmd(["git", "push", "git@github.com:lilydjwg/vim.git", "all"])
 
 if __name__ == '__main__':
   single_main()
