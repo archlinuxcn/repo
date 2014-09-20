50604932bae081508947b9fde283d809c9dbded5
lilydjwg <lilydjwg@gmail.com>
2014-09-20 15:18:36 +0800

    add lilac.py for python-cld2-hg and cld2-svn

---
+++ cld2-svn/lilac.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+
+from lilaclib import *
+
+build_prefix = 'extra-x86_64'
+
+def pre_build():
+  run_cmd(['makepkg', '-o'])
+  output = run_cmd(["git", "status", "-s", "PKGBUILD"]).strip()
+  if not output:
+    raise RuntimeError('no update available. something goes wrong?')
+
+def post_build():
+  git_add_files('PKGBUILD')
+  git_commit()
+
+if __name__ == '__main__':
+  single_main()
