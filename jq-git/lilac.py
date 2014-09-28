9de2ba2310ab2432e162fb79f6c7b2dd774e9c28
lilydjwg <lilydjwg@gmail.com>
2014-09-28 14:26:54 +0800

    jq-git: fix upstream PKGBUILD

--- jq-git/lilac.py
+++ jq-git/lilac.py
@@ -1,11 +1,24 @@
 #!/usr/bin/env python3
 
+import fileinput
+
 from lilaclib import *
 
 build_prefix = 'extra-x86_64'
 depends = ['oniguruma']
-pre_build = aur_pre_build
 post_build = aur_post_build
+
+def pre_build():
+  aur_pre_build()
+
+  with fileinput.input(files=('PKGBUILD',), inplace=True) as f:
+    for line in f:
+      line = line.rstrip('\n')
+      if line.startswith('depends=('):
+        line = 'depends=("oniguruma" ' + line[len('depends=('):]
+      elif line.lstrip().startswith('git describe'):
+        line = line.replace('describe', 'describe --tags')
+      print(line)
 
 if __name__ == '__main__':
   single_main()
