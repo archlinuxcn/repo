019b31fa1c01c1ba2c67e835cec7fe06a9a5c393
lilydjwg <lilydjwg@gmail.com>
2014-09-29 16:44:57 +0800

    update sass

--- ruby-sass/lilac.py
+++ ruby-sass/lilac.py
@@ -4,8 +4,24 @@ from lilaclib import *
 
 build_prefix = 'extra-x86_64'
 depends = ['ruby-yard', 'ruby-maruku']
-pre_build = aur_pre_build
-post_build = aur_post_build
+
+def pre_build():
+  # AUR is out-of-date
+  # aur_pre_build()
+  with fileinput.input(files=('PKGBUILD',), inplace=True) as f:
+    for line in f:
+      line = line.rstrip('\n')
+      if line.startswith('pkgver='):
+        line = 'pkgver=' + s.get('https://rubygems.org/api/v1/versions/sass.json').json()[0]['number']
+      elif line.startswith('sha256sums='):
+        continue
+      print(line)
+
+  run_cmd(['sh', '-c', 'makepkg -g >> PKGBUILD'])
+
+def post_build():
+  git_add_files('PKGBUILD')
+  git_commit()
 
 if __name__ == '__main__':
   single_main()
