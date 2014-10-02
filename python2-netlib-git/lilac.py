8db422b16ffb82c56cbf4bfd64309730d286fcac
lilydjwg <lilydjwg@gmail.com>
2014-10-02 13:46:57 +0800

    python2-netlib-git: update lilac.py

--- python2-netlib-git/lilac.py
+++ python2-netlib-git/lilac.py
@@ -3,8 +3,14 @@
 from lilaclib import *
 
 build_prefix = 'extra-x86_64'
-pre_build = aur_pre_build
 post_build = aur_post_build
+
+def pre_build():
+  aur_pre_build()
+  for line in edit_file('PKGBUILD'):
+    if line.startswith('makedepends=('):
+      line = line.replace(')', " 'python2-setuptools')")
+    print(line)
 
 if __name__ == '__main__':
   single_main()
