1945a48f5d7bb93c8a28e9f08f93558801b4ae77
lilydjwg <lilydjwg@gmail.com>
2014-10-02 13:47:54 +0800

    imagemagick-fftw: update lilac.py

--- imagemagick-fftw/lilac.py
+++ imagemagick-fftw/lilac.py
@@ -1,4 +1,3 @@
-import fileinput
 from types import SimpleNamespace
 
 from lilaclib import *
@@ -11,33 +10,31 @@ def pre_build():
   g.oldfiles = clean_directory()
   g.files = download_official_pkgbuild('imagemagick')
 
-  with fileinput.input(files=('PKGBUILD',), inplace=True) as f:
-    makedepends = False
-    packaging = False
-    for line in f:
-      line = line.rstrip('\n')
-      if line.startswith('pkgbase='):
-        continue
-      elif line.startswith('pkgname='):
-        line = 'pkgname=imagemagick-fftw'
-      elif line.startswith('makedepends=('):
-        makedepends = True
-      elif makedepends and line.endswith(')'):
-        line = line.replace(')', " 'fftw')")
-        makedepends = False
-      elif '--without-fftw' in line:
-        line = line.replace('--without-fftw', '--with-fftw')
-      elif line.startswith('package_imagemagick('):
-        line = 'package() {'
-        packaging = True
-      elif packaging and not line:
-        line = '  provides=("imagemagick=$pkgver")\n'
-        line += '  conflicts=("imagemagick")'
-      elif packaging and line.startswith('}'):
-        packaging = False
-      elif line.startswith('package_imagemagick-doc('):
-        break
-      print(line)
+  makedepends = False
+  packaging = False
+  for line in edit_file('PKGBUILD'):
+    if line.startswith('pkgbase='):
+      continue
+    elif line.startswith('pkgname='):
+      line = 'pkgname=imagemagick-fftw'
+    elif line.startswith('makedepends=('):
+      makedepends = True
+    elif makedepends and line.endswith(')'):
+      line = line.replace(')', " 'fftw')")
+      makedepends = False
+    elif '--without-fftw' in line:
+      line = line.replace('--without-fftw', '--with-fftw')
+    elif line.startswith('package_imagemagick('):
+      line = 'package() {'
+      packaging = True
+    elif packaging and not line:
+      line = '  provides=("imagemagick=$pkgver")\n'
+      line += '  conflicts=("imagemagick")'
+    elif packaging and line.startswith('}'):
+      packaging = False
+    elif line.startswith('package_imagemagick-doc('):
+      break
+    print(line)
 
 def post_build():
   git_rm_files(g.oldfiles)
