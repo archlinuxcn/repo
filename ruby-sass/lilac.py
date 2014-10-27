dcf112420be3c51e30e44608a35f3913a4529f2d
lilydjwg <lilydjwg@gmail.com>
2014-10-27 21:51:46 +0800

    ruby-sass/lilac.py: use edit_file instead

--- ruby-sass/lilac.py
+++ ruby-sass/lilac.py
@@ -10,14 +10,12 @@ depends = ['ruby-yard', 'ruby-maruku']
 def pre_build():
   # AUR is out-of-date
   # aur_pre_build()
-  with fileinput.input(files=('PKGBUILD',), inplace=True) as f:
-    for line in f:
-      line = line.rstrip('\n')
-      if line.startswith('pkgver='):
-        line = 'pkgver=' + s.get('https://rubygems.org/api/v1/versions/sass.json').json()[0]['number']
-      elif line.startswith('sha256sums='):
-        continue
-      print(line)
+  for line in edit_file('PKGBUILD'):
+    if line.startswith('pkgver='):
+      line = 'pkgver=' + s.get('https://rubygems.org/api/v1/versions/sass.json').json()[0]['number']
+    elif line.startswith('sha256sums='):
+      continue
+    print(line)
 
   run_cmd(['sh', '-c', 'makepkg -g >> PKGBUILD'])
 
