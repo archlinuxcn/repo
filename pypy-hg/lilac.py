93ecbc9e8b7172db9871ce1906664c204430d5dd
Yichao Yu <yyc1992@gmail.com>
2014-11-05 00:02:16 -0500

    [pypy-hg] do not error if there is no update

--- pypy-hg/lilac.py
+++ pypy-hg/lilac.py
@@ -2,7 +2,7 @@ from lilaclib import *
 
 build_prefix = ['archlinuxcn-i686', 'archlinuxcn-x86_64']
 
-pre_build = vcs_update
+pre_build = lambda: run_cmd(['makepkg', '-o'], use_pty=True)
 
 def post_build():
     git_add_files('PKGBUILD')
