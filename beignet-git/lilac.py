572e72dc9e2000fcb958c88c34a8af4a250b0fa5
Yichao Yu <yyc1992@gmail.com>
2014-11-05 00:10:23 -0500

    [beignet-git] add i686 build

--- beignet-git/lilac.py
+++ beignet-git/lilac.py
@@ -1,8 +1,8 @@
 from lilaclib import *
 
-build_prefix = ['archlinuxcn-x86_64']
+build_prefix = ['archlinuxcn-i686', 'archlinuxcn-x86_64']
 
-pre_build = vcs_update
+pre_build = lambda: run_cmd(['makepkg', '-o'], use_pty=True)
 
 def post_build():
     git_add_files('PKGBUILD')
