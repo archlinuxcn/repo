7562717fcc451f0421c1afe6a9824ecfa59845a1
Yichao Yu <yyc1992@gmail.com>
2014-11-05 00:06:45 -0500

    [lib32-pypy-hg] compile pyc, generate abi version, use pypy to compile

--- lib32-pypy-hg/lilac.py
+++ lib32-pypy-hg/lilac.py
@@ -2,7 +2,7 @@ from lilaclib import *
 
 build_prefix = ['multilib-archlinuxcn']
 
-pre_build = vcs_update
+pre_build = lambda: run_cmd(['makepkg', '-o'], use_pty=True)
 
 def post_build():
     git_add_files('PKGBUILD')
