c66c5374d16c5d4a29715c5be8785e5bba08bc83
lilydjwg <lilydjwg@gmail.com>
2014-11-24 12:16:23 +0800

    global depends on python2

--- global/lilac.py
+++ global/lilac.py
@@ -1,12 +1,16 @@
 #!/usr/bin/env python3
 
-import fileinput
-
 from lilaclib import *
 
 build_prefix = 'extra-x86_64'
 post_build = aur_post_build
-pre_build = aur_pre_build
+
+def pre_build():
+  aur_pre_build()
+  for line in edit_file('PKGBUILD'):
+    if line.startswith('depends='):
+      line = line + '\nmakedepends=("python2")'
+    print(line)
 
 if __name__ == '__main__':
   single_main()
