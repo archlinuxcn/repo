c0cb2add47bf2e4f208f9ee466518165e04adef3
Jiachen Yang <farseerfc@gmail.com>
2014-12-18 04:24:15 +0900

    fixing android-ndk, 7z x instead of exec binary

--- android-ndk/lilac.py
+++ android-ndk/lilac.py
@@ -7,8 +7,22 @@ post_build = aur_post_build
 
 def pre_build():
   # delete old source files as they have same names each version
-  run_cmd(["rm", "-f", "android-ndk_x86_64.bin", "android-ndk_i686.bin"])
+  run_cmd(["rm", "-f", "android-ndk_x85_64.bin", "android-ndk_i686.bin"])
   aur_pre_build()
 
+  remove_lines = ["chmod +x ${pkgname}_$CARCH.bin",
+                  "./${pkgname}_$CARCH.bin"]
+  first_time = True
+  for line in edit_file('PKGBUILD'):
+      if line.strip() in remove_lines:
+          if first_time:
+            print("7z x ${pkgname}_$CARCH.bin")
+            first_time = False
+      else:
+          print(line)
+          if line.startswith("arch="):
+              print("makedepends=('p7zip')")
+
+
 if __name__ == '__main__':
-  single_main()
+  single_main(build_prefix)
