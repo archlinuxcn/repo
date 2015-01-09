919af50fe88fb8b4500cbcb4280d551eea2d6171
lilydjwg <lilydjwg@gmail.com>
2015-01-09 21:00:38 +0800

    update ruby-sass to use aur

--- ruby-sass/lilac.py
+++ ruby-sass/lilac.py
@@ -6,16 +6,7 @@ build_prefix = 'extra-x86_64'
 depends = ['ruby-yard', 'ruby-maruku']
 
 def pre_build():
-  # AUR is out-of-date
-  # aur_pre_build()
-  for line in edit_file('PKGBUILD'):
-    if line.startswith('pkgver='):
-      line = 'pkgver=' + s.get('https://rubygems.org/api/v1/versions/sass.json').json()[0]['number']
-    elif line.startswith('sha256sums='):
-      continue
-    print(line)
-
-  run_cmd(['sh', '-c', 'makepkg -g >> PKGBUILD'])
+  aur_pre_build()
 
 def post_build():
   git_add_files('PKGBUILD')
