850c2cb6fe1f89b18aaa564d963c32be8301a34e
lilydjwg <lilydjwg@gmail.com>
2015-01-12 15:43:35 +0800

    fix gnome-screenruler and its dependencies

--- ruby-text/lilac.py
+++ ruby-text/lilac.py
@@ -5,21 +5,18 @@ import fileinput
 from lilaclib import *
 
 build_prefix = 'extra-x86_64'
-depends = ['ruby-locale', 'ruby-levenshtein', 'ruby-text']
+
 def pre_build():
-  # AUR is out-of-date
-  # aur_pre_build()
   for line in edit_file('PKGBUILD'):
     if line.startswith('build()'):
       line = 'package() {'
     elif line.startswith('pkgver='):
-      line = 'pkgver=' + s.get('https://rubygems.org/api/v1/versions/gettext.json').json()[0]['number']
+      line = 'pkgver=' + s.get('https://rubygems.org/api/v1/versions/text.json').json()[0]['number']
     elif line.startswith(('sha256sums=', 'md5sums=')):
       continue
     print(line)
   run_cmd(['sh', '-c', 'makepkg -g >> PKGBUILD'])
 
-# post_build = aur_post_build
 def post_build():
   git_add_files('PKGBUILD')
   git_commit()
