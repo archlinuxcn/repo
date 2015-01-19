0341ae962b53cdb9d3e3113a7259560510426938
Stephen Zhang <zsrkmyn@archlinuxcn.org>
2015-01-19 08:26:04 +0000

    fix lilac.py for sfnt2woff

--- sfnt2woff/lilac.py
+++ sfnt2woff/lilac.py
@@ -9,7 +9,7 @@ def pre_build():
 
   for line in edit_file('PKGBUILD'):
     if line.startwith('license'):
-        line = "license=('MPL' 'GPL2' 'LGPL2.1')\ndepends=('zlib')"
+      line = "license=('MPL' 'GPL2' 'LGPL2.1')\ndepends=('zlib')"
     print(line)
 
 
