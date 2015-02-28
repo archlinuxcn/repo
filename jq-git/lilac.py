1a78dd495095ca12f52ed4ed83e1e1adccf9f8ca
lilydjwg <lilydjwg@gmail.com>
2015-02-28 22:52:58 +0800

    jq-git: AUR upstream fixed its problem

--- jq-git/lilac.py
+++ jq-git/lilac.py
@@ -8,18 +8,7 @@ build_prefix = 'extra-x86_64'
 post_build = aur_post_build
 
 def pre_build():
-  aur_pre_build(do_vcs_update=False)
-
-  with fileinput.input(files=('PKGBUILD',), inplace=True) as f:
-    for line in f:
-      line = line.rstrip('\n')
-      if line.startswith('depends=('):
-        line = 'depends=("oniguruma" ' + line[len('depends=('):]
-      elif line.lstrip().startswith('git describe'):
-        line = line.replace('describe', 'describe --tags')
-      print(line)
-
-  vcs_update()
+  aur_pre_build()
 
 if __name__ == '__main__':
   single_main()
