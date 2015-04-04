f8c0fcbf83b2f566076374dd5082c89586eec594
Jiachen Yang <farseerfc@gmail.com>
2015-04-04 21:09:49 +0900

    fixing awesome34 lilac.py and PKGBUILD

--- awesome34/lilac.py
+++ awesome34/lilac.py
@@ -1,16 +1,46 @@
 #!/usr/bin/env python3
 #
-# This file is the most simple lilac.py file,
-# and it suits for most packages in AUR.
+# This is a complex version of lilac.py for building
+# a package from AUR.
+#
+# You can do something before/after building a package,
+# including modify the 'pkgver' and 'md5sum' in PKBUILD.
+#
+# This is especially useful when a AUR package is
+# out-of-date and you want to build a new one, or you
+# want to build a package directly from sourceforge but
+# using PKGBUILD from AUR.
+#
+# See also:
+# [1] ruby-sass/lilac.py
+# [2] aufs3-util-lily-git/lilac.py
+# [3] octave-general/lilac.py
 #
 
 from lilaclib import *
 
-depends = ["lua51-doc"]
+depends = ['lua51-doc']
 
 build_prefix = ['extra-x86_64', 'extra-i686']
-pre_build = aur_pre_build
+
+
+def pre_build():
+    aur_pre_build()
+
+    for line in edit_file('PKGBUILD'):
+        # edit PKGBUILD
+        if line.strip().startswith("'libev'"):
+            print("'libxcursor'")
+        if line.strip().startswith("patch -p1"):
+            line = "patch -p1 < ../awesome-lua51.patch"
+        print(line)
+
+
 post_build = aur_post_build
 
+# do some cleanup here after building the package, regardless of result
+# def post_build_always(success):
+#   pass
+
 if __name__ == '__main__':
-  single_main(build_prefix)
+    single_main(build_prefix)
