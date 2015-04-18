6ca251d515c2eecb0a977d569fda3c548e53cbd5
Jiachen Yang <farseerfc@gmail.com>
2015-04-18 17:45:24 +0900

    kdeconnect-frameworks-git: fixed lilac.py

--- kdeconnect-frameworks-git/lilac.py
+++ kdeconnect-frameworks-git/lilac.py
@@ -19,30 +19,10 @@
 
 from lilaclib import *
 
-build_prefix = ['extra-x86_64', 'extra-i686']
+build_prefix = ['extra-x86_64']
 
-
-def pre_build():
-    aur_pre_build()
-
-    for line in edit_file('PKGBUILD'):
-        # edit PKGBUILD
-        if line.strip().startswith("depends="):
-            words = line.split(" ")
-            words.remove("kcmutils-git")
-            line = " ".join(words)
-        if line.strip().startswith("makedepends="):
-            words = line.split(" ")
-            words.insert(-1, "'kcmutils>=5.9'")
-            line = " ".join(words)
-        print(line)
-
-
+pre_build = aur_pre_build
 post_build = aur_post_build
-
-# do some cleanup here after building the package, regardless of result
-# def post_build_always(success):
-#   pass
 
 if __name__ == '__main__':
     single_main(build_prefix)
