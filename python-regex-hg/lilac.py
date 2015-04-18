1ab2c59e494e57bc84cc4382aadad038d961461d
lilydjwg <lilydjwg@gmail.com>
2015-04-18 20:57:51 +0800

    python-regex-hg: update

--- python-regex-hg/lilac.py
+++ python-regex-hg/lilac.py
@@ -5,10 +5,7 @@ from lilaclib import *
 build_prefix = 'extra-x86_64'
 
 def pre_build():
-  run_cmd(['makepkg', '-o'])
-  output = run_cmd(["git", "status", "-s", "PKGBUILD"]).strip()
-  if not output:
-    raise RuntimeError('no update available. something goes wrong?')
+  vcs_update()
 
 def post_build():
   git_add_files('PKGBUILD')
