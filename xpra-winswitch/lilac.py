cf39e27542f15460f4257a9bc6e6f4c44481711e
Jiachen Yang <farseerfc@gmail.com>
2015-06-08 01:50:49 +0900

    xpra-winswitch: fixed dependencies

--- xpra-winswitch/lilac.py
+++ xpra-winswitch/lilac.py
@@ -9,8 +9,16 @@ from lilaclib import *
 depends = ["python2-lz4"]
 
 build_prefix = 'extra-x86_64'
-pre_build = aur_pre_build
 post_build = aur_post_build
+
+def pre_build():
+    aur_pre_build()
+
+    for line in edit_file('PKGBUILD'):
+        # edit PKGBUILD
+        if line.strip().startswith("depends"):
+            line = line + " 'libxkbfile'"
+        print(line)
 
 if __name__ == '__main__':
   single_main()
