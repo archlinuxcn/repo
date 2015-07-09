eccc953959098151fd3176f29bb74b6d90c69296
Stephen Zhang <zsrkmyn@archlinuxcn.org>
2015-07-09 07:41:42 +0000

    updpkg: universal-ctags-git

--- universal-ctags-git/lilac.py
+++ universal-ctags-git/lilac.py
@@ -3,8 +3,13 @@
 from lilaclib import *
 
 build_prefix = 'extra-x86_64'
-pre_build = aur_pre_build
-post_build = aur_post_build
+
+def pre_build():
+  vcs_update()
+
+def post_build():
+  git_add_files('PKGBUILD')
+  git_commit()
 
 if __name__ == '__main__':
   single_main(build_prefix)
