c2936856cff6d92d526e2ba516a988f6d7ce1ec3
inkn <i@inkn.ml>
2015-07-15 09:43:27 +0800

    addpkg python-peewee, python-wtforms,python-flask-peewee, python-wtf-peewee

---
+++ python-flask-peewee/lilac.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+#
+# This file is for pypi package building.
+#
+# You don't need to edit most things except 'depends=[]'.
+#
+# It is too simple to say something more :)
+#
+
+from lilaclib import *
+
+build_prefix = 'extra-x86_64'
+
+def pre_build():
+  pypi_pre_build(depends=['setuptools', 'flask', 'wtforms', 'peewee', 'wtf-peewee'])
+
+def post_build():
+  pypi_post_build()
+
+if __name__ == '__main__':
+  single_main()
