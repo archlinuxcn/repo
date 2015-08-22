73e583d626044daaadf56e90c2dc614bd621276f
Jiachen Yang <farseerfc@gmail.com>
2015-08-22 15:48:49 +0900

    replace telegram-desktop-{cn,git} with telegram-desktop-bin-dev

--- telegram-desktop-bin-dev/lilac.py
+++ telegram-desktop-bin-dev/lilac.py
@@ -6,14 +6,11 @@
 
 from lilaclib import *
 
-depends=["dee", "libunity", "libdbusmenu-gtk2", ('libindicator', 'libindicator-gtk2'), ("libappindicator", "libappindicator-gtk2")]
-
 build_prefix = 'extra-x86_64'
 pre_build = aur_pre_build
 post_build = aur_post_build
 
-
-
 if __name__ == '__main__':
     single_main(build_prefix)
+
 
