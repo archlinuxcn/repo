e7db41631bfc8e3286aa4876f7d514cfe9f70a4e
Jiachen Yang <farseerfc@gmail.com>
2015-09-22 23:09:00 +0900

    hacking qml-material-git/lilac.py

--- qml-material-git/lilac.py
+++ qml-material-git/lilac.py
@@ -7,36 +7,43 @@
 from lilaclib import *
 
 build_prefix = 'extra-x86_64'
-post_build = aur_post_build
 
-def pre_build():
-    # aur_pre_build()
+_g = SimpleNamespace()
 
-    # hack to change PKGBUILD before run makepkg -od
-    if os.path.exists('PKGBUILD'):
-      pkgver, pkgrel = get_pkgver_and_pkgrel()
-    else:
-      pkgver = None
+def pre_build(name=None, *, do_vcs_update=True):
+  if os.path.exists('PKGBUILD'):
+    pkgver, pkgrel = get_pkgver_and_pkgrel()
+  else:
+    pkgver = None
 
-    _g.aur_pre_files = clean_directory()
-    if name is None:
-      name = os.path.basename(os.getcwd())
-    _g.aur_building_files = download_aur_pkgbuild(name)
+  _g.aur_pre_files = clean_directory()
+  if name is None:
+    name = os.path.basename(os.getcwd())
+  _g.aur_building_files = download_aur_pkgbuild(name)
 
-    new_pkgver = get_pkgver_and_pkgrel()[0]
-    if pkgver and pkgver == new_pkgver:
-      # change pkgrel to what specified in PKGBUILD
-      update_pkgrel(pkgrel)
+  new_pkgver = get_pkgver_and_pkgrel()[0]
+  if pkgver and pkgver == new_pkgver:
+    # change pkgrel to what specified in PKGBUILD
+    update_pkgrel(pkgrel)
 
-    # change PKGBUILD
-    for line in edit_file('PKGBUILD'):
-        # edit PKGBUILD
-        if line.strip().startswith("prepare() {"):
-            line="build() {"
-        print(line)
+  # hack to change PKGBUILD before run makepkg -od
+  for line in edit_file('PKGBUILD'):
+      # edit PKGBUILD
+      if line.strip().startswith("prepare() {"):
+          line="build() {"
+      print(line)
 
-    if do_vcs_update and name.endswith(('-git', '-hg', '-svn', '-bzr')):
-      vcs_update()
+  if do_vcs_update and name.endswith(('-git', '-hg', '-svn', '-bzr')):
+    vcs_update()
+
+def post_build():
+  git_rm_files(_g.aur_pre_files)
+  git_add_files(_g.aur_building_files)
+  output = run_cmd(["git", "status", "-s", "."]).strip()
+  if output:
+    git_commit()
+  del _g.aur_pre_files, _g.aur_building_files
+
 
 if __name__ == '__main__':
     single_main(build_prefix)
