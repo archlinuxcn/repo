22c547ea99f7b6d69e98670d7b2cddb974170bcb
lilydjwg <lilydjwg@gmail.com>
2015-09-27 23:10:51 +0800

    update python-alembic

--- python-alembic/lilac.py
+++ python-alembic/lilac.py
@@ -4,9 +4,11 @@ from lilaclib import *
 
 build_prefix = 'extra-x86_64'
 post_build = aur_post_build
+depends = ['python-editor']
 
 def pre_build():
-  aur_pre_build()
+  # aur_pre_build()
+  pass
 
 if __name__ == '__main__':
   single_main()
