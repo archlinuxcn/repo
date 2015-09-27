8146f5c4ab88705314cd1025cb41b5065857eb2f
lilydjwg <lilydjwg@gmail.com>
2015-09-27 23:10:37 +0800

    add python-editor, required by python-alembic

---
+++ python-editor/lilac.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+
+from lilaclib import *
+
+build_prefix = 'extra-x86_64'
+post_build = aur_post_build
+
+def pre_build():
+  aur_pre_build()
+
+if __name__ == '__main__':
+  single_main()
+
+# vim:set ts=2 sw=2 et:
