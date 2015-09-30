1b9298ff8de13cc00bfdaa17f2cf61470bcb71c9
Jiachen Yang <farseerfc@gmail.com>
2015-09-30 11:59:31 +0200

    libfilteraudio-git: git clean up before aur_pre_buildx

--- libfilteraudio-git/lilac.py
+++ libfilteraudio-git/lilac.py
@@ -3,8 +3,11 @@
 from lilaclib import *
 
 build_prefix = 'extra-x86_64'
-pre_build = aur_pre_build
 post_build = aur_post_build
+
+def pre_build():
+    run_cmd(['git', 'clean', '-dfx'])
+    aur_pre_build()
 
 if __name__ == '__main__':
   single_main()
