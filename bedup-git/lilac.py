8a5ee3c5ba295a069658bc3cc97d73d627c8ae1c
lilydjwg <lilydjwg@gmail.com>
2015-10-02 15:24:27 +0800

    bedup-git: lilac 的 depends 需要注意顺序

--- bedup-git/lilac.py
+++ bedup-git/lilac.py
@@ -3,7 +3,7 @@ from lilaclib import *
 build_prefix = 'extra-x86_64'
 
 pre_build = vcs_update
-depends = ['python-alembic', 'python-editor']
+depends = ['python-editor', 'python-alembic']
 
 def post_build():
   git_add_files('PKGBUILD')
