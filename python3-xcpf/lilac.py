e711d18d84fd9507ce607de396a2954b0019ecd2
Jiachen Yang <farseerfc@gmail.com>
2015-10-08 22:09:59 +0900

    fixing pm2ml dependency

--- python3-xcpf/lilac.py
+++ python3-xcpf/lilac.py
@@ -7,12 +7,10 @@
 from lilaclib import *
 
 build_prefix = 'extra-x86_64'
-
-depends = ['python3-xcpf']
-
 pre_build = aur_pre_build
 post_build = aur_post_build
 
-
 if __name__ == '__main__':
-  single_main()
+    single_main(build_prefix)
+
+
