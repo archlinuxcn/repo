05103c7c4eaf2606a9eb4acaaa4ef8c550175cd5
Jingbei Li <i@jingbei.li>
2015-10-14 13:17:38 +0800

    Simplified the lilac.py for *natspec

--- libnatspec/lilac.py
+++ libnatspec/lilac.py
@@ -1,47 +1,14 @@
 #!/usr/bin/env python3
 #
-# This is a complex version of lilac.py for building
-# a package from AUR.
-#
-# You can do something before/after building a package,
-# including modify the 'pkgver' and 'md5sum' in PKBUILD.
-#
-# This is especially useful when a AUR package is
-# out-of-date and you want to build a new one, or you
-# want to build a package directly from sourceforge but
-# using PKGBUILD from AUR.
-#
-# See also:
-# [1] ruby-sass/lilac.py
-# [2] aufs3-util-lily-git/lilac.py
-# [3] octave-general/lilac.py
+# This file is the most simple lilac.py file,
+# and it suits for most packages in AUR.
 #
 
 from lilaclib import *
 
 build_prefix = 'extra-x86_64'
-# depends = []
-
-def pre_build():
-  # obtain base PKGBUILD, e.g.
-  aur_pre_build()
-
-  for line in edit_file('PKGBUILD'):
-    # edit PKGBUILD
-    if '.AURINFO' in line:
-        print('#'+line)
-    elif 'SKIP' in line:
-        print('#'+line)
-    else:
-        print(line)
-
-def post_build():
-  # do something after the package has successfully been built
-  aur_post_build()
-
-# do some cleanup here after building the package, regardless of result
-# def post_build_always(success):
-#   pass
+pre_build = aur_pre_build
+post_build = aur_post_build
 
 if __name__ == '__main__':
   single_main()
