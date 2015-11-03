ef12b97d75bcd374f17f18182a67a6ae1e10929f
Jiachen Yang <farseerfc@gmail.com>
2015-11-03 16:25:14 +0900

    updpkg: liri-browser-git

--- liri-browser-git/lilac.py
+++ liri-browser-git/lilac.py
@@ -15,14 +15,6 @@ def pre_build():
     run_cmd(["git", "clean", "-x", "-d", "-f", "."])
     aur_pre_build()
 
-    for line in edit_file('PKGBUILD'):
-        if line.strip().startswith('depends='):
-            words = line.split(' ')
-            words.insert(-1, "'qt5-multimedia'")
-            words.insert(-1, "'vlc'")
-            line = ' '.join(words)
-        print(line)
-
 if __name__ == '__main__':
     single_main(build_prefix)
 
