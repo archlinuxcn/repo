62002898075f41056a604050e3a94d3fde714086
Jiachen Yang <farseerfc@gmail.com>
2015-11-06 19:21:02 +0900

    switch back to default branch

--- qtox-git/lilac.py
+++ qtox-git/lilac.py
@@ -5,14 +5,8 @@ from lilaclib import *
 depends = ['libfilteraudio-git']
 
 build_prefix = 'extra-x86_64'
+pre_build = aur_pre_build
 post_build = aur_post_build
-
-def pre_build():
-    aur_pre_build()
-    for line in edit_file('PKGBUILD'):
-        if line.strip().startswith('source=('):
-            line='source=("$_pkgname::git+https://github.com/tux3/qTox.git#branch=newav_final_for_realsies")'
-        print(line)
 
 
 if __name__ == '__main__':
