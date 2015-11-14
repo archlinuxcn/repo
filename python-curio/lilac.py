1a9b30e873bec8fcb633a4250502c0ed06c1882e
lilydjwg <lilydjwg@gmail.com>
2015-11-15 00:31:21 +0800

    add python-curio

---
+++ python-curio/lilac.py
@@ -0,0 +1,14 @@
+from lilaclib import *
+
+build_prefix = 'extra-x86_64'
+
+def pre_build():
+  pypi_pre_build()
+
+def post_build():
+  git_add_files('PKGBUILD')
+  git_commit()
+  update_aur_repo()
+
+if __name__ == '__main__':
+  single_main()
