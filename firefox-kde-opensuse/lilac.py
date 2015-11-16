9c196fb9d44c35ee9440bc9421cbba0aaf3a27f4
Jiajun Wang <amesists@gmail.com>
2015-11-16 21:22:11 +0800

    fix lilac.py for firefox-kde-opensuse

--- firefox-kde-opensuse/lilac.py
+++ firefox-kde-opensuse/lilac.py
@@ -2,9 +2,34 @@
 
 from lilaclib import *
 
+import os
+import re
+import ntpath
+
 build_prefix = 'archlinuxcn-x86_64'
-pre_build = aur_pre_build
-post_build = aur_post_build
+
+def get_source_list():
+    buf = ''
+    for line in open("PKGBUILD"):
+        if not line.strip().startswith("#"):
+            buf += line
+
+    match = re.search('source=\(([^)]+)\)', buf)
+    result = match.group(1)
+
+    return [ntpath.basename(f) for f in result.split()]
+
+def pre_build():
+    # clean the patches
+    l = get_source_list()
+    for f in l:
+        if os.path.exists(f):
+            os.remove(f)
+
+    aur_pre_build()
+
+def post_build():
+    aur_post_build()
 
 if __name__ == '__main__':
   single_main()
