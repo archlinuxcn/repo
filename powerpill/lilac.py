38cdee2c9b806dc7e13776f57b136cb8e5b9083a
Jingbei Li <i@jingbei.li>
2015-11-25 14:15:51 +0800

    added powerpill/lilac.py fix #175 drop deepin-pygtk

---
+++ powerpill/lilac.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python3
+#
+# This file is the most simple lilac.py file,
+# and it suits for most packages in AUR.
+#
+
+from lilaclib import *
+
+build_prefix = 'extra-x86_64'
+depends = ['python3-xcpf' 'pm2ml']
+
+def pre_build():
+  # obtain base PKGBUILD, e.g.
+  aur_pre_build()
+
+  count=0
+  for line in edit_file('PKGBUILD'):
+    # edit PKGBUILD
+    count=count+1
+    if count in {14,18,22,24}:
+        print('#'+line)
+    else:
+        print(line)
+
+post_build = aur_post_build
+
+if __name__ == '__main__':
+  single_main()
