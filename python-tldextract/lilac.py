42167f5725a1943877d52b22df714900edafe39a
lilydjwg <lilydjwg@gmail.com>
2015-12-15 22:34:18 +0800

    python-tldextract: update lilac.py to update AUR

--- python-tldextract/lilac.py
+++ python-tldextract/lilac.py
@@ -13,27 +13,27 @@ def pre_build():
   md5sum = release['md5_digest']
   url = release['url']
 
-  with fileinput.input(files=('PKGBUILD',), inplace=True) as f:
-    oldver = None
-    for line in f:
-      line = line.rstrip('\n')
-      if line.startswith('pkgver='):
-        oldver = line.split('=', 1)[-1]
-        line = 'pkgver=' + pkgver
-      elif line.startswith('pkgrel='):
-        oldrel = int(line.split('=', 1)[-1])
-        if oldver != pkgver:
-          line = 'pkgrel=1'
-          # else we're rebuilding, leave as it is
-      elif line.startswith('source='):
-        line = 'source=(%s)' % url
-      elif line.startswith('md5sums='):
-        line = 'md5sums=(%s)' % md5sum
-      print(line)
+  oldver = None
+  for line in edit_file('PKGBUILD'):
+    line = line.rstrip('\n')
+    if line.startswith('pkgver='):
+      oldver = line.split('=', 1)[-1]
+      line = 'pkgver=' + pkgver
+    elif line.startswith('pkgrel='):
+      oldrel = int(line.split('=', 1)[-1])
+      if oldver != pkgver:
+        line = 'pkgrel=1'
+        # else we're rebuilding, leave as it is
+    elif line.startswith('source='):
+      line = 'source=(%s)' % url
+    elif line.startswith('md5sums='):
+      line = 'md5sums=(%s)' % md5sum
+    print(line)
 
 def post_build():
   git_add_files('PKGBUILD')
   git_commit()
+  update_aur_repo()
 
 if __name__ == '__main__':
   single_main()
