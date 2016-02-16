a9532b92607f2cbe394313f171660113a853c1e4
Jiachen Yang <farseerfc@gmail.com>
2016-02-16 18:05:22 +0900

    remove liri-{browser,player}-git fix #196

--- liri-browser-git/liri-browser.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-# Allow users to override command-line options
-if [[ -f ~/.config/liri-flags.conf ]]; then
-  LIRI_USER_FLAGS="$(cat ~/.config/liri-flags.conf)"
-fi
-
-# Launch
-exec /opt/liri-browser-build/liri-browser $LIRI_USER_FLAGS $@
