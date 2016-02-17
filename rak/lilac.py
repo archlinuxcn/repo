17827cdbf9b3250e1b22ae58482c192ad5e6d070
farkasity <farkasity@gmail.com>
2016-02-17 13:20:48 +0800

    use indent 2 space

--- rak/lilac.py
+++ rak/lilac.py
@@ -6,17 +6,14 @@ from lilaclib import *
 
 build_prefix = 'extra-x86_64'
 
-
 def pre_build():
-    for line in edit_file('PKGBUILD'):
-        if line.startswith('pkgver='):
-            line = 'pkgver=' + s.get('https://rubygems.org/api/v1/versions/rak.json').json()[0]['number']
-            print(line)
-            run_cmd(['sh', '-c', 'makepkg -g >> PKGBUILD'])
-
+  for line in edit_file('PKGBUILD'):
+    if line.startswith('pkgver='):
+      line = 'pkgver=' + s.get('https://rubygems.org/api/v1/versions/rak.json').json()[0]['number']
+    print(line)
+    run_cmd(['sh', '-c', 'makepkg -g >> PKGBUILD'])
 
 post_build = aur_post_build
 
-
 if __name__ == '__main__':
-    single_main()
+  single_main()
