03d0c9c9965d510de816252803aea281880b065c
farkasity <farkasity@gmail.com>
2016-02-18 11:21:07 +0800

    'update rak: fix lilac.py'

--- rak/lilac.py
+++ rak/lilac.py
@@ -1,19 +1,20 @@
 #!/usr/bin/env python3
 
 import fileinput
-
 from lilaclib import *
 
 build_prefix = 'extra-x86_64'
 
 def pre_build():
-  for line in edit_file('PKGBUILD'):
-    if line.startswith('pkgver='):
-      line = 'pkgver=' + s.get('https://rubygems.org/api/v1/versions/rak.json').json()[0]['number']
-    print(line)
-    run_cmd(['sh', '-c', 'makepkg -g >> PKGBUILD'])
+  json = s.get('https://rubygems.org/api/v1/versions/rak.json').json()
+  pkgver = json[0]['number']
+  sha256sums = run_cmd(['sh', '-c', 'makepkg -g 2> /dev/null']).rstrip()
 
-post_build = aur_post_build
+  run_cmd(['sh', '-c', 'sed -i "/sha256sums/s/^.*$/' + sha256sums + '/" PKGBUILD'])
+
+def post_build():
+  git_add_files('PKGBUILD')
+  git_commit()
 
 if __name__ == '__main__':
   single_main()
