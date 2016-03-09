eeb0788d4eb5381df9ce21e8ce85227e9af0d507
lilydjwg <lilydjwg@gmail.com>
2016-03-09 18:31:44 +0800

    python-tldextract: depends on python-idna now

--- python-tldextract/lilac.py
+++ python-tldextract/lilac.py
@@ -7,32 +7,13 @@ from lilaclib import *
 build_prefix = 'extra-x86_64'
 
 def pre_build():
-  info = get_pypi_info('tldextract')
-  pkgver = info['info']['version']
-  release = [x for x in info['releases'][pkgver] if x['packagetype'] == 'sdist'][0]
-  md5sum = release['md5_digest']
-  url = release['url']
-
-  oldver = None
-  for line in edit_file('PKGBUILD'):
-    line = line.rstrip('\n')
-    if line.startswith('pkgver='):
-      oldver = line.split('=', 1)[-1]
-      line = 'pkgver=' + pkgver
-    elif line.startswith('pkgrel='):
-      oldrel = int(line.split('=', 1)[-1])
-      if oldver != pkgver:
-        line = 'pkgrel=1'
-        # else we're rebuilding, leave as it is
-    elif line.startswith('source='):
-      line = 'source=(%s)' % url
-    elif line.startswith('md5sums='):
-      line = 'md5sums=(%s)' % md5sum
-    print(line)
+  pypi_pre_build(
+    pypi_name='tldextract',
+    depends=['python-idna'],
+  )
 
 def post_build():
-  git_add_files('PKGBUILD')
-  git_commit()
+  pypi_post_build()
   update_aur_repo()
 
 if __name__ == '__main__':
