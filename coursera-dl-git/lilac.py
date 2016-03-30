c5a695c9896714f6ade318ace48797a00b790922
Stephen Zhang <zsrkmyn@archlinuxcn.org>
2016-03-30 11:21:51 +0800

    fix lilac.py for coursera-dl-git

--- coursera-dl-git/lilac.py
+++ coursera-dl-git/lilac.py
@@ -3,8 +3,23 @@
 from lilaclib import *
 
 build_prefix = 'extra-x86_64'
-pre_build = aur_pre_build
 post_build = aur_post_build
+
+def pre_build(name=None, *, do_vcs_update=True):
+  if os.path.exists('PKGBUILD'):
+    pkgver, pkgrel = get_pkgver_and_pkgrel()
+  else:
+    pkgver = None
+
+  if name is None:
+    name = os.path.basename(os.getcwd())
+
+  new_pkgver = get_pkgver_and_pkgrel()[0]
+  if pkgver and pkgver == new_pkgver:
+    # change pkgrel to what specified in PKGBUILD
+    update_pkgrel(pkgrel)
+
+    vcs_update()
 
 if __name__ == '__main__':
   single_main(build_prefix)
