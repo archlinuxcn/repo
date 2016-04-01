fba055e0f56d77dbc623d7efb328a76dbf664aa5
Yichao Yu <yyc1992@gmail.com>
2016-04-01 13:26:28 -0400

    [llvm-julia] try to add lilac config file

---
+++ llvm-julia/lilac.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+#
+# This file is the most simple lilac.py file,
+#
+
+from lilaclib import *
+
+build_prefix = 'extra-x86_64'
+def pre_build():
+    pass
+def post_build():
+    pass
+
+if __name__ == '__main__':
+  single_main(build_prefix)
