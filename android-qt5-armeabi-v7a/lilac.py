05b9421dc7edf2a86e83ca075f4ac11c7dff3dca
wicast <wicastchen@hotmail.com>
2016-04-03 02:00:37 +0800

    [android-qt5-armeabi-v7a]lilac fix

--- android-qt5-armeabi-v7a/lilac.py
+++ android-qt5-armeabi-v7a/lilac.py
@@ -9,7 +9,7 @@ from lilaclib import *
 
 build_prefix = 'multilib-archlinuxcn'
 # pre_build = aur_pre_build
-post_build = aur_post_build
+# post_build = aur_post_build
 
 if __name__ == '__main__':
   single_main()
