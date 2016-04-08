3af9113a4209010adc67f4e9e546a6309b22d5ad
Jingbei Li <i@jingbei.li>
2016-04-08 16:47:48 +0800

    fix the encoding problem for pyfm/lilac.py

--- pyfm/lilac.py
+++ pyfm/lilac.py
@@ -1,14 +1,47 @@
 #!/usr/bin/env python3
 #
-# This file is the most simple lilac.py file,
-# and it suits for most packages in AUR.
+# This is a complex version of lilac.py for building
+# a package from AUR.
+#
+# You can do something before/after building a package,
+# including modify the 'pkgver' and 'md5sum' in PKBUILD.
+#
+# This is especially useful when a AUR package is
+# out-of-date and you want to build a new one, or you
+# want to build a package directly from sourceforge but
+# using PKGBUILD from AUR.
+#
+# See also:
+# [1] ruby-sass/lilac.py
+# [2] aufs3-util-lily-git/lilac.py
+# [3] octave-general/lilac.py
 #
 
 from lilaclib import *
 
 build_prefix = 'extra-x86_64'
-pre_build = aur_pre_build
-post_build = aur_post_build
+
+def pre_build():
+  # obtain base PKGBUILD, e.g.
+  aur_pre_build()
+
+  for line in edit_file('PKGBUILD'):
+    # edit PKGBUILD
+    if 'setup.py install' in line:
+        print(line.replace('python', 'LANG=en_US.UTF-8 python'))
+    elif 'mpg123' in line:
+        print(line)
+        print('makedepends=(\'python-setuptools\' \'python2-setuptools\')')
+    else:
+        print(line)
+
+def post_build():
+  # do something after the package has successfully been built
+  aur_post_build()
+
+# do some cleanup here after building the package, regardless of result
+# def post_build_always(success):
+#   pass
 
 if __name__ == '__main__':
   single_main()
