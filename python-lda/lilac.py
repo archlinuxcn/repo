00e9dd44df2ed4e71df763121c43677123cafa2b
wicast <wicastchen@hotmail.com>
2016-04-26 18:22:26 +0800

    [python-lda]disable pypi since license field is empty

--- python-lda/lilac.py
+++ python-lda/lilac.py
@@ -9,11 +9,14 @@ from lilaclib import *
 
 build_prefix = 'extra-x86_64'
 
-def pre_build():
-  pypi_pre_build(depends=['python-pbr'])
+pre_build = aur_pre_build
+post_build = aur_post_build
 
-def post_build():
-  pypi_post_build()
+# def pre_build():
+#   pypi_pre_build(depends=['python-pbr'])
+#
+# def post_build():
+#   pypi_post_build()
 
 if __name__ == '__main__':
   single_main()
