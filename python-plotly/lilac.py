27f7b2e5ab7d7119e40ae60a1df347f73725d663
wicast <wicastchen@hotmail.com>
2016-04-28 10:07:39 +0800

    [python-plotly]fix lilac error

--- python-plotly/lilac.py
+++ python-plotly/lilac.py
@@ -10,7 +10,7 @@ from lilaclib import *
 build_prefix = 'extra-x86_64'
 
 def pre_build():
-  pypi_pre_build(depends=['python-requests' 'python-pytz'])
+  pypi_pre_build(depends=['python-requests', 'python-pytz'])
 
 def post_build():
   pypi_post_build()
