b7df133fbbb6c359694eb4126567d1511b0c64d8
Jiachen Yang <farseerfc@gmail.com>
2016-05-01 00:46:34 +0900

    try fix vivaldi lilac.py

--- vivaldi/lilac.py
+++ vivaldi/lilac.py
@@ -9,12 +9,11 @@ import glob
 
 build_prefix = 'extra-x86_64'
 
-def pre_build():
-    aur_pre_build()
-    for f in glob.glob(".*"):
-        os.unlink(f)
+pre_build = aur_pre_build
 
-post_build = aur_post_build
+def post_build():
+    git_rm_files(glob.glob(".*"))
+    aur_post_build()
 
 if __name__ == '__main__':
     single_main(build_prefix)
