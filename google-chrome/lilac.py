7239a119f3cbda3600e38192871d6a703889c6e2
Stephen Zhang <zsrkmyn@archlinuxcn.org>
2016-05-31 03:26:12 +0800

    fix alpm-hooks for vuze, google-chrome-dev, google-chrome AGAIN!

--- google-chrome/lilac.py
+++ google-chrome/lilac.py
@@ -3,8 +3,35 @@
 from lilaclib import *
 
 build_prefix = 'extra-x86_64'
-pre_build = aur_pre_build
 post_build = aur_post_build
 
+_pkg_name = 'google-chrome'
+
+dotinstall = '''
+note() {
+    printf "${blue}==>${yellow} NOTE:${bold} $1${all_off}\n"
+}
+
+all_off="$(tput sgr0)"
+bold="${all_off}$(tput bold)"
+blue="${bold}$(tput setaf 4)"
+yellow="${bold}$(tput setaf 3)"
+
+post_install() {
+    note "Custom flags should be put directly in: ~/.config/chrome-flags.conf"
+    note "The launcher is called: 'google-chrome-stable'"
+}
+
+post_upgrade() {
+    post_install
+}
+'''
+
+def pre_build():
+  aur_pre_build()
+  with edit_file('%s.install' % _pkg_name) as f:
+    print(dotinstall)
+
+
 if __name__ == '__main__':
-  single_main()
+  single_main(build_prefix)
