439124bc369a4198feb62680ecba0272e4e1df65
lilydjwg <lilydjwg@gmail.com>
2016-06-25 09:49:42 +0800

    yamllint: missing depends

--- yamllint/lilac.py
+++ yamllint/lilac.py
@@ -7,7 +7,9 @@ from lilaclib import *
 build_prefix = 'extra-x86_64'
 
 def pre_build():
-  pypi_pre_build()
+  pypi_pre_build(
+    depends=['python-yaml'],
+  )
 
 def post_build():
   pypi_post_build()
