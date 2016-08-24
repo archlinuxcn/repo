94918a3889cdaf95b0b6dbf7e6f5099e5c8328fe
Jingbei Li <i@jingbei.li>
2016-08-24 17:18:49 +0800

    fix electronic-wechat-git/lilac.py

--- electronic-wechat-git/lilac.py
+++ electronic-wechat-git/lilac.py
@@ -26,13 +26,6 @@ def pre_build():
   # obtain base PKGBUILD, e.g.
   aur_pre_build()
 
-  for line in edit_file('PKGBUILD'):
-    # edit PKGBUILD
-    if '\'electron\'' in line:
-        print(line.replace('electron','electron-bin'))
-    else:
-        print(line)
-
 def post_build():
   # do something after the package has successfully been built
   aur_post_build()
