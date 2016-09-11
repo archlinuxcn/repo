0236a5fc1a7ecd63c1f7d7913b8ae7a1de1985e6
Jingbei Li <i@jingbei.li>
2016-09-11 16:52:03 +0800

    fix danmaku2ass-git/lilac.py

--- danmaku2ass-git/lilac.py
+++ danmaku2ass-git/lilac.py
@@ -4,7 +4,10 @@ from lilaclib import *
 
 build_prefix = 'extra-x86_64'
 
-pre_build = aur_pre_build
+def pre_build():
+  aur_pre_build()
+  run_cmd(['wget', '-O', 'PKGBUILD', 'https://raw.githubusercontent.com/m13253/danmaku2ass/master/PKGBUILD'])
+
 post_build = aur_post_build
 
 if __name__ == '__main__':
