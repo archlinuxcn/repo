aa31d8666f591ef6175e5fe5edafa0c42c4836f8
Jingbei Li <i@jingbei.li>
2016-09-12 17:40:32 +0800

    fix wps-office/lilac.py

--- wps-office/lilac.py
+++ wps-office/lilac.py
@@ -1,13 +1,16 @@
 #!/usr/bin/env python3
-#
-# This file is the most simple lilac.py file,
-# and it suits for most packages in AUR.
-#
 
 from lilaclib import *
 
-build_prefix = 'extra-x86_64'
-pre_build = aur_pre_build
+build_prefix = 'arch4edu-x86_64'
+
+def pre_build():
+  aur_pre_build()
+
+  for line in edit_file('PKGBUILD'):
+    if not 'PKGEXT=' in line:
+        print(line)
+
 post_build = aur_post_build
 
 if __name__ == '__main__':
