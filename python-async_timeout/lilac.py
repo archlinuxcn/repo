5d23b3b2c627cdbdb76b2c44bcd677541168c65e
依云 <lilydjwg@gmail.com>
2016-09-18 09:45:50 +0800

    python-aiohttp now requires python-async_timeout

--- python-async_timeout/lilac.py
+++ python-async_timeout/lilac.py
@@ -5,14 +5,10 @@ import fileinput
 from lilaclib import *
 
 build_prefix = 'extra-x86_64'
-depends = ['python-multidict-git', 'python-async_timeout']
 
 def pre_build():
   pypi_pre_build(
-    depends = ['python-chardet', 'python-multidict', 'python-async_timeout'],
     depends_setuptools = False,
-    makedepends = ['cython'],
-    arch = ['i686', 'x86_64'],
   )
 
 post_build = pypi_post_build
