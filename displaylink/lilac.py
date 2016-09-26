f0cd6117b42fd0134305b76d4640b98f59c363d1
renyuneyun <renyuneyun@gmail.com>
2016-09-26 14:29:34 +0100

    upgpkg: displaylink
    add lilac.py

---
+++ displaylink/lilac.py
@@ -0,0 +1,13 @@
+from lilaclib import *
+
+build_prefix = 'extra-x86_64'
+
+pre_build = vcs_update
+
+def post_build():
+  git_add_files('.')
+  git_commit()
+  update_aur_repo()
+
+if __name__ == '__main__':
+  single_main()
