5773ca467f9ce621e421dc3ee17bcfd58138b74a
renyuneyun <renyuneyun@gmail.com>
2016-09-27 13:58:50 +0100

    add 'linux-headers' to makedepends for evdi
    the building process of evdi (which is a dkms module) actually requires linux-headers which is neither in 'depends' nor 'makedepends', so it needs to be resolved before building

--- evdi/lilac.py
+++ evdi/lilac.py
@@ -4,7 +4,16 @@ from lilaclib import *
 
 build_prefix = 'extra-x86_64'
 
-pre_build = aur_pre_build
+def pre_build():
+    aur_pre_build()
+    for line in edit_file('PKGBUILD'):
+        if line.strip().startswith('makedepends'):
+            if line.find('linux-headers') == -1:
+                index = line.rfind(')')
+                right = line[index:]
+                line = line[:index] + ', linux-headers' + right
+        print(line)
+
 post_build = aur_post_build
 
 if __name__ == '__main__':
