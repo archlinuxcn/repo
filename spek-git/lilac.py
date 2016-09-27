7a57aad0d6f6ab05eb05cd3f2baf229efe195380
Jingbei Li <i@jingbei.li>
2016-09-27 19:07:29 +0800

    fix spek-git/lilac.py

--- spek-git/lilac.py
+++ spek-git/lilac.py
@@ -1,14 +1,18 @@
 #!/usr/bin/env python3
-#
-# This file is the most simple lilac.py file,
-# and it suits for most packages in AUR.
-#
 
 from lilaclib import *
 
 build_prefix = 'extra-x86_64'
-pre_build = aur_pre_build
+
+def pre_build():
+    aur_pre_build()
+    for line in edit_file('PKGBUILD'):
+        if 'git describe' in line:
+            print(line.replace('|',' 2>/dev/null |'))
+        else:
+            print(line)
+
 post_build = aur_post_build
 
 if __name__ == '__main__':
-  single_main()
+  single_main(build_prefix)
