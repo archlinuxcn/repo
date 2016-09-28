2b98cd69302ad208f901cfcb7316f114ad43fb46
renyuneyun <renyuneyun@gmail.com>
2016-09-28 20:42:51 +0100

    fix lilac.py for evdi
    avoid adding ',' to empty array

--- evdi/lilac.py
+++ evdi/lilac.py
@@ -4,14 +4,23 @@ from lilaclib import *
 
 build_prefix = 'extra-x86_64'
 
+def add_into_array(line, values):
+    l = line.find('(')
+    r = line.rfind(')')
+    arr_str = line[l+1:r]
+    arr = [x.strip().strip('"').strip("'") for x in arr_str.split(',')]
+    for dep in values:
+        if line.find(dep) == -1:
+            arr.append(dep)
+    line = line[:l] + tuple(arr).__str__()
+    return line
+
 def pre_build():
     aur_pre_build()
     for line in edit_file('PKGBUILD'):
         if line.strip().startswith('makedepends'):
-            if line.find('linux-headers') == -1:
-                index = line.rfind(')')
-                right = line[index:]
-                line = line[:index] + ', linux-headers' + right
+            makedeps = ['linux-headers']
+            line = add_into_array(line, makedeps)
         print(line)
 
 post_build = aur_post_build
