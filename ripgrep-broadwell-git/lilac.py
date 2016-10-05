047f434af86a71feb23aaef24c11e938caba10a0
lilydjwg <lilydjwg@gmail.com>
2016-10-05 10:55:30 +0800

    ripgrep-broadwell-git: needs rust-nightly

--- ripgrep-broadwell-git/lilac.py
+++ ripgrep-broadwell-git/lilac.py
@@ -1,6 +1,11 @@
 from lilaclib import *
 
 build_prefix = 'extra-x86_64'
+depends = [
+  ('rust-nightly', 'rust-std-nightly-x86_64-unknown-linux-gnu'),
+  ('rust-nightly', 'rust-nightly'),
+  ('rust-nightly', 'cargo-nightly'),
+]
 
 pre_build = vcs_update
 
