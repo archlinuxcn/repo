1e29ce660bf5c80407116483421620183a31c919
CUI Hao <cuihao.leo@gmail.com>
2016-10-23 09:24:33 +0800

    update lilac.py of gmt-coast

--- gmt-coast/lilac.py
+++ gmt-coast/lilac.py
@@ -1,8 +1,4 @@
 #!/usr/bin/env python3
-#
-# This file is the most simple lilac.py file,
-# and it suits for most packages in AUR.
-#
 
 from lilaclib import *
 
@@ -10,5 +6,11 @@ build_prefix = 'extra-x86_64'
 pre_build = aur_pre_build
 post_build = aur_post_build
 
+def pre_build():
+    aur_pre_build("gmt-coast")
+
+    for line in edit_file("PKGBUILD"):
+        print(line.replace("$startdir/src", "$srcdir"))
+
 if __name__ == '__main__':
-  single_main(build_prefix)
+    single_main(build_prefix)
