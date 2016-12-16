728c848c10c3ed7676ff7769d7d7b7f693bc396e
Jiachen Yang <farseerfc@gmail.com>
2016-12-16 15:46:47 +0900

    xpra-winswitch: add dependencies

--- python2-gtkglext/lilac.py
+++ python2-gtkglext/lilac.py
@@ -6,12 +6,10 @@
 
 from lilaclib import *
 
-depends = ["python2-lz4", "python-lz4", "rencode", "python2-gtkglext"]
-
 build_prefix = 'extra-x86_64'
+
 post_build = aur_post_build
 pre_build = aur_pre_build
-
 
 if __name__ == '__main__':
   single_main()
