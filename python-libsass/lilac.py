beda561dbce95c488a1ad4b40bc2d52332235da7
Peter Cai <xqsx43cxy@gmail.com>
2017-02-10 08:55:47 +0800

    addpkg: python-libsass

---
+++ python-libsass/lilac.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+
+from lilaclib import *
+
+build_prefix = 'extra-x86_64'
+
+def pre_build():
+  pypi_pre_build(
+    depends = ['python-six', 'libsass'],
+    depends_setuptools = True,
+    makedepends = ['gcc'],
+    arch = ['x86_64']
+  )
+
+def post_build():
+  pypi_post_build()
+  update_aur_repo()
+
+if __name__ == '__main__':
+  single_main()
