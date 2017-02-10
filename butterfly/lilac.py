1d98884e5902d4134addcb57e4d4cf8f2c7f23bb
Peter Cai <xqsx43cxy@gmail.com>
2017-02-10 09:09:24 +0800

    addpkg: butterfly

---
+++ butterfly/lilac.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+
+from lilaclib import *
+
+build_prefix = 'extra-x86_64'
+depends = ['tornado_systemd']
+
+def pre_build():
+  pypi_pre_build(
+    depends = ['python-pyopenssl', 'python-tornado', 'tornado_systemd'],
+    depends_setuptools = True,
+    optdepends = ['python-libsass'],
+    arch = ['any']
+  )
+
+def post_build():
+  pypi_post_build()
+  update_aur_repo()
+
+if __name__ == '__main__':
+  single_main()
