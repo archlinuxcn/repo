3bdf228d9663ce3db6227863161791876cddccf7
lilydjwg <lilydjwg@gmail.com>
2017-02-18 17:41:49 +0800

    python-tldextract: add .tld_set

--- python-tldextract/lilac.py
+++ python-tldextract/lilac.py
@@ -12,6 +12,16 @@ def pre_build():
     depends=['python-idna', 'python-requests-file'],
   )
 
+  for l in edit_file('PKGBUILD'):
+    if 'setup.py install' in l:
+      l += '''
+
+  _pyver=$(python -c 'import sysconfig; print(sysconfig.get_python_version())')
+  # use the snapshot version, because generating a new one requires Internat access and root permission
+  ln -s .tld_set_snapshot "$pkgdir/usr/lib/python$_pyver/site-packages/tldextract/.tld_set"
+'''
+    print(l)
+
 def post_build():
   pypi_post_build()
   update_aur_repo()
