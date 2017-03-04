721ea821621c126fbfee3217ff54dfd4f1ea604e
Jianfeng Zhang <swordfeng123@gmail.com>
2017-03-04 12:20:11 +0800

    poi: upg to 7.4.1 && auto check github tags in future

--- poi/lilac.py
+++ poi/lilac.py
@@ -8,7 +8,9 @@ from lilaclib import *
 
 build_prefix = 'extra-x86_64'
 pre_build = aur_pre_build
-post_build = aur_post_build
+def post_build():
+    aur_post_build()
+    update_aur_repo()
 
 if __name__ == '__main__':
   single_main()
