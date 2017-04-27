94d4b9c8c394b85a73ef08fa52a6d3150a07c224
lilydjwg <lilydjwg@gmail.com>
2017-04-27 18:48:50 +0800

    firefox-esr-i18n: fix pkgver

--- firefox-esr-i18n/lilac.py
+++ firefox-esr-i18n/lilac.py
@@ -5,11 +5,25 @@ from lilaclib import *
 build_prefix = 'extra-x86_64'
 
 def pre_build():
+  oldver, oldrel = _G.oldver.split('-', 1)
+  ver, rel = _G.newver.split('-', 1)
+  need_update_pkgrel = False
+
   for line in edit_file('PKGBUILD'):
-    if line.startswith('pkgver'):
-      print('pkgver=%s' % _G.newver)
+    if line.startswith('pkgver='):
+      print('pkgver=%s' % ver)
       continue
+    elif line.startswith('pkgrel='):
+      if oldver == ver:
+        need_update_pkgrel = True
+        print(line)
+      else:
+        print('pkgrel=1')
+
     print(line)
+
+  if need_update_pkgrel:
+    update_pkgrel()
 
   run_cmd(['updpkgsums'])
 
