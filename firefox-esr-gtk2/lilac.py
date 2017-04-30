3bd7fae9cf5310f293b0019da8a9342ab04958d2
lilydjwg <lilydjwg@gmail.com>
2017-04-30 12:55:05 +0800

    fix firefox-esr-gtk2

--- firefox-esr-gtk2/lilac.py
+++ firefox-esr-gtk2/lilac.py
@@ -10,6 +10,8 @@ def pre_build():
   for line in edit_file('PKGBUILD'):
     if 'MOZ_REQUIRE_SIGNING' in line:
       continue
+    if line.strip() == 'cd firefox-${pkgver}esr':
+      line += "\nsed -i '3abuild = false' media/libstagefright/binding/mp4parse_capi/Cargo.toml"
     print(line)
 
 post_build = aur_post_build
