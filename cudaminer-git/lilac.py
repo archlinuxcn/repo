d0b97206680a9bdcbfa2cec1af93d3e1d467b638
Jingbei Li <i@jingbei.li>
2017-05-03 19:54:12 +0800

    trigger rebuild for cudaminer-git

--- cudaminer-git/lilac.py
+++ cudaminer-git/lilac.py
@@ -1,9 +1,4 @@
 #!/usr/bin/env python3
-#
-# This file is the most simple lilac.py file,
-# and it suits for most packages in AUR.
-#
-
 from lilaclib import *
 
 build_prefix = 'extra-x86_64'
@@ -11,4 +6,4 @@ pre_build = aur_pre_build
 post_build = aur_post_build
 
 if __name__ == '__main__':
-  single_main()
+  single_main(build_prefix)
