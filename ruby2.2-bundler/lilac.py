6abcc2a4db425f3e4a0fac3d1b578ed869356b0e
Felix Yan <felixonmars@archlinux.org>
2017-06-02 20:21:08 +0800

    Add depends array

--- ruby2.2-bundler/lilac.py
+++ ruby2.2-bundler/lilac.py
@@ -9,6 +9,7 @@ from lilaclib import *
 build_prefix = 'archlinuxcn-x86_64'
 pre_build = aur_pre_build
 post_build = aur_post_build
+depends = ["ruby2.2"]
 
 if __name__ == '__main__':
   single_main()
