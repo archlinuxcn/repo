9ffea90aa4c5b8554de4174c6ed23ee7aae8764a
Jingbei Li <i@jingbei.li>
2017-07-11 20:16:27 +0800

    rm eula.html in foxitreader/lilac.py

--- foxitreader/lilac.py
+++ foxitreader/lilac.py
@@ -4,7 +4,11 @@ from lilaclib import *
 
 build_prefix = 'archlinuxcn-x86_64'
 depends = ['qt-installer-framework']
-pre_build = aur_pre_build
+
+def pre_build():
+    run_cmd(["rm", "-f", "eula.html"])
+    aur_pre_build()
+
 post_build = aur_post_build
 
 if __name__ == '__main__':
