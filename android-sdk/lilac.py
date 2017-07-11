65b08a30d61305e6c4e2cdbf648444bd253cf04a
Jingbei Li <i@jingbei.li>
2017-07-11 20:20:41 +0800

    fix android-sdk/lilac.py

--- android-sdk/lilac.py
+++ android-sdk/lilac.py
@@ -4,9 +4,10 @@ from lilaclib import *
 
 build_prefix = 'multilib'
 pre_build = aur_pre_build
+
 def post_build():
     run_cmd(['git','add','-f','license.html'])
-    git_add_files(['PKGBUILD', 'android-sdk.conf', 'android-sdk.csh', 'android-sdk.desktop', 'android-sdk.install', 'android-sdk.sh'])
+    git_add_files(['PKGBUILD', 'android-sdk.conf', 'android-sdk.csh', 'android-sdk.install', 'android-sdk.sh'])
     git_commit()
 
 if __name__ == '__main__':
