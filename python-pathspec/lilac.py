eb88a66c05b77676733d8ba0bf5b319a7a053db8
lilydjwg <lilydjwg@gmail.com>
2017-08-03 21:51:36 +0800

    yamllint: now depends on pathspec

--- python-pathspec/lilac.py
+++ python-pathspec/lilac.py
@@ -6,16 +6,11 @@ from lilaclib import *
 
 build_prefix = 'extra-x86_64'
 
-depends = ['python-pathspec']
-
 def pre_build():
-  pypi_pre_build(
-    depends=['python-yaml', 'python-pathspec'],
-  )
+  pypi_pre_build()
 
 def post_build():
   pypi_post_build()
-  update_aur_repo()
 
 if __name__ == '__main__':
   single_main()
