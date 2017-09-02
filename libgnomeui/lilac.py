6a716057d20d34c3b3e76b5d0706b7c987020c77
Jianqiu Zhang <zhangjianqiu13@gmail.com>
2017-09-03 00:06:46 +0800

    rebuild libgnomeui

--- libgnomeui/lilac.py
+++ libgnomeui/lilac.py
@@ -10,7 +10,7 @@ build_prefix = 'extra-x86_64'
 pre_build = aur_pre_build
 post_build = aur_post_build
 
-depends = [("libgnome", "libgnome-data"), "libgnome", "libbonoboui"]
+depends = ["orbit2", ("libgnome", "libgnome-data"), "gnome-vfs-nosmb", "libbonobo", "libgnome", "libbonoboui"]
 
 if __name__ == '__main__':
       single_main(build_prefix)
