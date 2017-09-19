a0b8d5a6c05a6bdba0ac727b96cb9dbb0dfeedfc
lilydjwg <lilydjwg@gmail.com>
2017-09-19 19:06:34 +0800

    add python-terminaltables and python-aiomonitor

--- python-aiomonitor/lilac.py
+++ python-aiomonitor/lilac.py
@@ -3,13 +3,14 @@
 from lilaclib import *
 
 build_prefix = 'extra-x86_64'
+depends = ['python-aioconsole', 'python-terminaltables']
 
 def pre_build():
-  pypi_pre_build()
+  pypi_pre_build(depends=depends[:])
 
 def post_build():
   pypi_post_build()
-  update_aur_repo()
+  # update_aur_repo()
 
 if __name__ == '__main__':
   single_main()
