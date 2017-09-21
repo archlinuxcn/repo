d174521d023ad846db71a12268ca61a787aef0a9
lilydjwg <lilydjwg@gmail.com>
2017-09-21 13:38:44 +0800

    add python-pycares and python-aiodns

---
+++ python-aiodns/lilac.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+
+from lilaclib import *
+
+build_prefix = 'extra-x86_64'
+depends = ['python-pycares']
+
+def pre_build():
+  pypi_pre_build(depends=['python-pycares'], license='MIT')
+
+def post_build():
+  pypi_post_build()
+
+if __name__ == '__main__':
+  single_main()
