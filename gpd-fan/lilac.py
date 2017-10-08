fb8fc9193e9a604b333b434382d96ead621c4f90
Jiachen Yang <farseerfc@gmail.com>
2017-10-08 19:23:10 +0900

    gpd-fan fix lilac.py

--- gpd-fan/lilac.py
+++ gpd-fan/lilac.py
@@ -1,19 +1,12 @@
-#!/usr/bin/env python3
-#
-# This file is the most simple lilac.py file,
-# and it suits for most packages in AUR.
-#
-
 from lilaclib import *
 
 build_prefix = 'extra-x86_64'
 
-def pre_build():
-    vcs_update()
+pre_build = vcs_update
 
-post_build = aur_post_build
+def post_build():
+  git_add_files('PKGBUILD')
+  git_commit()
 
 if __name__ == '__main__':
-    single_main(build_prefix)
-
-
+  single_main(build_prefix)
