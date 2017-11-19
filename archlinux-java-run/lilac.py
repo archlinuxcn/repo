c731b9dbcc365b8a1d040893b2f5e79e37c3e63b
Jiachen Yang <farseerfc@gmail.com>
2017-11-20 02:08:09 +0900

    add archlinux-java-run a dependency of yed

--- archlinux-java-run/lilac.py
+++ archlinux-java-run/lilac.py
@@ -18,31 +18,11 @@
 #
 
 from lilaclib import *
-import re
 
 build_prefix = 'extra-x86_64'
 
-depends = ['archlinux-java-run']
-
-
-def pre_build():
-    aur_pre_build()
-
-    for line in edit_file('PKGBUILD'):
-        # edit PKGBUILD
-        if line.strip().startswith("depends="):
-            depends = re.findall("depends=\s*\((.*)\)", line)[0]
-            words = depends.split(" ")
-            words.append("'java-environment'")
-            words.append("'bash'")
-            line = "depends=(%s)" % (" ".join(words))
-        print(line)
-
+pre_build = aur_pre_build
 post_build = aur_post_build
-
-# do some cleanup here after building the package, regardless of result
-# def post_build_always(success):
-#   pass
 
 if __name__ == '__main__':
     single_main(build_prefix)
