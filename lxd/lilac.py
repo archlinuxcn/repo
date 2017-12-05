3deca01b301c82c8a5a83ff6c97d7df25643d476
kasei <kasei@kasei.im>
2017-12-05 11:05:15 +0800

    updpkg:lxd 2.19-2.2 fix missing depend

--- lxd/lilac.py
+++ lxd/lilac.py
@@ -11,9 +11,7 @@ build_prefix = 'extra-x86_64'
 def pre_build():
     aur_pre_build()
     for line in edit_file('PKGBUILD'):
-        if 'git describe' in line:
-            print(line.replace('|',' 2>/dev/null |'))
-        elif 'makedepends' in line:
+        if 'makedepends' in line and not 'sqlite' in line:
             print(line.replace(')',' \'sqlite\')'))
         else:
             print(line)
