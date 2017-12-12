323b89e2994ea6906c2b2e580f8c66efa1773c76
Jiachen Yang <farseerfc@gmail.com>
2017-12-12 12:13:53 +0900

    plantuml fix java encoding error

--- plantuml/lilac.py
+++ plantuml/lilac.py
@@ -7,8 +7,15 @@
 from lilaclib import *
 
 build_prefix = 'extra-x86_64'
-pre_build = aur_pre_build
 post_build = aur_post_build
+
+def pre_build():
+    aur_pre_build()
+    for line in edit_file('PKGBUILD'):
+        if line.strip().startswith('ant dist'):
+            print('export JAVA_TOOL_OPTIONS=-Dfile.encoding=ISO-8859-1')
+        print(line)
+
 
 if __name__ == '__main__':
   single_main()
