918db67409d212176a2acf8f808ee48c5b9bdb16
Jingbei Li <i@jingbei.li>
2017-12-13 00:26:59 +0800

    replace ffmpeg-mpv-git with ffmpeg-git

--- mpv-git/lilac.py
+++ mpv-git/lilac.py
@@ -1,9 +1,17 @@
 #!/usr/bin/env python3
 from lilaclib import *
 
-build_prefix = 'extra-x86_64'
-depends = ['ffmpeg-mpv-git']
-pre_build = aur_pre_build
+build_prefix = 'archlinuxcn-x86_64'
+depends = ['ffmpeg-git']
+
+def pre_build():
+    aur_pre_build()
+    for line in edit_file('PKGBUILD'):
+        if 'depends=(' in line:
+            print(line.replace("'ffmpeg'","'ffmpeg-git'"))
+        else:
+            print(line)
+
 post_build = aur_post_build
 
 if __name__ == '__main__':
