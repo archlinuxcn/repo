45c9dccc8033c3b2a95241b71f4d879f16ac7e60
lilac <lilac@build.archlinuxcn.org>
2017-12-13 10:09:18 +0800

    auto update for package nvm

---
+++ nvm/update.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+if (( $# != 1 )); then
+    echo "Usage: $0 VERSION"
+    echo "Update pkgver to VERSION and then build and commit."
+    echo "You just need to test the package, review the commit and push."
+    exit 1
+fi
+
+VERSION=$1
+
+set -x
+
+sed -i "s/^pkgver=.*\$/pkgver=${VERSION}/" PKGBUILD
+updpkgsums
+mksrcinfo
+git add .
+git commit -m "Upgrade to v${VERSION}"
+makepkg
+sudo pacman -U nvm-${VERSION}-1-any.pkg.tar.xz
+
+set +x
+
+echo "Now smoke test the package, review the commit, and push"
