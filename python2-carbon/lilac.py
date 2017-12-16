0bac1cf66d0351635660bb86ccaed3e17e88c134
lilydjwg <lilydjwg@gmail.com>
2017-12-17 00:28:11 +0800

    python2-carbon: give up AUR version
    * do some restrictions in service file
    * use 127.0.0.1 by default; 0.0.0.0 is vulnerable to remote code execution
    * create necessary directories

--- python2-carbon/lilac.py
+++ python2-carbon/lilac.py
@@ -7,9 +7,25 @@ build_prefix = 'extra-x86_64'
 depends = ['python2-whisper']
 
 def pre_build():
-  aur_pre_build()
+  newver = _G.newver
+  pkgver, pkgrel = get_pkgver_and_pkgrel()
 
-post_build = aur_post_build
+  for line in edit_file('PKGBUILD'):
+    if line.startswith('pkgver=') and pkgver != newver:
+        line = f'pkgver={newver}'
+    elif line.startswith('pkgrel='):
+      if pkgver != newver:
+        line = 'pkgrel=1'
+      else:
+        line = f'pkgrel={int(pkgrel)+1}'
+
+    print(line)
+
+  run_cmd(["updpkgsums"])
+
+def post_build():
+  git_add_files('PKGBUILD')
+  git_commit()
 
 if __name__ == '__main__':
   single_main()
