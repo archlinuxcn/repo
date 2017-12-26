aa37bf1aab4a5cd0e189023a3155df865ad6dc23
Jingbei Li <i@jingbei.li>
2017-12-26 23:26:41 +0800

    fix bomi-git/lilac.py

--- bomi-git/lilac.py
+++ bomi-git/lilac.py
@@ -1,11 +1,14 @@
 #!/usr/bin/env python3
-
 from lilaclib import *
 
 build_prefix = 'archlinuxcn-x86_64'
 depends = ['libchardet']
-pre_build = aur_pre_build
+
+def pre_build():
+    aur_pre_build()
+    run_cmd('rm -rf bomi'.split(' '))
+
 post_build = aur_post_build
 
 if __name__ == '__main__':
-  single_main()
+  single_main(build_prefix)
