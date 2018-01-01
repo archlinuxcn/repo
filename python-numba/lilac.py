d010b4061a1519a2da484e455d8dbfbd3237cef7
Stephen Zhang <zsrkmyn@archlinuxcn.org>
2018-01-01 12:47:25 +0800

    updpkg: fix depends for python-numba

--- python-numba/lilac.py
+++ python-numba/lilac.py
@@ -4,7 +4,7 @@ build_prefix = 'archlinuxcn-x86_64'
 pre_build = aur_pre_build
 post_build = aur_post_build
 
-depends = ['python-llvmlite']
+depends = ['python-llvmlite', 'python-numpy']
 
 if __name__ == '__main__':
   single_main(build_prefix)
