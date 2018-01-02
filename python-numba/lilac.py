294ef2c7a8d083baa33bee6e8bd2abf0ed350533
Stephen Zhang <zsrkmyn@archlinuxcn.org>
2018-01-03 01:09:38 +0800

    updpkg: fix depends for python-numba

--- python-numba/lilac.py
+++ python-numba/lilac.py
@@ -1,10 +1,17 @@
 from lilaclib import *
 
 build_prefix = 'archlinuxcn-x86_64'
-pre_build = aur_pre_build
 post_build = aur_post_build
 
-depends = ['python-llvmlite', 'python-numpy']
+depends = ['python-llvmlite']
+
+def pre_build():
+  aur_pre_build()
+  for l in edit_file('PKGBUILD'):
+    if l.lstrip().startswith('depends='):
+      l = l.rstrip(' )')
+      l += " 'python-numpy')"
+    print(l)
 
 if __name__ == '__main__':
   single_main(build_prefix)
