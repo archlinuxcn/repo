366318116565471cd6f0aaf590c0530df92f7f22
Jingbei Li <i@jingbei.li>
2018-01-10 19:54:08 +0800

    simplify dnsmasq-china-list-git/lilac.py

--- dnsmasq-china-list-git/lilac.py
+++ dnsmasq-china-list-git/lilac.py
@@ -1,20 +1,8 @@
 #!/usr/bin/env python3
-
 from lilaclib import *
 
 build_prefix = 'extra-x86_64'
-
-def pre_build():
-  aur_pre_build()
-
-  for line in edit_file('PKGBUILD'):
-    if 'bogus-nxdomain.china.conf' in line:
-        print(line)
-        print(line.replace('bogus-nxdomain.china.conf','apple.china.conf'))
-        print(line.replace('bogus-nxdomain.china.conf','google.china.conf'))
-    else:
-        print(line)
-
+pre_build = aur_pre_build
 post_build = aur_post_build
 
 if __name__ == '__main__':
