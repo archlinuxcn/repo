bb9d3c381043dc47ccc235ae85f25f249b427878
Jiachen Yang <farseerfc@gmail.com>
2018-02-03 20:09:25 +0900

    plantuml: fix and trigger rebuild

--- plantuml/lilac.py
+++ plantuml/lilac.py
@@ -12,8 +12,8 @@ post_build = aur_post_build
 def pre_build():
     aur_pre_build()
     for line in edit_file('PKGBUILD'):
-        if line.strip().startswith('ant dist'):
-            print('export JAVA_TOOL_OPTIONS=-Dfile.encoding=ISO-8859-1')
+        if line.strip().startswith('sed'):
+            line = "    sed 's/target=\"1.6\"/target=\"1.7\"/g' -i build.xml"
         print(line)
 
 
