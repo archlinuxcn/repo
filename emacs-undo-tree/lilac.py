c11e2d777f093e361b89a1fdd5ec545fe741133a
Jianfeng Zhang <swordfeng123@gmail.com>
2018-02-11 17:00:03 +0800

    emacs-undo-tree: add git to makedepends

--- emacs-undo-tree/lilac.py
+++ emacs-undo-tree/lilac.py
@@ -3,8 +3,17 @@
 from lilaclib import *
 
 build_prefix = 'archlinuxcn-x86_64'
-pre_build = aur_pre_build
 post_build = aur_post_build
+
+def pre_build():
+    aur_pre_build()
+    for line in edit_file('PKGBUILD'):
+        if line.strip().startswith("depends"):
+            line += "\nmakedepends=('git')"
+        print(line)
+    git_add_files('PKGBUILD')
+    git_commit()
+
 
 if __name__ == '__main__':
     single_main(build_prefix)
