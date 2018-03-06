05ee316ba8b1fd93f678d9f26656a111257b8ba8
Jiachen Yang <farseerfc@gmail.com>
2018-03-06 15:56:38 +0900

    chrome-remote-desktop: fix source intergrety issue

--- chrome-remote-desktop/lilac.py
+++ chrome-remote-desktop/lilac.py
@@ -7,8 +7,14 @@
 from lilaclib import *
 
 build_prefix = 'extra-x86_64'
-pre_build = aur_pre_build
 post_build = aur_post_build
+
+def pre_build():
+    aur_pre_build()
+    for line in edit_file('PKGBUILD'):
+        if line.strip() == 'source_x86_64=("http://dl.google.com/linux/direct/>${pkgname}_current_amd64.deb")'
+            line = 'source_x86_64=("${pkgname}_${pkgver}_amd64.deb::http://dl.google.com/linux/direct/>${pkgname}_current_amd64.deb")'
+        print(line)
 
 if __name__ == '__main__':
   single_main()
