bffd22178ec52df6cc0fb5215ff7e7d7f2ff3bc6
Chih-Hsuan Yen <yan12125@gmail.com>
2018-03-13 23:53:19 +0800

    android-ndk: simplify android-ndk
    NDK are no longer distributed as *.bin files, so 7z-related changes are not
    necessary any more. Also, filenames of NDK zip files are always
    version-dependent now.

--- android-ndk/lilac.py
+++ android-ndk/lilac.py
@@ -5,25 +5,7 @@ from lilaclib import *
 depends = ["ncurses5-compat-libs"]
 build_prefix = 'extra-x86_64'
 post_build = aur_post_build
-
-def pre_build():
-  # delete old source files as they have same names each version
-  run_cmd(["rm", "-f", "android-ndk_x86_64.bin", "android-ndk_i686.bin", "android-ndk-r11-linux-x86_64.zip"])
-  aur_pre_build()
-
-  remove_lines = ["chmod +x ${pkgname}_$CARCH.bin",
-                  "./${pkgname}_$CARCH.bin"]
-  first_time = True
-  for line in edit_file('PKGBUILD'):
-      if line.strip() in remove_lines:
-          if first_time:
-            print("7z x -y ${pkgname}_$CARCH.bin")
-            first_time = False
-      else:
-          print(line)
-          if line.startswith("arch="):
-              print("makedepends=('p7zip')")
-
+pre_build = aur_pre_build
 
 if __name__ == '__main__':
   single_main(build_prefix)
