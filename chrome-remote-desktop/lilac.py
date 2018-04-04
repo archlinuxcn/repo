cac8762544764d0b86966e48b91ebf0f64e46ee7
Jiachen Yang <farseerfc@gmail.com>
2018-04-04 12:34:50 +0900

    chrome-remote-desktop fix lilac.py syntax error

--- chrome-remote-desktop/lilac.py
+++ chrome-remote-desktop/lilac.py
@@ -12,9 +12,9 @@ post_build = aur_post_build
 def pre_build():
     aur_pre_build()
     for line in edit_file('PKGBUILD'):
-        if line.strip() == 'source_x86_64=("http://dl.google.com/linux/direct/>${pkgname}_current_amd64.deb")'
+        if line.strip() == 'source_x86_64=("http://dl.google.com/linux/direct/${pkgname}_current_amd64.deb")':
             line = 'source_x86_64=("${pkgname}_${pkgver}_amd64.deb::http://dl.google.com/linux/direct/>${pkgname}_current_amd64.deb")'
         print(line)
 
 if __name__ == '__main__':
-  single_main()
+  single_main(build_prefix)
