2e0ffeaa918912a6c8443acdae2e94dbbfe70c22
Peter Cai <xqsx43cxy@gmail.com>
2018-04-18 07:31:05 +0800

    addpkg: standardnotes-desktop

---
+++ standardnotes-desktop/standardnotes-desktop.js
@@ -0,0 +1,30 @@
+#!/usr/bin/electron
+
+const name = 'standardnotes-desktop';
+
+const {app} = require('electron');
+const fs = require('fs');
+const path = require('path');
+
+// Change command name.
+const fd = fs.openSync('/proc/self/comm', fs.constants.O_WRONLY);
+fs.writeSync(fd, name);
+fs.closeSync(fd);
+
+// Remove first command line argument (/usr/bin/electron).
+process.argv.splice(0, 1);
+
+// Set application paths.
+//const appPath = path.join(path.dirname(__dirname), 'lib', name);
+const appPath = '/opt/standardnotes-desktop/app'
+const packageJson = require(path.join(appPath, 'package.json'));
+const productName = 'Standard Notes';
+app.setAppPath(appPath);
+app.setDesktopName(name + '.desktop');
+app.setName(productName);
+app.setPath('userCache', path.join(app.getPath('cache'), productName));
+app.setPath('userData', path.join(app.getPath('appData'), productName));
+app.setVersion(packageJson.version);
+
+// Run the application.
+require('module')._load(appPath, module, true);
