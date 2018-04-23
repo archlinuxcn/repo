a5f8b4183bbdb22c27dd13747643ddbfaddd8443
metal A-wing <shiyongxin@aliyun.com>
2018-04-23 13:19:42 +0800

    Fix websocketd lilac.py

--- websocketd/lilac.py
+++ websocketd/lilac.py
@@ -9,33 +9,11 @@ from lilaclib import *
 build_prefix = 'extra-x86_64'
 
 def pre_build():
-    oldver, oldrel = _G.oldver.split('-', 1)
-    ver, rel = _G.newver.split('-', 1)
-    need_update_pkgrel = False
-
-  for line in edit_file('PKGBUILD'):
-      if line.startswith('pkgver='):
-          print('pkgver=%s' % ver)
-      continue
-  elif line.startswith('pkgrel='):
-      if oldver == ver:
-          need_update_pkgrel = True
-        print(line)
-    else:
-        print('pkgrel=1')
-      continue
-
-  print(line)
-
-  if need_update_pkgrel:
-      update_pkgrel()
-
-  run_cmd(['updpkgsums'])
+  update_pkgver_and_pkgrel(_G.newver)
 
 def post_build():
-    git_add_files(['PKGBUILD'])
-    git_commit()
-
+  git_add_files('PKGBUILD')
+  git_commit()
 
 if __name__ == '__main__':
   single_main()
