4152fd75724da8b801582f5ace5fa591ea92d5ac
Winston Wu <winstonwu91@gmail.com>
2018-05-19 05:32:29 +0800

    addpkg: deadbeef-git

---
+++ deadbeef-plugin-musical-spectrum-git/lilac.py
@@ -0,0 +1,12 @@
+from lilaclib import *
+
+build_prefix = 'extra-x86_64'
+
+pre_build = vcs_update
+
+def post_build():
+  git_add_files('PKGBUILD')
+  git_commit()
+
+if __name__ == '__main__':
+  single_main()
