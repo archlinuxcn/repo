c700e645e205f0edb7bf1354771fe3307dce84a0
BennyYip <yebenmy@protonmail.com>
2018-05-20 15:24:17 +0800

    update mycli

--- mycli/lilac.py
+++ mycli/lilac.py
@@ -1,14 +1,27 @@
 #!/usr/bin/env python3
-#
-# This file is the most simple lilac.py file,
-# and it suits for most packages in AUR.
-#
 
 from lilaclib import *
 
 build_prefix = 'extra-x86_64'
-pre_build = aur_pre_build
-post_build = aur_post_build
+
+
+def pre_build():
+    pypi_pre_build(depends=[
+        'python'
+        'python-click'
+        'python-configobj'
+        'python-cryptography'
+        'python-prompt_toolkit'
+        'python-pygments'
+        'python-pymysql'
+        'python-sqlparse'
+        'python-cli-helpers'
+    ])
+
+
+def post_build():
+    pypi_post_build()
+
 
 if __name__ == '__main__':
-  single_main()
+    single_main()
