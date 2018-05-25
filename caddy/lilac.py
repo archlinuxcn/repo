b95fcb9a029a2460ab4f4acfa298167560e48f7a
axionl <axionl@aosc.io>
2018-05-25 13:02:09 +0800

    Add a patch for `caddy`

--- caddy/lilac.py
+++ caddy/lilac.py
@@ -7,7 +7,23 @@
 from lilaclib import *
 
 build_prefix = 'extra-x86_64'
-# pre_build = aur_pre_build
+
+def insert(path, i):
+    env_list = list("    export CGO_ENABLED=0\n")
+    with open('PKGBUILD', 'r') as file:
+        content = file.readlines()
+        content[i:] = env_list + content[i:]
+
+    with open('PKGBUILD', 'w') as file:
+        file.writelines(content)
+
+def pre_build():
+    path = 'PKGBUILD'
+    env = list("export CGO_ENABLED=0\n")
+    for i, line in enumerate(edit_file(path)):
+        if line.startwith('prepare()'):
+            insert(path, i+1)
+    
 post_build = aur_post_build
 
 if __name__ == '__main__':
