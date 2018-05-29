5469a0e39f302cbf2213e25a26be75533d858b6c
Ivan Ng <2967948168@qq.com>
2018-05-29 10:39:41 +0800

    set depends_setuptools = True in python-pgspecial/lilac.py

--- python-pgspecial/lilac.py
+++ python-pgspecial/lilac.py
@@ -9,7 +9,7 @@ from lilaclib import *
 build_prefix = 'extra-x86_64'
 pre_build = aur_pre_build
 post_build = aur_post_build
-depends = ['python-setuptools']
+depends_setuptools = True
 
 if __name__ == '__main__':
   single_main()
