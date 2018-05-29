30b2ceac9cd8e667a520f2201bc07793920b1af9
Ivan Ng <2967948168@qq.com>
2018-05-29 20:14:06 +0800

    pypi_pre_build(depends_setuptools=True) in python-pgspecial

--- python-pgspecial/lilac.py
+++ python-pgspecial/lilac.py
@@ -7,9 +7,10 @@
 from lilaclib import *
 
 build_prefix = 'extra-x86_64'
-pre_build = aur_pre_build
+def pre_build():
+    aur_pre_build()
+    pypi_pre_build(depends_setuptools=True)
 post_build = aur_post_build
-depends_setuptools = True
 
 if __name__ == '__main__':
   single_main()
