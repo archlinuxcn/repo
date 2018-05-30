71bb86b01b2167c620d8aa1ab05ed9eb701775e9
Jiachen Yang <farseerfc@gmail.com>
2018-05-30 14:58:39 +0900

    qtox-git: stop rm qtox git clone

--- qtox-git/lilac.py
+++ qtox-git/lilac.py
@@ -4,9 +4,7 @@ from lilaclib import *
 
 build_prefix = 'extra-x86_64'
 
-def pre_build():
-    run_cmd(["rm", "-rf", "qtox"])
-    vcs_update()
+pre_build = vcs_update
 
 def post_build():
     git_add_files("PKGBUILD")
