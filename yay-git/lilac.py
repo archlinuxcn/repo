072881542b899d1bba79cc27ab2e2605bd9f7579
axionl <axionl@aosc.io>
2018-05-30 13:43:51 +0800

    Update lilac.py for `yay-git`

--- yay-git/lilac.py
+++ yay-git/lilac.py
@@ -1,11 +1,7 @@
 from lilaclib import *
 
 build_prefix = 'extra-x86_64'
-
-
-def pre_build():
-	run_cmd(["rm", "-rf", "yay-git"])
-	vcs_update()
+pre_build = vcs_update
 
 def post_build():
   git_add_files('PKGBUILD')
