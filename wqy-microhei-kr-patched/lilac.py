7def07622d64d87cd09a0eb3c17eb145f51fa0f8
Jiachen Yang <farseerfc@gmail.com>
2018-06-01 17:50:10 +0900

    wqy-microhei-kr-patched: trigger fontconfig hook rebuild

--- wqy-microhei-kr-patched/lilac.py
+++ wqy-microhei-kr-patched/lilac.py
@@ -8,8 +8,17 @@ from lilaclib import *
 
 
 build_prefix = 'extra-x86_64'
-pre_build = aur_pre_build
 post_build = aur_post_build
+
+def pre_build ():
+    aur_pre_build()
+    for line in edit_file('PKGBUILD'):
+        if not line.startswith("install="): ## remove install file
+            print(line)
+    git_add_files(['PKGBUILD'])
+    git_rm_files(['wqy-microhei.install'])
+    git_commit()
+
 
 if __name__ == '__main__':
   single_main()
