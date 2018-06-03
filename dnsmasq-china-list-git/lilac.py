46efd50db64537fb30c1b1ed9590014341ed02e8
Jingbei Li <i@jingbei.li>
2018-06-03 16:33:36 +0800

    fix dnsmasq-china-list-git/lilac.py

--- dnsmasq-china-list-git/lilac.py
+++ dnsmasq-china-list-git/lilac.py
@@ -1,8 +1,15 @@
 #!/usr/bin/env python3
 from lilaclib import *
+import datetime
 
 build_prefix = 'extra-x86_64'
-pre_build = aur_pre_build
+
+def pre_build():
+    if str(datetime.datetime.now())[:8] < '20180604':
+        run_cmd('rm -rf dnsmasq-china-list'.split(' '))
+
+    aur_pre_build()
+
 post_build = aur_post_build
 
 if __name__ == '__main__':
