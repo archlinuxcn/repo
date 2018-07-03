8c714db8785cad210d56f893a6040bf352e69cef
Jingbei Li <i@jingbei.li>
2018-07-03 14:10:33 +0800

    fix ttf-wps-fonts/lilac.py

--- ttf-wps-fonts/lilac.py
+++ ttf-wps-fonts/lilac.py
@@ -1,8 +1,15 @@
 #!/usr/bin/env python3
 from lilaclib import *
+import datetime
 
 build_prefix = 'extra-x86_64'
-pre_build = aur_pre_build
+
+def pre_build():
+    if str(datetime.datetime.now())[:8] < '20180704':
+        run_cmd('rm -rf ttf-wps-fonts'.split(' '))
+
+    aur_pre_build()
+
 post_build = aur_post_build
 
 if __name__ == '__main__':
