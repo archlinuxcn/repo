bff35646608398de5a629fa8dc2362bb63a61272
axionl <axionl@aosc.io>
2018-07-12 09:03:56 +0800

    [Update] oni-bin: auto update aur repo.

--- oni-bin/lilac.py
+++ oni-bin/lilac.py
@@ -3,8 +3,19 @@
 from lilaclib import *
 
 build_prefix = 'extra-x86_64'
-pre_build = aur_pre_build
-post_build = aur_post_build
+
+def pre_build():
+    version = _G.newver.replace('-', '')
+    for line in edit_file('PKGBUILD'):
+        if line.startswith('pkgver='):
+            line = f'pkgver={version}'
+        print(line)
+    run_cmd(['updpkgsums'])
+
+def post_build():
+    git_add_files('PKGBUILD')
+    git_commit()
+    update_aur_repo
 
 if __name__ == '__main__':
   single_main()
