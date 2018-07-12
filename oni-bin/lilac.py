84c3be2b21df6150a883d81ee48ea714eb2c2d2d
axionl <axionl@aosc.io>
2018-07-12 12:00:47 +0800

    [Update] oni-bin: lilac.py

--- oni-bin/lilac.py
+++ oni-bin/lilac.py
@@ -5,17 +5,13 @@ from lilaclib import *
 build_prefix = 'extra-x86_64'
 
 def pre_build():
-    version = _G.newver.replace('-', '')
-    for line in edit_file('PKGBUILD'):
-        if line.startswith('pkgver='):
-            line = f'pkgver={version}'
-        print(line)
+    update_pkgver_and_pkgrel(version)
     run_cmd(['updpkgsums'])
 
 def post_build():
     git_add_files('PKGBUILD')
     git_commit()
-    update_aur_repo
+    update_aur_repo()
 
 if __name__ == '__main__':
   single_main()
