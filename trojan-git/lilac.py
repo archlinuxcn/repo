cf781334e30aad26e0b0980fe1c665cc865d777f
axionl <axionl@aosc.io>
2018-07-14 13:27:39 +0800

    [Testing] trojan-git: add aur_pre_build.

--- trojan-git/lilac.py
+++ trojan-git/lilac.py
@@ -6,13 +6,16 @@
 
 from lilaclib import *
 
+build_prefix = 'extra-x86_64'
+
+def pre_build():
+    aur_pre_build()
+    vcs_update()
+
 def post_build():
     git_add_files('PKGBUILD')
     git_commit()
     update_aur_repo()
-
-build_prefix = 'extra-x86_64'
-pre_build = vcs_update
 
 if __name__ == '__main__':
     single_main(build_prefix)
