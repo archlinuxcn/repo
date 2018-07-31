b67e82ba961ae1c60c306b427e1a4c82c8277520
a-wing <shiyongxin@aliyun.com>
2018-07-31 18:26:10 +0800

    Fix hmcl lilac.py makedepends

--- hmcl/lilac.py
+++ hmcl/lilac.py
@@ -7,7 +7,10 @@
 from lilaclib import *
 
 build_prefix = 'extra-x86_64'
-pre_build = aur_pre_build
+def pre_build():
+  aur_pre_build()
+  add_makedepends(["imagemagick"])
+
 post_build = aur_post_build
 
 if __name__ == '__main__':
