32a6881cf97ed9e8733f74dc38b0024361809d6b
attenuation <ouyangjun1999@gmail.com>
2018-08-01 23:59:00 +0800

    update besttrace lilac

--- besttrace/lilac.py
+++ besttrace/lilac.py
@@ -7,7 +7,12 @@
 from lilaclib import *
 
 build_prefix = 'extra-x86_64'
-pre_build = aur_pre_build
+def pre_build():
+    aur_pre_build()
+    for line in edit_file('PKGBUILD'):
+        if line.strip() == 'source=("https://cdn.ipip.net/17mon/besttrace4linux.zip")':
+            line = 'source=("besttrace-$pkgver.zip::https://cdn.ipip.net/17mon/besttrace4linux.zip")'
+        print(line)
 post_build = aur_post_build
 
 if __name__ == '__main__':
