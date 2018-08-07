def2d7b3d8c9be8910e669d1419f36ab59ced063
lilydjwg <lilydjwg@gmail.com>
2018-08-07 15:14:25 +0800

    add graphite-api

---
+++ graphite-api/lilac.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+
+import fileinput
+
+from lilaclib import *
+
+build_prefix = 'extra-x86_64'
+
+def pre_build():
+  update_pkgver_and_pkgrel(_G.newver)
+
+def post_build():
+  git_add_files('PKGBUILD')
+  git_commit()
+  update_aur_repo()
+
+if __name__ == '__main__':
+  single_main()
