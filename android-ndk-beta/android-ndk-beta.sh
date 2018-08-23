2412ca91f1768951b57907a90e135b171608bd8a
Chih-Hsuan Yen <yan12125@gmail.com>
2018-08-23 23:00:18 +0800

    android-ndk-beta: update to r18 beta 2 and misc improvements
    * address https://lists.archlinux.org/pipermail/aur-general/2018-August/034249.html
    * Make it more consistent with android-ndk

--- android-ndk-beta/android-ndk-beta.sh
+++ android-ndk-beta/android-ndk-beta.sh
@@ -1,4 +1,4 @@
-export PATH=$PATH:/opt/android-ndk-beta
+export PATH="$PATH:/opt/android-ndk-beta"
 export ANDROID_NDK=/opt/android-ndk-beta
 # Some programs such as gradle ask this as well:
 export ANDROID_NDK_HOME=/opt/android-ndk-beta
