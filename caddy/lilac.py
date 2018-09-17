b695f7a86acaa624aa3a8bb7cf9c4eb8b168e516
axionl <i@axionl.me>
2018-09-17 09:10:14 +0800

    [Update] caddy: add some plugin as default.

--- caddy/lilac.py
+++ caddy/lilac.py
@@ -9,15 +9,13 @@ from lilaclib import *
 build_prefix = 'extra-x86_64'
 
 def pre_build():
-    aur_pre_build()
     for line in edit_file('PKGBUILD'):
         if line.startswith('prepare() {'):
             line += "\n    export CGO_ENABLED=0"
         print(line)
-        
     git_add_files('PKGBUILD')
     git_commit()
-    
+
 post_build = aur_post_build
 
 if __name__ == '__main__':
