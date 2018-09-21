835c711e0acfa66cb18e6b9470d27e5e8566c116
Winston Wu <winstonwu91@gmail.com>
2018-09-21 23:58:17 +0800

    Panoply is now getting sha1sum from nasa web

--- panoply/lilac.py
+++ panoply/lilac.py
@@ -2,17 +2,13 @@ from lilaclib import *
 
 build_prefix = 'extra-x86_64'
 
-def _get_new_version():
-  web = s.get('https://www.giss.nasa.gov/tools/panoply/download/')
-  return re.search(r'\d\.\d\.\d', web.text).group()
-
 def pre_build():
-  update_pkgver_and_pkgrel(_G.newver.lstrip('v'))
+  update_pkgver_and_pkgrel(_G.newver.lstrip('v'),updpkgsums=False)
 
 def post_build():
   git_add_files('PKGBUILD')
   git_commit()
-#  update_aur_repo()
+  update_aur_repo()
 
 if __name__ == '__main__':
   single_main()
