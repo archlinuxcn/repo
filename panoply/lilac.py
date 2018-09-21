3f006109b620f3e77b921552855c30012884098a
Winston Wu <winstonwu91@gmail.com>
2018-09-21 22:33:11 +0800

    Trigger rebuild for Boost 1.68 update

--- panoply/lilac.py
+++ panoply/lilac.py
@@ -2,6 +2,10 @@ from lilaclib import *
 
 build_prefix = 'extra-x86_64'
 
+def _get_new_version():
+  web = s.get('https://www.giss.nasa.gov/tools/panoply/download/')
+  return re.search(r'\d\.\d\.\d', web.text).group()
+
 def pre_build():
   update_pkgver_and_pkgrel(_G.newver.lstrip('v'))
 
