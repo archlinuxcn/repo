28311c1a68cafc5ea276163dac8b2d4ee7916b02
Chih-Hsuan Yen <yan12125@gmail.com>
2018-09-22 10:22:52 +0800

    socat2: new package, replacing socat2-git

--- socat2/lilac.py
+++ socat2/lilac.py
@@ -8,11 +8,17 @@ from lilaclib import *
 
 build_prefix = 'extra-x86_64'
 
+
+def pre_build():
+    update_pkgver_and_pkgrel(_G.newver)
+    run_cmd(['updpkgsums'])
+
+
 def post_build():
     git_add_files('PKGBUILD')
     git_commit()
+    update_aur_repo()
 
-pre_build = vcs_update
 
 if __name__ == '__main__':
     single_main(build_prefix)
