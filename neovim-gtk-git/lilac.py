d998f5a4001dbb5bf7ab14dee7b04c1aea221e9e
axionl <i@axionl.me>
2018-10-01 00:06:41 +0800

    [Add Pkg] neovim-gtk-git: gtk ui for neovim.

---
+++ neovim-gtk-git/lilac.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+#
+# This file is the most simple lilac.py file,
+# and it suits for most packages in AUR.
+#
+
+from lilaclib import *
+
+build_prefix = 'extra-x86_64'
+
+def pre_build():
+    aur_pre_build()
+    vcs_update()
+
+def post_build():
+    git_add_files('PKGBUILD')
+    git_commit()
+
+if __name__ == '__main__':
+    single_main(build_prefix)
