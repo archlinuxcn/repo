6b6cb755c5fcf457c077a8a299a11c17ec96dbb3
Jingbei Li <i@jingbei.li>
2018-10-04 20:31:39 +0800

    fix gnome-shell-extension-topicons-plus-git/lilac.py

--- gnome-shell-extension-topicons-plus-git/lilac.py
+++ gnome-shell-extension-topicons-plus-git/lilac.py
@@ -1,10 +1,16 @@
 #!/usr/bin/env python3
 
 from lilaclib import *
-update_on = [{'aur':'gnome-shell-extension-topicons-plus-git', 'use_last_modified':True}, {'github':'huttli/TopIcons'}]
 
 build_prefix = 'extra-x86_64'
-pre_build = aur_pre_build
+update_on = [{'aur':'gnome-shell-extension-topicons-plus-git', 'use_last_modified':True}, {'github':'huttli/TopIcons-plus'}]
+
+def pre_build():
+    if str(datetime.datetime.now())[:8] < '20181006':
+        run_cmd('rm -rf TopIcons-plus'.split(' '))
+
+    aur_pre_build()
+
 post_build = aur_post_build
 
 if __name__ == '__main__':
