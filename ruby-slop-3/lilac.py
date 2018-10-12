25d06f008d8470d35cfbe97a4fe7210b9c6acb74
a-wing <1@233.email>
2018-10-12 10:55:15 +0800

    Fix ruby-slop-3 lilac

--- ruby-slop-3/lilac.py
+++ ruby-slop-3/lilac.py
@@ -7,11 +7,8 @@
 from lilaclib import *
 
 build_prefix = 'extra-x86_64'
-pre_build = vcs_update
-
-def post_build():
-  git_add_files('PKGBUILD')
-  git_commit()
+pre_build = aur_pre_build
+post_build = aur_post_build
 
 if __name__ == '__main__':
   single_main()
