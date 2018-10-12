fbcf1681c0aabdb92c820c76c71b39e95bf2ba32
Xuanwo <xuanwo.cn@gmail.com>
2018-10-12 09:57:22 +0800

    add coredns

---
+++ coredns/lilac.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+
+from lilaclib import *
+
+build_prefix = 'extra-x86_64'
+
+update_on = [{'aur':'coredns'}]
+
+pre_build = aur_pre_build
+post_build = aur_post_build
+
+if __name__ == '__main__':
+        single_main()
