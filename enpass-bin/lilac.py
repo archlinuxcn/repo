4ac75d5c40c99b1dd14d93e1a04a0ee6ae47d68a
Xuanwo <xuanwo.cn@gmail.com>
2018-10-13 10:20:32 +0800

    enpass-bin: fix enpass-bin.install missing

--- enpass-bin/lilac.py
+++ enpass-bin/lilac.py
@@ -12,19 +12,5 @@ update_on = [{'aur':'enpass-bin'}]
 pre_build = aur_pre_build
 post_build = aur_post_build
 
-def pre_build():
-    aur_pre_build()
-
-    for line in edit_file('PKGBUILD'):
-        if line == "install='enpass-bin.install'":
-            line = ""
-        print(line)
-
-def post_build():
-    aur_post_build()
-
-    git_add_files(['PKGBUILD'])
-    git_commit()
-
 if __name__ == '__main__':
     single_main()
