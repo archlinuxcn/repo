8b672f23664165c3fb5a9b8e09c2d09cbc9869fa
Jiachen YANG <farseerfc@gmail.com>
2018-10-16 15:16:42 +0900

    add boost-65-compat

---
+++ boost-65-compat/lilac.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+#
+# This file is the most simple lilac.py file,
+# and it suits for most packages in AUR.
+#
+
+from lilaclib import *
+
+build_prefix = 'extra-x86_64'
+
+update_on=[{
+  'aur': 'boost-65-compat',
+}]
+
+pre_build = vcs_update
+
+def post_build():
+  git_add_files('PKGBUILD')
+  git_commit()
+
+if __name__ == '__main__':
+  single_main()
