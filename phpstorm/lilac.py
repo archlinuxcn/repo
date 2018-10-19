fa2a9b805f2cd4d2afde968089e9e18efb2ad518
Xuanwo <xuanwo@archlinuxcn.org>
2018-10-19 10:24:33 +0800

    add phpstorm

---
+++ phpstorm/lilac.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+#
+# This file is the most simple lilac.py file,
+# and it suits for most packages in AUR.
+#
+
+from lilaclib import *
+
+build_prefix = 'extra-x86_64'
+update_on = [{'aur':'phpstorm'}]
+
+pre_build = aur_pre_build
+post_build = aur_post_build
+
+if __name__ == '__main__':
+    single_main()
