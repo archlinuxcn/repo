6ca6d5b89c6ba5ef5acd9fc5350f56bf36a5bf33
Chih-Hsuan Yen <yan12125@gmail.com>
2018-10-26 23:18:13 +0800

    google-cloud-sdk: migrated to lilac.yaml

--- google-cloud-sdk/lilac.py
+++ google-cloud-sdk/lilac.py
@@ -1,13 +1,1 @@
-from lilaclib import *
-
-update_on = [{
-  'aur': 'google-cloud-sdk'
-}]
-
-build_prefix = 'extra-x86_64'
-
-pre_build = aur_pre_build
-post_build = aur_post_build
-
-if __name__ == '__main__':
-  single_main()
+# empty for test lilac.yaml
