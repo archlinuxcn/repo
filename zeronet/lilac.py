db5916426b79df5e6fdaa73e827d343cb8345e15
TobiichiAmane <tobiichiamane@archlinuxcn.org>
2018-10-26 09:45:05 +0800

    Add lilac.py for package zeronet

---
+++ zeronet/lilac.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+
+from lilaclib import *
+
+build_prefix = 'extra-x86_64'
+
+def pre_build():
+  update_pkgver_and_pkgrel(_G.newver)
+
+def post_build():
+  git_add_files('PKGBUILD')
+  git_commit()
+
+if __name__ == '__main__':
+  single_main()
