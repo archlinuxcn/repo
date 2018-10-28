2973a7cf44ee99d7ba242dce8ab12317f06aa223
axionl <i@axionl.me>
2018-10-29 01:29:45 +0900

    [Edited] caddy: update the PKGBUILD file.

--- caddy/lilac.py
+++ caddy/lilac.py
@@ -8,15 +8,15 @@ from lilaclib import *
 
 build_prefix = 'extra-x86_64'
 
+
 def pre_build():
-    for line in edit_file('PKGBUILD'):
-        if line.startswith('prepare() {'):
-            line += "\n    export CGO_ENABLED=0"
-        print(line)
+    update_pkgver_and_pkgrel(_G.newver.replace('v', ''))
+
+
+def post_build():
     git_add_files('PKGBUILD')
     git_commit()
 
-post_build = aur_post_build
 
 if __name__ == '__main__':
-  single_main()
+    single_main()
