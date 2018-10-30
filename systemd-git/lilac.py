f20f8a57f6e2897306b5d04abc20b68545c6730c
axionl <i@axionl.me>
2018-10-30 10:11:13 +0900

    [Fixed] systemd-git: change the upstream to github systemd/systemd

--- systemd-git/lilac.py
+++ systemd-git/lilac.py
@@ -6,9 +6,12 @@
 
 from lilaclib import *
 
+def post_build():
+    git_add_files('PKGBUILD')
+    git_commit()
+
 build_prefix = 'extra-x86_64'
-pre_build = aur_pre_build
-post_build = aur_post_build
+pre_build = vcs_update
 
 if __name__ == '__main__':
-  single_main()
+    single_main(build_prefix)
