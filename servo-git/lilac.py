8b6c148db2537d856a55446174ee9af7af52fa58
Jiachen Yang <farseerfc@gmail.com>
2018-11-02 12:37:44 +0900

    servo-git: pr21644 is merged in upstream

--- servo-git/lilac.py
+++ servo-git/lilac.py
@@ -19,12 +19,8 @@ def post_build():
     update_aur_repo()
 
 def pre_build():
-    # run updpkgsums before and after vcs_update
-    # as the PR is constantly updating to be mergable with the master branch
-    run_cmd(['rm', 'pr21644.patch'])
     run_cmd(['updpkgsums'])
     vcs_update()
-    run_cmd(['updpkgsums'])
 
 if __name__ == '__main__':
     single_main(build_prefix)
