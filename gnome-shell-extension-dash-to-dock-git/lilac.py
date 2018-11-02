8e0830d33cee6a7d5a04ba7feaef2e2e97aa43c7
Jianfeng Zhang <swordfeng123@gmail.com>
2018-11-01 20:58:08 -0700

    gnome-shell-extension-dash-to-dock-git: workaround #885

--- gnome-shell-extension-dash-to-dock-git/lilac.py
+++ gnome-shell-extension-dash-to-dock-git/lilac.py
@@ -3,7 +3,14 @@
 from lilaclib import *
 
 build_prefix = 'extra-x86_64'
-pre_build = aur_pre_build
+def pre_build():
+    aur_pre_build()
+    for line in edit_file('PKGBUILD'):
+        if 'gnome-shell<3.$' not in line:
+            print(line)
+        else:
+            print(line.replace('gnome-shell<3.$', 'gnome-shell<1:3.$'))
+
 post_build = aur_post_build
 
 if __name__ == '__main__':
