19defdf52f0030532a29cc4900b534f99ee724b0
axionl <i@axionl.me>
2018-11-02 10:34:07 +0900

    [Fixed] oni-bin: auto update from beta version

--- oni-bin/lilac.py
+++ oni-bin/lilac.py
@@ -4,14 +4,30 @@ from lilaclib import *
 
 build_prefix = 'extra-x86_64'
 
+
 def pre_build():
-    update_pkgver_and_pkgrel(_G.newver.lstrip('v'))
+    _betaver = _G.newver.lstrip('v')
+
+    pkgver, pkgrel = get_pkgver_and_pkgrel()
+
+    for line in edit_file('PKGBUILD'):
+        if line.startwith('_betaver=') and pkgver != _betaver.replace('-', ''):
+            line = f'_betaver={_betaver}'
+        elif line.startwith('pkgrel='):
+            if pkgver != _betaver.replace('-', ''):
+                line = 'pkgrel=1'
+            else:
+                line = f'pkgrel={int(pkgrel)+1}'
+    print(line)
+
     run_cmd(['updpkgsums'])
+
 
 def post_build():
     git_add_files('PKGBUILD')
     git_commit()
     update_aur_repo()
 
+
 if __name__ == '__main__':
-  single_main()
+    single_main()
