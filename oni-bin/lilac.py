b4ca01ba1f1454ba0911c7915c2634bca6fc4ecb
axionl <i@axionl.me>
2018-11-04 00:01:52 +0900

    [Fixed] oni-bin: update from aur repo

--- oni-bin/lilac.py
+++ oni-bin/lilac.py
@@ -1,33 +1,14 @@
 #!/usr/bin/env python3
+#
+# This file is the most simple lilac.py file,
+# and it suits for most packages in AUR.
+#
 
 from lilaclib import *
 
 build_prefix = 'extra-x86_64'
-
-
-def pre_build():
-    _betaver = _G.newver.lstrip('v')
-
-    pkgver, pkgrel = get_pkgver_and_pkgrel()
-
-    for line in edit_file('PKGBUILD'):
-        if line.startswith('_betaver=') and pkgver != _betaver.replace('-', ''):
-            line = f'_betaver={_betaver}'
-        elif line.startswith('pkgrel='):
-            if pkgver != _betaver.replace('-', ''):
-                line = 'pkgrel=1'
-            else:
-                line = f'pkgrel={int(pkgrel)+1}'
-    print(line)
-
-    run_cmd(['updpkgsums'])
-
-
-def post_build():
-    git_add_files('PKGBUILD')
-    git_commit()
-    update_aur_repo()
-
+pre_build = aur_pre_build
+post_build = aur_post_build
 
 if __name__ == '__main__':
-    single_main()
+    single_main(build_prefix)
