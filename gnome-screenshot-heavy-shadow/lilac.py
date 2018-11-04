8e81562fac368c73acf53c8f6f37d67d0f4e0abe
Jiachen YANG <farseerfc@gmail.com>
2018-11-04 23:30:20 +0900

    gnome-screenshot-heavy-shadow remove groups

--- gnome-screenshot-heavy-shadow/lilac.py
+++ gnome-screenshot-heavy-shadow/lilac.py
@@ -8,8 +8,14 @@ from lilaclib import *
 
 
 build_prefix = 'extra-x86_64'
-pre_build = aur_pre_build
 post_build = aur_post_build
+
+def pre_build():
+	aur_pre_build()
+	for line in edit_file('PKGBUILD'):
+		if line.startswith('groups='):
+			continue
+		print(line)
 
 
 if __name__ == '__main__':
