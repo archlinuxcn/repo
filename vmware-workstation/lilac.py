76021d50f12925c4b5d8387c568e4673e42c9c64
Sakuraba Amane <tobiichiamane@archlinuxcn.org>
2018-11-05 10:58:53 +0800

    Switch to lilac.yaml for vmware-workstation

--- vmware-workstation/lilac.py
+++ vmware-workstation/lilac.py
@@ -1,10 +1,10 @@
 #!/usr/bin/env python3
+# Empty lilac.py to test lilac.yaml
+#from lilaclib import *
 
-from lilaclib import *
+#build_prefix = 'archlinuxcn-x86_64'
+#pre_build = aur_pre_build
+#post_build = aur_post_build
 
-build_prefix = 'archlinuxcn-x86_64'
-pre_build = aur_pre_build
-post_build = aur_post_build
-
-if __name__ == '__main__':
-  single_main()
+#if __name__ == '__main__':
+#  single_main()
