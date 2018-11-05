d3d6eac0e10bec5a2b3ee32a3d9dfe24be443cdf
Sakuraba Amane <tobiichiamane@archlinuxcn.org>
2018-11-05 08:20:32 +0800

    Update pkgrel and lilac.py for nautilus-typeahead

--- nautilus-typeahead/lilac.py
+++ nautilus-typeahead/lilac.py
@@ -3,8 +3,15 @@
 from lilaclib import *
 
 build_prefix = 'extra-x86_64'
-pre_build = aur_pre_build
 post_build = aur_post_build
+
+# Remove all groups when fetching from AUR
+def pre_build():
+	aur_pre_build()
+	for line in edit_file('PKGBUILD'):
+		if line.startswith('groups='):
+			continue
+		print(line)
 
 if __name__ == '__main__':
   single_main()
