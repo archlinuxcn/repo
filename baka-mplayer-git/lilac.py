b92a963d1acad37433dc7793cee091d4681155af
yk <yk@archlinuxcn.org>
2018-11-06 01:54:13 +0800

    upgpkg: baka-mplayer-git

--- baka-mplayer-git/lilac.py
+++ baka-mplayer-git/lilac.py
@@ -7,7 +7,16 @@
 from lilaclib import *
 
 build_prefix = 'archlinuxcn-x86_64'
-pre_build = aur_pre_build
+
+def pre_build():
+    run_cmd(["rm", "-rf", "emacs-git"])
+    aur_pre_build()
+    for line in edit_file('PKGBUILD'):
+            if line.startswith('replaces='):
+                    continue
+            print(line)
+
+
 post_build = aur_post_build
 
 if __name__ == '__main__':
