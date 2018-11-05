a4f7e4ede1ed7d7de82672f7ed07d4fc465a9c9d
axionl <i@axionl.me>
2018-11-06 00:45:57 +0900

    [Update] quaternion: testing the lilac.yaml

--- quaternion/lilac.py
+++ quaternion/lilac.py
@@ -4,11 +4,9 @@ from lilaclib import *
 
 build_prefix = 'extra-x86_64'
 
-def pre_build():
-	aur_pre_build()
-	add_makedepends(["qt5ct"])
+pre_build = aur_pre_build
 
 post_build = aur_post_build
 
 if __name__ == '__main__':
-  single_main()
+    single_main()
