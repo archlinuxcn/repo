c82c3d53b8a6ab86419b5c5a11ccca2d181d265d
yk <yk@archlinuxcn.org>
2018-11-06 01:47:27 +0800

    upgpkg: emacs-git

--- emacs-git/lilac.py
+++ emacs-git/lilac.py
@@ -11,6 +11,11 @@ build_prefix = 'archlinuxcn-x86_64'
 def pre_build():
     run_cmd(["rm", "-rf", "emacs-git"])
     aur_pre_build()
+    for line in edit_file('PKGBUILD'):
+            if line.startswith('replaces='):
+                    continue
+            print(line)
+
 
 post_build = aur_post_build
 
