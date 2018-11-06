5ebed902283d8193e7daf7aba75c36c8f7ac55fe
axionl <i@axionl.me>
2018-11-06 18:10:21 +0900

    [AddPkg] fish-git: add the fish-shell for testing

---
+++ fish-git/lilac.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+#
+# This file is the most simple lilac.py file,
+# and it suits for most packages in AUR.
+#
+
+from lilaclib import *
+
+def post_build():
+    git_add_files('PKGBUILD')
+    git_commit()
+
+build_prefix = 'extra-x86_64'
+pre_build = vcs_update
+
+if __name__ == '__main__':
+    single_main(build_prefix)
