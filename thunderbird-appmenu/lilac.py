3a772c0ad247bce8d8b53a87df975d815df2f121
Jiachen YANG <farseerfc@gmail.com>
2018-11-06 23:07:13 +0900

    thunderbird-appmenu: remove update_aur_repo

--- thunderbird-appmenu/lilac.py
+++ thunderbird-appmenu/lilac.py
@@ -5,13 +5,11 @@ from lilaclib import *
 
 
 def pre_build():
-  print(_G.newver)
   update_pkgver_and_pkgrel(_G.newver[:_G.newver.find('-')])
 
 def post_build():
   git_add_files('PKGBUILD')
   git_commit()
-  update_aur_repo()
 
 if __name__ == '__main__':
   single_main()
