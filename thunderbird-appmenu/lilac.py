40996c5ff630ab47189820463d7961f6809607fd
Jiachen YANG <farseerfc@gmail.com>
2018-11-06 12:21:22 +0900

    thunderbid-appmenu strip pkgrel from _G.newver

--- thunderbird-appmenu/lilac.py
+++ thunderbird-appmenu/lilac.py
@@ -5,7 +5,7 @@ from lilaclib import *
 
 
 def pre_build():
-  update_pkgver_and_pkgrel(_G.newver)
+  update_pkgver_and_pkgrel(_G.newver[:_G.newver.find('-')])
 
 def post_build():
   git_add_files('PKGBUILD')
