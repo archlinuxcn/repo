cad25886ad07f505390ed17e78684d2ad868bfc0
axionl <i@axionl.me>
2018-11-06 17:33:37 +0900

    [AddPkg] zafiro-icon-theme: A icon pack flat with light colors.

---
+++ zafiro-icon-theme/lilac.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+#
+
+from lilaclib import *
+
+
+def pre_build():
+    update_pkgver_and_pkgrel(_G.newver.lstrip('v'))
+
+
+def post_build():
+    git_add_files('PKGBUILD')
+    git_commit()
+    update_aur_repo()
+
+
+if __name__ == '__main__':
+    single_main()
