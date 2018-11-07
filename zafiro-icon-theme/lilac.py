06911e819c7282cd48fb17945461c006afdafe6f
axionl <i@axionl.me>
2018-11-07 18:22:46 +0900

    [Update] zafiro-icon-theme: update from aur

--- zafiro-icon-theme/lilac.py
+++ zafiro-icon-theme/lilac.py
@@ -1,18 +1,1 @@
-#!/usr/bin/env python3
-#
-
-from lilaclib import *
-
-
-def pre_build():
-    update_pkgver_and_pkgrel(_G.newver.lstrip('v'))
-
-
-def post_build():
-    git_add_files('PKGBUILD')
-    git_commit()
-    update_aur_repo()
-
-
-if __name__ == '__main__':
-    single_main()
+# empty for test lilac.yaml
