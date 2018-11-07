291377b8ce497063ac19b12fe8bd54ec3abaaf32
Sakuraba Amane <tobiichiamane@archlinuxcn.org>
2018-11-07 23:03:01 +0800

    Remove groups for nautilus-typeahead && update lilac.py

--- nautilus-typeahead/lilac.py
+++ nautilus-typeahead/lilac.py
@@ -9,7 +9,7 @@ post_build = aur_post_build
 def pre_build():
 	aur_pre_build()
 	for line in edit_file('PKGBUILD'):
-		if line.startswith('groups='):
+		if line.strip().startswith('groups='):
 			continue
 		print(line)
 
