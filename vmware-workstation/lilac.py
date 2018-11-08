a0850cbaad7df464c0bb8dd67cb4f1a41fa05bdc
Sakuraba Amane <tobiichiamane@archlinuxcn.org>
2018-11-08 19:09:10 +0800

    Split package vmware-workstation && add pre-patched vmware-workstation-macos

--- vmware-workstation/lilac.py
+++ vmware-workstation/lilac.py
@@ -1,10 +1,2 @@
 #!/usr/bin/env python3
-# Empty lilac.py to test lilac.yaml
-#from lilaclib import *
-
-#build_prefix = 'archlinuxcn-x86_64'
-#pre_build = aur_pre_build
-#post_build = aur_post_build
-
-#if __name__ == '__main__':
-#  single_main()
+#Empty lilac.py to test lilac.yaml
