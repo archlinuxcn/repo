7860d27aa5dc1095ab8f0baccc9afd9794cfc81b
Jingbei Li <i@jingbei.li>
2018-11-09 13:26:10 +0800

    updated vim-fcitx/lilac.py

--- vim-fcitx/lilac.py
+++ vim-fcitx/lilac.py
@@ -1,14 +1,17 @@
 #!/usr/bin/env python3
-#
-# This file is the most simple lilac.py file,
-# and it suits for most packages in AUR.
-#
-
 from lilaclib import *
 
-build_prefix = 'archlinuxcn-x86_64'
-pre_build = aur_pre_build
+update_on = [{'aur':None}]
+build_prefix = 'extra-x86_64'
+
+def pre_build():
+    aur_pre_build()
+
+    for line in edit_file('PKGBUILD'):
+        if not line.startswith('groups=('):
+            print(line)
+
 post_build = aur_post_build
 
 if __name__ == '__main__':
-  single_main()
+    single_main(build_prefix)
