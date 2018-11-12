f8426ec4f6d49210c927fb4184d3d87114dac2ed
Sakuraba Amane <tobiichiamane@archlinuxcn.org>
2018-11-12 10:28:35 +0800

    Switch msbuild-stable to lilac.yaml

--- msbuild-stable/lilac.py
+++ msbuild-stable/lilac.py
@@ -1,10 +1,10 @@
 #!/usr/bin/env python3
 
-from lilaclib import *
+#from lilaclib import *
 
-build_prefix = 'extra-x86_64'
-pre_build = aur_pre_build
-post_build = aur_post_build
+#build_prefix = 'extra-x86_64'
+#pre_build = aur_pre_build
+#post_build = aur_post_build
 
-if __name__ == '__main__':
-  single_main()
+#if __name__ == '__main__':
+#  single_main()
