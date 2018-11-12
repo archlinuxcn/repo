eb86c1e1a2ec53cdf7d65b842484e1204d0044aa
Sakuraba Amane <tobiichiamane@archlinuxcn.org>
2018-11-12 10:19:57 +0800

    Switch chromium-vaapi to lilac.yaml

--- chromium-vaapi/lilac.py
+++ chromium-vaapi/lilac.py
@@ -1,11 +1,11 @@
 #!/usr/bin/env python3
 
-from lilaclib import *
+#from lilaclib import *
 
-build_prefix = 'extra-x86_64'
-pre_build = aur_pre_build
-post_build = aur_post_build
-time_limit_hours = 4
+#build_prefix = 'extra-x86_64'
+#pre_build = aur_pre_build
+#post_build = aur_post_build
+#time_limit_hours = 4
 
-if __name__ == '__main__':
-  single_main()
+#if __name__ == '__main__':
+#  single_main()
