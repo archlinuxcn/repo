8c27d07094500144a3b3a436cc31644fec265426
Sakuraba Amane <tobiichiamane@archlinuxcn.org>
2018-11-12 10:40:01 +0800

    Switch lilacinfo to lilac.yaml

--- lilacinfo/lilac.py
+++ lilacinfo/lilac.py
@@ -1,11 +1,11 @@
 from lilaclib import *
 
-build_prefix = 'extra-x86_64'
-pre_build = vcs_update
+#build_prefix = 'extra-x86_64'
+#pre_build = vcs_update
 
 def post_build():
   git_add_files('PKGBUILD')
   git_commit()
 
-if __name__ == '__main__':
-  single_main()
+#if __name__ == '__main__':
+#  single_main()
