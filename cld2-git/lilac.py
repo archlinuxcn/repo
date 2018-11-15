075b2402af1bb0bf30494375ccc0bc5c2db61b7c
lilydjwg <lilydjwg@gmail.com>
2018-11-15 19:58:26 +0800

    Switch cld2-git to lilac.yaml

--- cld2-git/lilac.py
+++ cld2-git/lilac.py
@@ -1,8 +1,9 @@
+# Trimmed lilac.py
 #!/usr/bin/env python3
 
 from lilaclib import *
 
-build_prefix = 'extra-x86_64'
+#build_prefix = 'extra-x86_64'
 
 def pre_build():
   vcs_update()
@@ -12,5 +13,5 @@ def post_build():
   git_commit()
   update_aur_repo()
 
-if __name__ == '__main__':
-  single_main()
+#if __name__ == '__main__':
+#  single_main()
