40a9bb55dadb907a812fc6b2152e0c9163300e9f
Jingbei Li <i@jingbei.li>
2018-11-15 14:16:32 +0800

    trigger rebuild for vim-tern

--- vim-tern/lilac.py
+++ vim-tern/lilac.py
@@ -3,7 +3,13 @@ from lilaclib import *
 
 update_on = [{'aur':None}, {'github':'ternjs/tern_for_vim'}]
 build_prefix = 'extra-x86_64'
-pre_build = aur_pre_build
+
+def pre_build():
+    if str(datetime.datetime.now())[:8] < '20180604':
+        run_cmd('rm -rf tern_for_vim'.split(' '))
+
+    aur_pre_build()
+
 post_build = aur_post_build
 
 if __name__ == '__main__':
