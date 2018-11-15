ba904bc8248526abf672bd71b4ab2db5f8651947
Jiachen YANG <farseerfc@gmail.com>
2018-11-15 17:08:06 +0900

    zfs-dkms-git follow AUR as it is managed by archzfs-bot

--- zfs-dkms-git/lilac.py
+++ zfs-dkms-git/lilac.py
@@ -19,18 +19,3 @@
 
 from lilaclib import *
 
-build_prefix = 'extra-x86_64'
-
-
-def pre_build():
-    aur_pre_build()
-    add_makedepends(["python2"])
-
-post_build = aur_post_build
-
-# do some cleanup here after building the package, regardless of result
-# def post_build_always(success):
-#   pass
-
-if __name__ == '__main__':
-    single_main(build_prefix)
