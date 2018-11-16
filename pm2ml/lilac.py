b3fbd9068306d0bfbdff2b6cffff4e3bc110a48c
Jiachen YANG <farseerfc@gmail.com>
2018-11-16 20:50:15 +0900

    Switch pm2ml to lilac.yaml

--- pm2ml/lilac.py
+++ pm2ml/lilac.py
@@ -1,3 +1,4 @@
+# Trimmed lilac.py
 #!/usr/bin/env python3
 #
 # This file is the most simple lilac.py file,
@@ -6,12 +7,12 @@
 
 from lilaclib import *
 
-build_prefix = 'archlinuxcn-x86_64'
+#build_prefix = 'archlinuxcn-x86_64'
 depends = ['python3-xcgf', 'python3-xcpf']
 
-pre_build = aur_pre_build
+#pre_build = aur_pre_build
 
-post_build = aur_post_build
+#post_build = aur_post_build
 
-if __name__ == '__main__':
-  single_main()
+#if __name__ == '__main__':
+#  single_main()
