42e568cd3453c2afc5da90b556d57e0324adbaa6
Jiachen YANG <farseerfc@gmail.com>
2018-11-16 20:50:02 +0900

    Switch openvisualtraceroute to lilac.yaml

--- openvisualtraceroute/lilac.py
+++ openvisualtraceroute/lilac.py
@@ -1,8 +1,9 @@
+# Trimmed lilac.py
 from lilaclib import *
 
-build_prefix = 'extra-x86_64'
-pre_build = aur_pre_build
-post_build = aur_post_build
+#build_prefix = 'extra-x86_64'
+#pre_build = aur_pre_build
+#post_build = aur_post_build
 
-if __name__ == '__main__':
-  single_main()
+#if __name__ == '__main__':
+#  single_main()
