1863e9498d12077fd70765bcbdab66ed58f5f856
Jiachen YANG <farseerfc@gmail.com>
2018-11-16 20:50:59 +0900

    Switch tdrop-git to lilac.yaml

--- tdrop-git/lilac.py
+++ tdrop-git/lilac.py
@@ -1,3 +1,4 @@
+# Trimmed lilac.py
 #!/usr/bin/env python3
 #
 # This file is the most simple lilac.py file,
@@ -7,12 +8,12 @@
 from lilaclib import *
 
 
-build_prefix = 'extra-x86_64'
-pre_build = vcs_update
+#build_prefix = 'extra-x86_64'
+#pre_build = vcs_update
 
 def post_build():
   git_add_files('PKGBUILD')
   git_commit()
 
-if __name__ == '__main__':
-  single_main()
+#if __name__ == '__main__':
+#  single_main()
