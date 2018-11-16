26e286fe44c048a753d6e59b6d53faddba14d4c5
Jiachen YANG <farseerfc@gmail.com>
2018-11-16 20:50:40 +0900

    Switch rar to lilac.yaml

--- rar/lilac.py
+++ rar/lilac.py
@@ -1,12 +1,13 @@
+# Trimmed lilac.py
 #!/usr/bin/env python3
 
 from lilaclib import *
 
-build_prefix = 'extra-x86_64'
+#build_prefix = 'extra-x86_64'
 
 
-pre_build = aur_pre_build
-post_build = aur_post_build
+#pre_build = aur_pre_build
+#post_build = aur_post_build
 
-if __name__ == '__main__':
-    single_main(build_prefix)
+#if __name__ == '__main__':
+#    single_main(build_prefix)
