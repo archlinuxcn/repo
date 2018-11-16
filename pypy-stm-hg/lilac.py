98cf8e947b7ae6dfb1b9685b985fdb7a4148d0c2
Jiachen YANG <farseerfc@gmail.com>
2018-11-16 20:50:32 +0900

    Switch pypy-stm-hg to lilac.yaml

--- pypy-stm-hg/lilac.py
+++ pypy-stm-hg/lilac.py
@@ -1,3 +1,4 @@
+# Trimmed lilac.py
 #!/usr/bin/env python3
 #
 # This file is the most simple lilac.py file,
@@ -12,14 +13,14 @@ depends = [
     ('llvm-pypy-stm', 'clang-pypy-stm')
 ]
 
-build_prefix = 'extra-x86_64'
+#build_prefix = 'extra-x86_64'
 
-pre_build = vcs_update
+#pre_build = vcs_update
 
 def post_build():
     git_add_files("PKGBUILD")
     git_commit()
     update_aur_repo()
 
-if __name__ == '__main__':
-  single_main(build_prefix)
+#if __name__ == '__main__':
+#  single_main(build_prefix)
