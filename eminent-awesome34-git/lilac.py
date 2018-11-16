2b85f54202c4c5f4ca4f2636c6c96ccdde97d58b
Jiachen YANG <farseerfc@gmail.com>
2018-11-16 20:41:06 +0900

    Switch eminent-awesome34-git to lilac.yaml

--- eminent-awesome34-git/lilac.py
+++ eminent-awesome34-git/lilac.py
@@ -1,3 +1,4 @@
+# Trimmed lilac.py
 #!/usr/bin/env python3
 #
 # This is a complex version of lilac.py for building
@@ -21,7 +22,7 @@ from lilaclib import *
 
 depends = ['awesome34']
 
-build_prefix = 'extra-x86_64'
+#build_prefix = 'extra-x86_64'
 
 
 def pre_build():
@@ -34,11 +35,11 @@ def pre_build():
         print(line)
 
 
-post_build = aur_post_build
+#post_build = aur_post_build
 
 # do some cleanup here after building the package, regardless of result
 # def post_build_always(success):
 #   pass
 
-if __name__ == '__main__':
-    single_main(build_prefix)
+#if __name__ == '__main__':
+#    single_main(build_prefix)
