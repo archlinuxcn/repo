8d191507b90c15c2cf3f71652db843f63aaa7afd
Jiachen YANG <farseerfc@gmail.com>
2018-11-16 20:48:49 +0900

    Switch netease-musicbox-git to lilac.yaml

--- netease-musicbox-git/lilac.py
+++ netease-musicbox-git/lilac.py
@@ -1,3 +1,4 @@
+# Trimmed lilac.py
 #!/usr/bin/env python3
 #
 # This file is the most simple lilac.py file,
@@ -10,8 +11,8 @@ def post_build():
     git_add_files('PKGBUILD')
     git_commit()
 
-build_prefix = 'archlinuxcn-x86_64'
-pre_build = vcs_update
+#build_prefix = 'archlinuxcn-x86_64'
+#pre_build = vcs_update
 
-if __name__ == '__main__':
-    single_main(build_prefix)
+#if __name__ == '__main__':
+#    single_main(build_prefix)
