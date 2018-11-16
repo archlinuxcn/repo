1621ff20860b3829b7f321e1ad715618bb5a561d
Jiachen YANG <farseerfc@gmail.com>
2018-11-16 20:47:50 +0900

    Switch masterpdfeditor to lilac.yaml

--- masterpdfeditor/lilac.py
+++ masterpdfeditor/lilac.py
@@ -1,3 +1,4 @@
+# Trimmed lilac.py
 #!/usr/bin/env python3
 #
 # This file is the most simple lilac.py file,
@@ -6,13 +7,13 @@
 
 from lilaclib import *
 
-build_prefix = 'extra-x86_64'
-post_build = aur_post_build
+#build_prefix = 'extra-x86_64'
+#post_build = aur_post_build
 
 def pre_build():
     run_cmd(["rm", "-f",  "master-pdf-editor-4.0.60_qt5.amd64.tar.gz"])
     aur_pre_build()
 
 
-if __name__ == '__main__':
-  single_main(build_prefix)
+#if __name__ == '__main__':
+#  single_main(build_prefix)
