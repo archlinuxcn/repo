2918e9b89373a490ac7fbd885d9b69061d45b31d
Jingbei Li <i@jingbei.li>
2018-11-16 15:24:50 +0800

    fix vim-tern/lilac.py

--- vim-tern/lilac.py
+++ vim-tern/lilac.py
@@ -1,11 +1,12 @@
 #!/usr/bin/env python3
 from lilaclib import *
+import datetime
 
 update_on = [{'aur':None}, {'github':'ternjs/tern_for_vim'}]
 build_prefix = 'extra-x86_64'
 
 def pre_build():
-    if str(datetime.datetime.now())[:8] < '20180604':
+    if str(datetime.datetime.now())[:8] < '20181117':
         run_cmd('rm -rf tern_for_vim'.split(' '))
 
     aur_pre_build()
