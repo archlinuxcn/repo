55596d614d9196e27c350ab0f1803a7bfd67daea
Jiachen YANG <farseerfc@gmail.com>
2018-11-16 20:51:05 +0900

    Switch typora to lilac.yaml

--- typora/lilac.py
+++ typora/lilac.py
@@ -1,3 +1,4 @@
+# Trimmed lilac.py
 #!/usr/bin/env python3
 #
 # This file is the most simple lilac.py file,
@@ -7,15 +8,15 @@
 from lilaclib import *
 
 
-build_prefix = 'extra-x86_64'
+#build_prefix = 'extra-x86_64'
 
 
 def pre_build():
     run_cmd(["updpkgsums"])
     aur_pre_build()
 
-post_build = aur_post_build
+#post_build = aur_post_build
 
-if __name__ == '__main__':
-    single_main(build_prefix)
+#if __name__ == '__main__':
+#    single_main(build_prefix)
 
