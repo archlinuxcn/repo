4268f8931b1ca55a3c830f199ef761380b58ee8a
Jiachen YANG <farseerfc@gmail.com>
2018-11-16 20:51:19 +0900

    Switch vivaldi to lilac.yaml

--- vivaldi/lilac.py
+++ vivaldi/lilac.py
@@ -1,3 +1,4 @@
+# Trimmed lilac.py
 #!/usr/bin/env python3
 #
 # This file is the most simple lilac.py file,
@@ -7,7 +8,7 @@
 from lilaclib import *
 import glob
 
-build_prefix = 'extra-x86_64'
+#build_prefix = 'extra-x86_64'
 
 _g = SimpleNamespace()
 
@@ -41,6 +42,6 @@ def post_build():
   del _g.aur_pre_files, _g.aur_building_files
 
 
-if __name__ == '__main__':
-    single_main(build_prefix)
+#if __name__ == '__main__':
+#    single_main(build_prefix)
 
