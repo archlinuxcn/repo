d4eaba4eae7691db80af58f375ed96c7cdbb7076
lilydjwg <lilydjwg@gmail.com>
2018-11-17 11:47:06 +0800

    Switch global to lilac.yaml

--- global/lilac.py
+++ global/lilac.py
@@ -1,9 +1,10 @@
+# Trimmed lilac.py
 #!/usr/bin/env python3
 
 from lilaclib import *
 
-build_prefix = 'extra-x86_64'
-post_build = aur_post_build
+#build_prefix = 'extra-x86_64'
+#post_build = aur_post_build
 
 def pre_build():
   aur_pre_build()
@@ -12,5 +13,5 @@ def pre_build():
       line = line + '\nmakedepends=("python2" "python2-pygments")'
     print(line)
 
-if __name__ == '__main__':
-  single_main()
+#if __name__ == '__main__':
+#  single_main()
