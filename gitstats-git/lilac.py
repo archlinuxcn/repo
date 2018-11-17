fb7fe28f4dad6b7cec3c9de1b95216d466fac5c1
lilydjwg <lilydjwg@gmail.com>
2018-11-17 11:47:30 +0800

    Switch gitstats-git to lilac.yaml

--- gitstats-git/lilac.py
+++ gitstats-git/lilac.py
@@ -1,10 +1,11 @@
+# Trimmed lilac.py
 #!/usr/bin/env python3
 
 from lilaclib import *
 
-build_prefix = 'extra-x86_64'
-post_build = aur_post_build
-pre_build = aur_pre_build
+#build_prefix = 'extra-x86_64'
+#post_build = aur_post_build
+#pre_build = aur_pre_build
 
-if __name__ == '__main__':
-  single_main()
+#if __name__ == '__main__':
+#  single_main()
