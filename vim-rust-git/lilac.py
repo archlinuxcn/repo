01576a00863644f2b2baf8d82375269b446187dd
lilydjwg <lilydjwg@gmail.com>
2018-11-19 14:01:50 +0800

    Switch vim-rust-git to lilac.yaml

--- vim-rust-git/lilac.py
+++ vim-rust-git/lilac.py
@@ -1,14 +1,15 @@
+# Trimmed lilac.py
 #!/usr/bin/env python3
 
 from lilaclib import *
 
-build_prefix = 'extra-x86_64'
+#build_prefix = 'extra-x86_64'
 
-pre_build = vcs_update
+#pre_build = vcs_update
 
 def post_build():
   git_add_files('PKGBUILD')
   git_commit()
 
-if __name__ == '__main__':
-  single_main()
+#if __name__ == '__main__':
+#  single_main()
