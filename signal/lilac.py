642e6b3f7635a3c4af9e198cc75ce6c645facc54
axionl <i@axionl.me>
2018-11-20 00:11:03 +0900

    Switch signal to lilac.yaml

--- signal/lilac.py
+++ signal/lilac.py
@@ -1,10 +1,11 @@
+# Trimmed lilac.py
 #!/usr/bin/env python3
 
 from lilaclib import *
 
-build_prefix = 'archlinuxcn-x86_64'
-pre_build = aur_pre_build
-post_build = aur_post_build
+#build_prefix = 'archlinuxcn-x86_64'
+#pre_build = aur_pre_build
+#post_build = aur_post_build
 
-if __name__ == '__main__':
-  single_main()
+#if __name__ == '__main__':
+#  single_main()
