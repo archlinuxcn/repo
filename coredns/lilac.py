b72ee75dad1f78142b7c756ba9f39387ca5671a4
Xuanwo <xuanwo@archlinuxcn.org>
2018-11-19 16:37:39 +0800

    coredns: Switch to lilac yaml

--- coredns/lilac.py
+++ coredns/lilac.py
@@ -2,11 +2,11 @@
 
 from lilaclib import *
 
-build_prefix = 'extra-x86_64'
-update_on = [{
-    'github': 'coredns/coredns',
-    'use_latest_release': True
-}]
+# build_prefix = 'extra-x86_64'
+# update_on = [{
+#    'github': 'coredns/coredns',
+#    'use_latest_release': True
+#}]
 
 def pre_build():
     update_pkgver_and_pkgrel(_G.newver.lstrip('v'))
@@ -16,5 +16,5 @@ def post_build():
     git_add_files('PKGBUILD')
     git_commit()
 
-if __name__ == '__main__':
-        single_main()
+#if __name__ == '__main__':
+#        single_main()
