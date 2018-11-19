6fe98e4c3491d115ae9c0c1d43a47ff485190196
axionl <i@axionl.me>
2018-11-20 00:13:22 +0900

    [Update] nvchecker 2 yaml

--- trizen/lilac.py
+++ trizen/lilac.py
@@ -6,9 +6,4 @@
 
 from lilaclib import *
 
-build_prefix = 'extra-x86_64'
-pre_build = aur_pre_build
-post_build = aur_post_build
-
-if __name__ == '__main__':
-  single_main()
+ update_on = [{'aur':'trizen'}] 
