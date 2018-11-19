55c1f43a43cbe2c97f1b850a186ae3df6bb72b82
lilydjwg <lilydjwg@gmail.com>
2018-11-19 14:09:35 +0800

    Switch bcc-git to lilac.yaml

--- bcc-git/lilac.py
+++ bcc-git/lilac.py
@@ -1,8 +1,9 @@
+# Trimmed lilac.py
 #!/usr/bin/env python3
 
 from lilaclib import *
 
-build_prefix = 'extra-x86_64'
+#build_prefix = 'extra-x86_64'
 
 def pre_build():
   vcs_update()
@@ -11,5 +12,5 @@ def post_build():
   git_add_files('PKGBUILD')
   git_commit()
 
-if __name__ == '__main__':
-  single_main(build_prefix)
+#if __name__ == '__main__':
+#  single_main(build_prefix)
