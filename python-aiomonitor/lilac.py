6b140491dd9e888d5c805e5a3d4e49d3dfd2f7e7
lilydjwg <lilydjwg@gmail.com>
2018-11-19 14:11:16 +0800

    Switch python-aiomonitor to lilac.yaml

--- python-aiomonitor/lilac.py
+++ python-aiomonitor/lilac.py
@@ -1,16 +1,8 @@
-#!/usr/bin/env python3
-
 from lilaclib import *
 
-build_prefix = 'extra-x86_64'
-depends = ['python-aioconsole']
-
 def pre_build():
-  pypi_pre_build(depends=depends[:] + ['python-terminaltables'])
+  pypi_pre_build(depends=['python-aioconsole', 'python-terminaltables'])
 
 def post_build():
   pypi_post_build()
   update_aur_repo()
-
-if __name__ == '__main__':
-  single_main()
