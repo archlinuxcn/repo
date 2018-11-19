83bb0943e0b3d612b9e331b024fdb73ad7e87cd7
lilydjwg <lilydjwg@gmail.com>
2018-11-19 14:07:53 +0800

    Switch python2-uniout to lilac.yaml

--- python2-uniout/lilac.py
+++ python2-uniout/lilac.py
@@ -1,10 +1,11 @@
+# Trimmed lilac.py
 #!/usr/bin/env python3
 
 import fileinput
 
 from lilaclib import *
 
-build_prefix = 'extra-x86_64'
+#build_prefix = 'extra-x86_64'
 
 def pre_build():
   pypi_pre_build(python2=True, depends_setuptools=False)
@@ -13,5 +14,5 @@ def post_build():
   pypi_post_build()
   update_aur_repo()
 
-if __name__ == '__main__':
-  single_main()
+#if __name__ == '__main__':
+#  single_main()
