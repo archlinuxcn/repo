ea2a2eb1d891a0575dcb108aa6dda1e27744d911
axionl <i@axionl.me>
2018-11-20 00:11:16 +0900

    Switch vim-ripgrep-git to lilac.yaml

--- vim-ripgrep-git/lilac.py
+++ vim-ripgrep-git/lilac.py
@@ -1,3 +1,4 @@
+# Trimmed lilac.py
 #!/usr/bin/env python3
 #
 # This file is the most simple lilac.py file,
@@ -11,8 +12,8 @@ def post_build():
     git_commit()
     update_aur_repo()
 
-build_prefix = 'extra-x86_64'
-pre_build = vcs_update
+#build_prefix = 'extra-x86_64'
+#pre_build = vcs_update
 
-if __name__ == '__main__':
-    single_main(build_prefix)
+#if __name__ == '__main__':
+#    single_main(build_prefix)
