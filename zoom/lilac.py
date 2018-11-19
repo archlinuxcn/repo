7c3fa90508cb146162371b434ff3b8d853c03a9b
Xuanwo <xuanwo@archlinuxcn.org>
2018-11-19 16:47:49 +0800

    Switch zoom to lilac.yaml

--- zoom/lilac.py
+++ zoom/lilac.py
@@ -1,3 +1,4 @@
+# Trimmed lilac.py
 #!/usr/bin/env python3
 #
 # This file is the most simple lilac.py file,
@@ -6,11 +7,11 @@
 
 from lilaclib import *
 
-build_prefix = 'extra-x86_64'
+#build_prefix = 'extra-x86_64'
 update_on = [{'aur':'zoom'}]
 
-pre_build = aur_pre_build
-post_build = aur_post_build
+#pre_build = aur_pre_build
+#post_build = aur_post_build
 
-if __name__ == '__main__':
-    single_main()
+#if __name__ == '__main__':
+#    single_main()
