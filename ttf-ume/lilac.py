88a7d29109a2e55250ee9e74a82c4807b1a294db
lilydjwg <lilydjwg@gmail.com>
2018-11-19 14:01:38 +0800

    Switch ttf-ume to lilac.yaml

--- ttf-ume/lilac.py
+++ ttf-ume/lilac.py
@@ -1,14 +1,15 @@
+# Trimmed lilac.py
 #!/usr/bin/env python3
 
 from lilaclib import *
 
-build_prefix = 'extra-x86_64'
-post_build = aur_post_build
+#build_prefix = 'extra-x86_64'
+#post_build = aur_post_build
 
 def pre_build():
   aur_pre_build()
 
-if __name__ == '__main__':
-  single_main()
+#if __name__ == '__main__':
+#  single_main()
 
 # vim:set ts=2 sw=2 et:
