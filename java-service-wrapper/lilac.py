003b253596091f04613fe02c614ce947af6bed8d
KenOokamiHoro <horo@yoitsu.moe>
2018-11-21 19:44:05 +0800

    add package: parsoid parsoid-git

--- java-service-wrapper/lilac.py
+++ java-service-wrapper/lilac.py
@@ -4,14 +4,21 @@
 # and it suits for most packages in AUR.
 #
 
-'''
+
 from lilaclib import *
 
-build_prefix = 'extra-x86_64'
-pre_build = aur_pre_build
-post_build = aur_post_build
+#build_prefix = 'extra-x86_64'
+#pre_build = aur_pre_build
+#post_build = aur_post_build
+
+def pre_build():
+    aur_pre_build()
+    for line in edit_file('PKGBUILD'):
+        if "java-environment>=" in line:
+            line = line.replace("java-environment>=7","jdk8-openjdk")
+            print(line)
+
 
 if __name__ == '__main__':
   single_main()
-'''
 
