d9914fdef1005dca6d110ed219188fff14971119
lilydjwg <lilydjwg@gmail.com>
2018-11-21 16:10:40 +0800

    Switch python2-carbon to lilac.yaml

--- python2-carbon/lilac.py
+++ python2-carbon/lilac.py
@@ -1,17 +1,7 @@
-#!/usr/bin/env python3
-
 from lilaclib import *
-
-build_prefix = 'extra-x86_64'
-
-depends = ['python2-whisper']
 
 def pre_build():
   update_pkgver_and_pkgrel(_G.newver)
 
 def post_build():
-  git_add_files('PKGBUILD')
-  git_commit()
-
-if __name__ == '__main__':
-  single_main()
+  git_pkgbuild_commit()
