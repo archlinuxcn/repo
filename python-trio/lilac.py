bb0a38e79290f0334d8ebe2715520f2ccbf96ca6
lilydjwg <lilydjwg@gmail.com>
2018-11-21 16:11:48 +0800

    Switch python-trio to lilac.yaml

--- python-trio/lilac.py
+++ python-trio/lilac.py
@@ -1,10 +1,4 @@
-#!/usr/bin/env python3
-
 from lilaclib import *
-
-build_prefix = 'extra-x86_64'
-
-depends = ['python-outcome', 'python-sniffio']
 
 def pre_build():
   pypi_pre_build(
@@ -13,9 +7,5 @@ def pre_build():
     depends_setuptools=False)
 
 def post_build():
-  git_add_files('PKGBUILD')
-  git_commit()
+  git_pkgbuild_commit()
   update_aur_repo()
-
-if __name__ == '__main__':
-  single_main()
