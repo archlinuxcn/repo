5d16ed77f9bee5cacb4ae5b06f4232a44190b02d
lilydjwg <lilydjwg@gmail.com>
2018-11-21 16:09:47 +0800

    Switch zfs-dkms to lilac.yaml

--- zfs-dkms/lilac.py
+++ zfs-dkms/lilac.py
@@ -1,23 +1,8 @@
-#!/usr/bin/env python3
-
-from types import SimpleNamespace
-
 from lilaclib import *
-
-build_prefix = 'extra-x86_64'
-
-depends = [
-  ('spl-dkms', 'spl-utils'),
-  ('spl-dkms', 'spl-dkms'),
-]
 
 def pre_build():
   _, newver = _G.newver.split("-")
   update_pkgver_and_pkgrel(newver)
 
 def post_build():
-  git_add_files('PKGBUILD')
-  git_commit()
-
-if __name__ == '__main__':
-  single_main()
+  git_pkgbuild_commit()
