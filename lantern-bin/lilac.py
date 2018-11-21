d9d6a8141a8cb5011b0d0a76489ecbf79d2676b9
lilydjwg <lilydjwg@gmail.com>
2018-11-21 16:08:26 +0800

    Switch lantern-bin to lilac.yaml

--- lantern-bin/lilac.py
+++ lantern-bin/lilac.py
@@ -1,23 +1,8 @@
-#!/usr/bin/env python3
-
 from lilaclib import *
-
-build_prefix = 'extra-x86_64'
 
 def pre_build():
   version = _G.newver.replace('-', '')
-
-  for line in edit_file('PKGBUILD'):
-    if line.startswith('pkgver='):
-      line = f'pkgver={version}'
-
-    print(line)
-
-  run_cmd(['updpkgsums'])
+  update_pkgver_and_pkgrel(version)
 
 def post_build():
-  git_add_files('PKGBUILD')
-  git_commit()
-
-if __name__ == '__main__':
-  single_main()
+  git_pkgbuild_commit()
