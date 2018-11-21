90df522f5a797e6c0384ef55ea5eba121fdc95c7
a-wing <1@233.email>
2018-11-21 21:28:53 +0800

    Switch websocketd to lilac.yaml

--- websocketd/lilac.py
+++ websocketd/lilac.py
@@ -1,19 +1,6 @@
-#!/usr/bin/env python3
-#
-# Auto update if have upstream new ver. but pkgver drop 'v'
-# eg: pkgver v0.1 => 0.1
-#
 
 from lilaclib import *
-
-build_prefix = 'extra-x86_64'
 
 def pre_build():
   update_pkgver_and_pkgrel(_G.newver.lstrip('v'))
 
-def post_build():
-  git_add_files('PKGBUILD')
-  git_commit()
-
-if __name__ == '__main__':
-  single_main()
