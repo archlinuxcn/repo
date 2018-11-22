bd704121bad19cc50ec207d50e133f8024b62609
lilydjwg <lilydjwg@gmail.com>
2018-11-22 20:14:06 +0800

    switch julia-git to lilac.yaml so that lilac won't think I'm the maintainer

--- julia-git/lilac.py
+++ julia-git/lilac.py
@@ -1,9 +1,5 @@
 from lilaclib import *
 
-build_prefix = 'archlinuxcn-x86_64'
-depends = ['libunwind-git', 'openspecfun-git', 'openblas-lapack-git',
-           'libutf8proc-git', 'openlibm-git']
-
 pre_build = vcs_update
 
 def post_build():
