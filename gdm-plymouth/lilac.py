fe08b19d004b4ea2721f1e978542ec83d09b5a05
Shengyu Zhang <la@archlinuxcn.org>
2018-11-22 12:51:45 +0800

    gdm-plymouth: should not in group gnome
    ref: #905

--- gdm-plymouth/lilac.py
+++ gdm-plymouth/lilac.py
@@ -4,11 +4,18 @@
 # and it suits for most packages in AUR.
 #
 
+import re
 from lilaclib import *
 
 build_prefix = 'archlinuxcn-x86_64'
-pre_build = aur_pre_build
 post_build = aur_post_build
+
+def pre_build():
+  aur_pre_build()
+  for line in edit_file('PKGBUILD'):
+    if re.match("\s*groups=.*", line):
+      continue
+    print(line)
 
 if __name__ == '__main__':
   single_main()
