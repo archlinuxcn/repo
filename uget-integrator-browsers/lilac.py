2e177320fcd25f42630fadc8a155093062fea938
Sasasu <su@sasasu.me>
2018-11-23 09:54:47 +0800

    Switch sasasu's package to lilac.yaml

--- uget-integrator-browsers/lilac.py
+++ uget-integrator-browsers/lilac.py
@@ -1,8 +1,6 @@
 #!/usr/bin/env python3
 from lilaclib import *
 
-build_prefix = 'archlinuxcn-x86_64'
-
 def _get_new_version():
   new_verion = s.get("https://api.github.com/repos/ugetdm/uget-integrator/tags").json()[0]
   return new_verion['name'][1:]
@@ -19,6 +17,3 @@ def post_build():
   git_add_files("PKGBUILD")
   git_commit()
   update_aur_repo()
-
-if __name__ == '__main__':
-  single_main()
