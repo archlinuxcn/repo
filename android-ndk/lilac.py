be1fac5cbf79eb9d9e7848621d32d4f0f26fbada
Chih-Hsuan Yen <yan12125@gmail.com>
2018-11-23 10:37:04 +0800

    Switch android-ndk to lilac.yaml

--- android-ndk/lilac.py
+++ android-ndk/lilac.py
@@ -1,20 +1,12 @@
-#!/usr/bin/env python3
+# Trimmed lilac.py
 
 from lilaclib import *
-
-build_prefix = 'extra-x86_64'
 
 
 def pre_build():
     update_pkgver_and_pkgrel(_G.newver)
-    run_cmd(['updpkgsums'])
 
 
 def post_build():
-    git_add_files('PKGBUILD')
-    git_commit()
+    git_pkgbuild_commit()
     update_aur_repo()
-
-
-if __name__ == '__main__':
-    single_main(build_prefix)
