5b5a7b0be97643615c1f217ce67be1914ec5c2ae
bennyyip <yebenmy@protonmail.com>
2018-11-23 14:34:37 +0800

    Switch cppman-git to lilac.yaml

--- cppman-git/lilac.py
+++ cppman-git/lilac.py
@@ -1,9 +1,10 @@
+# Trimmed lilac.py
 #!/usr/bin/env python3
 
 from lilaclib import *
 
-build_prefix = 'extra-x86_64'
-pre_build = vcs_update
+#build_prefix = 'extra-x86_64'
+#pre_build = vcs_update
 
 
 def post_build():
@@ -11,5 +12,5 @@ def post_build():
     git_commit()
 
 
-if __name__ == '__main__':
-    single_main()
+#if __name__ == '__main__':
+#    single_main()
