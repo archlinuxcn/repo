f38d57bebcd80e9e4f7eeaf6455c6fff1017c723
bennyyip <yebenmy@protonmail.com>
2018-11-23 14:34:10 +0800

    cppman-git: track github

--- cppman-git/lilac.py
+++ cppman-git/lilac.py
@@ -1,14 +1,15 @@
 #!/usr/bin/env python3
-#
-# This file is the most simple lilac.py file,
-# and it suits for most packages in AUR.
-#
 
 from lilaclib import *
 
 build_prefix = 'extra-x86_64'
-pre_build = aur_pre_build
-post_build = aur_post_build
+pre_build = vcs_update
+
+
+def post_build():
+    git_add_files('PKGBUILD')
+    git_commit()
+
 
 if __name__ == '__main__':
-  single_main()
+    single_main()
