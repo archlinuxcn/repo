9e163d05f528b7636aa678f6237748f9812bd0f4
a-wing <1@233.email>
2018-11-23 11:22:02 +0800

    Switch annie to lilac.yaml

--- annie/lilac.py
+++ annie/lilac.py
@@ -1,19 +1,7 @@
 #!/usr/bin/env python3
-#
-# This file is the most simple lilac.py file,
-# and it suits for most packages in AUR.
-#
 
 from lilaclib import *
-
-build_prefix = 'extra-x86_64'
 
 def pre_build():
   update_pkgver_and_pkgrel(_G.newver.lstrip('v'))
 
-def post_build():
-  git_add_files('PKGBUILD')
-  git_commit()
-
-if __name__ == '__main__':
-  single_main()
