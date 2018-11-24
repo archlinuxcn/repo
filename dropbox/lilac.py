6b927e90defc0d52c7b932f5c89926ddade7af2d
Chih-Hsuan Yen <yan12125@gmail.com>
2018-11-24 08:37:17 +0800

    Switch dropbox to lilac.yaml

--- dropbox/lilac.py
+++ dropbox/lilac.py
@@ -1,18 +1,11 @@
+# Trimmed lilac.py
 from lilaclib import *
-
-build_prefix = 'extra-x86_64'
 
 
 def pre_build():
     update_pkgver_and_pkgrel(_G.newver)
-    run_cmd(['updpkgsums'])
 
 
 def post_build():
-    git_add_files('PKGBUILD')
-    git_commit()
+    git_pkgbuild_commit()
     update_aur_repo()
-
-
-if __name__ == '__main__':
-    single_main()
