2e8857f7e05a53c9289c718ec4af4997949c1aa6
Chih-Hsuan Yen <yan12125@gmail.com>
2018-11-24 08:45:22 +0800

    Switch lxqt-build-tools-git to lilac.yaml

--- lxqt-build-tools-git/lilac.py
+++ lxqt-build-tools-git/lilac.py
@@ -1,12 +1,1 @@
-from lilaclib import *
-
-build_prefix = 'extra-x86_64'
-
-def post_build():
-    git_add_files('PKGBUILD')
-    git_commit()
-
-pre_build = vcs_update
-
-if __name__ == '__main__':
-    single_main(build_prefix)
+# Trimmed lilac.py
