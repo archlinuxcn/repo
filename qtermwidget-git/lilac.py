53123ff1384bb9450391b6c0d22afb30e5ccacfb
Chih-Hsuan Yen <yan12125@gmail.com>
2018-11-24 08:45:31 +0800

    Switch qtermwidget-git to lilac.yaml

--- qtermwidget-git/lilac.py
+++ qtermwidget-git/lilac.py
@@ -1,14 +1,1 @@
-from lilaclib import *
-
-build_prefix = 'extra-x86_64'
-
-depends = ['lxqt-build-tools-git']
-
-def post_build():
-    git_add_files('PKGBUILD')
-    git_commit()
-
-pre_build = vcs_update
-
-if __name__ == '__main__':
-    single_main(build_prefix)
+# Trimmed lilac.py
