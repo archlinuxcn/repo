83548e7f3efc48c058ef1bd5d7f3fd139fc3a1e4
Chih-Hsuan Yen <yan12125@gmail.com>
2018-11-24 08:37:15 +0800

    Switch darling-dmg-git to lilac.yaml

--- darling-dmg-git/lilac.py
+++ darling-dmg-git/lilac.py
@@ -1,18 +1,1 @@
-#!/usr/bin/env python3
-#
-# This file is the most simple lilac.py file,
-# and it suits for most packages in AUR.
-#
-
-from lilaclib import *
-
-build_prefix = 'extra-x86_64'
-
-def post_build():
-    git_add_files('PKGBUILD')
-    git_commit()
-
-pre_build = vcs_update
-
-if __name__ == '__main__':
-    single_main(build_prefix)
+# Trimmed lilac.py
