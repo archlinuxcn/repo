d04587f36247961a0db9e889e915675e8ff3f03d
a-wing <1@233.email>
2018-11-28 14:11:11 +0800

    Update ruby-pry

--- ruby-pry/lilac.py
+++ ruby-pry/lilac.py
@@ -1,21 +1,6 @@
-# Trimmed lilac.py
-#!/usr/bin/env python3
-#
-# This file is the most simple lilac.py file,
-# and it suits for most packages in AUR.
-#
 
 from lilaclib import *
 
-depends=['ruby-coderay', 'ruby-method_source', 'ruby-slop-3']
+def pre_build():
+  update_pkgver_and_pkgrel(_G.newver)
 
-#update_on = [{
-#    'aur': 'ruby-pry'
-#}]
-
-#build_prefix = 'archlinuxcn-x86_64'
-#pre_build = aur_pre_build
-#post_build = aur_post_build
-
-#if __name__ == '__main__':
-#  single_main()
