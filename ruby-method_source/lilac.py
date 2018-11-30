3778a2bdd8a04484926196e8d11327b3c29837ad
a-wing <1@233.email>
2018-11-30 12:43:16 +0800

    Update ruby-method_source && Enable lilac update AUR

--- ruby-method_source/lilac.py
+++ ruby-method_source/lilac.py
@@ -1,15 +1,11 @@
-# Trimmed lilac.py
-#!/usr/bin/env python3
-#
-# This file is the most simple lilac.py file,
-# and it suits for most packages in AUR.
-#
-
 from lilaclib import *
 
-#build_prefix = 'extra-x86_64'
-#pre_build = aur_pre_build
-#post_build = aur_post_build
+def pre_build():
+  update_pkgver_and_pkgrel(_G.newver)
 
-#if __name__ == '__main__':
-#  single_main()
+def post_build():
+  #git_add_files('PKGBUILD')
+  #git_commit()
+  git_pkgbuild_commit
+  update_aur_repo()
+
