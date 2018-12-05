4b8bce35772500d64858018b8e0d9104b0bca979
lilac <lilac@build.archlinuxcn.org>
2018-12-05 09:40:40 +0800

    auto update for package google-chrome

---
+++ google-chrome/update_version.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+set -euxo pipefail
+
+# Get channel
+CHANNEL=$(awk -F '=' '/^_channel/{ print $2 }' PKGBUILD)
+PKG="google-chrome-${CHANNEL}"
+
+# Get latest version
+VER=$(curl -sL "https://dl.google.com/linux/chrome/rpm/stable/x86_64/repodata/other.xml.gz" | gzip -df |
+	tr ' ' '\n' | grep -e name= -e ver= | cut -d '"' -f2 | sed 'N;s/\n/ /' |
+	grep "${PKG}" | cut -d ' ' -f 2)
+
+# Insert latest version into PKGBUILD and update hashes
+sed -i "s/^pkgver=.*/pkgver=${VER}/" PKGBUILD
+updpkgsums
+
+# Check whether this changed anything
+if (git diff --exit-code PKGBUILD); then
+	echo "Package ${PKG} has most recent version ${VER}"
+	exit 0
+fi
+
+# Update .SRCINFO
+makepkg --printsrcinfo >.SRCINFO
+
+# Commit changes
+git add PKGBUILD .SRCINFO
+git commit -m "${PKG} v${VER}"
