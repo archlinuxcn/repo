52d26f880a7149961ddd30b259f0c6e516e81503
Jianqiu Zhang <zhangjianqiu13@gmail.com>
2018-12-09 22:04:35 +0800

    Switch bilidan-git to lilac.yaml

--- bilidan-git/lilac.py
+++ bilidan-git/lilac.py
@@ -1,11 +1,12 @@
+# Trimmed lilac.py
 #!/usr/bin/env python3
 
 from lilaclib import *
 
-build_prefix = 'archlinuxcn-x86_64'
+#build_prefix = 'archlinuxcn-x86_64'
 
-pre_build = aur_pre_build
-post_build = aur_post_build
+#pre_build = aur_pre_build
+#post_build = aur_post_build
 
-if __name__ == '__main__':
-  single_main()
+#if __name__ == '__main__':
+#  single_main()
