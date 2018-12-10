925cf4f1d0d7436658133e030394c6f473c3b5d1
Jianqiu Zhang <zhangjianqiu13@gmail.com>
2018-12-10 15:33:40 +0800

    Fix simgear
    Signed-off-by: Jianqiu Zhang <zhangjianqiu13@gmail.com>

--- simgear/lilac.py
+++ simgear/lilac.py
@@ -3,10 +3,18 @@ import os
 
 depends = []
 
+build_prefix = 'extra-x86_64'
+
 def pre_build():
     for line in edit_file('PKGBUILD'): 
         if line.startswith('depends='):
-            line = "depends=('glut')"
+            print("depends=('glut')")
+            continue
         if line.startswith('makedepends='):
-            line = "makedepends=('boost' 'cmake' 'mesa' 'openscenegraph34' 'plib' 'freealut' 'glu')"
+            print("makedepends=('boost' 'cmake' 'mesa' 'openscenegraph34' 'plib' 'freealut' 'glu')")
+            continue
+        else:
+            print(line) 
 
+# if __name__ == '__main__':
+#     single_main('extra-x86_64')
