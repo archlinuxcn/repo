c19d4bac39518911112f68fa85f358c0e61aa1ed
Jiachen YANG <farseerfc@gmail.com>
2018-12-10 18:28:20 +0900

    add libiconv

---
+++ libiconv/lilac.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+#
+# This is a complex version of lilac.py for building
+# a package from AUR.
+#
+# You can do something before/after building a package,
+# including modify the 'pkgver' and 'md5sum' in PKBUILD.
+#
+# This is especially useful when a AUR package is
+# out-of-date and you want to build a new one, or you
+# want to build a package directly from sourceforge but
+# using PKGBUILD from AUR.
+#
+# See also:
+# [1] ruby-sass/lilac.py
+# [2] aufs3-util-lily-git/lilac.py
+# [3] octave-general/lilac.py
+#
+
+from lilaclib import *
+
