892a334ad8ee65232b0f4ebd0030a294551b5ea4
Jianqiu Zhang <zhangjianqiu13@gmail.com>
2018-12-12 09:00:30 +0800

    Fix shutter dep
    Signed-off-by: Jianqiu Zhang <zhangjianqiu13@gmail.com>

--- shutter/lilac.py
+++ shutter/lilac.py
@@ -7,11 +7,11 @@
 
 from lilaclib import *
 
-#build_prefix = 'extra-x86_64'
+build_prefix = 'extra-x86_64'
 #pre_build = aur_pre_build
 #post_build = aur_post_build
 
-depends = ["orbit2", ("libgnome", "libgnome-data"), "gnome-vfs-nosmb", "libbonobo", "libgnome", "libbonoboui", "gnome-perl", "perl-gnome2-wnck", "perl-gtk2-imageview", "perl-gtk2-unique"]
+depends = ["orbit2", ("libgnome", "libgnome-data"), "gnome-vfs-nosmb", "libbonobo", "libgnome", "libbonoboui", "gnome-perl", "perl-gnome2-wnck", "perl-gtk2-imageview", "perl-gtk2-unique", "perl-json-maybexs"]
 
-#if __name__ == '__main__':
-#      single_main(build_prefix)
+if __name__ == '__main__':
+      single_main(build_prefix)
