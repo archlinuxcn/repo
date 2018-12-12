5e01bd22b3d2d4a94ae02eae6051f3ff458c1ddd
Jiachen YANG <farseerfc@gmail.com>
2018-12-13 00:13:34 +0900

    zfs-dkms-git fixing depends patching

--- zfs-dkms-git/lilac.py
+++ zfs-dkms-git/lilac.py
@@ -22,7 +22,7 @@ from lilaclib import *
 def pre_build():
     aur_pre_build()
     for line in edit_file('PKGBUILD'):
-        if line.strip().startwith('depends='):
+        if line.strip().startswith('depends='):
             print('depends=("zfs-utils-git" "lsb-release" "dkms")')
         else:
             print(line)
