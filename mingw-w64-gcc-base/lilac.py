bb8779d445cc905a1d15468b4ce22ad7a86c4567
Jianqiu Zhang <zhangjianqiu13@gmail.com>
2018-12-14 15:50:56 +0800

    mingw-w64-* remove depends from lilac.py
    Signed-off-by: Jianqiu Zhang <zhangjianqiu13@gmail.com>

--- mingw-w64-gcc-base/lilac.py
+++ mingw-w64-gcc-base/lilac.py
@@ -1,7 +1,7 @@
 from lilaclib import *
 
 build_prefix = "extra-x86_64"
-depends = ['mingw-w64-binutils', 'mingw-w64-headers', ('mingw-w64-winpthreads', 'mingw-w64-headers-bootstrap')]
+# depends = ['mingw-w64-binutils', 'mingw-w64-headers', ('mingw-w64-winpthreads', 'mingw-w64-headers-bootstrap')]
 
 if __name__ == "__main__":
     single_main('extra-x86_64')
