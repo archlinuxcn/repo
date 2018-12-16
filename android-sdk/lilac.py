af3da9ab433498dd58448cb3248b4c896fbee952
lilydjwg <lilydjwg@gmail.com>
2018-12-16 16:15:31 +0800

    Switch android-sdk to lilac.yaml

--- android-sdk/lilac.py
+++ android-sdk/lilac.py
@@ -1,14 +1,15 @@
+# Trimmed lilac.py
 #!/usr/bin/env python3
 
 from lilaclib import *
 
-build_prefix = 'multilib'
-pre_build = aur_pre_build
+#build_prefix = 'multilib'
+#pre_build = aur_pre_build
 
 def post_build():
     run_cmd(['git','add','-f','license.html'])
     git_add_files(['PKGBUILD', 'android-sdk.conf', 'android-sdk.csh', 'android-sdk.install', 'android-sdk.sh'])
     git_commit()
 
-if __name__ == '__main__':
-  single_main(build_prefix)
+#if __name__ == '__main__':
+#  single_main(build_prefix)
