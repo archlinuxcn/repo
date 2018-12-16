fd843f8b63cd890b374e77a903e809ac787a7c02
Sakuraba Amane <tobiichiamane@archlinuxcn.org>
2018-12-16 12:33:20 +0800

    Make vscodium update from git

--- vscodium/lilac.py
+++ vscodium/lilac.py
@@ -8,8 +8,13 @@
 from lilaclib import *
 
 #build_prefix = 'extra-x86_64'
-#pre_build = aur_pre_build
-#post_build = aur_post_build
+
+def pre_build():
+  update_pkgver_and_pkgrel(_G.newver)
+
+def post_build():
+  git_add_files('PKGBUILD')
+  git_commit()
 
 #if __name__ == '__main__':
 #  single_main()
