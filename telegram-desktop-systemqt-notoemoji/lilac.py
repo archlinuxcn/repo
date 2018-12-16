b8f5b63b90260620ec76f1573db96ed339851db8
OriginCode <origincoder@yahoo.com>
2018-12-16 12:11:30 +0800

    Switch telegram-desktop-systemqt-notoemoji to lilac.yaml

--- telegram-desktop-systemqt-notoemoji/lilac.py
+++ telegram-desktop-systemqt-notoemoji/lilac.py
@@ -1,3 +1,4 @@
+# Trimmed lilac.py
 #!/usr/bin/env python3
 #
 # This file is the most simple lilac.py file,
@@ -6,9 +7,9 @@
 
 from lilaclib import *
 
-build_prefix = 'extra-x86_64'
-pre_build = aur_pre_build
-post_build = aur_post_build
+#build_prefix = 'extra-x86_64'
+#pre_build = aur_pre_build
+#post_build = aur_post_build
 
-if __name__ == '__main__':
-  single_main()
+#if __name__ == '__main__':
+#  single_main()
