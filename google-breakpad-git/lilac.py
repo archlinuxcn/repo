eeceafec4527570fd9ebb917b44ad57b0a73054d
lilydjwg <lilydjwg@gmail.com>
2018-12-16 16:12:16 +0800

    Switch google-breakpad-git to lilac.yaml

--- google-breakpad-git/lilac.py
+++ google-breakpad-git/lilac.py
@@ -1,3 +1,4 @@
+# Trimmed lilac.py
 #!/usr/bin/env python3
 #
 # This file is the most simple lilac.py file,
@@ -8,8 +9,8 @@ from lilaclib import *
 
 depends = ['depot-tools-git']
 
-build_prefix = 'extra-x86_64'
-post_build = aur_post_build
+#build_prefix = 'extra-x86_64'
+#post_build = aur_post_build
 
 def pre_build():
     aur_pre_build("google-breakpad-git", do_vcs_update=False)
@@ -20,5 +21,5 @@ def pre_build():
         else:
             print(line.replace("mkdir ", "mkdir -p "))
 
-if __name__ == '__main__':
-    single_main(build_prefix)
+#if __name__ == '__main__':
+#    single_main(build_prefix)
