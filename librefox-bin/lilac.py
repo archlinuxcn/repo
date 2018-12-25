01b2edd1050c7ba90929b4b9eb012fde0611a967
KenOokamiHoro <horo@yoitsu.moe>
2018-12-25 15:03:39 +0800

    addpkg: librefox

---
+++ librefox-bin/lilac.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python3
+
+from lilaclib import *
+
+build_prefix = 'extra-x86_64'
+
+
+def pre_build():
+    # prepare PKGBUILD etc.
+    pass
+
+
+def post_build():
+    # do something after successful build
+    url, ver, firefox_ver = s.head(
+        "https://github.com/intika/Librefox/releases/latest").headers['Location'].split("-v")
+    for line in edit_file('PKGBUILD'):
+        if 'pkgver=' in line:
+            line = 'pkgver='+ver
+        if '_firefox_ver=' in line:
+            line = '_firefox_ver'+firefox_ver
+        print(line)
+
+    run_cmd(['updpkgsums'])
+    git_add_files('PKGBUILD')
+    git_commit()
+
+
+# def post_build_always():
+#   clean up, regardless of build result
+
+if __name__ == '__main__':
+    single_main()
