e62e0e1db65be1478f99e81a8ae040fb09391e9d
axionl <i@axionl.me>
2018-12-27 23:26:02 +0900

    [Update] netease-musicbox-git: remove the useless patch

--- netease-musicbox-git/lilac.py
+++ netease-musicbox-git/lilac.py
@@ -1,18 +1,9 @@
 # Trimmed lilac.py
 #!/usr/bin/env python3
-#
-# This file is the most simple lilac.py file,
-# and it suits for most packages in AUR.
-#
 
 from lilaclib import *
 
 def post_build():
     git_add_files('PKGBUILD')
     git_commit()
-
-#build_prefix = 'archlinuxcn-x86_64'
-#pre_build = vcs_update
-
-#if __name__ == '__main__':
-#    single_main(build_prefix)
+    update_aur_repo()
