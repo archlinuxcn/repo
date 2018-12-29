de2e206e3a7b6161134066be0bedcc9926039023
Bruce Zhang <zttt183525594@gmail.com>
2018-12-29 21:18:27 +0800

    python-fuocore: 2.3a0
    Signed-off-by: Bruce Zhang <zttt183525594@gmail.com>

--- python-fuocore/lilac.py
+++ python-fuocore/lilac.py
@@ -3,7 +3,7 @@
 from lilaclib import *
 
 def pre_build():
-  pypi_pre_build(depends=['python-pycrypto', 'python-fuzzywuzzy'])
+  pypi_pre_build(depends=['python', 'mpv', 'python-beautifulsoup4', 'python-marshmallow', 'python-pycrypto', 'python-requests', 'python-mutagen', 'python-fuzzywuzzy', 'python-setuptools'], license='MIT')
 
 def post_build():
   pypi_post_build()
