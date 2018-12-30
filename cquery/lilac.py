8619a0f7a5be18f6c27e62c52f0c2a1526e43535
Chih-Hsuan Yen <yen@chyen.cc>
2018-12-30 18:18:16 +0800

    Switch cquery to lilac.yaml

--- cquery/lilac.py
+++ cquery/lilac.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python3
+# Trimmed lilac.py
 #
 # This file is the most simple lilac.py file,
 # and it suits for most packages in AUR.
@@ -6,7 +6,6 @@
 
 from lilaclib import *
 
-build_prefix = 'extra-x86_64'
 
 def pre_build():
   update_pkgver_and_pkgrel(_G.newver)
@@ -16,5 +15,3 @@ def post_build():
   git_commit()
   update_aur_repo()
 
-if __name__ == '__main__':
-  single_main()
