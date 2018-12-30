d0a333660d794cadc28c1331b19898d66fc88839
Chih-Hsuan Yen <yen@chyen.cc>
2018-12-30 18:16:56 +0800

    Switch gdm-plymouth to lilac.yaml

--- gdm-plymouth/lilac.py
+++ gdm-plymouth/lilac.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python3
+# Trimmed lilac.py
 #
 # This file is the most simple lilac.py file,
 # and it suits for most packages in AUR.
@@ -7,8 +7,6 @@
 import re
 from lilaclib import *
 
-build_prefix = 'archlinuxcn-x86_64'
-post_build = aur_post_build
 
 def pre_build():
   aur_pre_build()
@@ -17,5 +15,3 @@ def pre_build():
       continue
     print(line)
 
-if __name__ == '__main__':
-  single_main()
