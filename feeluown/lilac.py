183158e95e1d9a2e0724294d3d2d8d4b70b0109c
Bruce Zhang <zttt183525594@gmail.com>
2019-01-02 17:20:17 +0800

    feeluown: rebuild
    Signed-off-by: Bruce Zhang <zttt183525594@gmail.com>

--- feeluown/lilac.py
+++ feeluown/lilac.py
@@ -3,7 +3,29 @@
 from lilaclib import *
 
 def pre_build():
-  pypi_pre_build(depends=['python-quamash', 'python-fuocore', 'python-pyqt5'], license='GPL3')
+  oldver, oldrel = _G.oldver.split('-', 1)
+  ver, rel = _G.newver.split('-', 1)
+  need_update_pkgrel = False
+
+  for line in edit_file('PKGBUILD'):
+    if line.startswith('pkgver='):
+      print('pkgver=%s' % ver)
+      continue
+    elif line.startswith('pkgrel='):
+      if oldver == ver:
+        need_update_pkgrel = True
+        print(line)
+      else:
+        print('pkgrel=1')
+      continue
+
+    print(line)
+
+  if need_update_pkgrel:
+    update_pkgrel()
+
+  run_cmd(['updpkgsums'])
 
 def post_build():
-  pypi_post_build()
+  git_add_files('PKGBUILD')
+  git_commit()
