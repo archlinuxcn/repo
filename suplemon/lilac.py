25611eb34e7fea5e83c720c52696e9e1c829ba96
Jiachen YANG <farseerfc@gmail.com>
2019-01-04 03:04:25 +0900

    suplemon autoupdate on aur

--- suplemon/lilac.py
+++ suplemon/lilac.py
@@ -1,15 +1,11 @@
-# Trimmed lilac.py
 #!/usr/bin/env python3
-#
-# This file is the most simple lilac.py file,
-# and it suits for most packages in AUR.
-#
 
 from lilaclib import *
 
-#build_prefix = 'extra-x86_64'
-#post_build = aur_post_build
-#pre_build = aur_pre_build
+def pre_build():
+    update_pkgver_and_pkgrel(_G.newver)
 
-#if __name__ == '__main__':
-#  single_main()
+def post_build():
+    git_add_files('PKGBUILD')
+    git_commit()
+    update_aur_repo()
