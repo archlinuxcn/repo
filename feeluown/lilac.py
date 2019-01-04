7f568ccb5f8ba7799d8acf2cd09e6d2e17442de6
Bruce Zhang <zttt183525594@gmail.com>
2019-01-04 10:06:27 +0800

    feeluown: lilac.py
    Signed-off-by: Bruce Zhang <zttt183525594@gmail.com>

--- feeluown/lilac.py
+++ feeluown/lilac.py
@@ -3,29 +3,11 @@
 from lilaclib import *
 
 def pre_build():
-  oldver, oldrel = _G.oldver.split('-', 1)
-  ver, rel = _G.newver.split('-', 1)
-  need_update_pkgrel = False
-
-  for line in edit_file('PKGBUILD'):
-    if line.startswith('pkgver='):
-      print('pkgver=%s' % ver)
-      continue
-    elif line.startswith('pkgrel='):
-      if oldver == ver:
-        need_update_pkgrel = True
-        print(line)
-      else:
-        print('pkgrel=1')
-      continue
-
-    print(line)
-
-  if need_update_pkgrel:
-    update_pkgrel()
-
-  run_cmd(['updpkgsums'])
+  update_pkgver_and_pkgrel(_G.newver, updpkgsums=True)
 
 def post_build():
   git_add_files('PKGBUILD')
   git_commit()
+
+if __name__ == '__main__':
+  single_main()
