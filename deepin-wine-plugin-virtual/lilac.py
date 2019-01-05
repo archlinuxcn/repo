477f3b5733c02438e8704a147cfaa5850937b068
Chih-Hsuan Yen <yen@chyen.cc>
2019-01-05 22:34:59 +0800

    Switch deepin-wine-plugin-virtual to lilac.yaml

--- deepin-wine-plugin-virtual/lilac.py
+++ deepin-wine-plugin-virtual/lilac.py
@@ -1,9 +1,4 @@
-#!/usr/bin/env python3
+# Trimmed lilac.py
 from lilaclib import *
 
-build_prefix = 'multilib'
-pre_build = aur_pre_build
-post_build = aur_post_build
 
-if __name__ == '__main__':
-  single_main(build_prefix)
