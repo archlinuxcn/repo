e47920e9a9e655330af6bee570de1ff15802e02b
Chih-Hsuan Yen <yen@chyen.cc>
2019-01-05 22:43:01 +0800

    Switch jdk8 to lilac.yaml

--- jdk8/lilac.py
+++ jdk8/lilac.py
@@ -1,11 +1,6 @@
-#!/usr/bin/env python3
+# Trimmed lilac.py
 
 from lilaclib import *
 
-build_prefix = 'extra-x86_64'
 
-pre_build = aur_pre_build
-post_build = aur_post_build
 
-if __name__ == '__main__':
-  single_main(build_prefix)
