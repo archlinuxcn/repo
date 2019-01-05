2a801ceeb68caebe2e57482254fa85c77bfd05c4
Chih-Hsuan Yen <yen@chyen.cc>
2019-01-05 22:47:12 +0800

    Switch gnome-vfs-nosmb to lilac.yaml

--- gnome-vfs-nosmb/lilac.py
+++ gnome-vfs-nosmb/lilac.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python3
+# Trimmed lilac.py
 #
 # This file is the most simple lilac.py file,
 # and it suits for most packages in AUR.
@@ -6,11 +6,6 @@
 
 from lilaclib import *
 
-build_prefix = 'extra-x86_64'
-pre_build = aur_pre_build
-post_build = aur_post_build
 
 depends = ["gnome-mime-data"]
 
-if __name__ == '__main__':
-      single_main(build_prefix)
