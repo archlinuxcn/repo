a69b4c1f88f4a642702b5357067dd5485a05491e
Chih-Hsuan Yen <yen@chyen.cc>
2019-01-05 22:44:11 +0800

    Switch python-pgspecial to lilac.yaml

--- python-pgspecial/lilac.py
+++ python-pgspecial/lilac.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python3
+# Trimmed lilac.py
 #
 # This file is the most simple lilac.py file,
 # and it suits for most packages in AUR.
@@ -6,11 +6,7 @@
 
 from lilaclib import *
 
-build_prefix = 'extra-x86_64'
 def pre_build():
     aur_pre_build()
     pypi_pre_build(depends_setuptools=True)
-post_build = aur_post_build
 
-if __name__ == '__main__':
-  single_main()
