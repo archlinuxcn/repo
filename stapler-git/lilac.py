6e1069bd75945d843d8cd24078c717be09af4a79
Chih-Hsuan Yen <yen@chyen.cc>
2019-01-05 22:51:19 +0800

    Switch stapler-git to lilac.yaml

--- stapler-git/lilac.py
+++ stapler-git/lilac.py
@@ -1,10 +1,5 @@
-#!/usr/bin/env python3
+# Trimmed lilac.py
 
 from lilaclib import *
 
-build_prefix = 'archlinuxcn-x86_64'
-pre_build = aur_pre_build
-post_build = aur_post_build
 
-if __name__ == '__main__':
-  single_main(build_prefix)
