8d4dba2d3491e1a274f22189714f96b1bfa532a7
Jiachen YANG <farseerfc@gmail.com>
2019-01-07 02:50:04 +0900

    linux-clear change _subarch to 26

--- linux-clear/lilac.py
+++ linux-clear/lilac.py
@@ -1,1 +1,11 @@
 from lilaclib import *
+
+def pre_build():
+    aur_pre_build()
+    for line in edit_file('PKGBUILD'):
+        if line.strip().startswith('_subarch='):
+            print('_subarch=26')
+        else:
+            print(line)
+
+
