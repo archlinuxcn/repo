658be7f758de4527c2055f14567c565ac2d9821d
lilydjwg <lilydjwg@gmail.com>
2019-01-21 23:38:59 +0800

    python-telethon: adopt and update
    see also #933.

--- archlinuxcn/python-telethon/lilac.py
+++ archlinuxcn/python-telethon/lilac.py
@@ -1,16 +1,8 @@
-# Trimmed lilac.py
-#!/usr/bin/env python3
-
 from lilaclib import *
 
-#build_prefix = 'extra-x86_64'
-
 def pre_build():
-  pypi_pre_build(depends = ['python-pyaes', 'python-rsa'])
+  pypi_pre_build(depends = ['python-pyaes', 'python-rsa', 'python-async_generator'])
 
 def post_build():
   pypi_post_build()
   update_aur_repo()
-
-#if __name__ == '__main__':
-#  single_main()
