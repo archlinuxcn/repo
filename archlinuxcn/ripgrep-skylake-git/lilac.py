1185082439dd9ddafac46cc0ddcfe13a2293385a
lilydjwg <lilydjwg@gmail.com>
2019-01-23 19:53:33 +0800

    ripgrep-skylake-git: avx-accel feature is removed
    also move depends from lilac.py to lilac.yaml

--- archlinuxcn/ripgrep-skylake-git/lilac.py
+++ archlinuxcn/ripgrep-skylake-git/lilac.py
@@ -1,16 +1,9 @@
 from lilaclib import *
 
-depends = [
-  ('rust-nightly', 'rust-std-nightly-x86_64-unknown-linux-gnu'),
-  ('rust-nightly', 'rust-nightly'),
-  ('rust-nightly', 'cargo-nightly'),
-]
-
 pre_build = vcs_update
 
 def post_build():
-  git_add_files('PKGBUILD')
-  git_commit()
+  git_pkgbuild_commit()
 
 if __name__ == '__main__':
   single_main()
