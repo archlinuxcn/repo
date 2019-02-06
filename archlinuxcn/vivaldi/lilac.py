50d6c2283d5be11a54c68258d9243e1ae8458533
Jiachen YANG <farseerfc@gmail.com>
2019-02-07 03:51:29 +0900

    vivaldi: fixing non-LFS path

--- archlinuxcn/vivaldi/lilac.py
+++ archlinuxcn/vivaldi/lilac.py
@@ -7,3 +7,40 @@
 
 from lilaclib import *
 
+PATCH = b"""
+diff --git a/archlinuxcn/vivaldi/PKGBUILD b/archlinuxcn/vivaldi/PKGBUILD
+index 2b1aeb2df..9a9868e2a 100644
+--- a/archlinuxcn/vivaldi/PKGBUILD
++++ b/archlinuxcn/vivaldi/PKGBUILD
+@@ -4,7 +4,7 @@
+ pkgname=vivaldi
+ _rpmversion=2.3.1440.41-1
+ pkgver=2.3.1440.41
+-pkgrel=2
++pkgrel=3
+ pkgdesc='An advanced browser made with the power user in mind.'
+ url="https://vivaldi.com"
+ options=(!strip !zipman)
+@@ -48,5 +48,11 @@ package() {
+         | sed -rne 's/.*(<html lang.*>.*html>).*/\1/p' \
+         | w3m -I 'utf-8' -T 'text/html' \
+         > "$pkgdir/usr/share/licenses/$pkgname/eula.txt"
++
++    # remove anything under /bin
++    rm -rf $pkgdir/bin
++    # move anything under /share
++    mv $pkgdir/share/* $pkgdir/usr/share/
++    rmdir $pkgdir/share
+ }
+"""
+
+import subprocess
+
+def apply_patch(filename, patch):
+    patch_proc = subprocess.Popen(["patch", "-p1", filename], stdin=subprocess.PIPE)
+    patch_proc.communicate(patch)
+
+def pre_build():
+    aur_pre_build()
+    apply_patch("PKGBUILD", PATCH)
+
