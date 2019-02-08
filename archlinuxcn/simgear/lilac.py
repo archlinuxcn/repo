229980f4f287419e1ce710e94584f7f885f8a739
Jianqiu Zhang <zhangjianqiu13@gmail.com>
2019-02-08 10:00:22 +0800

    simgear, flightgear: fixup
    Signed-off-by: Jianqiu Zhang <zhangjianqiu13@gmail.com>

--- archlinuxcn/simgear/lilac.py
+++ archlinuxcn/simgear/lilac.py
@@ -6,15 +6,16 @@ depends = []
 build_prefix = 'extra-x86_64'
 
 def pre_build():
-    for line in edit_file('PKGBUILD'): 
-        if line.startswith('depends='):
-            print("depends=('glut')")
-            continue
-        if line.startswith('makedepends='):
-            print("makedepends=('boost' 'cmake' 'mesa' 'openscenegraph34' 'plib' 'freealut' 'glu')")
-            continue
-        else:
-            print(line) 
+    aur_pre_build()
+    # for line in edit_file('PKGBUILD'):
+    #     if line.startswith('depends='):
+    #         print("depends=('glut')")
+    #         continue
+    #     if line.startswith('makedepends='):
+    #         print("makedepends=('boost' 'cmake' 'mesa' 'openscenegraph34' 'plib' 'freealut' 'glu')")
+    #         continue
+    #     else:
+    #         print(line)
 
 # if __name__ == '__main__':
 #     single_main('extra-x86_64')
