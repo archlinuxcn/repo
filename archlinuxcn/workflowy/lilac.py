3e12e4f579453bc367b63f195f9d4ca55925d533
Xuanwo <xuanwo@archlinuxcn.org>
2019-02-11 13:19:31 +0800

    Remove comments for workflowy

--- archlinuxcn/workflowy/lilac.py
+++ archlinuxcn/workflowy/lilac.py
@@ -2,12 +2,6 @@
 
 from lilaclib import *
 
-# build_prefix = 'extra-x86_64'
-# update_on = [{
-#    'github': 'coredns/coredns',
-#    'use_latest_release': True
-#}]
-
 def pre_build():
     update_pkgver_and_pkgrel(_G.newver.lstrip('v'))
     run_cmd(['updpkgsums'])
@@ -15,6 +9,3 @@ def pre_build():
 def post_build():
     git_add_files('PKGBUILD')
     git_commit()
-
-#if __name__ == '__main__':
-#        single_main()
