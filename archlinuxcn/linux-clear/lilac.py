a6366a7df1913305dab83f74f1186283620d4e49
Jiachen YANG <farseerfc@gmail.com>
2019-02-14 08:45:01 +0900

    linux-clear: try fixing crypto_user module

--- archlinuxcn/linux-clear/lilac.py
+++ archlinuxcn/linux-clear/lilac.py
@@ -1,5 +1,38 @@
 from lilaclib import *
 
+PATCH="""
+diff --git a/archlinuxcn/linux-clear/PKGBUILD b/archlinuxcn/linux-clear/PKGBUILD
+index 54d613ee5..45e32e7ab 100644
+--- a/archlinuxcn/linux-clear/PKGBUILD
++++ b/archlinuxcn/linux-clear/PKGBUILD
+@@ -93,7 +93,8 @@ source=(
+ _kernelname=${pkgbase#linux}
+ : ${_kernelname:=-clear}
+ 
+-prepare() {
++
++build() {
+     cd ${_srcname}
+ 
+     ### Add upstream patches
+@@ -180,10 +181,6 @@ CONFIG_MODULE_COMPRESS_XZ=y|' ./.config
+     ### Save configuration for later reuse
+ 
+         cp -Tf ./.config "${startdir}/config-${pkgver}-${pkgrel}${_kernelname}"
+-}
+-
+-build() {
+-    cd ${_srcname}
+ 
+     make bzImage modules
+ }
+"""
+
+import subprocess
+def apply_patch(filename, patch):
+    patch_proc = subprocess.Popen(["patch", "-p1", filename], stdin=subprocess.PIPE)
+    patch_proc.communicate(patch)   
+
 def pre_build():
     aur_pre_build()
     for line in edit_file('PKGBUILD'):
@@ -7,5 +40,6 @@ def pre_build():
             print('_subarch=26')
         else:
             print(line)
-
+    apply_patch("PKGBUILD", PATCH)
+    update_pkgrel()
 
