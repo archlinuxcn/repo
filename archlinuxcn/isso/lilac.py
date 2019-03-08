673fec6afe7781ae30d7e6d205a96298273df6b2
Peter Cai <xqsx43cxy@gmail.com>
2019-03-08 09:05:52 +0800

    isso: depend on the normal python-html5lib

--- archlinuxcn/isso/lilac.py
+++ archlinuxcn/isso/lilac.py
@@ -4,7 +4,7 @@ from lilaclib import *
 
 def pre_build():
     pypi_pre_build(
-        depends = ['python-bleach', 'python-jinja', 'python-werkzeug', 'python-html5lib-9x07', 'python-misaka', 'python-itsdangerous', 'python-six', 'python-cffi', 'sqlite'],
+        depends = ['python-bleach', 'python-jinja', 'python-werkzeug', 'python-html5lib', 'python-misaka', 'python-itsdangerous', 'python-six', 'python-cffi', 'sqlite'],
         depends_setuptools = True,
         arch = ['any']
     )
