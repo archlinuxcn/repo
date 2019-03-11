05b1c19efa70220f5aecbb3f549479db4dca99ce
Jiachen YANG <farseerfc@gmail.com>
2019-03-12 00:18:55 +0900

    add kaidan-git and qxmpp

---
+++ archlinuxcn/qxmpp/lilac.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+#
+# This file is the most simple lilac.py file,
+# and it suits for most packages in AUR.
+#
+
+from lilaclib import *
+
+build_prefix = 'extra-x86_64'
+
+if __name__ == '__main__':
+  single_main()
+
