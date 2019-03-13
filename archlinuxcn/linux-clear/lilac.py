67bbd54eeabcc8ed2fafea88070f0167af6c0647
Megumi_fox <i@megumifox.com>
2019-03-13 22:26:27 +0800

    linux-clear: CONFIG_MODULE_SIG_FORCE=n

--- archlinuxcn/linux-clear/lilac.py
+++ archlinuxcn/linux-clear/lilac.py
@@ -5,6 +5,10 @@ def pre_build():
     for line in edit_file('PKGBUILD'):
         if line.strip().startswith('_subarch='):
             print('_subarch=26')
+        elif line.strip() == "### Copying i915 firmware and intel-ucode":
+            print('### Disable modle sig force')
+            print('	    sed -i "s|CONFIG_MODULE_SIG_FORCE=y|CONFIG_MODULE_SIG_FORCE=n|g" ./.config ')
+            print(line)
         else:
             print(line)
 
