fd5fc7f550ca4a75c3b33c9de5154daf5354eabc
Jiachen YANG <farseerfc@gmail.com>
2019-03-19 00:30:36 +0900

    linux-clear: fixing subarch in PKGBUILD and lilac.py

--- archlinuxcn/linux-clear/lilac.py
+++ archlinuxcn/linux-clear/lilac.py
@@ -8,11 +8,13 @@ def pre_build():
     for line in edit_file('PKGBUILD'):
         if line.strip().startswith('_subarch='):
             print('_subarch=26')
-        if line.strip().startswith('_major='):
+        elif line.strip().startswith('_major='):
             major = line[len('_major='):].strip()
-        if line.strip().startswith('_minor='):
+            print(line)
+        elif line.strip().startswith('_minor='):
             minor = line[len('_minor='):].strip()
-        if line.strip().startswith('pkgver='):
+            print(line)
+        elif line.strip().startswith('pkgver='):
             # evaluate and replace pkgver= variable
             print(f"pkgver={major}.{minor}")
         elif line.strip() == "### Copying i915 firmware and intel-ucode":
