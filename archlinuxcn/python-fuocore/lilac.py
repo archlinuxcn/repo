e368599cbd60fb0df8e97b4c81767d35722f07e5
Bruce Zhang <zttt183525594@gmail.com>
2019-03-18 18:10:57 +0800

    feeluown: move fuocore module into feeluown
    Signed-off-by: Bruce Zhang <zttt183525594@gmail.com>

--- archlinuxcn/python-fuocore/lilac.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env python3
-
-from lilaclib import *
-
-def pre_build():
-  pypi_pre_build(depends=['python', 'mpv', 'python-beautifulsoup4', 'python-marshmallow', 'python-pycrypto', 'python-requests', 'python-mutagen', 'python-fuzzywuzzy', 'python-setuptools'], license='MIT')
-
-def post_build():
-  pypi_post_build()
