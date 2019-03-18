e2bdc4f2243a0a81e2eaa0373164f83bbcf99d4d
Jiachen YANG <farseerfc@gmail.com>
2019-03-18 23:59:09 +0900

    linux-clear: fixing pkgver in lilac.py

--- archlinuxcn/linux-clear/lilac.py
+++ archlinuxcn/linux-clear/lilac.py
@@ -1,10 +1,20 @@
+#!/usr/bin/python3
+
 from lilaclib import *
 
 def pre_build():
     aur_pre_build()
+    major,minor = "", ""
     for line in edit_file('PKGBUILD'):
         if line.strip().startswith('_subarch='):
             print('_subarch=26')
+        if line.strip().startswith('_major='):
+            major = line[len('_major='):].strip()
+        if line.strip().startswith('_minor='):
+            minor = line[len('_minor='):].strip()
+        if line.strip().startswith('pkgver='):
+            # evaluate and replace pkgver= variable
+            print(f"pkgver={major}.{minor}")
         elif line.strip() == "### Copying i915 firmware and intel-ucode":
             print('### Disable modle sig force')
             print('	    sed -i "s|CONFIG_MODULE_SIG_FORCE=y|CONFIG_MODULE_SIG_FORCE=n|g" ./.config ')
@@ -13,3 +23,5 @@ def pre_build():
             print(line)
 
 
+if __name__== '__main__':
+    single_main('extra-x86_64')
