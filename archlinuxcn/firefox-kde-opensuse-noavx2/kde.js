1f67679f76e989e150e98d66880062c1a5aee6cb
Jiachen YANG <farseerfc@gmail.com>
2019-04-08 23:35:59 +0900

    firefox-kde-opensuse-noavx2: merge into firefox-kde-opensuse PKGBUILD

--- archlinuxcn/firefox-kde-opensuse-noavx2/kde.js
+++
@@ -1,2 +0,0 @@
-pref("browser.preferences.instantApply", false);
-pref("browser.backspace_action", 0);
--- archlinuxcn/firefox-kde-opensuse/lilac.py
+++ archlinuxcn/firefox-kde-opensuse/lilac.py
@@ -5,20 +5,11 @@ build_prefix = 'extra-x86_64'
 time_limit_hours = 4
 
 def pre_build():
-    aur_pre_build()
-
-    for line in edit_file('PKGBUILD'):
-        print(line.replace("'cargo'", '').replace("'libvpx'", ''))
-    for line in edit_file('mozconfig'):
-        if 'libvpx' in line or 'enable-rust-simd' in line:
-            print('#' + line)
-        else:
-            print(line)
     update_pkgrel()
     run_cmd(['updpkgsums'])
 
 
-post_build = aur_post_build
+post_build = git_pkgbuild_commit
 
 if __name__ == '__main__':
     single_main(build_prefix)
