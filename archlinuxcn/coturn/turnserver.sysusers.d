0f9a2a9a11f881ea080a657f6ae57ee08f151b7c
a-wing <1@233.email>
2019-04-17 18:29:26 +0800

    Addpkg coturn

---
+++ archlinuxcn/coturn/turnserver.sysusers.d
@@ -0,0 +1,1 @@
+u turnserver - "TurnServer user" /etc/turnserver
---
+++ archlinuxcn/coturn/turnserver.tmpfiles.d
@@ -0,0 +1,3 @@
+d /etc/turnserver 0700 turnserver turnserver
+Z /etc/turnserver/turnserver.conf 0600 turnserver turnserver
+d /run/turnserver 0700 turnserver turnserver
