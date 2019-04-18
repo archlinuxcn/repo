749e23ef4ac024c7d813b97e290c9e5f13848967
Megumi_fox <i@megumifox.com>
2019-04-19 00:45:59 +0800

    linux-clear: add lilac.py and fix loop.

---
+++ archlinuxcn/linux-clear/lilac.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+
+from lilaclib import *
+
+def pre_build():
+    aur_pre_build()
+    major,minor = "", ""
+    for line in edit_file('PKGBUILD'):
+        if line.strip().startswith('_subarch='):
+            print('_subarch=26')
+        elif line.strip().startswith('_major='):
+            major = line[len('_major='):].strip()
+            print(line)
+        elif line.strip().startswith('_minor='):
+            minor = line[len('_minor='):].strip()
+            print(line)
+        elif line.strip().startswith('pkgver='):
+            # evaluate and replace pkgver= variable
+            print(f"pkgver={major}.{minor}")
+        elif line.strip() == "### Compress modules":
+            print('    ### Disable modle sig force')
+            print('	    sed -i "s|CONFIG_MODULE_SIG_FORCE=y|CONFIG_MODULE_SIG_FORCE=n|g" ./.config ')
+            print(line)
+        else:
+            print(line)
+
+
+if __name__== '__main__':
+    single_main('extra-x86_64')
