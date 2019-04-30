afaa4e3479a2033c12fc1b432ee084e0cf1e670c
Butui Hu <hot123tea123@gmail.com>
2019-04-30 11:26:31 +0800

    add 3dslicer-bin

---
+++ archlinuxcn/3dslicer-bin/lilac.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+
+from lilaclib import *
+import subprocess
+
+def pre_build():
+  update_pkgver_and_pkgrel(_G.newver.lstrip('v'))
+  cmd = "curl https://download.slicer.org/ |grep bitstream |cut -d '/' -f 3 |cut -d '\"' -f 1 |sed -n 3p"
+  newstr = subprocess.run(cmd, stdout=subprocess.PIPE, shell=True).stdout.decode('utf-8')[0:-1]
+
+  cmd = "grep bitstream PKGBUILD|cut -f 5 -d '/'
+  oldstr = subprocess.run(cmd, stdout=subprocess.PIPE, shell=True).stdout.decode('utf-8')[0:-2]
+
+  if oldstr == newstr:
+    raise RuntimeError("URL is not updated yet.")
+  cmd = f"sed -i '/bitstream/s/{oldstr}/{newstr}/' PKGBUILD"
+  subprocess.run(cmd, shell=True)
+
+def post_build():
+  git_add_files('PKGBUILD')
+  git_commit()
+# vim:set ts=2 sw=2 et:
+
