bd11a17c5b2081e31ac0cfe1509e8d9a822aeac6
Butui Hu <hot123tea123@gmail.com>
2019-04-30 16:12:08 +0800

    3dslicer: update lilac.py

--- archlinuxcn/3dslicer-bin/lilac.py
+++ archlinuxcn/3dslicer-bin/lilac.py
@@ -5,15 +5,16 @@ import subprocess
 
 def pre_build():
   update_pkgver_and_pkgrel(_G.newver.lstrip('v'))
-  cmd = "curl https://download.slicer.org/ |grep bitstream |cut -d '/' -f 3 |cut -d '\"' -f 1 |sed -n 3p"
-  newstr = subprocess.run(cmd, stdout=subprocess.PIPE, shell=True).stdout.decode('utf-8')[0:-1]
+  cmd = "curl https://download.slicer.org/ | grep bitstream | cut -d '\"' -f 2"
+  newstr = subprocess.run(cmd, stdout=subprocess.PIPE, shell=True).stdout.decode('utf-8')
+  newstr = newstr.split('\n')[2].replace('/bitstream/', '').replace('/bitstream/', '')
 
-  cmd = "grep bitstream PKGBUILD|cut -f 5 -d '/'
+  cmd = "grep bitstream PKGBUILD|cut -f 5 -d \'/\'"
   oldstr = subprocess.run(cmd, stdout=subprocess.PIPE, shell=True).stdout.decode('utf-8')[0:-2]
 
   if oldstr == newstr:
     raise RuntimeError("URL is not updated yet.")
-  cmd = f"sed -i '/bitstream/s/{oldstr}/{newstr}/' PKGBUILD"
+  cmd = f"sed -i \'/bitstream/s/{oldstr}/{newstr}/\' PKGBUILD"
   subprocess.run(cmd, shell=True)
 
 def post_build():
