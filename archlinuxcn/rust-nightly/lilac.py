52107fa7332d8b5f54be3a27fff1f0655a6d3a62
lilydjwg <lilydjwg@gmail.com>
2019-05-31 17:04:36 +0800

    rust-nightly: and rls

--- archlinuxcn/rust-nightly/lilac.py
+++ archlinuxcn/rust-nightly/lilac.py
@@ -54,12 +54,20 @@ def prepare():
   version = toml['pkg']['rust']['version'].split('-', 1)[0]
   cargo_version = toml['pkg']['cargo']['version'].split('-', 1)[0]
   rustfmt_version = toml['pkg']['rustfmt-preview']['version'].split('-', 1)[0]
+
   clippy_version = toml['pkg']['clippy-preview']['version'].split('-', 1)[0]
   try:
     clippy_url = toml['pkg']['clippy-preview']['target'] \
         ['x86_64-unknown-linux-gnu']['xz_url']
   except KeyError:
     return 'no clippy available?'
+
+  rls_version = toml['pkg']['rls-preview']['version'].split('-', 1)[0]
+  try:
+    rls_url = toml['pkg']['rls-preview']['target'] \
+        ['x86_64-unknown-linux-gnu']['xz_url']
+  except KeyError:
+    return 'no rls available?'
 
   stds = [Std(target, toml['pkg']['rust-std']['target'][target])
           for target in STDS]
@@ -75,6 +83,8 @@ def prepare():
     rustfmt_version = rustfmt_version,
     clippy_version = clippy_version,
     clippy_url = clippy_url,
+    rls_version = rls_version,
+    rls_url = rls_url,
   )
 
 def pre_build():
