73f40d78e90ab7573a14f6e879664e223cdc53b7
Butui Hu <hot123tea123@gmail.com>
2019-06-01 20:57:20 +0800

    pycharm-professional: fix pre & post build error

--- archlinuxcn/pycharm-professional/lilac.py
+++ archlinuxcn/pycharm-professional/lilac.py
@@ -3,10 +3,9 @@
 from lilaclib import *
 
 def pre_build():
-  update_pkgver_and_pkgrel(_G.newver.lstrip('v'))
+  aur_pre_build()
 
 def post_build():
-  git_add_files('PKGBUILD')
-  git_commit()
+  aur_post_build()
 # vim:set ts=2 sw=2 et:
 
