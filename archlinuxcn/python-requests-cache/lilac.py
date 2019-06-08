68938766f0fb45196bee2a3e2eebf8cd64f2fe4d
axionl <i@axionl.me>
2019-06-08 13:59:55 +0800

    [Update] netease-musicbox: add python-requests-cache and trigger to rebuild

--- archlinuxcn/python-requests-cache/lilac.py
+++ archlinuxcn/python-requests-cache/lilac.py
@@ -3,8 +3,10 @@
 
 from lilaclib import *
 
+
 def pre_build():
-    update_pkgver_and_pkgrel(_G.newver.replace('v', ''))
+    update_pkgver_and_pkgrel(_G.newver)
+
 
 def post_build():
     git_add_files('PKGBUILD')
