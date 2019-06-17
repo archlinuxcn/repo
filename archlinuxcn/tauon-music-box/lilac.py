d00825d7cba383930721426f70cd21ffcce9c371
axionl <i@axionl.me>
2019-06-17 14:20:29 +0800

    [Update] tauon-music-box: readd some package as depends

--- archlinuxcn/tauon-music-box/lilac.py
+++ archlinuxcn/tauon-music-box/lilac.py
@@ -1,22 +1,13 @@
 # Trimmed lilac.py
 #!/usr/bin/env python3
-#
-# This file is the most simple lilac.py file,
-# and it suits for most packages in AUR.
-#
 
 from lilaclib import *
 
-#build_prefix = 'extra-x86_64'
-
-repo_depends = ["python-hsaudiotag3k", "python-stagger-git"]
 
 def pre_build():
-	update_pkgver_and_pkgrel(_G.newver.lstrip('v'))
+    update_pkgver_and_pkgrel(_G.newver.lstrip('v'))
+
 
 def post_build():
-	git_add_files('PKGBUILD')
-	git_commit()
-
-#if __name__ == '__main__':
-#    single_main()
+    git_add_files('PKGBUILD')
+    git_commit()
