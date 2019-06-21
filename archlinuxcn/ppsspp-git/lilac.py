9a76cbe6211d365cc0761c0e30b848e7f84fd0b4
Jiachen YANG <farseerfc@gmail.com>
2019-06-21 15:06:57 +0900

    ppsspp-git: fix depends python also add in lilac

--- archlinuxcn/ppsspp-git/lilac.py
+++ archlinuxcn/ppsspp-git/lilac.py
@@ -7,9 +7,9 @@
 
 from lilaclib import *
 
-#build_prefix = 'extra-x86_64'
-
-#pre_build = aur_pre_build
+def pre_build():
+    aur_pre_build()
+    add_depends(['python'])
 #post_build = aur_post_build
 
 #if __name__ == '__main__':
