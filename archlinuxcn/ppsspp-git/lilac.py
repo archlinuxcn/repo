8961e8342db7637fa7d09ce9b84428175f0a0ab6
Jiachen YANG <farseerfc@gmail.com>
2019-07-03 13:36:06 +0900

    ppsspp-git: we no longer need to add python dependency

--- archlinuxcn/ppsspp-git/lilac.py
+++ archlinuxcn/ppsspp-git/lilac.py
@@ -7,12 +7,8 @@
 
 from lilaclib import *
 
-def pre_build():
-    aur_pre_build()
-    add_depends(['python'])
-#post_build = aur_post_build
 
-#if __name__ == '__main__':
-#    single_main(build_prefix)
+if __name__ == '__main__':
+    single_main(build_prefix)
 
 
