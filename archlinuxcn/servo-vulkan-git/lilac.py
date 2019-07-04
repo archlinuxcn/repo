b74cfe8f2a758986e97f75470cac57590c76aa83
Jiachen YANG <farseerfc@gmail.com>
2019-07-05 02:15:06 +0900

    addpkg servo-vulkan-git

---
+++ archlinuxcn/servo-vulkan-git/lilac.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+#
+# This file is the most simple lilac.py file,
+# and it suits for most packages in AUR.
+#
+
+from lilaclib import *
+
+def post_build():
+    git_add_files('PKGBUILD')
+    git_commit()
+
+def pre_build():
+    run_cmd(['updpkgsums'])
+    vcs_update()
+
+if __name__ == '__main__':
+    single_main()
