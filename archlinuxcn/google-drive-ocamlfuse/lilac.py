f21b177124cce75de0484471a7cce4e30f1e53ad
Jianfeng Zhang <swordfeng123@gmail.com>
2019-07-07 01:40:58 -0700

    google-drive-ocamlfuse: fix build

--- archlinuxcn/google-drive-ocamlfuse/lilac.py
+++ archlinuxcn/google-drive-ocamlfuse/lilac.py
@@ -1,4 +1,9 @@
 #!/usr/bin/env python3
 
-#from lilaclib import *
+from lilaclib import *
 
+def pre_build():
+    aur_pre_build('google-drive-ocamlfuse-opam')
+    for line in edit_file('PKGBUILD'):
+        line = line.replace('google-drive-ocamlfuse-opam', 'google-drive-ocamlfuse')
+        print(line)
