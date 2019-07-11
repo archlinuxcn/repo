71460a76d1ecf5d0b16bf55ba8dca9f4734561e6
lilac <lilac@build.archlinuxcn.org>
2019-07-11 15:08:05 +0800

    qt5-base-nostatx, wpa_supplicant-git, xfwm4-devel: orphaned for a long time, removing. closes #1226

--- archlinuxcn/qt5-base-nostatx/lilac.py
+++
@@ -1,8 +0,0 @@
-from lilaclib import aur_pre_build, edit_file
-
-def pre_build():
-    aur_pre_build()
-
-    for line in edit_file('PKGBUILD'):
-        if not line.startswith('groups=('):
-            print(line)
