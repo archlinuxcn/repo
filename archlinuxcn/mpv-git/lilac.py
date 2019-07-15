30f7fa6e5cad17f10415fe9efe0a308a9b1416b5
Jingbei Li <i@jingbei.li>
2019-07-15 18:07:50 +0800

    fixed mpv-git/lilac.py

--- archlinuxcn/mpv-git/lilac.py
+++ archlinuxcn/mpv-git/lilac.py
@@ -7,7 +7,16 @@ repo_depends = ['ffmpeg-git']
 build_prefix = 'extra-x86_64'
 
 def pre_build():
+
+    old_pkgver, old_pkgrel = get_pkgver_and_pkgrel()
+    run_cmd(['rm', '-f', 'PKGBUILD'])
+
     aur_pre_build()
+
+    new_pkgver, new_pkgrel = get_pkgver_and_pkgrel()
+    if old_pkgver == new_pkgver and new_pkgrel <= old_pkgrel:
+        update_pkgrel(old_pkgrel+1)
+
     for line in edit_file('PKGBUILD'):
         if 'depends=(' in line:
             print(line)
