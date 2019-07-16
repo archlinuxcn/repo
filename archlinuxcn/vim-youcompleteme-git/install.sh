87d93e97006cad6785d6e80b387bff6d630add33
Dct Mei <dctxmei@gmail.com>
2019-07-16 14:24:40 +0900

    fix for package vim-youcompleteme-git

--- archlinuxcn/vim-youcompleteme-git/install.sh
+++ archlinuxcn/vim-youcompleteme-git/install.sh
@@ -1,28 +1,32 @@
 update_vim_help() {
   echo -n "Updating Vim help tags..."
   /usr/bin/vim --noplugin -u NONE -U NONE \
-    --cmd ":helptags /usr/share/vim/vimfiles/doc" --cmd ":q" > /dev/null 2>&1
+    --cmd ":helptags /usr/share/vim/vimfiles/doc" --cmd ":q" >/dev/null 2>&1
   echo "done."
 }
 
 post_install() {
   update_vim_help
-  #echo "==> ------------------------------------------------------------------------------------------------------------------------------"
-  #echo
-  #echo "==> add: \"let g:ycm_global_ycm_extra_conf = '/usr/share/vim/vimfiles/third_party/ycmd/cpp/ycm/.ycm_extra_conf.py'\" to your vimrc"
-  #echo
-  #echo "==> ------------------------------------------------------------------------------------------------------------------------------"
+  echo "For rust support be sure to setup a default toolchain with rustup"
+  echo "rls, rust-analysis and rust-src are needed for whatever toolchain you are using."
+  echo "Example:"
+  echo -e "\trustup toolchain install nightly"
+  echo -e "\trustup default nightly"
+  echo -e "\trustup component add rls rust-analysis rust-src"
+  echo -e "\n\n\n"
 }
 
 post_upgrade() {
   update_vim_help
+  echo "For rust support be sure to setup a default toolchain with rustup"
+  echo "rls, rust-analysis and rust-src are needed for whatever toolchain you are using."
+  echo "Example:"
+  echo -e "\trustup toolchain install nightly"
+  echo -e "\trustup default nightly"
+  echo -e "\trustup component add rls rust-analysis rust-src"
+  echo -e "\n\n\n"
 }
 
 post_remove() {
   update_vim_help
-  #echo "==> ---------------------------------------------------------------------------------------------------------------------------------"
-  #echo
-  #echo "==> remove: \"let g:ycm_global_ycm_extra_conf = '/usr/share/vim/vimfiles/third_party/ycmd/cpp/ycm/.ycm_extra_conf.py'\" in your vimrc"
-  #echo
-  #echo "==> ---------------------------------------------------------------------------------------------------------------------------------"
 }
