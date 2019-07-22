d813dc103cae89a3e325bc4232acef5de2589157
edward-p <edward@edward-p.xyz>
2019-07-22 22:49:16 +0800

    plymouth-theme-arch-beat: new package

---
+++ archlinuxcn/plymouth-theme-arch-beat/lilac.py
@@ -0,0 +1,12 @@
+# Trimmed lilac.py
+#!/usr/bin/env python3
+#
+# This file is the most simple lilac.py file,
+# and it suits for most packages in AUR.
+#
+
+from lilaclib import *
+
+def pre_build():
+  update_pkgver_and_pkgrel(_G.newver)
+
