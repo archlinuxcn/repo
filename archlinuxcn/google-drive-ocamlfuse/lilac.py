3a692009d7c714695392fd4b1c9ab424fa41f81a
Jianfeng Zhang <swordfeng123@gmail.com>
2019-07-22 21:39:03 -0700

    google-drive-ocamlfuse: force lilac to do vcs update

--- archlinuxcn/google-drive-ocamlfuse/lilac.py
+++ archlinuxcn/google-drive-ocamlfuse/lilac.py
@@ -3,7 +3,7 @@
 from lilaclib import *
 
 def pre_build():
-    aur_pre_build('google-drive-ocamlfuse-opam')
+    aur_pre_build('google-drive-ocamlfuse-opam', do_vcs_update=True)
     for line in edit_file('PKGBUILD'):
         line = line.replace('google-drive-ocamlfuse-opam', 'google-drive-ocamlfuse')
         print(line)
