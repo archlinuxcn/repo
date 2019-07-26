aaf116dbe5822d315acf222a926d7a59294a2a9e
Jiachen YANG <farseerfc@gmail.com>
2019-07-26 11:22:18 +0900

    firefox-kde-opensuse: use oldver if oldver is newer than newver

--- archlinuxcn/firefox-kde-opensuse/lilac.py
+++ archlinuxcn/firefox-kde-opensuse/lilac.py
@@ -1,11 +1,17 @@
 #!/usr/bin/env python3
 from lilaclib import *
+from pyalpm import vercmp
 
 build_prefix = 'extra-x86_64'
 time_limit_hours = 4
 
 def pre_build():
-    update_pkgver_and_pkgrel(_G.newver[:_G.newver.find('-')], updpkgsums=True)
+    oldver, oldpkgrel = _G.oldver.split('-')
+    newver, newpkgrel = _G.newver.split('-')
+    if vercmp(newver, oldver) >= 0:
+        update_pkgver_and_pkgrel(newver, updpkgsums=True)
+    else:
+        update_pkgver_and_pkgrel(oldver, updpkgsums=True)
 
 
 post_build = git_pkgbuild_commit
