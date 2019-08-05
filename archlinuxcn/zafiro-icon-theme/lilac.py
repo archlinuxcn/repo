3190b4c955be4f8c64dad710eb2e237450df12a6
axionl <i@axionl.me>
2019-08-05 19:40:53 +0800

    [Update] zafiro-icon-theme: 1.0 version

--- archlinuxcn/zafiro-icon-theme/lilac.py
+++ archlinuxcn/zafiro-icon-theme/lilac.py
@@ -4,7 +4,7 @@
 from lilaclib import *
 
 def pre_build():
-    update_pkgver_and_pkgrel(_G.newver.replace('v', ''))
+    update_pkgver_and_pkgrel(_G.newver)
 
 def post_build():
     git_add_files('PKGBUILD')
