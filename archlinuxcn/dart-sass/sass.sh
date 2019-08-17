e83c4477a77c8172d0b916e7df1b122155f2c012
axionl <i@axionl.me>
2019-08-18 02:10:21 +0800

    [Fixed] dart-sass: sass.sh

--- archlinuxcn/dart-sass/sass.sh
+++ archlinuxcn/dart-sass/sass.sh
@@ -1,7 +1,3 @@
 #!/usr/bin/sh
 
-INSTALL_NAME="sass"
-DIR="/usr/lib/${INSTALL_NAME}"
-
-cd "${DIR}" || exit 1
-exec /usr/bin/dart ./app.snapshot "${@}"
+exec /usr/bin/dart /usr/lib/sass/app.snapshot "${@}"
