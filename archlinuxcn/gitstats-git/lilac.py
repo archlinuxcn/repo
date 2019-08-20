d7f68af5aa4dd8ee689a53fc07e7a3ccfd3f8700
lilydjwg <lilydjwg@gmail.com>
2019-08-20 18:07:18 +0800

    gitstats-git: switch to a Python 3 fork

--- archlinuxcn/gitstats-git/lilac.py
+++ archlinuxcn/gitstats-git/lilac.py
@@ -1,11 +1,7 @@
-# Trimmed lilac.py
-#!/usr/bin/env python3
-
 from lilaclib import *
 
-#build_prefix = 'extra-x86_64'
-#post_build = aur_post_build
-#pre_build = aur_pre_build
+def pre_build():
+  vcs_update()
 
-#if __name__ == '__main__':
-#  single_main()
+def post_build():
+  git_pkgbuild_commit()
