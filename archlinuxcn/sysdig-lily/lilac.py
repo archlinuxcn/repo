4f5eeda78dc25e7f2af3ae7118929e05fd467f7a
lilydjwg <lilydjwg@gmail.com>
2019-08-23 21:05:58 +0800

    bbswitch-lily, sysdig-lily, virtualbox-modules-lily, wireguard-lily: stricter depends to prevent accidental broken upgrades

--- archlinuxcn/sysdig-lily/lilac.py
+++ archlinuxcn/sysdig-lily/lilac.py
@@ -1,7 +1,17 @@
 from lilaclib import *
 
 def pre_build():
-  update_pkgver_and_pkgrel(_G.newver.split('-')[0])
+  depends_lily_re = re.compile(
+    r'''^(depends=.*?)['"]?linux-lily[^'") ]*['"]?''')
+
+  kernel = _G.newvers[1]
+  update_pkgver_and_pkgrel(_G.newver)
+  for line in edit_file('PKGBUILD'):
+    m = depends_lily_re.match(line)
+    if m:
+      line = depends_lily_re.sub(
+        r'\1"linux-lily=%s"' % kernel, line)
+    print(line)
 
 def post_build():
   git_pkgbuild_commit()
