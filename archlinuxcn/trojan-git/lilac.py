a82ff2b5af798dcdeb4b428df434f66847b1ab5b
axionl <i@axionl.me>
2019-09-28 12:37:19 +0800

    [Fixed] trojan-git: update lilac.yaml

--- archlinuxcn/trojan-git/lilac.py
+++ archlinuxcn/trojan-git/lilac.py
@@ -7,18 +7,9 @@
 
 from lilaclib import *
 
-#build_prefix = 'extra-x86_64'
-
-
-def pre_build():
-    aur_pre_build()
-    vcs_update()
-
-
 def post_build():
-    git_add_files('PKGBUILD')
-    git_commit()
+    git_pkgbuild_commit()
     update_aur_repo()
 
-# if __name__ == '__main__':
-#    single_main(build_prefix)
+if __name__ == '__main__':
+    single_main(build_prefix)
