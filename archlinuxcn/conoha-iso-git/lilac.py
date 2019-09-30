2ded68dcc9848c81b94d494bf80ff35e6a3cbe1d
Jiachen YANG <farseerfc@gmail.com>
2019-09-30 21:24:25 +0900

    conoha-iso-git: fix go dep init and update AUR

--- archlinuxcn/conoha-iso-git/lilac.py
+++ archlinuxcn/conoha-iso-git/lilac.py
@@ -12,6 +12,10 @@ build_prefix = 'extra-x86_64'
 #pre_build = aur_pre_build
 #post_build = aur_post_build
 
+def post_build():
+    git_pkgbuild_commit()
+    update_aur_repo()
+
 if __name__ == '__main__':
     single_main(build_prefix)
 
