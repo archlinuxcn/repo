d025b097c913a1902f388c4a03167e00d78e6498
Jiachen YANG <farseerfc@gmail.com>
2019-10-28 13:53:23 +0900

    farseerfc's .BUILDINFO rebuild, see #1351

--- archlinuxcn/ipad_charge/lilac.py
+++ archlinuxcn/ipad_charge/lilac.py
@@ -7,32 +7,10 @@
 
 from lilaclib import *
 
-#build_prefix = 'extra-x86_64'
-PATCH = b"""
-diff --git a/ipad_charge/PKGBUILD b/ipad_charge/PKGBUILD
-index 6802cd1..55bc6b7 100644
---- a/ipad_charge/PKGBUILD
-+++ b/ipad_charge/PKGBUILD
-@@ -10,7 +10,7 @@ source=("http://www.rainbow-software.org/linux_files/${pkgname}_${pkgver}.tar.gz
- "95-ipad_charge.rules.patch"
- "ipad_charge.c.patch"
- )
--depends=('udev')
-+depends=('udev' 'libusb')
- makedepends=('gcc')
- md5sums=('09b8c600efd747a36c9cc320516326cf'
-          'bfc9325716cc8fcedc04f13fcf7c8693'
-"""
-
-import subprocess
-
-def apply_patch(filename, patch):
-    patch_proc = subprocess.Popen(["patch", "-p1", filename], stdin=subprocess.PIPE)
-    patch_proc.communicate(patch)
 
 def pre_build():
     aur_pre_build()
-    apply_patch("PKGBUILD", PATCH)
+    add_depends(['libusb'])
 
 #post_build = aur_post_build
 
