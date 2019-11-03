93d29b5d465fa44024286d206f9fa223abecce11
oldherl <oldherl@gmail.com>
2019-11-03 19:37:36 +0800

    addpkg: ds9

---
+++ archlinuxcn/ds9/lilac.py
@@ -0,0 +1,17 @@
+# Trimmed lilac.py
+#!/usr/bin/env python3
+
+from lilaclib import *
+
+# TODO(oldherl): this package bundles tcl/tk sources
+# Should we remove it and use Arch dependencies some day?
+def pre_build():
+    aur_pre_build()
+    add_depends(['libxml2', 'libx11', 'desktop-file-utils'])
+
+#post_build = aur_post_build
+
+#if __name__ == '__main__':
+#  single_main(build_prefix)
+
+
