5284e9b33ea0e758dd508d321854233e04a3db7e
Chih-Hsuan Yen <yan12125@gmail.com>
2019-11-09 11:57:57 +0800

    nodejs-web-ext: add vcs_update()
    The second package affected by --holdver of lilac :/
    
    The first one is python-onnxruntime 4786b9bc7f59af023381683748131914e5f7b62c

--- archlinuxcn/nodejs-web-ext/lilac.py
+++ archlinuxcn/nodejs-web-ext/lilac.py
@@ -1,9 +1,14 @@
-# Trimmed lilac.py
-
-from lilaclib import *
+from lilaclib import (
+    git_pkgbuild_commit,
+    update_aur_repo,
+    update_pkgver_and_pkgrel,
+    vcs_update,
+)
 
 
 def pre_build():
+    # git repos are not updated by default as lilac uses --holdver
+    vcs_update()
     update_pkgver_and_pkgrel(_G.newver)
 
 
