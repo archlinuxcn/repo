30e97105ba5f06839f65c5f8b23cb620119a0d21
Jingbei Li <i@jingbei.li>
2019-11-16 10:00:49 +0800

    python 3.8 rebuild #1378

--- archlinuxcn/python3-aur/lilac.py
+++ archlinuxcn/python3-aur/lilac.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 from lilaclib import *
 
-update_on = [{'aur':None}]
+update_on = [{'aur':None}, {'alias': 'python'}, {'alias':'python'}]
 build_prefix = 'extra-x86_64'
 repo_depends = ['python3-xcpf', 'python3-xcgf']
 pre_build = aur_pre_build
