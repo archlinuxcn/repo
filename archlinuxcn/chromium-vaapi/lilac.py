341b7204fd8a32da01d35e1d832207921111abb0
Megumi_fox <i@megumifox.com>
2019-11-20 20:12:29 +0800

    chromium-vaapi: remove jumbo_build

--- archlinuxcn/chromium-vaapi/lilac.py
+++ archlinuxcn/chromium-vaapi/lilac.py
@@ -3,17 +3,8 @@
 from lilaclib import *
 
 def pre_build():
-
     aur_pre_build()
-    
-    for line in edit_file('PKGBUILD'):
-        if line.strip().startswith('local _flags=('):
-            print(line)
-            print("    'use_jumbo_build = true'")
-        else:
-            print(line)
-    
-    run_cmd(["updpkgsums"])
+
 
 
 #if __name__ == '__main__':
