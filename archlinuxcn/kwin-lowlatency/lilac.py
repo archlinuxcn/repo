9487cbedd059045508c296bde56ab7631646c7d1
Peter Cai <xqsx43cxy@gmail.com>
2019-11-29 19:03:15 +0800

    kwin-lowlatency: remove groups

--- archlinuxcn/kwin-lowlatency/lilac.py
+++ archlinuxcn/kwin-lowlatency/lilac.py
@@ -4,4 +4,10 @@ from lilaclib import *
 
 def pre_build():
     aur_pre_build()
+
+    for line in edit_file('PKGBUILD'):
+        if line.strip().startswith('groups='):
+            continue
+        print(line)
+
     vcs_update() # Avoid lilac cache for git sources
