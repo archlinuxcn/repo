ae4a5103c763f4447b1a4bd407e5b3e31fee7625
Jiachen YANG <farseerfc@gmail.com>
2019-11-30 16:06:19 +0900

    addpkg: mindustry-git

---
+++ archlinuxcn/mindustry-git/lilac.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+#
+# This file is the most simple lilac.py file,
+# and it suits for most packages in AUR.
+#
+
+from lilaclib import *
+
+def post_build():
+    git_add_files('PKGBUILD')
+    git_commit()
+    update_aur_repo()
+
+def pre_build():
+    run_cmd(['updpkgsums'])
+    vcs_update()
+
+if __name__ == '__main__':
+    single_main()
---
+++ archlinuxcn/mindustry-git/mindustry.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+exec java -jar /usr/share/java/mindustry/Mindustry.jar "$@"
