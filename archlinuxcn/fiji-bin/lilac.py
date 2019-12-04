cc2c6d7989126054bd062281a88d7ac443035a76
Butui Hu <hot123tea123@gmail.com>
2019-12-04 16:59:32 +0800

    fiji-bin: update on official download page, not github

--- archlinuxcn/fiji-bin/lilac.py
+++ archlinuxcn/fiji-bin/lilac.py
@@ -3,7 +3,7 @@
 from lilaclib import *
 
 def pre_build():
-  update_pkgver_and_pkgrel(_G.newver.replace('fiji-','').replace('-', '_'))
+  update_pkgver_and_pkgrel(_G.newver.replace('-','.'))
 
 def post_build():
   git_add_files('PKGBUILD')
