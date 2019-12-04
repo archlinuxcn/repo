cacce06a1fd4b5d633135dd5a61f3d3cc58590c9
Jingbei Li <i@jingbei.li>
2019-12-04 13:48:38 +0800

    fixed bomi-git/lilac.* #1418

--- archlinuxcn/bomi-git/lilac.py
+++ archlinuxcn/bomi-git/lilac.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 from lilaclib import *
 
-update_on = [{'aur':None}, {'github':'d-s-x/bomi'}]
+update_on = [{'aur':None}, {'github':'demokritos/bomi'}, {'archpkg': 'libdvdread'}]
 build_prefix = 'extra-x86_64'
 repo_depends = ['libchardet']
 pre_build = aur_pre_build
