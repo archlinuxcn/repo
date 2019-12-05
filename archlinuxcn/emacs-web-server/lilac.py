18551e74ddfe656e3f11e305dbfeb176b4721891
Poscat <poscat@mail.poscat.moe>
2019-12-05 21:06:12 +0800

    Add package emacs-web-server to repo

---
+++ archlinuxcn/emacs-web-server/lilac.py
@@ -0,0 +1,14 @@
+from lilaclib import *
+
+def pre_build():
+    aur_pre_build()
+    if _G.newver is not None:
+        update_pkgver_and_pkgrel(_G.newver.lstrip('version-'))
+    else:
+        raise Exception("nvchecker failed (newver is None)")
+
+
+def post_build():
+    aur_post_build()
+    update_aur_repo()
+
