89f1b0110c3724c53bf40694b88b99a060808456
Jiachen YANG <farseerfc@gmail.com>
2019-12-05 17:16:47 +0900

    scilab: fix typo

--- archlinuxcn/scilab/lilac.py
+++ archlinuxcn/scilab/lilac.py
@@ -2,7 +2,7 @@ from lilaclib import *
 
 def pre_build():
     for line in edit_file('PKGBUILD'):
-        if line.strip().startwith('makedepends='):
+        if line.strip().startswith('makedepends='):
             line = "makedepends=('jdk8-openjdk' 'ant>=1.9.0'"
         print(line)
 
