e7cd6f30af49fe858b7b9b5b0e03283e3109ed3a
Chih-Hsuan Yen <yan12125@gmail.com>
2019-12-05 22:32:12 +0800

    3dslicer-nightly-bin: do not use nvchecker in lilac.py
    See discussions in https://github.com/yan12125/repo/commit/6219c04eec7e807e99645371d2ab031144ae8198

--- archlinuxcn/3dslicer-nightly-bin/lilac.py
+++ archlinuxcn/3dslicer-nightly-bin/lilac.py
@@ -1,20 +1,10 @@
 #!/usr/bin/env python3
 
 from lilaclib import *
-from nvchecker.get_version import get_version
-import asyncio
 
 
 def pre_build():
-    version_conf = {'url': 'https://download.slicer.org',
-                    'regex': 'version (\d+.\d+.\d+)'}
-    revision_conf = {'url': 'https://download.slicer.org',
-                     'regex': 'revision (\d+.\d+.\d+)'}
-    loop = asyncio.new_event_loop()
-    asyncio.set_event_loop(loop)
-    version = loop.run_until_complete(get_version(name='3dslicer-nightly-bin', conf=version_conf))
-    revision = loop.run_until_complete(get_version(name='3dslicer-nightly-bin', conf=revision_conf))
-    loop.close()
+    version, revision = _G.newvers
     newver = version + '.r' + revision
     update_pkgver_and_pkgrel(newver)
 
