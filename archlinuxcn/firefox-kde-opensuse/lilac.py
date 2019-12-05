670f8cb7a0e322b8c5910f716a0dc0489f817477
Jiachen YANG <farseerfc@gmail.com>
2019-12-05 15:50:22 +0900

    firefox-kde-opensuse: remove debug option to speedup building

--- archlinuxcn/firefox-kde-opensuse/lilac.py
+++ archlinuxcn/firefox-kde-opensuse/lilac.py
@@ -3,10 +3,11 @@ from lilaclib import *
 from pyalpm import vercmp
 
 build_prefix = 'extra-x86_64'
-time_limit_hours = 4
+time_limit_hours = 5
 
 def pre_build():
     aur_pre_build()
+    # add_into_array('options', "'debug'")
 
 if __name__ == '__main__':
     single_main(build_prefix)
