8d2032d0cd81aaaf240b216b0ecec41c5d86d61e
Jiachen YANG <farseerfc@gmail.com>
2019-12-06 16:00:50 +0900

    scilab: help configure find jdk8-openjdk

--- archlinuxcn/scilab/lilac.py
+++ archlinuxcn/scilab/lilac.py
@@ -4,5 +4,7 @@ def pre_build():
     for line in edit_file('PKGBUILD'):
         if line.strip().startswith('makedepends='):
             line = "makedepends=('jdk8-openjdk' 'ant>=1.9.0'"
+        if line.strip().startswith('./configure'):
+            line = '  ./configure --with-jdk=/usr/lib/jvm/java-8-openjdk/ \\'
         print(line)
 
