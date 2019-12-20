5bebfae9ea890efcdd8c736357331d15560ad201
Chih-Hsuan Yen <yan12125@gmail.com>
2019-12-20 19:06:02 +0800

    python-onnxruntime: fix building and misc changes
    * Do not update AUR package during rebuilds
    * Remove unneeded vcs_update; lilac runs it automatically
      https://github.com/archlinuxcn/lilac/commit/01d844a4512d7a4b7a677df21f82d896da0d1683

--- archlinuxcn/python-onnxruntime/lilac.py
+++ archlinuxcn/python-onnxruntime/lilac.py
@@ -2,16 +2,14 @@ from lilaclib import (
     update_pkgver_and_pkgrel,
     git_pkgbuild_commit,
     update_aur_repo,
-    vcs_update,
 )
 
 
 def pre_build():
-    # git repos are not updated by default as lilac uses --holdver
-    vcs_update()
     update_pkgver_and_pkgrel(_G.newver)
 
 
 def post_build():
     git_pkgbuild_commit()
-    update_aur_repo()
+    if _G.newver != _G.oldver:
+        update_aur_repo()
