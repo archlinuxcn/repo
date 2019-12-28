344108b2d0b9c8ddd7a239d43893029c9762be9f
Sasasu <su@sasasu.me>
2019-12-28 22:24:40 +0800

    mycli: remove python-sqlparse-cli_helpers depend

--- archlinuxcn/mycli/lilac.py
+++ archlinuxcn/mycli/lilac.py
@@ -6,15 +6,9 @@ build_prefix = "extra-x86_64"
 
 def pre_build():
   aur_pre_build()
-  for line in edit_file("PKGBUILD"):
-    if "'python-sqlparse'" in line:
-      print("'python-sqlparse-cli_helpers'")
-    else:
-      print(line)
 
 def post_build():
   aur_post_build()
-
 
 if __name__ == "__main__":
   single_main(build_prefix)
