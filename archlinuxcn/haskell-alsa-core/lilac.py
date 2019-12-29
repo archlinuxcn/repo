1557ace11cad84bfc20dab2b3574501cd81317e1
Poscat <poscat@mail.poscat.moe>
2019-12-29 22:55:57 +0800

    haskell-alsa-core: Bump pkgrel upon rebuild

--- archlinuxcn/haskell-alsa-core/lilac.py
+++ archlinuxcn/haskell-alsa-core/lilac.py
@@ -4,6 +4,7 @@ def pre_build():
     aur_pre_build()
     add_into_array("depends", ["alsa-lib"])
     add_into_array("makedepends", ["alsa-lib"])
+    update_pkgrel()
 
 def post_build():
     aur_post_build()
