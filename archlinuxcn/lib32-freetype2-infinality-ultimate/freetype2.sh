243c9993774d0907745f26127aee70230fb4e2b5
lilac <lilac@build.archlinuxcn.org>
2020-01-03 02:02:41 +0800

    auto update for package lib32-freetype2-infinality-ultimate

---
+++ archlinuxcn/lib32-freetype2-infinality-ultimate/freetype2.sh
@@ -0,0 +1,12 @@
+# Subpixel hinting mode can be chosen by setting the right TrueType interpreter
+# version. The available settings are:
+#
+#     truetype:interpreter-version=35  # Classic mode (default in 2.6)
+#     truetype:interpreter-version=38  # Infinality mode
+#     truetype:interpreter-version=40  # Minimal mode (default in 2.7)
+#
+# There are more properties that can be set, separated by whitespace. Please
+# refer to the FreeType documentation for details.
+
+# Uncomment and configure below
+#export FREETYPE_PROPERTIES="truetype:interpreter-version=40"
