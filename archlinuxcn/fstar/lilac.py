c2599e1bc35b4d1bb9d4ce967e3e2499d1eb7a49
Poscat <poscat@mail.poscat.moe>
2020-02-11 18:46:35 +0800

    F*: fix syntax error

--- archlinuxcn/fstar/lilac.py
+++ archlinuxcn/fstar/lilac.py
@@ -3,4 +3,4 @@ from lilaclib import *
 def pre_build():
     aur_pre_build()
     for line in edit_file('PKGBUILD'):
-        print(line.replace("z3-git", "z3")
+        print(line.replace("z3-git", "z3"))
