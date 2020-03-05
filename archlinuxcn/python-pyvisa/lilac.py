6eed639ea998def7749a012bc26b38298b4568b1
Chih-Hsuan Yen <yan12125@gmail.com>
2020-03-05 19:04:23 +0800

    python-pyvisa & python-pyvisa-py: new packages

---
+++ archlinuxcn/python-pyvisa/lilac.py
@@ -0,0 +1,10 @@
+from lilaclib import (
+    git_pkgbuild_commit,
+    update_pkgver_and_pkgrel,
+)
+
+def pre_build():
+    update_pkgver_and_pkgrel(_G.newver)
+
+def post_build():
+    git_pkgbuild_commit()
