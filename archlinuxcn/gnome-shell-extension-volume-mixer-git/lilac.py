fd1f314dca7bf468c5d1278ad9e11c4e8256c377
Jingbei Li <i@jingbei.li>
2020-03-17 17:39:08 +0800

    fixed gnome-shell-extension-volume-mixer-git/lilac.py

--- archlinuxcn/gnome-shell-extension-volume-mixer-git/lilac.py
+++ archlinuxcn/gnome-shell-extension-volume-mixer-git/lilac.py
@@ -5,9 +5,9 @@ update_on = [{'aur':None}, {'github':'aleho/gnome-shell-volume-mixer'}]
 build_prefix = 'extra-x86_64'
 
 def pre_build():
-    aur_pre_build()
+    aur_pre_build(do_vcs_update=False)
     run_cmd('wget -O 1521.patch https://patch-diff.githubusercontent.com/raw/archlinuxcn/repo/pull/1521.patch'.split(' '))
-    run_cmd('patch -p3 < 1521.patch')
+    run_cmd(['sh', '-c', 'patch -p3 < 1521.patch'])
 
 post_build = aur_post_build
 
