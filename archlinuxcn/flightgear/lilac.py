826c7d477e9da0e2d0e680a1903d3fc1de7d29d4
Jianqiu Zhang <zhangjianqiu13@gmail.com>
2020-04-20 13:56:15 -0700

    flightgear: bump pkgrel and use latest dependency

--- archlinuxcn/flightgear/lilac.py
+++ archlinuxcn/flightgear/lilac.py
@@ -9,7 +9,7 @@ def pre_build():
     aur_pre_build()
     for line in edit_file('PKGBUILD'):
         if line.startswith('depends='):
-            print("depends=('libxmu' 'openscenegraph34' 'openal' 'qt5-svg')")
+            print("depends=('libxmu' 'openscenegraph' 'openal' 'qt5-svg')")
             continue
         if line.startswith('makedepends='):
             print("makedepends=('boost' 'cmake' 'mesa' 'sharutils' 'simgear' 'qt5-base' 'qt5-declarative' 'plib' 'glu' 'libxrandr' 'subversion')")
