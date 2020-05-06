e1ab8ba8aaaee5a209d5b77b5a280d7ad145a3c2
Jianfeng Zhang <swordfeng123@gmail.com>
2020-05-06 10:33:42 -0700

    gnome-shell-extension-dash-to-dock-git: switch back to 'master' branch Closes #1600.

--- archlinuxcn/gnome-shell-extension-dash-to-dock-git/lilac.py
+++ archlinuxcn/gnome-shell-extension-dash-to-dock-git/lilac.py
@@ -5,11 +5,11 @@ from lilaclib import *
 
 def pre_build():
     aur_pre_build()
-    for line in edit_file('PKGBUILD'):
-        if line.startswith('_giturl='):
-            print('_giturl="git+https://github.com/micheleg/dash-to-dock/#branch=gnome-3-36"')
-        else:
-            print(line)
+    # for line in edit_file('PKGBUILD'):
+    #     if line.startswith('_giturl='):
+    #         print('_giturl="git+https://github.com/micheleg/dash-to-dock/#branch=gnome-3-36"')
+    #     else:
+    #         print(line)
 
 #if __name__ == '__main__':
 #    single_main(build_prefix)
