88e2e03d2a0bc4709f500a468a95b4021b629637
edward-p <edward@edward-p.xyz>
2020-05-21 14:14:22 +0800

    hplip-plugin: no lstrip('v') for _G.newver

--- archlinuxcn/hplip-plugin/lilac.py
+++ archlinuxcn/hplip-plugin/lilac.py
@@ -3,7 +3,7 @@
 from lilaclib import *
 
 def pre_build():
-  update_pkgver_and_pkgrel(_G.newver.lstrip('v'))
+  update_pkgver_and_pkgrel(_G.newver)
 
 def post_build():
   git_add_files('PKGBUILD')
