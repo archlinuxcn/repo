32568a6b1c55415d1e57f77b264c9dda1518445a
lilac <lilac@build.archlinuxcn.org>
2020-05-27 09:47:35 +0800

    auto update for package google-chrome-dev

--- archlinuxcn/google-chrome-dev/google-chrome-unstable.sh
+++ archlinuxcn/google-chrome-dev/google-chrome-unstable.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 
+XDG_CONFIG_HOME=${XDG_CONFIG_HOME:-~/.config}
+
 # Allow users to override command-line options
-if [[ -f ~/.config/chrome-dev-flags.conf ]]; then
-   CHROME_USER_FLAGS="$(cat ~/.config/chrome-dev-flags.conf)"
+if [[ -f $XDG_CONFIG_HOME/chrome-dev-flags.conf ]]; then
+	CHROME_USER_FLAGS="$(cat $XDG_CONFIG_HOME/chrome-dev-flags.conf)"
 fi
 
 # Launch
