cacc697b0c220f2af7deceb622b5e535deb492bf
Jiachen YANG <farseerfc@gmail.com>
2020-06-04 10:48:13 +0900

    addpkg: chez-scheme

---
+++ archlinuxcn/chez-scheme/lilac.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+#
+# This file is the most simple lilac.py file,
+# and it suits for most packages in AUR.
+#
+
+from lilaclib import *
+
+build_prefix = 'extra-x86_64'
+
+def pre_build():
+  aur_pre_build()
+  try:
+    os.unlink('build')
+  except FileNotFoundError:
+    return
+
+post_build = aur_post_build
+
+if __name__ == '__main__':
+  single_main()
+
