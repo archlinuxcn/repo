f65b1ffeee2fcecbad5187e83d1ef314ba3f6e57
lilac <lilac@build.archlinuxcn.org>
2020-06-04 09:58:17 +0800

    auto update for package google-chrome

--- archlinuxcn/google-chrome/google-chrome-stable.sh
+++ archlinuxcn/google-chrome/google-chrome-stable.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 
+XDG_CONFIG_HOME=${XDG_CONFIG_HOME:-~/.config}
+
 # Allow users to override command-line options
-if [[ -f ~/.config/chrome-flags.conf ]]; then
-   CHROME_USER_FLAGS="$(cat ~/.config/chrome-flags.conf)"
+if [[ -f $XDG_CONFIG_HOME/chrome-flags.conf ]]; then
+   CHROME_USER_FLAGS="$(cat $XDG_CONFIG_HOME/chrome-flags.conf)"
 fi
 
 # Launch
