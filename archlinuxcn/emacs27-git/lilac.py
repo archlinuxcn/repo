a1a0bbcd0394efcb288fce715b867072ad87f2aa
yk <yk@archlinuxcn.org>
2020-06-18 15:56:53 +0800

    add lilac files: emacs27-git #1671

---
+++ archlinuxcn/emacs27-git/lilac.py
@@ -0,0 +1,23 @@
+# Trimmed lilac.py
+#!/usr/bin/env python3
+#
+# This file is the most simple lilac.py file,
+# and it suits for most packages in AUR.
+#
+
+from lilaclib import *
+
+#build_prefix = 'archlinuxcn-x86_64'
+
+def pre_build():
+    aur_pre_build()
+    for line in edit_file('PKGBUILD'):
+            if line.startswith('replaces='):
+                    continue
+            print(line)
+
+
+#post_build = aur_post_build
+
+#if __name__ == '__main__':
+#  single_main()
