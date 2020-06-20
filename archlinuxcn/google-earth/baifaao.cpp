a0eceb007ab779d91197ae422f55846e0299f8bb
lilac <lilac@build.archlinuxcn.org>
2020-06-21 01:50:25 +0800

    auto update for package google-earth

--- archlinuxcn/google-earth/baifaao.cpp
+++
@@ -1,7 +0,0 @@
-/* amirpli 2013/11/28 */
-#include <QtCore/QAtomicInt>
-extern "C" {
-        int _Z34QBasicAtomicInt_fetchAndAddOrderedPVii(QAtomicInt* a, int b) {
-                return a->fetchAndAddOrdered(b);
-        }
-}
