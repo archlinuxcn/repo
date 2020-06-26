bbbdff64a6466c38b6b49228e58c3900a37682c4
edward-p <edward@edward-p.xyz>
2020-06-26 17:51:59 +0800

    papermc: use cmd in update_on

--- archlinuxcn/papermc/lilac.py
+++ archlinuxcn/papermc/lilac.py
@@ -4,13 +4,6 @@
 from lilaclib import *
 
 def pre_build():
-    _pkgver = requests.get('https://papermc.io/api/v1/paper').json().get('versions')[0]
-    _build = requests.get('https://papermc.io/api/v1/paper' + '/' + _pkgver).json().get('builds').get('latest')
-    with open('PKGBUILD') as f:
-        pkgbuild = f.read()
-    pkgbuild=re.sub(r'''(?<=^_pkgver=)['"]?([\d.]+)['"]?''', _pkgver, pkgbuild, count=1, flags=re.MULTILINE)
-    pkgbuild=re.sub(r'''(?<=^_build=)['"]?([\d.]+)['"]?''', _build, pkgbuild, count=1, flags=re.MULTILINE)
-    with open('PKGBUILD', 'w') as f:
-        f.write(pkgbuild)
-    run_cmd(['updpkgsums'])
+  update_pkgver_and_pkgrel(_G.newver)
+  run_cmd(['updpkgsums'])
 
