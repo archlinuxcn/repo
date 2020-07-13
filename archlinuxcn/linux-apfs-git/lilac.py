2b470446a54efb154a32ff0964effd8cf2ead84c
lilac <lilac@build.archlinuxcn.org>
2020-07-13 15:08:25 +0800

    Removing maintainers from 1 orphaned packages:
    - gconf: depended by minecraft (@petronny), rawstudio-git (@heavysink), palemoon (@heavysink), chromedriver (@dctxmei), mailspring (@dctxmei), github-desktop-bin (@heavysink)
    
    closes #1694

--- archlinuxcn/linux-apfs-git/lilac.py
+++
@@ -1,8 +0,0 @@
-from lilaclib import git_pkgbuild_commit, update_pkgrel, vcs_update
-
-def pre_build():
-    update_pkgrel()
-    vcs_update()
-
-def post_build():
-    git_pkgbuild_commit()
--- archlinuxcn/wget2-git/lilac.py
+++
@@ -1,9 +0,0 @@
-from lilaclib import aur_pre_build, aur_post_build, add_makedepends
-
-def pre_build():
-    aur_pre_build()
-    # wget for ./bootstrap and doxygen/pandoc for building docs
-    add_makedepends(['wget', 'doxygen', 'pandoc'])
-
-def post_build():
-    aur_post_build()
