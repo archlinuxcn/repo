dcb20e6d1d4b5a4b2df45ce4fc64987c16b9ae93
Megumi_fox <i@megumifox.com>
2020-07-20 16:21:52 +0800

    megacmd: rstrip('_Linux')

--- archlinuxcn/megacmd/lilac.py
+++ archlinuxcn/megacmd/lilac.py
@@ -3,7 +3,7 @@ from lilaclib import *
 
 
 def pre_build():
-    update_pkgver_and_pkgrel(_G.newver.rstrip('Linux').lstrip())
+    update_pkgver_and_pkgrel(_G.newver.rstrip('_Linux').lstrip())
 
 def post_build():
     git_add_files('PKGBUILD')
