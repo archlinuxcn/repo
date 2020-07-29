3b54073468504e8b2e3b3b42243a3b8638c5b317
lilac <lilac@build.archlinuxcn.org>
2020-07-29 12:50:37 +0800

    google-chrome-dev: auto updated to 86.0.4214.2-1

--- archlinuxcn/google-chrome-dev/update_version.sh
+++ archlinuxcn/google-chrome-dev/update_version.sh
@@ -6,22 +6,24 @@ CHANNEL=$(awk -F '=' '/^_channel/{ print $2 }' PKGBUILD)
 PKG="google-chrome-${CHANNEL}"
 
 # Get latest version
-VER=$(curl -sL "https://dl.google.com/linux/chrome/rpm/stable/x86_64/repodata/other.xml.gz" | gzip -df |
-	tr ' ' '\n' | grep -e name= -e ver= | cut -d '"' -f2 | sed 'N;s/\n/ /' |
-	grep "${PKG}" | cut -d ' ' -f 2)
+VER=$(curl -sSf https://dl.google.com/linux/chrome/deb/dists/stable/main/binary-amd64/Packages |
+	grep -A1 "Package: ${PKG}" |
+	awk '/Version/{print $2}' |
+	cut -d '-' -f1)
 
 # Insert latest version into PKGBUILD and update hashes
 sed -i \
 	-e "s/^pkgver=.*/pkgver=${VER}/" \
 	-e 's/pkgrel=.*/pkgrel=1/' \
 	PKGBUILD
-updpkgsums
 
 # Check whether this changed anything
 if (git diff --exit-code PKGBUILD); then
 	echo "Package ${PKG} has most recent version ${VER}"
 	exit 0
 fi
+
+updpkgsums
 
 # Update .SRCINFO
 makepkg --printsrcinfo >.SRCINFO
