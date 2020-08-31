d2a065462d43bac83820562ddde2089b9915cc7d
Dct Mei <dctxmei@gmail.com>
2020-08-31 19:58:29 +0800

    fix: Check lilac.yaml to match nvchecker v2.0
    1. View commit 04766bc00c02288bee718f428b54c0915ba31a38 .

--- archlinuxcn/okteto/lilac.py
+++ archlinuxcn/okteto/lilac.py
@@ -1,9 +1,10 @@
+#!/usr/bin/python3
+
 from lilaclib import *
 
 def pre_build():
-  update_pkgver_and_pkgrel(_G.newver)
+    update_pkgver_and_pkgrel(_G.newver)
 
 def post_build():
-  git_pkgbuild_commit()
-  update_aur_repo()
-
+    git_pkgbuild_commit()
+    update_aur_repo()
