ff9033c9b04d7a345f15daaa49989da1b3dccfb1
lilac <lilac@build.archlinuxcn.org>
2020-09-11 15:13:54 +0800

    Removing 33 orphaned packages:
    - abiword-gtk2
    - angrysearch
    - assimp-net
    - brother-hl2140
    - cla
    - clup
    - drive
    - glib
    - gnome-search-tool-no-nautilus
    - gottet
    - gradio
    - gtk-theme-arc-git
    - hal-flash-git
    - lumina-desktop
    - monogame-git
    - nant
    - nautilus-data
    - nodejs-tiddlywiki
    - numix-icon-theme-git
    - pacnew-auto-git
    - qarma-git
    - shine
    - silentcast
    - simple-mtpfs
    - stapler-git
    - tchrome
    - vertex-themes
    - wallch
    - xavs
    - xfce4-mpc-plugin-update
    - xfdashboard
    - xinput_calibrator
    - python-pypdf2
    
    closes #1844

--- archlinuxcn/monogame-git/2mgfx.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/sh
-exec /usr/bin/mono $MONO_OPTIONS /usr/lib/monogame/2MGFX.exe "$@"
--- archlinuxcn/numix-icon-theme-git/git.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-mksrcinfo
-git add --all .
-echo "####################################"
-echo "Write your commit comment!"
-echo "####################################"
-read input
-git commit -m "$input"
-git push -u origin master
