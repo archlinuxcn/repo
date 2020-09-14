0337aa684083ea97e4d5eca2f45ce03f3903769a
lilydjwg <lilydjwg@gmail.com>
2020-09-14 17:00:44 +0800

    Revert "Removing 6 orphaned packages that no packages depend on"
    This reverts commit 8f5668943e0abd8b9eec71b995d0f5b5a2d97cf5.
    
    There was a bug in the code that had removed depended packages.

---
+++ archlinuxcn/fsharp/lilac.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+
+from lilaclib import *
+
+def pre_build():
+  aur_pre_build()
+  for l in edit_file('PKGBUILD'):
+    l = l.replace("'msbuild'", "'mono-msbuild'")
+    print(l)
