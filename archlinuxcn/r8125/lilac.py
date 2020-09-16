65333c351060ba52b98c6f43e3435d8464609835
Dct Mei <dctxmei@gmail.com>
2020-09-16 14:41:51 +0800

    r8125: rebuild
    1. Follow the update of the linux package to rebuild.
    2. Specify the version of the linux package through run_cmd.

---
+++ archlinuxcn/r8125/lilac.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+
+from lilaclib import *
+
+def pre_build():
+    run_cmd(['sh', '-c', 'sed \'1,15{/ depends=/d;}\' -i PKGBUILD'])
+    run_cmd(['sh', '-c', 'sed "/makedepends=/i depends=('linux=$(curl -s https://www.archlinux.org/packages/core/x86_64/linux/ | grep version | grep content | awk -F '"' '{print $4}')')" -i PKGBUILD'])
