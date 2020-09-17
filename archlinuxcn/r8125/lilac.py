021a2f4a566523eb68751add16fdda958e9cd0c8
Dct Mei <dctxmei@gmail.com>
2020-09-17 11:26:02 +0800

    r8125: rebuild
    1. Modify the lilac.py file and try to build again.

--- archlinuxcn/r8125/lilac.py
+++ archlinuxcn/r8125/lilac.py
@@ -3,5 +3,6 @@
 from lilaclib import *
 
 def pre_build():
+    aur_pre_build()
     run_cmd(['sh', '-c', 'sed \'1,15{/ depends=/d;}\' -i PKGBUILD'])
     run_cmd(['sh', '-c', 'sed "/makedepends=/i depends=(\'linux=$(curl -s https://www.archlinux.org/packages/core/x86_64/linux/ | grep version | grep content | awk -F \'"\' \'{print $4}\')\')" -i PKGBUILD'])
