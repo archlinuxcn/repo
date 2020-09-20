46b0e3c7f7386ab106e5a84c00322a1ae9a0a55d
Dct Mei <dctxmei@gmail.com>
2020-09-20 12:36:36 +0800

    create_ap: rebuild
    1. Modify the lilac.py file again.

--- archlinuxcn/create_ap/lilac.py
+++ archlinuxcn/create_ap/lilac.py
@@ -7,4 +7,9 @@ def pre_build():
     for line in edit_file('PKGBUILD'):
         if line.startswith('validpgpkeys'):
             line = "validpgpkeys=()"
+        elif line.startswith('source'):
+            line = line + ')'
+        elif line.startswith('sig'):
+            line = '#' + line
         print(line)
+    run_cmd(['updpkgsums'])
