092a01db12113ce794875b53e42234181fc55b9f
Dct Mei <dctxmei@gmail.com>
2020-09-21 14:57:31 +0800

    create_ap: rebuild
    1. I deployed lilac in the virtual machine and tested it, and now it is
       the correct modification of the configuration file.

--- archlinuxcn/create_ap/lilac.py
+++ archlinuxcn/create_ap/lilac.py
@@ -5,11 +5,11 @@ from lilaclib import *
 def pre_build():
     aur_pre_build()
     for line in edit_file('PKGBUILD'):
-        if line.startswith('validpgpkeys'):
-            line = "validpgpkeys=()"
-        elif line.startswith('source'):
-            line = line + ')'
-        elif line.startswith('sig'):
+        if line.startswith('validpgpkeys='):
+            line = '#' + line + '\n' + "validpgpkeys=()"
+        elif line.startswith('source='):
+            line = '#' + line + '\n' + line + ')'
+        elif line.endswith('.sig")'):
             line = '#' + line
         print(line)
     run_cmd(['updpkgsums'])
