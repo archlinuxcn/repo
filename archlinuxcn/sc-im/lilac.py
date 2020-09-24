b1bf6e971603e4b9b028840dd85913d5ff0a6727
lilac <lilac@build.archlinuxcn.org>
2020-09-24 15:08:12 +0800

    fakeroot-tcp, obexftp, openobex, r8168-dkms, sc-im: orphaned, removing. closes #1832

--- archlinuxcn/sc-im/lilac.py
+++
@@ -1,9 +0,0 @@
-from lilaclib import *
-
-def pre_build():
-    aur_pre_build()
-    for line in edit_file('PKGBUILD'):
-        if line.strip().startswith('MAKEFLAGS='):
-            print('')
-        else:
-            print(line)
