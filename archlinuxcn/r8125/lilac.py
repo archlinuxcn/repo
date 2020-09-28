561fc4294debb834bc9cb373b8e7073e0ffa13a0
Dct Mei <dctxmei@gmail.com>
2020-09-28 14:22:39 +0800

    r8125: rebuild
    1. The problem that new packages are older than those in the warehouse
       has been solved.

--- archlinuxcn/r8125/lilac.py
+++ archlinuxcn/r8125/lilac.py
@@ -3,6 +3,10 @@
 from lilaclib import *
 
 def pre_build():
+    oldver, oldrel = get_pkgver_and_pkgrel()
     aur_pre_build()
+    newver, newrel = get_pkgver_and_pkgrel()
+    if oldver == newver:
+        update_pkgrel(rel=int(oldrel + 1))
     run_cmd(['sh', '-c', 'sed \'1,15{/ depends=/d;}\' -i PKGBUILD'])
     run_cmd(['sh', '-c', 'sed "/makedepends=/i depends=(\'linux=$(curl -s https://www.archlinux.org/packages/core/x86_64/linux/ | grep version | grep content | awk -F \'"\' \'{print $4}\')\')" -i PKGBUILD'])
