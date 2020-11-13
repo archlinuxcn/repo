2edca40a2a379d1cf22cdb9cf080c2f03f479a6d
oldherl <oldherl@gmail.com>
2020-11-14 00:10:52 +0800

    xf86-input-libinput-git-oldherl: change git am to git apply

--- archlinuxcn/xf86-input-libinput-git-oldherl/lilac.py
+++ archlinuxcn/xf86-input-libinput-git-oldherl/lilac.py
@@ -39,9 +39,7 @@ def pre_build():
       line = '''
 prepare() {
   cd xf86-input-libinput
-  git config user.name "builduser"
-  git config user.email "builduser@example.com"
-  git am ../0001-scroll-scale.patch
+  git apply ../0001-scroll-scale.patch
 }
 
 ''' + line
