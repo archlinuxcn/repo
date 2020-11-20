d289d96a71e1690affc288524bd2736ece871067
DDoSolitary <DDoSolitary@gmail.com>
2020-11-20 22:48:55 +0800

    libpdfium-nojs: force to call prepare() in pkgver()
    This is required because archlinuxcn/lilac#124 added the --noprepare
    flag when calling makepkg, but this package finds and checks out the
    latest version in prepare() before using pkgver() to print that version.
    
    This is a bit hacky but it works... Probably it's better to revert the
    aforementioned pull request as PKGBUILD's documentation explicitly
    states that pkgver() is called after prepare().

--- archlinuxcn/libpdfium-nojs/lilac.py
+++ archlinuxcn/libpdfium-nojs/lilac.py
@@ -3,7 +3,16 @@
 from lilaclib import *
 
 def pre_build():
-    aur_pre_build(do_vcs_update=True)
+    aur_pre_build()
+    for line in edit_file('PKGBUILD'):
+        if line.startswith('prepare()'):
+            print('_' + line)
+        elif line.startswith('pkgver()'):
+            print(line)
+            print('  _prepare 1>&2')
+        else:
+            print(line)
+    vcs_update()
 
 def post_build():
     aur_post_build()
