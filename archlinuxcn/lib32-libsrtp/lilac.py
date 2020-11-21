94da519c03460babe59de8d07fd56f5152468c70
DDoSolitary <DDoSolitary@gmail.com>
2020-11-21 15:46:51 +0800

    lib32-libsrtp: remove the pkgver function

--- archlinuxcn/lib32-libsrtp/lilac.py
+++ archlinuxcn/lib32-libsrtp/lilac.py
@@ -4,6 +4,11 @@ from lilaclib import *
 
 def pre_build():
     aur_pre_build()
+    for line in edit_file('PKGBUILD'):
+        if line.startswith('pkgver()'):
+            print('_' + line)
+        else:
+            print(line)
     add_provides(['libsrtp2.so'])
 
 def post_build():
