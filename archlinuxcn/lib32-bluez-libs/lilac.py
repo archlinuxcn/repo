fef933f3ad73d9f5f87b2bc9ab3ab4e8f04f30c4
DDoSolitary <DDoSolitary@gmail.com>
2020-11-22 21:04:40 +0800

    lib32-bluez-libs: fix incorrect soname provides for split packages

--- archlinuxcn/lib32-bluez-libs/lilac.py
+++ archlinuxcn/lib32-bluez-libs/lilac.py
@@ -4,7 +4,10 @@ from lilaclib import *
 
 def pre_build():
     aur_pre_build()
-    add_provides(['libbluetooth.so'])
+    for line in edit_file('PKGBUILD'):
+        print(line)
+        if line.startswith('package_lib32-bluez-libs()'):
+            print('  provides=("libbluetooth.so")')
 
 def post_build():
     aur_post_build()
