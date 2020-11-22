1627feeca3b0a36001a8bf2cc5b0fc5aa12b793e
DDoSolitary <DDoSolitary@gmail.com>
2020-11-22 21:22:50 +0800

    lib32-libwmf: fix add_provides and add_depends

--- archlinuxcn/lib32-libwmf/lilac.py
+++ archlinuxcn/lib32-libwmf/lilac.py
@@ -4,8 +4,8 @@ from lilaclib import *
 
 def pre_build():
     aur_pre_build()
-    add_depends(['libfreetype.so', 'libglib-2.0.so', 'libgobject-2.0.so'])
-    add_provides(['libwmf.so', 'libwmflite.so'])
+    add_depends(['lib32-libx11', 'libfreetype.so', 'libglib-2.0.so', 'libgobject-2.0.so'])
+    add_provides(['libwmf-0.2.so', 'libwmflite-0.2.so'])
 
 def post_build():
     aur_post_build()
