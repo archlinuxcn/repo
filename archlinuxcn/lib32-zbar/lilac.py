c3de072607eaaf56cd1d53f2b0dfabb31ece8712
DDoSolitary <DDoSolitary@gmail.com>
2020-11-24 00:45:38 +0800

    addpkg: lib32-zbar

---
+++ archlinuxcn/lib32-zbar/lilac.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+
+from lilaclib import *
+
+def pre_build():
+    aur_pre_build()
+    add_depends(['libdbus-1.so', 'libjpeg.so'])
+    add_provides(['libzbar.so'])
+
+def post_build():
+    aur_post_build()
