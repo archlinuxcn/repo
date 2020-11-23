60ae21d48375aa0f08255db50980c5b9095c83b7
DDoSolitary <DDoSolitary@gmail.com>
2020-11-23 12:43:46 +0800

    lib32-libraw: remove automake-1.15 makedepends

--- archlinuxcn/lib32-libraw/lilac.py
+++ archlinuxcn/lib32-libraw/lilac.py
@@ -6,6 +6,9 @@ def pre_build():
     aur_pre_build()
     add_depends(['libjasper.so', 'libjpeg.so'])
     add_provides(['libraw.so', 'libraw_r.so'])
+    for line in edit_file('PKGBUILD'):
+        if not line.startswith('makedepends='):
+            print(line)
 
 def post_build():
     aur_post_build()
