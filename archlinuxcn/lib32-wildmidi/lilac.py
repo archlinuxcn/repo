9bc5fb91939996d306811189fbff916ea8b7cb23
DDoSolitary <DDoSolitary@gmail.com>
2020-11-26 22:29:43 +0800

    Revert "lib32-wildmidi: fix wrong path in wildmidi.pc"
    This reverts commit 97c92326762a68d0626a840e1912034519bab608.
    
    Fixed in AUR.

--- archlinuxcn/lib32-wildmidi/lilac.py
+++ archlinuxcn/lib32-wildmidi/lilac.py
@@ -6,12 +6,6 @@ def pre_build():
     aur_pre_build()
     add_depends(['libasound.so'])
     add_provides(['libWildMidi.so'])
-    for line in edit_file('PKGBUILD'):
-        stripped_line = line.strip()
-        if stripped_line != 'mv lib lib32':
-            print(line)
-        if stripped_line.startswith('cmake '):
-            print('        -DLIB_SUFFIX=32 \\')
 
 def post_build():
     aur_post_build()
