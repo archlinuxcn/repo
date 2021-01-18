7f22f1d26338a7f20fab96eed47667de71905c66
a-wing <1@233.email>
2021-01-18 21:32:09 +0800

    iredis: refactor

--- archlinuxcn/iredis/lilac.py
+++ archlinuxcn/iredis/lilac.py
@@ -1,9 +1,12 @@
+#!/usr/bin/env python3
+
 from lilaclib import *
 
 def pre_build():
-  update_pkgver_and_pkgrel(_G.newver)
+    pypi_pre_build(depends = [], depends_setuptools = False)
 
 def post_build():
-  git_add_files('PKGBUILD')
-  git_commit()
+    git_add_files('PKGBUILD')
+    git_commit()
+    update_aur_repo()
 
