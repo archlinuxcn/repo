39d663084fb36b58cc47e48164c95b51f615abd3
DDoSolitary <DDoSolitary@gmail.com>
2021-01-18 12:44:18 +0800

    lib32-zbar: remove PKGBUILD patching
    Fixed by AUR maintainer.

--- archlinuxcn/lib32-zbar/lilac.py
+++ archlinuxcn/lib32-zbar/lilac.py
@@ -6,14 +6,6 @@ def pre_build():
     aur_pre_build()
     add_depends(['libdbus-1.so', 'libjpeg.so'])
     add_provides(['libzbar.so'])
-    pattern = re.compile("['\"]?lib32-imagemagick['\"]? ?")
-    for line in edit_file('PKGBUILD'):
-        if line.startswith('depends='):
-            print(pattern.sub('', line))
-        elif line.strip().startswith('./configure '):
-            print(line + ' --without-imagemagick')
-        else:
-            print(line)
 
 
 def post_build():
