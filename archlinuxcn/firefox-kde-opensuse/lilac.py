fb81ad96a7cd34439e627a77356bfa85719edaf5
Jiachen YANG <farseerfc@gmail.com>
2021-01-22 09:19:27 +0900

    firefox-kde-opensuse: disable pgo for now

--- archlinuxcn/firefox-kde-opensuse/lilac.py
+++ archlinuxcn/firefox-kde-opensuse/lilac.py
@@ -11,6 +11,11 @@ def pre_build():
   newver, newrel = get_pkgver_and_pkgrel()
   if oldver == newver:
     update_pkgrel(rel=int(oldrel + 1))
+  for line in edit_file('PKGBUILD'):
+      if line.startswith('_pgo='):
+          line = '_pgo=false'
+      else:
+          print(line)
 
 if __name__ == '__main__':
   single_main(build_prefix)
