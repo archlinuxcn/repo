9d2333f43e5a4592d2cb7bf17637c2133b3d484b
Winston Wu <winstonwu91@gmail.com>
2021-01-29 04:01:32 +0800

    Update wine-valve

--- archlinuxcn/wine-valve/lilac.py
+++ archlinuxcn/wine-valve/lilac.py
@@ -2,8 +2,11 @@ from lilaclib import *
 
 def pre_build():
   newver = _G.newver.removeprefix('experimental-wine-')
-  newver = newver.removesuffix("-shmem-20201218c")
-  newver = newver.replace("-",".")
+  for line in edit_file('PKGBUILD'):
+      if line.startswith('_pkgver'):
+          line = "_pkgver='" + newver + "'"
+      print(line)
+  newver = newver.replace("-",".") 
   update_pkgver_and_pkgrel(newver)
 
 def post_build():
