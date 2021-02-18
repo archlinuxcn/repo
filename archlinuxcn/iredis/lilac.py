2a0f378a114f89284bf020e7cb9d9249d6c31316
a-wing <1@233.email>
2021-02-18 10:02:10 +0800

    iredis: fix depends on lilac.py

--- archlinuxcn/iredis/lilac.py
+++ archlinuxcn/iredis/lilac.py
@@ -3,7 +3,18 @@
 from lilaclib import *
 
 def pre_build():
-    pypi_pre_build(depends_setuptools = False)
+    pypi_pre_build(depends = [
+        'python',
+        'python-redis',
+        'python-prompt_toolkit',
+        'python-pygments',
+        'python-mistune',
+        'python-configobj',
+        'python-click',
+        'python-pendulum',
+        'python-importlib_resources',
+        'python-wcwidth',
+        ], depends_setuptools = False)
 
 def post_build():
     git_add_files('PKGBUILD')
