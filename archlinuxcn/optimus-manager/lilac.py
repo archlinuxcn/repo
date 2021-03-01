77c7e73f814f5a02f32b2d3e1b975e5f8f809a1f
Outvi V <git@outv.im>
2021-03-01 10:51:53 +0800

    feat: optimus-manager@1.4-1 with fixes

--- archlinuxcn/optimus-manager/lilac.py
+++ archlinuxcn/optimus-manager/lilac.py
@@ -2,14 +2,18 @@
 
 from lilaclib import *
 
+
 def pre_build():
     aur_pre_build()
     pattern = re.compile("['\"]?python-setuptools['\"]? ?")
     for line in edit_file('PKGBUILD'):
         if line.startswith('depends='):
             print(pattern.sub('', line))
+        elif "xsetup-intel.sh" in line:
+            print("")
         else:
             print(line)
+
 
 def post_build():
     aur_post_build()
