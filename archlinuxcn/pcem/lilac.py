e4303e076cc7bfc87bde675ec714766793095a1d
Winston Wu <winstonwu91@gmail.com>
2021-03-26 03:06:45 +0800

    [pcem] Use lilac to replace wxgtk2 to wxgtk3

---
+++ archlinuxcn/pcem/lilac.py
@@ -0,0 +1,9 @@
+from lilaclib import aur_pre_build, edit_file
+
+def pre_build():
+    aur_pre_build('pcem')
+
+    for line in edit_file('PKGBUILD'):
+        if line.startswith('depends'):
+            line = line.replace('wxgtk2', 'wxgtk3')
+        print(line)
