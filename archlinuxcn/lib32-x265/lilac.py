0a2fac793d239b12ee4f04ba155a5fe6b4c4e137
DDoSolitary <DDoSolitary@gmail.com>
2021-03-31 23:54:37 +0800

    lib32-x265: fix so library not installed

--- archlinuxcn/lib32-x265/lilac.py
+++ archlinuxcn/lib32-x265/lilac.py
@@ -1,10 +1,12 @@
 #!/usr/bin/env python3
 
+import re
 from lilaclib import *
 
 def pre_build():
     aur_pre_build()
     add_depends(['libnuma.so'])
+    add_makedepends(['git'])
 
 def post_build():
     aur_post_build()
