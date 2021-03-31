86d379780a17fd3c1d05dfe9ec917e6c5d5e8220
Dct Mei <dctxmei@yandex.com>
2021-03-31 10:45:56 +0800

    surface-control: rebuild
    issue #2151

--- archlinuxcn/surface-control/lilac.py
+++ archlinuxcn/surface-control/lilac.py
@@ -6,5 +6,5 @@ def pre_build():
     aur_pre_build()
     for line in edit_file('PKGBUILD'):
         if line.startswith('makedepends='):
-            line = '#' + line + '\n' + "makedepends=('git' 'rust')"
+            line = '#' + line + '\n' + "makedepends=('cargo' 'git' 'rust' 'systemd')"
         print(line)
