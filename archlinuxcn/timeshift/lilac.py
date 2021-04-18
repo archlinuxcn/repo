c0b9f74efdba75c009fc695ddc88eb944456f44b
DuckSoft <realducksoft@gmail.com>
2021-04-19 07:35:50 +0800

    timeshift: add depends

--- archlinuxcn/timeshift/lilac.py
+++ archlinuxcn/timeshift/lilac.py
@@ -5,6 +5,9 @@
 #build_prefix = 'extra-x86_64'
 #pre_build = aur_pre_build
 #post_build = aur_post_build
+def post_build():
+  add_depends(('which',))
+  aur_post_build()
 
 #if __name__ == '__main__':
 #  single_main()
