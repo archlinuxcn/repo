ec323f1e90334b44544ed54184b6b697d854c080
edward-p <edward@edward-p.xyz>
2021-05-08 11:59:39 +0800

    proxmark3-iceman-git: update_aur_repo after commit

--- archlinuxcn/proxmark3-iceman-git/lilac.py
+++ archlinuxcn/proxmark3-iceman-git/lilac.py
@@ -8,9 +8,9 @@
 from lilaclib import *
 
 def post_build():
-    update_aur_repo()
     git_add_files("PKGBUILD")
     git_commit()
+    update_aur_repo()
 
 #if __name__ == '__main__':
 #  single_main()
