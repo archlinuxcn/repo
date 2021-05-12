508b499320061717518ff6bd97457680b38db994
lilac <lilac@build.archlinuxcn.org>
2021-05-13 04:48:36 +0800

    thunderbird-appmenu: auto updated to 78.10.1-1

--- archlinuxcn/thunderbird-appmenu/trigger.sh
+++ archlinuxcn/thunderbird-appmenu/trigger.sh
@@ -1,16 +1,17 @@
+ver="$(curl https://releases.mozilla.org/pub/thunderbird/releases/ | sed -rn 's/([^0-9]*)([0-9]*\.[0-9]*?(\.[0-9]*)).*/\2/p' | sort -V | tail -n1)"
+sed -r "s/(pkgver=)(.*)/\1$ver/" -i PKGBUILD
 makepkg --printsrcinfo > .SRCINFO
 
-ver="$(grep pkgver < .SRCINFO | sed 's/.*pkgver = //g')"
 ver_msg="autohook $ver"
-
 git commit -am "$ver_msg"
 git push
+
 (
   [ -e 'home:nicman23/thunderbird-appmenu-bin/' ] || osc co home:nicman23 thunderbird-appmenu-bin
   sed "s/PKGVER/${ver}/g" _service \
    > home:nicman23/thunderbird-appmenu-bin/_service
   cd home:nicman23/thunderbird-appmenu-bin/
-  osc ci -m $ver_msg
+  osc commit -m $ver_msg
 )
 
 [ -e thunderbird-appmenu-bin ] || git clone ssh://aur@aur.archlinux.org/thunderbird-appmenu-bin.git
