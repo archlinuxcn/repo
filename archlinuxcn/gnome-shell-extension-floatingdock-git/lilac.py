9675431469621d27af020da2130c166c31a3988d
Hu Butui <hot123tea123@gmail.com>
2021-05-17 20:50:06 +0800

    addpkg: gnome-shell-extension-floatingdock-git

---
+++ archlinuxcn/gnome-shell-extension-floatingdock-git/lilac.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+
+from lilaclib import *
+
+
+def pre_build():
+    update_pkgrel()
+    vcs_update()
+
+
+def post_build():
+    git_pkgbuild_commit()
+    update_aur_repo()
+# vim:set ts=2 sw=2 et:
+
