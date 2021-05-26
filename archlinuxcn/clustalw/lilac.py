b4bcab7c9815eb907737bded1038b80071622f80
Hu Butui <hot123tea123@gmail.com>
2021-05-26 23:07:40 +0800

    addpkg: clustalw

---
+++ archlinuxcn/clustalw/lilac.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+
+from lilaclib import *
+
+
+def pre_build():
+  update_pkgver_and_pkgrel(_G.newver.lstrip('v'))
+
+def post_build():
+  git_pkgbuild_commit()
+# vim:set ts=2 sw=2 et:
+
