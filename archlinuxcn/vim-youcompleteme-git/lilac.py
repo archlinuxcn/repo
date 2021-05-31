3e57ec36523d2bedbaca63f714a02bbd57a74814
Coelacanthus <CoelacanthusHex@gmail.com>
2021-05-31 22:04:25 +0800

    feat(vim-youcompleteme-git): using system clang
    [1]: https://aur.archlinux.org/cgit/aur.git/commit/?h=vim-youcompleteme-git&id=88d59671e2511f8bda3a5e812fd5009c5a95511c

--- archlinuxcn/vim-youcompleteme-git/lilac.py
+++ archlinuxcn/vim-youcompleteme-git/lilac.py
@@ -5,3 +5,4 @@ from lilaclib import *
 def pre_build():
     aur_pre_build()
     run_cmd(['sh', '-c', 'sed \'/vim-plugins/d\' -i PKGBUILD'])
+    run_cmd(['sh', '-c', 'sed \'s/_use_system_clang="OFF"/_use_system_clang="ON"/\' -i PKGBUILD'])
