201f8e460533cc2237bfb7d72b8112e24b3ab411
Hu Butui <hot123tea123@gmail.com>
2021-06-02 21:12:08 +0800

    r-biostrings: update lilac.py and it's deps' lilac.py

--- archlinuxcn/r-zlibbioc/lilac.py
+++ archlinuxcn/r-zlibbioc/lilac.py
@@ -5,7 +5,7 @@ def pre_build():
     for line in edit_file('PKGBUILD'):
         if line.startswith('_pkgver='):
             line = f'_pkgver={_G.newver}'
-            print(line)
+        print(line)
     update_pkgver_and_pkgrel(_G.newver.replace(':', '.').replace('-', '.'))
 
 def post_build():
