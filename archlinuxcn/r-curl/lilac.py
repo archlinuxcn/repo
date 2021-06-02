ede6344cc060bfdfe67fe840e726903f5a886fab
Hu Butui <hot123tea123@gmail.com>
2021-06-02 23:59:02 +0800

    r-curl: update lilac.yaml and lilac.py

--- archlinuxcn/r-curl/lilac.py
+++ archlinuxcn/r-curl/lilac.py
@@ -1,13 +1,11 @@
 #!/usr/bin/env python3
-
 from lilaclib import *
-
 
 def pre_build():
     for line in edit_file('PKGBUILD'):
         if line.startswith('_pkgver='):
             line = f'_pkgver={_G.newver}'
-            print(line)
+        print(line)
     update_pkgver_and_pkgrel(_G.newver.replace(':', '.').replace('-', '.'))
 
 def post_build():
