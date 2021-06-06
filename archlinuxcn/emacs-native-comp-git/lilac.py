504c0d30004d8756480b334a75ec749f7ab80920
yk <yk@archlinuxcn.org>
2021-06-06 23:01:33 +0800

    fix: emacs-native-comp-git, install script path

--- archlinuxcn/emacs-native-comp-git/lilac.py
+++ archlinuxcn/emacs-native-comp-git/lilac.py
@@ -21,6 +21,8 @@ def pre_build():
             line = 'JIT="YES"'
         if line.startswith('AOT='):
             line = 'AOT="YES"'
+        if line.startswith('install='):
+            line = 'emacs-git.install'
         if line.startswith('source='):
             line = 'source=("emacs-git::git://github.com/emacs-mirror/emacs.git")'
         print(line)
