cba7ed53d9d38e5447c45f13b5a735dbeae923db
Hu Butui <hot123tea123@gmail.com>
2021-06-09 09:49:33 +0800

    mafft: update lilac.py

--- archlinuxcn/mafft/lilac.py
+++ archlinuxcn/mafft/lilac.py
@@ -2,7 +2,7 @@
 from lilaclib import *
 
 def pre_build():
-    update_pkgver_and_pkgrel(_G.newver())
+    update_pkgver_and_pkgrel(_G.newver.lstrip('v'))
 
 def post_build():
     git_pkgbuild_commit()
