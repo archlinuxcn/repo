186334787cb3dbfe0ceac51511842b421d9eb606
lilydjwg <lilydjwg@gmail.com>
2021-06-11 13:37:13 +0800

    pacman-lily: remove patch that upstream now has

--- archlinuxcn/pacman-lily/lilac.py
+++ archlinuxcn/pacman-lily/lilac.py
@@ -26,7 +26,7 @@ def pre_build():
     elif line.startswith('source=('):
       line = line.replace('(', '(pacman.patch\n        pacman-syncdb\n        ')
     elif line.startswith('sha256sums=('):
-      line = line.replace('(', '(a4ebcc619f88b1b3067d9ac8c1e6dab8963d50980c15e0b366ae221be94be26a\n            207d5cee261bba18e650bbd2c249ffd8fe9c1dbd7de6b241d8bf011848faa70b\n            ')
+      line = line.replace('(', '(61e0b85dadb341ea14e5aa28b0b1edf98871bc3dffd1083fb5775fc28a65e113\n            207d5cee261bba18e650bbd2c249ffd8fe9c1dbd7de6b241d8bf011848faa70b\n            ')
     elif line.startswith('package() '):
       package = True
     elif package and line.startswith('}'):
