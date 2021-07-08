7412e8d1794574895760eda4cca06b247ea6fdd9
DDoSolitary <DDoSolitary@gmail.com>
2021-07-08 13:39:57 +0800

    lib32-rtmpdump: add library dependencies

--- archlinuxcn/lib32-rtmpdump/lilac.py
+++ archlinuxcn/lib32-rtmpdump/lilac.py
@@ -4,7 +4,7 @@ from lilaclib import *
 
 def pre_build():
     aur_pre_build(maintainers='rodrigo21')
-    add_depends(['libhogweed.so', 'libnettle.so'])
+    add_depends(['libhogweed.so', 'libnettle.so', 'libgnutls.so', 'libz.so'])
     add_provides(['librtmp.so'])
 
 def post_build():
