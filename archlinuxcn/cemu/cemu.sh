5015bf73d49a4452fabcb232643cc16649c4e0a1
lilac <lilac@build.archlinuxcn.org>
2021-08-07 13:42:31 +0800

    cemu: auto updated to 1.25.1-2

--- archlinuxcn/cemu/cemu.sh
+++ archlinuxcn/cemu/cemu.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 export WINEPREFIX="$HOME/.cemu/wine"
-export WINEDLLOVERRIDES="mscoree=;mshtml=;dbghelp.dll=n,b"
+export WINEDLLOVERRIDES="mscoree=;mshtml=;cemuhook.dll=n,b"
 
 # Allows to play BotW with Polaris video cards
 export R600_DEBUG="nohyperz"
@@ -8,12 +8,17 @@ export R600_DEBUG="nohyperz"
 if [ ! -d "$HOME"/.cemu ] ; then
   mkdir -p "$HOME"/.cemu/wine || exit 1
   ln -s /usr/share/cemu/Cemu.exe "$HOME"/.cemu/Cemu.exe || exit 1
-  ln -s /usr/share/cemu/dbghelp.dll "$HOME"/.cemu/dbghelp.dll || exit 1
+  ln -s /usr/share/cemu/cemuhook.dll "$HOME"/.cemu/cemuhook.dll || exit 1
   ln -s /usr/share/cemu/keystone.dll "$HOME"/.cemu/keystone.dll || exit 1
   ln -s /usr/share/cemu/sharedFonts "$HOME"/.cemu/sharedFonts || exit 1
   cp -r /usr/share/cemu/gameProfiles "$HOME"/.cemu/ || exit 1
   cp -r /usr/share/cemu/mlc01 "$HOME"/.cemu/ || exit 1
   cp -r /usr/share/cemu/shaderCache "$HOME"/.cemu/ || exit 1
+fi
+
+if [ -d "$HOME"/.cemu ] && [ -L "$HOME"/.cemu/dbghelp.dll ] ; then
+  rm "$HOME"/.cemu/dbghelp.dll
+  ln -s /usr/share/cemu/cemuhook.dll "$HOME"/.cemu/cemuhook.dll || exit 1
 fi
 
 if [ ! -L "$HOME"/.cemu/sharedFonts ] && [ ! -d "$HOME"/.cemu/sharedFonts ] ; then
@@ -26,9 +31,10 @@ fi
 
 if [ ! -f "$HOME"/.cemu/wine/drive_c/windows/syswow64/vcruntime140.dll ]; then
   if [ -n "`whereis zenity|grep bin`" ]; then
-    zenity --info  --title 'Cemu' --text 'Installing wine dependencies.\n\nThe process may take a few minutes'
+    winetricks arch=32 -q vcrun2017 | zenity --title 'Cemu' --text 'Installing wine dependencies.\n\nThe process may take a few minutes' --progress --pulsate --no-cancel --auto-close
+  else
+    winetricks arch=32 -q vcrun2017
   fi
-  winetricks -q vcrun2017
   winetricks settings win7
 fi
 cd ~/.cemu
