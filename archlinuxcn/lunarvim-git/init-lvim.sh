d4e6f95c4c6b7860e41611ea12cd0a6916ff0e4e
edward-p <edward@edward-p.xyz>
2021-08-09 01:32:52 +0800

    lunarvim-git: using prebuild treesitter parsers

--- archlinuxcn/lunarvim-git/init-lvim.sh
+++ archlinuxcn/lunarvim-git/init-lvim.sh
@@ -1,12 +1,19 @@
 #!/bin/bash
 mkdir -p ~/.config/lvim
-cp /usr/share/doc/lunarvim/config.example.lua ~/.config/lvim/config.lua
+ln -s /usr/share/doc/lunarvim/config.example-no-ts.lua ~/.config/lvim/config.lua
 echo "Installing Packer..."
 git clone https://github.com/wbthomason/packer.nvim ~/.local/share/lunarvim/site/pack/packer/start/packer.nvim
 echo "PackerInstall..."
 lvim --headless +'autocmd User PackerComplete sleep 100m | qall' +PackerInstall
 echo "PackerSync..."
 lvim --headless +'autocmd User PackerComplete sleep 100m | qall' +PackerSync
+rm ~/.config/lvim/config.lua
+cp /usr/share/doc/lunarvim/config.example.lua ~/.config/lvim/config.lua
+echo "Installing treesitter parsers.."
+ln -s /usr/share/lunarvim/prebuild/nvim-treesitter/parser/* \
+	~/.local/share/lunarvim/site/pack/packer/start/nvim-treesitter/parser/
+ln -s /usr/share/lunarvim/prebuild/nvim-treesitter/parser-info/* \
+	~/.local/share/lunarvim/site/pack/packer/start/nvim-treesitter/parser-info/
 echo
 echo "lunarvim runtime is inited for $(whoami)"
 echo "clean up by:"
