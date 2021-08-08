c365eac3eb9d1c0fae789a0618b386908ed59f9d
edward-p <edward@edward-p.xyz>
2021-08-08 21:02:07 +0800

    lunarvim-git: fix init-lvim.sh

--- archlinuxcn/lunarvim-git/init-lvim.sh
+++ archlinuxcn/lunarvim-git/init-lvim.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
 mkdir -p ~/.config/lvim
 cp /usr/share/doc/lunarvim/config.example.lua ~/.config/lvim/config.lua
+echo "Installing Packer..."
+git clone https://github.com/wbthomason/packer.nvim ~/.local/share/lunarvim/site/pack/packer/start/packer.nvim
+echo "PackerInstall..."
 lvim --headless +'autocmd User PackerComplete sleep 100m | qall' +PackerInstall
+echo "PackerSync..."
 lvim --headless +'autocmd User PackerComplete sleep 100m | qall' +PackerSync
 echo
 echo "lunarvim runtime is inited for $(whoami)"
