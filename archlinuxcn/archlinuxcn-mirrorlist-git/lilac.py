6b166b753fb7195fcdb0ab4ccf69a16ade8a8947
lilydjwg <lilydjwg@gmail.com>
2021-08-19 18:42:20 +0800

    archlinuxcn-mirrorlist-git: take maintainership
    See #2431.

--- archlinuxcn/archlinuxcn-mirrorlist-git/lilac.py
+++ archlinuxcn/archlinuxcn-mirrorlist-git/lilac.py
@@ -1,14 +1,8 @@
 from lilaclib import *
 
-build_prefix = 'extra-x86_64'
-
 def pre_build():
-  run_cmd(["rm", "-rf", "mirrorlist-repo"])
   vcs_update()
 
 def post_build():
   git_add_files('PKGBUILD')
   git_commit()
-
-if __name__ == '__main__':
-  single_main(build_prefix)
