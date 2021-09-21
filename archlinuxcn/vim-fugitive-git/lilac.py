23c8dc0ebc94c491ff8642ac5f95efd924391d17
DDoSolitary <DDoSolitary@gmail.com>
2021-09-21 12:27:43 +0800

    vim-fugitive-git: fix git describe

--- archlinuxcn/vim-fugitive-git/lilac.py
+++ archlinuxcn/vim-fugitive-git/lilac.py
@@ -5,6 +5,7 @@ from lilaclib import *
 def pre_build():
     aur_pre_build(maintainers='xythrez')
     run_cmd(['sed', '-i', '/vim-plugins/d', 'PKGBUILD'])
+    run_cmd(['sed', '-i', "s/git describe/\\0 --match 'v[0-9]*'/", 'PKGBUILD'])
 
 def post_build():
     aur_post_build()
