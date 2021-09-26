94b14d73f07269288298f01d576a91985b6ee9b6
lilydjwg <lilydjwg@gmail.com>
2021-09-26 12:56:59 +0800

    pytohn-mwclient: update depends

--- archlinuxcn/python-mwclient/lilac.py
+++ archlinuxcn/python-mwclient/lilac.py
@@ -1,7 +1,7 @@
 from lilaclib import *
 
 def pre_build():
-  pypi_pre_build(depends=['python-requests-oauthlib'])
+  pypi_pre_build(depends=['python-requests-oauthlib', 'python-six'])
 
 def post_build():
   pypi_post_build()
