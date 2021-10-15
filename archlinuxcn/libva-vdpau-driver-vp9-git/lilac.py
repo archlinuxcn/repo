68d371e30c7acc6c544f4be45ed983c70fea51c2
DDoSolitary <DDoSolitary@gmail.com>
2021-10-15 23:57:15 +0800

    libva-vdpau-driver-vp9-git: fix maintainer list

--- archlinuxcn/libva-vdpau-driver-vp9-git/lilac.py
+++ archlinuxcn/libva-vdpau-driver-vp9-git/lilac.py
@@ -3,7 +3,7 @@
 from lilaclib import *
 
 def pre_build():
-    aur_pre_build(maintainers='Terence')
+    aur_pre_build(maintainers=['Terence', 'xuanruiqi'])
     add_makedepends(['git'])
 
 def post_build():
