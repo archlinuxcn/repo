6837a987ed1b93e4781ac0026c43779a543cb4f1
DDoSolitary <DDoSolitary@gmail.com>
2021-10-24 22:36:37 +0800

    lib32-gssdp: fix soname provides

--- archlinuxcn/lib32-gssdp/lilac.py
+++ archlinuxcn/lib32-gssdp/lilac.py
@@ -5,7 +5,7 @@ from lilaclib import *
 def pre_build():
     aur_pre_build(maintainers='rodrigo21')
     add_depends(['libgio-2.0.so', 'libglib-2.0.so', 'libgobject-2.0.so'])
-    add_provides(['libgssdp-${pkgver%.*}.so'])
+    add_provides(['libgssdp-1.2.so'])
 
 def post_build():
     check_library_provides()
