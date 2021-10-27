bb08ee16b3b805b39d090c74c97d1062f1fec062
DDoSolitary <DDoSolitary@gmail.com>
2021-10-27 13:51:47 +0800

    lib32-x264: fix compatibility with newer x264

--- archlinuxcn/lib32-x264/lilac.py
+++ archlinuxcn/lib32-x264/lilac.py
@@ -5,6 +5,8 @@ from lilaclib import *
 def pre_build():
     aur_pre_build(maintainers='oxalin')
     add_depends(['liblsmash.so'])
+    for line in edit_file('PKGBUILD'):
+        print(line.replace('x264=', 'x264>='))
 
 def post_build():
     check_library_provides()
