b37401ec78fe43a8a6f09b95ed0e9a3763ac7e52
lilac <lilac@build.archlinuxcn.org>
2021-11-23 04:56:29 +0800

    thunderbird-appmenu: auto updated to 91.3.2-5

--- archlinuxcn/thunderbird-appmenu/trigger.sh
+++ archlinuxcn/thunderbird-appmenu/trigger.sh
@@ -1,26 +1,29 @@
 cd "$(dirname "$0")"
-ver="$(curl https://releases.mozilla.org/pub/thunderbird/releases/ | sed -rn 's/([^0-9]*)([0-9]*\.[0-9]*?(\.[0-9]*)).*/\2/p' | sort -V | tail -n1)"
+
+eval $(cat PKGBUILD| grep -P '^_pkgname=')
+eval $(cat PKGBUILD| grep -P '^pkgrel=')
+
+ver="$(curl https://releases.mozilla.org/pub/${_pkgname}/releases/ | sed -rn 's/([^0-9]*)([0-9]*\.[0-9]*?(\.[0-9]*)).*/\2/p' | sort -V | tail -n1)"
 #ver=91.0
 sed -r "s/(pkgver=)(.*)/\1$ver/" -i PKGBUILD
-makepkg --printsrcinfo > .SRCINFO
-eval $(cat PKGBUILD| grep pkgrel=)
 
+makepkg --printsrcinfo > .SRCINFO
 ver_msg="autohook $ver"
 git commit -am "$ver_msg"
 git push
 
 (
-  [ -e 'home:nicman23/thunderbird-appmenu-bin/' ] || osc co home:nicman23 thunderbird-appmenu-bin
+  [ -e 'home:nicman23/${_pkgname}-appmenu-bin/' ] || osc co home:nicman23 ${_pkgname}-appmenu-bin
   sed "s/PKGVER/${ver}/g" _service \
-   > home:nicman23/thunderbird-appmenu-bin/_service
-  cd home:nicman23/thunderbird-appmenu-bin/
+   > home:nicman23/${_pkgname}-appmenu-bin/_service
+  cd home:nicman23/${_pkgname}-appmenu-bin/
   osc commit -m "$ver_msg"
   osc results -w
 )
 
 sleep 30m
-[ -e thunderbird-appmenu-bin ] || git clone ssh://aur@aur.archlinux.org/thunderbird-appmenu-bin.git
-cd thunderbird-appmenu-bin
+[ -e ${_pkgname}-appmenu-bin ] || git clone ssh://aur@aur.archlinux.org/${_pkgname}-appmenu-bin.git
+cd ${_pkgname}-appmenu-bin
 sed "s/^pkgver=.*/pkgver=${ver}/g" -i PKGBUILD
 sed "s/^pkgrel=.*/pkgrel=${pkgrel}/g" -i PKGBUILD
 
