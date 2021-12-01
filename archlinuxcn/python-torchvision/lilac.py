43f767aea1773a8ac260110544b71f7d3f95dfeb
Hu Butui <hot123tea123@gmail.com>
2021-12-01 22:44:57 +0800

    python-torchvision: rebuild and remove check, close #2564

--- archlinuxcn/python-torchvision/lilac.py
+++ archlinuxcn/python-torchvision/lilac.py
@@ -2,14 +2,12 @@
 
 from lilaclib import *
 
-makepkg_args = ['--nocheck']
 
 def pre_build():
   update_pkgver_and_pkgrel(_G.newver.lstrip('v'))
 
 def post_build():
-  git_add_files('PKGBUILD')
-  git_commit()
+  git_pkgbuild_commit()
   update_aur_repo()
 # vim:set ts=2 sw=2 et:
 
