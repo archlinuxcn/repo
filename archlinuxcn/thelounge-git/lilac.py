4ab132e322b37b8adf1d0e42de9a52d3915e4f5d
yk <yk@archlinuxcn.org>
2021-12-04 23:16:34 +0800

    fix: thelounge-git, pkgver

---
+++ archlinuxcn/thelounge-git/lilac.py
@@ -0,0 +1,18 @@
+# Trimmed lilac.py
+#!/usr/bin/env python3
+#
+# This file is the most simple lilac.py file,
+# and it suits for most packages in AUR.
+#
+
+from lilaclib import *
+
+def pre_build():
+    aur_pre_build(maintainers=['S13ntist'])
+    for line in edit_file('PKGBUILD'):
+        if "git describe --long --tags" in line:
+            print('    printf "%s.%s" "$(git describe --tags --long | cut -d- -f1)" "$(git rev-list --count HEAD)"')
+        print(line)
+
+#if __name__ == '__main__':
+#  single_main()
