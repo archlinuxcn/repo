3363f4de9a12a1a73811234b54c1870c5a421603
yk <yk@archlinuxcn.org>
2021-12-05 11:25:25 +0800

    cleanup: emacs-git&emacs-native-comp-git

--- archlinuxcn/emacs-native-comp-git/lilac.py
+++ archlinuxcn/emacs-native-comp-git/lilac.py
@@ -1,16 +1,8 @@
-# Trimmed lilac.py
 #!/usr/bin/env python3
-#
-# This file is the most simple lilac.py file,
-# and it suits for most packages in AUR.
-#
 
 from lilaclib import *
 
-#build_prefix = 'archlinuxcn-x86_64'
-
 def pre_build():
-    # run_cmd(["rm", "-rf", "emacs-git"])
     aur_pre_build('emacs-git', maintainers=['vorbote'])
     for line in edit_file('PKGBUILD'):
         if line.startswith('pkgname='):
@@ -26,9 +18,3 @@ def pre_build():
         if line.startswith('source='):
             line = 'source=("emacs-git::git://github.com/emacs-mirror/emacs.git")'
         print(line)
-
-
-#post_build = aur_post_build
-
-#if __name__ == '__main__':
-#  single_main()
