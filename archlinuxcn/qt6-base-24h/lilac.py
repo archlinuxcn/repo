3f2e9fb954bdb4047b8c0d5507efa7c572207cf5
oldherl <oldherl@gmail.com>
2021-12-11 16:59:18 +0800

    qt6-base-24h: first try

---
+++ archlinuxcn/qt6-base-24h/lilac.py
@@ -0,0 +1,79 @@
+#!/usr/bin/env python3
+
+from types import SimpleNamespace
+
+from lilaclib import *
+
+g = SimpleNamespace()
+
+#build_prefix = 'extra-x86_64'
+
+def pre_build():
+  g.files = download_official_pkgbuild('qt6-base')
+
+  conflict_string=""
+  for line in open('qt6-pkgs').readlines():
+    p = line.strip()
+    conflict_string = conflict_string + '"' + p + '>$pkgver" '
+
+  prepare = False
+  checks = ''
+  for line in edit_file('PKGBUILD'):
+    if line.startswith('pkgrel='):
+      line = line + '.1'
+    elif line.startswith('pkgname='):
+      line = 'pkgname=qt6-base-24h'
+      checks = checks + '1'
+    elif line.startswith('pkgdesc='):
+      line = "pkgdesc='A cross-platform application and UI framework. This package uses 24-hour notation in all locales.'"
+      checks = checks + 'b'
+    elif line.startswith('makedepends=('):
+      line = line.replace('(', "(python2 ")
+      checks = checks + '2'
+    elif line.startswith('groups=('):
+      line = '''
+provides=("qt6-base=$pkgver")
+conflicts=(qt6-base ''' + conflict_string + ")" # remove official groups
+      checks = checks + '4'
+    elif line.startswith('_pkgfqn='):
+      line = line.replace('${pkgname/6-/}', '${_origpkgname/6-/}')
+      checks = checks + '5'
+    elif line.startswith('source=('):
+      line = line.replace('=(', '''=(
+      oldherl-24h.patch
+      'https://build.archlinuxcn.org/~oldherl/files/cldr/36/core.zip'
+      ''')
+      checks = checks + '6'
+    elif line.startswith('sha256sums=('):
+      line = line.replace('=(', '''=(
+      '901a764c896559bb472d5ecee4af9ee006b235f73e56959de9ded477a10e5fc6'
+      '07279e56c1f4266d140b907ef3ec379dce0a99542303a9628562ac5fe460ba43'
+      ''')
+      checks = checks + '7'
+    elif line.startswith('prepare('):
+      prepare = True
+      checks = checks + '8'
+    elif prepare and line.startswith('}'):
+      line = '''
+  patch -p1 -i ../oldherl-24h.patch
+  cd util/locale_database
+  echo "This is slow. It takes about 4 minutes on my desktop."
+  ./cldr2qlocalexml.py ../../../ > ./24h.xml
+  ./qlocalexml2cpp.py ./24h.xml ../..
+''' + line
+      prepare = False
+      checks = checks + '9'
+    elif line.startswith('package_'):
+      # other split packages. do not build them.
+      line = 'no' + line
+      logger.info('removed: %s', line)
+    print(line)
+  if len(checks) != 9:
+    raise ValueError('PKGBUILD editing not completed. checks=' + checks)
+
+def post_build():
+  git_add_files(g.files)
+  git_commit()
+
+#if __name__ == '__main__':
+#  single_main()
