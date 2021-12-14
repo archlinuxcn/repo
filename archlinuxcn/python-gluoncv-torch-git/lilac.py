2fa85001892dc4eba36bff5ef8af77c4cce1fcef
Hu Butui <hot123tea123@gmail.com>
2021-12-14 09:25:33 +0800

    python-gluoncv-torch-git: rebuild for python 3.10

--- archlinuxcn/python-gluoncv-torch-git/lilac.py
+++ archlinuxcn/python-gluoncv-torch-git/lilac.py
@@ -2,13 +2,12 @@
 
 from lilaclib import *
 
+
 def pre_build():
-  update_pkgrel()
-  update_pkgver_and_pkgrel("0.0.3."+_G.newver)
+    update_pkgrel()
+    update_pkgver_and_pkgrel("0.0.3." + _G.newver)
+
 
 def post_build():
-  git_add_files('PKGBUILD')
-  git_commit()
-  update_aur_repo()
-# vim:set ts=2 sw=2 et:
-
+    git_pkgbuild_commit()
+    update_aur_repo()
