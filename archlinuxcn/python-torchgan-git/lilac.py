a6b191a821286b520050917eb017e4cee8c516c4
Hu Butui <hot123tea123@gmail.com>
2021-12-14 09:21:19 +0800

    python-torchgan-git: rebuild for python 3.10

--- archlinuxcn/python-torchgan-git/lilac.py
+++ archlinuxcn/python-torchgan-git/lilac.py
@@ -2,13 +2,12 @@
 
 from lilaclib import *
 
+
 def pre_build():
-  update_pkgrel()
-  vcs_update()
+    update_pkgrel()
+    vcs_update()
+
 
 def post_build():
-  git_add_files('PKGBUILD')
-  git_commit()
-  update_aur_repo()
-# vim:set ts=2 sw=2 et:
-
+    git_pkgbuild_commit()
+    update_aur_repo()
