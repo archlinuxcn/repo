4681a834d3997236e81ec8b462345768c1b060ed
Yichao Yu <yyc1992@gmail.com>
2021-12-16 10:30:45 -0500

    [python-django-formtools] Add dependencies and trigger rebuild

--- archlinuxcn/python-django-formtools/lilac.py
+++ archlinuxcn/python-django-formtools/lilac.py
@@ -1,4 +1,4 @@
 from lilaclib import *
 
 def pre_build():
-  pypi_pre_build(depends = [], depends_setuptools = False)
+  pypi_pre_build(depends = ['python-django', 'python-setuptools-scm'], depends_setuptools = False)
