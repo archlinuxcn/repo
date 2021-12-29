25d76ebf6bc137ca11b824ff267901ebee3a70df
DDoSolitary <DDoSolitary@gmail.com>
2021-12-29 19:30:56 +0800

    lib32-gupnp: fix library provides

--- archlinuxcn/lib32-gupnp/lilac.py
+++ archlinuxcn/lib32-gupnp/lilac.py
@@ -5,7 +5,7 @@ from lilaclib import *
 def pre_build():
     aur_pre_build(maintainers='rodrigo21')
     add_depends(['libgio-2.0.so', 'libglib-2.0.so', 'libgmodule-2.0.so', 'libgobject-2.0.so', 'libgssdp-1.2.so', 'libuuid.so'])
-    add_provides(['libgupnp-${pkgver%.*}.so'])
+    add_provides(['libgupnp-1.2.so'])
 
 def post_build():
     check_library_provides()
