5e9a68baeeb1297401473f4581b4d7202b70edd2
Winston Wu <winstonwu91@gmail.com>
2021-12-29 11:29:37 +0800

    [tome4] Update

---
+++ archlinuxcn/tome4/lilac.py
@@ -0,0 +1,16 @@
+# Trimmed lilac.py
+from lilaclib import *
+
+#build_prefix = 'extra-x86_64'
+
+def pre_build():
+  update_pkgver_and_pkgrel(_G.newver.lstrip('tome4-'))
+  run_cmd(['updpkgsums'])
+
+def post_build():
+  git_add_files('PKGBUILD')
+  git_commit()
+  update_aur_repo()
+
+#if __name__ == '__main__':
+#  single_main()
