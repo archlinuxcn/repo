9341cbc8d9fcb91b666634c2d598d545d7ca6012
oldherl <oldherl@gmail.com>
2021-12-31 21:21:58 +0800

    bypy-git: fix aur maintainer

--- archlinuxcn/bypy-git/lilac.py
+++ archlinuxcn/bypy-git/lilac.py
@@ -2,8 +2,11 @@
 from lilaclib import *
 
 build_prefix = 'extra-x86_64'
-pre_build = aur_pre_build
+def pre_build():
+    aur_pre_build(maintainers="oldherl")
+
 post_build = aur_post_build
 
 if __name__ == '__main__':
     single_main(build_prefix)
+
