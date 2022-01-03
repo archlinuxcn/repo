48e0fb3e7eef5b05cc17d49bb7a3260572415580
DDoSolitary <DDoSolitary@gmail.com>
2022-01-03 23:27:48 +0800

    lib32-libass: fix library provides

--- archlinuxcn/lib32-libass/lilac.py
+++ archlinuxcn/lib32-libass/lilac.py
@@ -5,6 +5,7 @@ from lilaclib import *
 def pre_build():
     aur_pre_build(maintainers='rodrigo21')
     add_depends(['libfontconfig.so', 'libfreetype.so', 'libfribidi.so', 'libharfbuzz.so'])
+    add_provides(['libass.so'])
 
 def post_build():
     check_library_provides()
