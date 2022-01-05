8b43319ceeabf19db97b190cd9ccfb5d155714a8
yk <yk@archlinuxcn.org>
2022-01-05 21:51:06 +0800

    fixpkg: emacs-native-comp-git #2616

--- archlinuxcn/emacs-native-comp-git/lilac.py
+++ archlinuxcn/emacs-native-comp-git/lilac.py
@@ -4,28 +4,41 @@ from lilaclib import *
 
 def pre_build():
     aur_pre_build('emacs-git', maintainers=['vorbote'])
+    checks = ''
     for line in edit_file('PKGBUILD'):
-        if line.startswith('pkgname='):
-            line = 'pkgname="emacs-native-comp-git"'
+        if line.startswith('  pkgname="emacs-git"'):
+            line = '  pkgname="emacs-native-comp-git"'
+            checks = checks + '1'
 
         if line.startswith('replaces='):
+            checks = checks + '2'
             continue
         if line.startswith('source='):
             line = 'source=("emacs-git::git://github.com/emacs-mirror/emacs.git")'
+            checks = checks + '3'
 
 
         if line.startswith('JIT='):
             line = 'JIT="YES"'
+            checks = checks + '4'
         if line.startswith('AOT='):
             line = 'AOT="YES"'
+            checks = checks + '5'
 
         if line.startswith('XWIDGETS='):
             line = 'XWIDGETS="YES"'
+            checks = checks + '6'
 
         if line.startswith('install='):
             line = 'install=emacs-git.install'
+            checks = checks + '7'
 
         # fix libxpm
         if line.startswith('depends='):
             line = 'depends=("${depends_nox[@]}" "harfbuzz" "libxpm")'
+            checks = checks + '8'
         print(line)
+
+    # make sure PGKBUILD is modified
+    if len(checks) != 8:
+        raise ValueError('PKGBUILD editing not completed. checks=' + checks)
