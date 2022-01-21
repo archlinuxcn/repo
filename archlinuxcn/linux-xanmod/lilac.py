772fa9a86bdb3151bbf94f94eac898e4b7ff98b0
yk <yk@archlinuxcn.org>
2022-01-21 21:06:44 +0800

    fix: linux-xanmod{,-edge}, set pkgrel properly #2634

---
+++ archlinuxcn/linux-xanmod/lilac.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+
+from lilaclib import *
+
+def pre_build():
+    aur_pre_build('linux-xanmod', maintainers=['figue'])
+
+    # pkgrel is set to ${xanmod}
+    xanmod = 0
+    for line in edit_file('PKGBUILD'):
+        if line.startswith('xanmod='):
+            xanmod = int(line.split("=")[-1])
+
+        if line.startswith('pkgrel='):
+            line = "pkgrel=" + str(xanmod)
+
+        print(line)
+
+    # make sure pkgrel is modified
+    if xanmod == 0:
+        raise ValueError('pkgrel is not set properly')
