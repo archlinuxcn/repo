ab1d9e6aec08ff2984bd45ca4535f5db9d50eacc
Hu Butui <hot123tea123@gmail.com>
2022-02-27 21:01:06 +0800

    python-pydicom-seg: switch to PEP 517

--- archlinuxcn/python-pydicom-seg/lilac.py
+++ archlinuxcn/python-pydicom-seg/lilac.py
@@ -4,8 +4,9 @@ from lilaclib import *
 
 
 def pre_build():
-  update_pkgver_and_pkgrel(_G.newver.lstrip('v'))
+    update_pkgver_and_pkgrel(_G.newver.lstrip("v"))
+
 
 def post_build():
-  git_pkgbuild_commit()
-  update_aur_repo()
+    git_pkgbuild_commit()
+    update_aur_repo()
