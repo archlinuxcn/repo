7b62286b2b274d1f3b13cd43a6d1e79e36c033a9
Winston Wu <winstonwu91@gmail.com>
2022-04-16 00:33:14 +0800

    [gcc10] Try to fix build

---
+++ archlinuxcn/gcc10/lilac.py
@@ -0,0 +1,14 @@
+from lilaclib import aur_pre_build, edit_file
+
+def pre_build():
+    aur_pre_build('gcc10',maintainers=['jonathon'])
+
+    for line in edit_file('PKGBUILD'):
+        if 'gcc/configure' in line:
+            print(line)
+            print("sed -i '/HAVE_LIMITS_H/d' libiberty/fibheap.c\n")
+            print("sed -i -e '/limits.h/{n;d}' libiberty/fibheap.c\n")
+            print("sed -i '/HAVE_FCNTL_H/d' libiberty/pex-unix.c\n")
+            print("sed -i -e '/fcntl.h/{n;d}' libiberty/pex-unix.c\n")
+        else:
+            print(line)
