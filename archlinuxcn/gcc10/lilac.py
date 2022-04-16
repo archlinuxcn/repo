80eb8f84e57ee0f92ee63195657a93e2f38a40e5
Winston Wu <winstonwu91@gmail.com>
2022-04-17 04:34:01 +0800

    [gcc10] Fix lilac.py

--- archlinuxcn/gcc10/lilac.py
+++ archlinuxcn/gcc10/lilac.py
@@ -4,7 +4,7 @@ def pre_build():
     aur_pre_build('gcc10',maintainers=['jonathon'])
 
     for line in edit_file('PKGBUILD'):
-        if 'gcc/configure' in line:
+        if 'sed -i' in line and 'gcc/configure' in line:
             print(line)
             print("sed -i '/HAVE_LIMITS_H/d' libiberty/fibheap.c\n")
             print("sed -i -e '/limits.h/{n;d}' libiberty/fibheap.c\n")
