6eb68fb785d1679a7f5355be5f8a67b17225bc0a
lilydjwg <lilydjwg@gmail.com>
2022-05-08 11:48:21 +0800

    add wayfire-lily-git and its dependencies

---
+++ archlinuxcn/xorg-xwayland-lily/lilac.py
@@ -0,0 +1,39 @@
+from types import SimpleNamespace
+
+from lilaclib import *
+
+g = SimpleNamespace()
+
+def pre_build():
+  g.files = download_official_pkgbuild('xorg-xwayland')
+
+  prepare = False
+  sumwhich = 0
+  for line in edit_file('PKGBUILD'):
+    if line.startswith('pkgname='):
+      line = 'pkgname=xorg-xwayland-lily'
+    if line.startswith('pkgdesc='):
+      line = line[:-1] + ', with !733 HiDPI patch"'
+    elif line.startswith('provides'):
+      line = "provides=('xorg-server-xwayland' 'xorg-xwayland')"
+    elif line.startswith('conflicts'):
+      line = "conflicts=('xorg-server-xwayland' 'xorg-xwayland')"
+    elif line.startswith('validgpgkeys='):
+      line += '''
+
+prepare() {
+  cd "${srcdir}/xwayland-$pkgver"
+  patch -Np1 < ../hidpi.patch
+}
+'''
+    elif line.startswith('source=('):
+      line = line.replace(')', ' hidpi.patch)')
+    elif "'SKIP'" in line:
+      line = line.replace(')', ' d0e7dfe38157947dc4b3a3e7ac0c867d06eeb8d760035227a7cb4aca987a91053cc3c7c4f50d4ade6f1eb7fad631a173ceb75c6b9a896dc22a3aba2221264131)')
+    elif line.startswith('groups='):
+      continue
+    print(line)
+
+def post_build():
+  git_add_files(g.files)
+  git_commit()
