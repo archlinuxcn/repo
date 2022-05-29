b0bb7925624425bdaa7afcbecfefed639bddd31c
lilydjwg <lilydjwg@gmail.com>
2022-05-29 22:31:24 +0800

    add celestia
    closes #2791.

---
+++ archlinuxcn/celestia/lilac.py
@@ -0,0 +1,11 @@
+from lilaclib import (
+  git_pkgbuild_commit,
+  update_pkgver_and_pkgrel,
+  _G,
+)
+
+def pre_build():
+  update_pkgver_and_pkgrel(_G.newver)
+
+def post_build():
+  git_pkgbuild_commit()
