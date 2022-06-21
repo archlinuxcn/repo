cbbc27942642be84a179c3dc2778637ca73064b3
ZeekoZhu <vaezt@outlook.com>
2022-06-21 14:56:40 +0800

    refactor: remove nodejs from makedeps

--- archlinuxcn/rime-sbxlm-sbzr/fetch-release.sh
+++ archlinuxcn/rime-sbxlm-sbzr/fetch-release.sh
@@ -1,3 +1,5 @@
 #!/usr/bin/bash
-curl -X GET --header 'Content-Type: application/json;charset=UTF-8' 'https://gitee.com/api/v5/repos/sbxlm/sbxlm/releases/latest' -o releases.json
+variant=$1
 
+releases=$(curl -X GET --header 'Content-Type: application/json;chartset=UTF-8' 'https://gitee.com/api/v5/repos/sbxlm/sbxlm/releases/latest')
+echo $releases | jq -r ".assets[] | select(.name) | select(.name | contains(\"$variant\")) | .browser_download_url"
