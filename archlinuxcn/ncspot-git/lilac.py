0c77877f080f31255c1b518f21fc815fb14c6627
yk <yk@archlinuxcn.org>
2022-07-02 13:50:00 +0800

    ncspot-git: disable lto

---
+++ archlinuxcn/ncspot-git/lilac.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+
+from lilaclib import *
+
+def pre_build():
+    aur_pre_build(maintainers=['gugylucky'])
+
+    for line in edit_file('PKGBUILD'):
+        if "pkgname=" in line:
+            print("options=(!lto)")
+        print(line)
