8214250129b353c51c5b372528a4870923621021
yk <yk@archlinuxcn.org>
2022-07-13 16:53:06 +0800

    fix: sayonara-player-git build error

--- archlinuxcn/sayonara-player-git/lilac.py
+++ archlinuxcn/sayonara-player-git/lilac.py
@@ -3,6 +3,7 @@
 from lilaclib import *
 
 def pre_build():
+    aur_pre_build(maintainers=["FabioLolix", "FixedTorres"])
     for line in edit_file('PKGBUILD'):
         if "DWITH_TESTS" in line:
             line = ''
