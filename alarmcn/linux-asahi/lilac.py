2ea66fd65c28e50272892fd8ad81bd1a072c095d
Yichao Yu <yyc1992@gmail.com>
2022-07-15 17:07:25 -0400

    [linux-asahi] fix downloading

--- alarmcn/linux-asahi/lilac.py
+++ alarmcn/linux-asahi/lilac.py
@@ -3,9 +3,10 @@
 from lilaclib import *
 
 def pre_build():
-    run_cmd(["sh", "-c", 'curl -sSO https://github.com/AsahiLinux/PKGBUILDs/archive/refs/heads/main.zip'])
+    run_cmd(["sh", "-c", 'curl -L -sSO https://github.com/AsahiLinux/PKGBUILDs/archive/refs/heads/main.zip'])
+    run_cmd(['unzip', 'main.zip'])
     run_cmd(["sh", "-c", 'mv PKGBUILDs-main/linux-asahi/* .'])
-    run_cmd(['rm', '-rf', 'PKGBUILDs-main/'])
+    run_cmd(['rm', '-rf', 'PKGBUILDs-main/', 'main.zip'])
     run_cmd(["updpkgsums"])
 
 def post_build():
