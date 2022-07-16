8c748129316c2e7a0df4cb805bc2231f9d865f0f
Yichao Yu <yyc1992@gmail.com>
2022-07-16 07:47:09 -0400

    [linux-asahi] try a different way to download

--- alarmcn/linux-asahi/lilac.py
+++ alarmcn/linux-asahi/lilac.py
@@ -3,11 +3,7 @@
 from lilaclib import *
 
 def pre_build():
-    run_cmd(["sh", "-c", 'curl -L -sSO https://github.com/AsahiLinux/PKGBUILDs/archive/refs/heads/main.zip'])
-    run_cmd(['unzip', 'main.zip'])
-    run_cmd(["sh", "-c", 'mv PKGBUILDs-main/linux-asahi/* .'])
-    run_cmd(['rm', '-rf', 'PKGBUILDs-main/', 'main.zip'])
-    run_cmd(["updpkgsums"])
+    run_cmd(['sh', './get_pkg.sh', 'linux-asahi'])
 
 def post_build():
     git_pkgbuild_commit()
