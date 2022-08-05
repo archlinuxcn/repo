0db2653db8c77edbec8da7cbafcd6c21513443ce
Winston Wu <winstonwu91@gmail.com>
2022-08-05 15:01:48 +0800

    [dia-git] Update

--- archlinuxcn/pcem/lilac.py
+++ archlinuxcn/pcem/lilac.py
@@ -1,7 +1,7 @@
 from lilaclib import aur_pre_build, edit_file
 
 def pre_build():
-    aur_pre_build('pcem',maintainers=['aaronp'])
+    aur_pre_build('pcem',maintainers=['njensen20'])
 
     for line in edit_file('PKGBUILD'):
         if line.startswith('depends'):
