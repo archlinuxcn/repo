b4bfa5cc4980483325edff3553f82eaf016b2392
Coelacanthus <CoelacanthusHex@gmail.com>
2022-08-10 22:16:00 +0800

    proton-ge-custom-opt: use pip to install afdko
    Signed-off-by: Coelacanthus <CoelacanthusHex@gmail.com>

--- archlinuxcn/proton-ge-custom-opt/lilac.py
+++ archlinuxcn/proton-ge-custom-opt/lilac.py
@@ -1,27 +1,11 @@
-from types import SimpleNamespace
-
 from lilaclib import *
-
-g = SimpleNamespace()
 
 def pre_build():
   
-  aur_pre_build(name='proton-ge-custom', maintainers=['loathingkernel'])
-  
   for line in edit_file('PKGBUILD'):
-    if 'nocona' in line:
-      line = line.replace('nocona', 'x86-64-v3')
-    elif 'core-avx2' in line:
-      line = line.replace('core-avx2', 'generic')
-    elif '-C opt-level=2' in line:
-      line = line.replace('-C opt-level=2', '-C opt-level=3')
-    elif line.startswith('pkgname='):
-      line = 'pkgname=proton-ge-custom-opt\n_pkgname=proton-ge-custom'
-    elif r'${pkgname}' in line:
-      line = line.replace(r'${pkgname}', r'${_pkgname}')
-
+    if line.startswith('_srctag='):
+      line = '_srctag=' + _G.newver
     print(line)
 
 def post_build():
-  git_add_files('PKGBUILD')
-  git_commit()
+  git_pkgbuild_commit()
