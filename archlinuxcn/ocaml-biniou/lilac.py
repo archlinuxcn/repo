99bd850efcc698cdd50a1e31a7b7d89d5744d8fb
Dct Mei <dctxmei@yandex.com>
2022-08-15 00:58:29 +0800

    ocaml-biniou: fix lilac.py

--- archlinuxcn/ocaml-biniou/lilac.py
+++ archlinuxcn/ocaml-biniou/lilac.py
@@ -7,8 +7,8 @@ def pre_build():
     for line in edit_file('PKGBUILD'):
         if line.startswith('depends='):
             line = ('#' + line + '\n'
-                    'depends=('camlp-streams' 'ocaml-easy-format')')
+                    "depends=('camlp-streams' 'ocaml-easy-format')")
         elif line.startswith('makedepends='):
             line = ('#' + line + '\n'
-                    'makedepends=('dune' 'ocaml')')
+                    "makedepends=('dune' 'ocaml')")
         print(line)
