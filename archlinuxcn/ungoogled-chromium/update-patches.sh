e8eb608e34b5e98219281d8bf83aa4f17e463c35
lilac <lilac@build.archlinuxcn.org>
2022-09-13 22:55:00 +0800

    ungoogled-chromium: auto updated to 105.0.5195.102-3

--- archlinuxcn/ungoogled-chromium/update-patches.sh
+++ archlinuxcn/ungoogled-chromium/update-patches.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
 
 mkdir tmp
-mv ozone-add-va-api-support-to-wayland.patch tmp
+mv ozone-add-va-api-support-to-wayland.patch remove-main-main10-profile-limit.patch tmp
 
 rm *.patch
 git clone https://github.com/archlinux/svntogit-packages.git --branch packages/chromium --single-branch chr
 mv chr/trunk/*.patch .
 nvim -d PKGBUILD chr/trunk/PKGBUILD
+makepkg --printsrcinfo > .SRCINFO
 rm -rf chr
 
 mv tmp/* .
