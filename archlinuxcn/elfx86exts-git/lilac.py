d6737aaf4d91df7ef3825ae4df6030474b1ace94
oldherl <oldherl@gmail.com>
2022-10-04 20:09:40 +0800

    addpkg: elfx86exts-git

---
+++ archlinuxcn/elfx86exts-git/lilac.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+
+from lilaclib import *
+
+def pre_build():
+    update_pkgver_and_pkgrel(_G.newver)
+def post_build():
+    #check_library_provides()
+    git_pkgbuild_commit()
