c5fc6b44e0c4c4f62e27ce7e4243d774112ec118
lilydjwg <lilydjwg@gmail.com>
2022-10-26 18:34:33 +0800

    xorg-xwayland-lily: update hidpi.patch

--- archlinuxcn/xorg-xwayland-lily/lilac.py
+++ archlinuxcn/xorg-xwayland-lily/lilac.py
@@ -23,7 +23,7 @@ def pre_build():
     elif line.startswith('.patch )'):
       line = line.replace(')', ' hidpi.patch)')
     elif "d0c87face4485050db134e5ed14d930bdae05d81149b2b573b97fc6dd96d9234e709d6f0523221747293da20cbd012e1e1da00e12b227f98597ffa320bcd3e3c" in line:
-      line = line.replace(')', '\n            d0e7dfe38157947dc4b3a3e7ac0c867d06eeb8d760035227a7cb4aca987a91053cc3c7c4f50d4ade6f1eb7fad631a173ceb75c6b9a896dc22a3aba2221264131)')
+      line = line.replace(')', '\n            7132c44d9bbf1c5f93906ef301a6e946f0909da0b9273f397281e681ad0da77d62b960a9dd0a640c7209548192fefba9bde03c493f3a89d08cd73a12844bb518)')
     elif line.startswith('groups='):
       continue
     print(line)
