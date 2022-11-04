ba9e078fae241c9454cb2e207c77c96990caaf1b
lilydjwg <lilydjwg@gmail.com>
2022-11-04 15:24:00 +0800

    xorg-xwayland-lily: fix patch not applied again

--- archlinuxcn/xorg-xwayland-lily/lilac.py
+++ archlinuxcn/xorg-xwayland-lily/lilac.py
@@ -17,12 +17,16 @@ def pre_build():
     elif line.startswith('conflicts'):
       line = "conflicts=('xorg-server-xwayland' 'xorg-xwayland')"
     elif line.startswith('validpgpkeys='):
-      line += '''\noptions=('debug' 'strip')'''
-    elif line.lstrip().startswith('patch -Np1'):
-      line += '''\n  patch -Np1 < ../hidpi.patch'''
-    elif line.endswith('.patch )'):
+      line += '''
+options=('debug' 'strip')
+
+prepare() {
+  cd "${srcdir}/xwayland-$pkgver"
+  patch -Np1 < ../hidpi.patch
+}'''
+    elif line.startswith('source=('):
       line = line.replace(')', ' hidpi.patch)')
-    elif "d0c87face4485050db134e5ed14d930bdae05d81149b2b573b97fc6dd96d9234e709d6f0523221747293da20cbd012e1e1da00e12b227f98597ffa320bcd3e3c" in line:
+    elif "'SKIP'" in line:
       line = line.replace(')', '\n            7132c44d9bbf1c5f93906ef301a6e946f0909da0b9273f397281e681ad0da77d62b960a9dd0a640c7209548192fefba9bde03c493f3a89d08cd73a12844bb518)')
     elif line.startswith('groups='):
       continue
