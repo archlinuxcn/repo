9273647300e92e9e0eb237afa7d418336e908af5
lilydjwg <lilydjwg@gmail.com>
2022-11-05 13:36:32 +0800

    pacman-lily: adjust edits

--- archlinuxcn/pacman-lily/lilac.py
+++ archlinuxcn/pacman-lily/lilac.py
@@ -7,7 +7,6 @@ g = SimpleNamespace()
 def pre_build():
   g.files = download_official_pkgbuild('pacman')
 
-  build = False
   package = False
   for line in edit_file('PKGBUILD'):
     if line.startswith('pkgname='):
@@ -16,13 +15,8 @@ def pre_build():
       line = line[:-1] + ' (no disabling-server-on-error)"'
     elif line.startswith('provides=('):
       line = line.replace(')', ' "pacman=$pkgver")\n') + "conflicts=('pacman')"
-    elif line.startswith('build() '):
-      build = True
     elif line.startswith('groups=('):
       continue
-    elif build and line.startswith('  cd '):
-      line += '\n  patch -p1 -i "${srcdir}/pacman.patch"'
-      build = False
     elif line.startswith('source=('):
       line = line.replace('(', '(pacman.patch\n        pacman-syncdb\n        ')
     elif line.startswith('sha256sums=('):
@@ -35,6 +29,8 @@ def pre_build():
 
     if '$pkgname' in line:
       line = line.replace('$pkgname', 'pacman')
+    if '${pkgname}' in line:
+      line = line.replace('${pkgname}', 'pacman')
     print(line)
 
 def post_build():
