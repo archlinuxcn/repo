539cad7369061cee81295fc8bfda282f0f11a515
lilydjwg <lilydjwg@gmail.com>
2022-11-06 13:10:26 +0800

    python-statsd: upstream switched to pep517

--- archlinuxcn/python-statsd/lilac.py
+++ archlinuxcn/python-statsd/lilac.py
@@ -1,7 +1,11 @@
 from lilaclib import *
 
 def pre_build():
-  pypi_pre_build()
+  pypi_pre_build(
+    depends_setuptools = False,
+    pep517 = True,
+    makedepends = ['python-setuptools'],
+  )
 
 def post_build():
   git_pkgbuild_commit()
