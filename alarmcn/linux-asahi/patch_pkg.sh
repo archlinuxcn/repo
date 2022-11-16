b93b92defedacc7da02ecd4b269e3ac92dc50480
Yichao Yu <yyc1992@gmail.com>
2022-11-16 08:47:48 -0500

    [linux-asahi] update patching

--- alarmcn/linux-asahi/patch_pkg.sh
+++ alarmcn/linux-asahi/patch_pkg.sh
@@ -1,12 +1,12 @@
 #!/bin/sh
 
-sed -i -e 's/_package()/_origin_package()/' PKGBUILD
+sed -i -e 's/_package_kernel()/_origin_package_kernel()/' PKGBUILD
 
 cat >> PKGBUILD <<EOF
-_package() {
-  _origin_package
+_package_kernel() {
+  _origin_package_kernel
   cd "\$srcdir/\$_srcname"
-  local kernver="\$(<version)"
+  local kernver="\$(<\$O/version)"
   local modulesdir="\$pkgdir/usr/lib/modules/\$kernver"
   install -Dm644 "\$(make -s image_name)" "\$modulesdir/vmlinuz"
   install -Dm644 arch/arm64/boot/Image "\$modulesdir/vmlinuz-nogz"
