c777655bfee7360d09ffc0f3f2ac68ae874da9cf
lilac <lilac@build.archlinuxcn.org>
2023-01-06 07:01:43 +0800

    powder-toy: auto updated to 97.0.352-1

--- archlinuxcn/powder-toy/powder-toy.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env sh
-set -eu
-
-pkgname=powder-toy
-data_home="${XDG_DATA_HOME:-${HOME}/.local/share}/${pkgname}"
-
-mkdir -p "${data_home}"
-cd "${data_home}"
-exec "/usr/lib/${pkgname}/${pkgname}" "$@"
