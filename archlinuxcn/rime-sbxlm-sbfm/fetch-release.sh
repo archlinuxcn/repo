bbdd573a264fc25eefeabdad06ca6ca3b4e940fd
ZeekoZhu <vaezt@outlook.com>
2023-01-06 13:57:13 +0800

    rime-sbxlm: remove deprecated packages

--- archlinuxcn/rime-sbxlm-sbfm/fetch-release.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/bash
-variant=$1
-
-releases=$(curl -X GET --header 'Content-Type: application/json;chartset=UTF-8' 'https://gitee.com/api/v5/repos/sbxlm/sbxlm/releases/latest')
-echo $releases | jq -r ".assets[] | select(.name) | select(.name | contains(\"$variant\")) | .browser_download_url"
