f8d60d34a91b22db54716937038ba069bbda4c67
ZeekoZhu <vaezt@outlook.com>
2023-01-20 12:53:37 +0800

    rime-sbxlm: update fetching release api

--- archlinuxcn/rime-sbxlm/fetch-release.sh
+++ archlinuxcn/rime-sbxlm/fetch-release.sh
@@ -1,5 +1,22 @@
 #!/usr/bin/bash
 variant=$1
+prop=$2 # url or version
 
 releases=$(curl -X GET --header 'Content-Type: application/json;chartset=UTF-8' 'https://gitee.com/api/v5/repos/sbxlm/sbxlm/releases/latest')
-echo $releases | jq -r ".assets[] | select(.name) | select(.name | contains(\"$variant\")) | .browser_download_url"
+# switch prop
+case $prop in
+    url)
+        download_url=$(echo $releases | jq -r ".assets[] | select(.name) | select(.name | contains(\"$variant\")) | .browser_download_url")
+        echo $download_url
+        ;;
+    version)
+        version=$(echo $releases | jq -r ".tag_name")
+        echo $version
+        ;;
+    *)
+        echo "Invalid property"
+        exit 1
+        ;;
+esac
+
+
