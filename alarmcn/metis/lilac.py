6891924c65a4d050dc651a564c069404bb0b868d
Yichao Yu <yyc1992@gmail.com>
2023-01-25 22:09:20 -0500

    [metis] Try fixing checksum mismatch

--- alarmcn/metis/lilac.py
+++ alarmcn/metis/lilac.py
@@ -5,8 +5,8 @@ from lilaclib import *
 def pre_build():
     _newver = _G.newver.lstrip('v')
     newver = _newver.replace('-', '.')
-    update_pkgver_and_pkgrel(newver)
     for line in edit_file('PKGBUILD'):
         if line.strip().startswith("_pkgver"):
             line = f'_pkgver={_newver}'
         print(line)
+    update_pkgver_and_pkgrel(newver)
