83d87b42a238a25cd4af8f81719795c249cf1994
Peter Cai <xqsx43cxy@gmail.com>
2023-02-20 21:04:12 +0800

    shattered-pixel-dungeon: Fix OpenJDK path

--- archlinuxcn/shattered-pixel-dungeon/shattered-pixel-dungeon.sh
+++ archlinuxcn/shattered-pixel-dungeon/shattered-pixel-dungeon.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
-export PATH=/usr/lib/jvm/java-11-openjdk/jre/bin/:$PATH
+export PATH=/usr/lib/jvm/java-11-openjdk/bin/:$PATH
 java -jar /usr/share/shattered-pixel-dungeon/shattered-pixel-dungeon.jar
