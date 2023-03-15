d5a7b8cc739e18d940d05237472eaa772b2ea6e9
Integral <integral@mail.dlut.edu.cn>
2023-03-15 11:50:02 +0800

    Modify gimp-devel

---
+++ archlinuxcn/gimp-devel/lilac.py
@@ -0,0 +1,3 @@
+#!/usr/bin/python
+
+print(edit_file('PKGBUILD').replace("40c8175505598219e379bfd25c3b0a37bbd42ac35c06990683f1dca622a0a6a1", "e012d022fe53eaf4cd2fc08f07cb0377fb14c8f791e42d13027983e41f7f4fc2"))
