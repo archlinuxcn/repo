fc3e031168fc22fadc60ebe2fb41bdcd03acbdf9
Integral <integral@mail.dlut.edu.cn>
2023-03-15 19:45:05 +0800

    Modify kitemmodels-git

--- archlinuxcn/kitemmodels-git/lilac.py
+++ archlinuxcn/kitemmodels-git/lilac.py
@@ -3,5 +3,5 @@
 def pre_build():
     for line in edit_file('PKGBUILD'):
         if line.startswith('makedepends'):
-            print line.replace("sip4", "sip")
+            print (line.replace("sip4", "sip"))
         print(line)
