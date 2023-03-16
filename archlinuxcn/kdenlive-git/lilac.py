55679e49c89d17987cff1525ad14d931d09ef127
Integral <integral@mail.dlut.edu.cn>
2023-03-16 12:54:20 +0800

    Modify kdenlive-git

--- archlinuxcn/kdenlive-git/lilac.py
+++ archlinuxcn/kdenlive-git/lilac.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python
 
 def pre_build():
+    aur_pre_build(maintainers=['evorster'])
+
     for line in edit_file("PKGBUILD"):
         if not (line.strip().startswith("cp -v")):
             print (line)
