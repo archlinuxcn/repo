68f4211f8f89670e85755d0bf1f5776e9fed5c28
Integral <integral@mail.dlut.edu.cn>
2023-03-16 13:08:13 +0800

    Modify sip4

--- archlinuxcn/sip4/lilac.py
+++ archlinuxcn/sip4/lilac.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python
 
 def pre_build():
+    aur_pre_build(maintainers=['arojas'])
+
     for line in edit_file('PKGBUILD'):
         if not (line.strip().startswith("replaces=('sip")):
             print (line)
