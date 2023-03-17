2eb12d627c4b6a481937c79370db50ba04435b31
Integral <71180087+Integral-Tech@users.noreply.github.com>
2023-03-18 01:17:24 +0800

    Modify hyprland-hidpi-xprop-git

--- archlinuxcn/hyprland-hidpi-xprop-git/lilac.py
+++ archlinuxcn/hyprland-hidpi-xprop-git/lilac.py
@@ -6,4 +6,4 @@ def pre_build():
     aur_pre_build(maintainers=['Aleksana','q234rty'])
 
     for line in edit_file('PKGBUILD'):
-        print line.replace('xorg-xwayland-hidpi-xprop','xorg-xwayland-lily')
+        print (line.replace('xorg-xwayland-hidpi-xprop','xorg-xwayland-lily'))
