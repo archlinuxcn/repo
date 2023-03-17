053dd9e0b8ab11d131b267ce8c2d2e3c80f5ea54
Integral <integral@mail.dlut.edu.cn>
2023-03-17 23:56:08 +0800

    Modify hyprland-nvidia-hidpi-git & hyprland-hidpi-xprop-git

---
+++ archlinuxcn/hyprland-nvidia-hidpi-git/lilac.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+
+from lilaclib import *
+
+def pre_build():
+    aur_pre_build(maintainers=['epiphyllosper'])
+
+    for line in edit_file('PKGBUILD'):
+        if line.strip().startswith('xorg-xwayland-hidpi-xprop'):
+            print ('    xorg-xwayland-lily')
+        print (line)
