f76063f65df450a7a58ef27ce5c82ea0c2c7c5a0
Integral <integral@mail.dlut.edu.cn>
2023-03-20 12:16:46 +0800

    Modify olive-git

--- archlinuxcn/olive-git/lilac.py
+++ archlinuxcn/olive-git/lilac.py
@@ -6,4 +6,4 @@ def pre_build():
     aur_pre_build(maintainers=['tecnotercio'])
 
     for line in edit_file('PKGBUILD'):
-        print (line.replace('opentimelineio0.14', 'opentimeline-git'))
+        print (line.replace('opentimelineio0.14', 'opentimelineio-git'))
