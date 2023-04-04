fefe44d17f3d35bfc533d6272a486150086d4c5c
Integral <integral@murena.io>
2023-04-04 21:21:49 +0800

    Modify sysmontask-git

--- archlinuxcn/sysmontask-git/lilac.py
+++ archlinuxcn/sysmontask-git/lilac.py
@@ -8,5 +8,5 @@ def pre_build():
     for line in edit_file('PKGBUILD'):
         if line.startswith('pkgname='):
             print ('pkgname=sysmontask-git')
-        else
+        else:
             print (line)
