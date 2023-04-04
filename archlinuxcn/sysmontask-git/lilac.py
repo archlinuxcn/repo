576e563814bd95392763938ae200efb0b7dd807b
Integral <integral@murena.io>
2023-04-04 21:17:39 +0800

    Add sysmontask-git

---
+++ archlinuxcn/sysmontask-git/lilac.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+
+from lilaclib import *
+
+def pre_build():
+    aur_pre_build(name='sysmontask-git', maintainers=['yochananmarqos','camel-neeraj'])
+
+    for line in edit_file('PKGBUILD'):
+        if line.startswith('pkgname='):
+            print ('pkgname=sysmontask-git')
+        else
+            print (line)
