7606c8735706fe831375239125d81c6a8988ec31
Integral <integral@murena.io>
2023-04-08 12:32:52 +0800

    Modify depends of hyprland-hidpi-xprop-git

--- archlinuxcn/hyprland-hidpi-xprop-git/lilac.py
+++ archlinuxcn/hyprland-hidpi-xprop-git/lilac.py
@@ -4,6 +4,7 @@ from lilaclib import *
 
 def pre_build():
     aur_pre_build(maintainers=['Aleksana', 'q234rty'])
+    add_depends(['libdisplay-info.so'])
 
     for line in edit_file('PKGBUILD'):
         line = line.replace('libdisplay-info', 'libdisplay-info-git')
