2b0c7ae801b1bf3a7a8815a2d2b13f407de6969d
Integral <integral@murena.io>
2023-04-08 16:32:10 +0800

    Remove redundant replace in hyprland-git

--- archlinuxcn/hyprland-git/lilac.py
+++ archlinuxcn/hyprland-git/lilac.py
@@ -5,6 +5,3 @@ from lilaclib import *
 def pre_build():
     aur_pre_build(maintainers=['That1Calculator'])
     add_depends(['libdisplay-info.so'])
-
-    for line in edit_file('PKGBUILD'):
-        print (line.replace('libdisplay-info', 'libdisplay-info-git'))
