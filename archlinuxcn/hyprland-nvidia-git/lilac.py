e47c785fe85cb9187a16b7b137910d2414d9dd21
Integral <integral@murena.io>
2023-04-08 16:33:27 +0800

    Remove redundant replace in hyprland-nvidia-git

--- archlinuxcn/hyprland-nvidia-git/lilac.py
+++ archlinuxcn/hyprland-nvidia-git/lilac.py
@@ -5,6 +5,3 @@ from lilaclib import *
 def pre_build():
     aur_pre_build(maintainers=['tohmais'])
     add_depends(['libdisplay-info.so'])
-
-    for line in edit_file('PKGBUILD'):
-        print (line.replace('libdisplay-info', 'libdisplay-info-git'))
