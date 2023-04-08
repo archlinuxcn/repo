8e7dbbd32069c1daa74838e64058854bdacb75bf
Integral <integral@mail.dlut.edu.cn>
2023-04-08 23:33:03 +0800

    Modify hyprland-nvidia-hidpi-git (AUR orphaned)

--- archlinuxcn/hyprland-nvidia-hidpi-git/lilac.py
+++ archlinuxcn/hyprland-nvidia-hidpi-git/lilac.py
@@ -3,9 +3,14 @@
 from lilaclib import *
 
 def pre_build():
-    aur_pre_build(maintainers=['epiphyllosper'])
+    update_pkgrel()
+    vcs_update()
 
     for line in edit_file('PKGBUILD'):
         line = line.replace('libdisplay-info', 'libdisplay-info-git')
         line = line.replace('xorg-xwayland-hidpi-xprop', 'xorg-xwayland-lily')
         print (line)
+
+def post_build():
+    git_pkgbuild_commit()
+    update_aur_repo()
