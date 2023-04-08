a74e1ac161f106e6ca64b31e087fd23df21fb497
Integral <integral@murena.io>
2023-04-08 12:36:04 +0800

    Modify depends of hyprland-nvidia-git

--- archlinuxcn/hyprland-nvidia-git/lilac.py
+++ archlinuxcn/hyprland-nvidia-git/lilac.py
@@ -4,6 +4,7 @@ from lilaclib import *
 
 def pre_build():
     aur_pre_build(maintainers=['tohmais'])
+    add_depends(['libdisplay-info.so'])
 
     for line in edit_file('PKGBUILD'):
         print (line.replace('libdisplay-info', 'libdisplay-info-git'))
