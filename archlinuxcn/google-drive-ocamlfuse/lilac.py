f464c42cc24716e28a57797751d48faeb28f7e73
lilac <lilac@build.archlinuxcn.org>
2023-04-14 16:54:18 +0800

    emacs-queue, google-drive-ocamlfuse, emacs-evil, emacs-undo-tree: orphaned, removing. closes #3251

--- archlinuxcn/google-drive-ocamlfuse/lilac.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env python3
-
-from lilaclib import *
-
-def pre_build():
-    aur_pre_build('google-drive-ocamlfuse-opam', do_vcs_update=True, maintainers='swordfeng')
-    for line in edit_file('PKGBUILD'):
-        line = line.replace('google-drive-ocamlfuse-opam', 'google-drive-ocamlfuse')
-        print(line)
