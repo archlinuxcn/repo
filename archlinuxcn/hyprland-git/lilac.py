3c72d226eb560916023a50af4ca3093b4ed7ebd3
Integral <integral@murena.io>
2023-04-22 10:02:10 +0800

    Ignore warnings of compiling in hyprland-git

--- archlinuxcn/hyprland-git/lilac.py
+++ archlinuxcn/hyprland-git/lilac.py
@@ -6,4 +6,8 @@ def pre_build():
     aur_pre_build(maintainers=['That1Calculator'])
 
     for line in edit_file('PKGBUILD'):
-        print (line.replace('libdisplay-info', 'libdisplay-info-git'))
+        if line.strip().startswith('build()'):
+            print (line)
+            print ('export CFLAGS+=w')
+        else
+            print (line.replace('libdisplay-info', 'libdisplay-info-git'))
