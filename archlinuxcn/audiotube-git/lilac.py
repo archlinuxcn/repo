c626c812790c42eb2eeb08a014aa71b68ab6818c
Integral <integral@murena.io>
2023-04-25 08:18:23 +0800

    Add futuresql-git to the depends of audiotube-git

--- archlinuxcn/audiotube-git/lilac.py
+++ archlinuxcn/audiotube-git/lilac.py
@@ -4,6 +4,7 @@ from lilaclib import *
 
 def pre_build():
     aur_pre_build(maintainers=['gcala'])
+    add_depends(['futuresql-git'])
 
     for line in edit_file('PKGBUILD'):
         line = line.replace("'fakeroot'", "")
