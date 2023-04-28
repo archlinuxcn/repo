c168431b44de60ccdb988991db1cbd1bccb4b0e1
Integral <integral@mail.dlut.edu.cn>
2023-04-28 23:46:13 +0800

    Remove redundant CFLAGS in hyprland-git

--- archlinuxcn/hyprland-git/lilac.py
+++ archlinuxcn/hyprland-git/lilac.py
@@ -5,8 +5,3 @@ from lilaclib import *
 def pre_build():
     aur_pre_build(maintainers=['That1Calculator'])
     add_depends(['libdisplay-info.so'])
-
-    for line in edit_file('PKGBUILD'):
-        print (line)
-        if line.strip().startswith('build()'):
-            print ('export CFLAGS+=-w')
