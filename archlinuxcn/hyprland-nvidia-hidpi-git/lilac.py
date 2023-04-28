3c53147cd7b37bccc5dc18d8a8e126352571aabb
Integral <integral@mail.dlut.edu.cn>
2023-04-28 23:49:33 +0800

    Remove redundant aur_pre_build in hyprland-nvidia-hidpi-git

--- archlinuxcn/hyprland-nvidia-hidpi-git/lilac.py
+++ archlinuxcn/hyprland-nvidia-hidpi-git/lilac.py
@@ -3,12 +3,11 @@
 from lilaclib import *
 
 def pre_build():
-    aur_pre_build(maintainers=['epiphyllosper'])
     add_depends(['libdisplay-info.so'])
 
     for line in edit_file('PKGBUILD'):
         print (line.replace('xorg-xwayland-hidpi-xprop', 'xorg-xwayland-lily'))
-        
+
     update_pkgrel()
     vcs_update()
 
