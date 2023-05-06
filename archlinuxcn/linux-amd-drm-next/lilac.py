6b87ae38fc5dac9e434dbf7dcceef0ecdfb46e6a
Winston Wu <winstonwu91@gmail.com>
2023-05-07 01:47:06 +0800

    [linux-amd-drm-next] Corrent lilac.py

--- archlinuxcn/linux-amd-drm-next/lilac.py
+++ archlinuxcn/linux-amd-drm-next/lilac.py
@@ -1,11 +1,11 @@
 from lilaclib import *
 
 def pre_build():
-  newver = _G.newver.removeprefix('amd-drm-next-')
+  newver = _G.newver.removeprefix('amd-drm-fixes-')
 
   for line in edit_file('PKGBUILD'):
       if line.startswith('_tag'):
-          line = "_tag='amd-drm-next-" + newver + "'"
+          line = "_tag='amd-drm-fixes-" + newver + "'"
       print(line)
   newver2 = newver.replace("-",".")
   update_pkgver_and_pkgrel(newver2)
