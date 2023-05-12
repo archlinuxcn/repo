8afe08d54d54d8c1879a8af46877c1bb6653ac17
Integral <integral@murena.io>
2023-05-13 00:25:20 +0800

    Fix typo in osc

--- archlinuxcn/osc/lilac.py
+++ archlinuxcn/osc/lilac.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
 
 def pre_build():
-    add_depends(['python-cryptography', 'python-urllib3'])
     aur_pre_build(maintainers=['Thaodan'])
+    add_depends(['python-cryptography', 'python-urllib3'])
