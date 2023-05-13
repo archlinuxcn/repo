a0d340880061012a59df371b8bd947a8cda2431a
Integral <integral@mail.dlut.edu.cn>
2023-05-13 20:33:50 +0800

    Fix unknown terminal error in build() of qcm-git

--- archlinuxcn/qcm-git/lilac.py
+++ archlinuxcn/qcm-git/lilac.py
@@ -5,3 +5,8 @@ from lilaclib import *
 def pre_build():
     aur_pre_build(maintainers=['Kimiblock'])
     add_depends(['qt6-quick3d'])
+
+    for line in edit_file('PKGBUILD'):
+        print (line)
+        if line.startswith('function _info'):
+            print ('export TERMINFO=/usr/lib/terminfo')
