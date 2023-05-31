ddb262feb08727b145e13a70d9bff57521cd64a5
Integral <integral@murena.io>
2023-05-31 14:22:37 +0800

    Add linux-ck-x64-v4

---
+++ archlinuxcn/linux-ck-x64-v4/lilac.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+
+from lilaclib import *
+
+def pre_build():
+    aur_pre_build(name='linux-ck', maintainers=['graysky'])
+
+    for line in edit_file('PKGBUILD'):
+        if line.strip().startswith('_subarch='):
+            print('_subarch=42') # Generic-x86-64-v4
+        elif line.strip().startswith('pkgbase='):
+            print('pkgbase=linux-ck-x64-v4')
+        else:
+            print(line)
