c326305b49ab77f2d2123b7815bd25bdf70ed074
Integral <integral@mail.dlut.edu.cn>
2023-05-31 11:27:31 +0800

    Remove waybar-hyprland

--- archlinuxcn/waybar-hyprland/lilac.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/python3
-
-from lilaclib import *
-
-def pre_build():
-    aur_pre_build(maintainers=['PowerBall253'])
-    add_depends(['playerctl'])
-    update_pkgver_and_pkgrel(_G.newver)
