a7abeb474c4f175bcc8b34d876ed27a6e22827e4
Integral <integral@murena.io>
2023-05-31 14:17:00 +0800

    Modify pkgbase of linux-ck-x64-v3

--- archlinuxcn/linux-ck-x64-v3/lilac.py
+++ archlinuxcn/linux-ck-x64-v3/lilac.py
@@ -6,7 +6,9 @@ def pre_build():
     aur_pre_build(name='linux-ck', maintainers=['graysky'])
 
     for line in edit_file('PKGBUILD'):
-        if line.strip().startswith('_subarch'):
+        if line.strip().startswith('_subarch='):
             print('_subarch=41') # Generic-x86-64-v3
+        elif line.strip().startswith('pkgbase='):
+            print('pkgbase=linux-ck-x64-v3')
         else:
             print(line)
