4ce2d315c424b5561337ed574b91e174b6f5a142
Integral <integral@mail.dlut.edu.cn>
2023-07-06 12:14:49 +0800

    Modify LICENSE path of waybar-git

--- archlinuxcn/waybar-git/lilac.py
+++ archlinuxcn/waybar-git/lilac.py
@@ -4,6 +4,6 @@ from lilaclib import *
 
 def pre_build():
     aur_pre_build(maintainers=['Alexays','BlueGone','maximbaz'])
-    
+
     for line in edit_file('PKGBUILD'):
-        print(line.replace('LICENSE', '${pkgname}/LICENSE'))
+        print(line.replace('LICENSE', '${srcdir}/${pkgname}/LICENSE'))
