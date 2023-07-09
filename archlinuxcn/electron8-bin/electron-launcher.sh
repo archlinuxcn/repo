2bf6e049283bb460ebae5e359db24fa5dafdf742
lilac <lilac@build.archlinuxcn.org>
2023-07-09 20:21:11 +0800

    electron8-bin: auto updated to 1:8.5.5-5

--- archlinuxcn/electron8-bin/electron-launcher.sh
+++ archlinuxcn/electron8-bin/electron-launcher.sh
@@ -7,13 +7,13 @@ flags_file="${XDG_CONFIG_HOME:-$HOME/.config}/%%PKGNAME%%-flags.conf"
 declare -a flags
 
 if [[ -f "${flags_file}" ]]; then
-    mapfile -t < "${flags_file}"
+	mapfile -t < "${flags_file}"
 fi
 
 for line in "${MAPFILE[@]}"; do
-    if [[ ! "${line}" =~ ^[[:space:]]*#.* ]]; then
-        flags+=("${line}")
-    fi
+	if [[ ! "${line}" =~ ^[[:space:]]*#.* ]] && [[ -n "${line}" ]]; then
+		flags+=("${line}")
+	fi
 done
 
 exec /usr/lib/%%PKGNAME%%/%%PROJECTNAME%% "${flags[@]}" "$@"
