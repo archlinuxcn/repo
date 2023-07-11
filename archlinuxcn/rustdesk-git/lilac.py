434b6c859600f87e5b131db0e6efa00de48445ff
Integral <integral@murena.io>
2023-07-11 13:57:00 +0800

    Modify pkgver() of rustdesk-git

---
+++ archlinuxcn/rustdesk-git/lilac.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+
+from lilaclib import *
+
+def pre_build():
+    aur_pre_build(maintainers=['taotieren','Ataraxy','severach'])
+    
+    is_pkgver = false
+    for line in edit_file('PKGBUILD'):
+        if not is_pkgver
+            print(line)
+        else
+            if line.strip().startswith('}'):
+                is_pkgver = true 
+            else
+                continue
+
+        if line.strip().startswith('pkgver()'):
+            is_pkgver = true
+            print("set -u")
+            print("cd \"${srcdir}/${_pkgname}\"")
+            print("local _ver")
+            print("_ver=\"$(git describe --long --tags | sed -e 's/\([^-]*-g\)/r\\1/' -e 's/-/./g')\"")
+            print("_ver=\"${_ver#nightly.}\"")
+            print("echo \"${_ver}\"")
+            print("set +u")
