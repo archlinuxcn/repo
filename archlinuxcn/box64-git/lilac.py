8058b00c3cfb0c4bf41e6ff79c365a6ec42b6745
Integral <integral@murena.io>
2023-07-18 13:45:33 +0800

    Fix typo in pkgver() of box64-git

--- archlinuxcn/box64-git/lilac.py
+++ archlinuxcn/box64-git/lilac.py
@@ -8,3 +8,5 @@ def pre_build():
     for line in edit_file('PKGBUILD'):
         if line.strip().startswith('git describe --long --tags'):
             print("git describe --long --tags | sed 's/\\([^-]*-g\\)/r\\1/;s/-/./g;s/v//'")
+        else:
+            print(line)
