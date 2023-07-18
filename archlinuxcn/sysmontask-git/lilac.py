cdcb14551f6309ca24e44c3708d0b6d8380fd8f9
Integral <integral@murena.io>
2023-07-18 14:13:27 +0800

    Modify pkgver() and epoch of sysmontask-git

--- archlinuxcn/sysmontask-git/lilac.py
+++ archlinuxcn/sysmontask-git/lilac.py
@@ -6,7 +6,11 @@ def pre_build():
     aur_pre_build(name='sysmontask', maintainers=['yochananmarqos','camel-neeraj'])
 
     for line in edit_file('PKGBUILD'):
-        if line.startswith('pkgname='):
+        if line.strip().startswith('pkgname='):
             print ('pkgname=sysmontask-git')
+        elif line.strip().startswith('git describe'):
+            print ('git describe --long --tags --exclude v1.x.x | sed \'s/\\([^-]*-g\\)/r\\1/;s/-/./g;s/v//\'')
+        elif line.strip().startswith('epoch='):
+            print ('epoch=2')
         else:
             print (line.replace('$pkgname','sysmontask'))
