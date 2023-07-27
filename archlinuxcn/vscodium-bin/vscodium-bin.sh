063a450238be1b0ae60b8359bc4a528a25fea952
lilac <lilac@build.archlinuxcn.org>
2023-07-27 20:27:24 +0800

    vscodium-bin: auto updated to 1.80.1.23208-2

--- archlinuxcn/vscodium-bin/vscodium-bin.sh
+++ archlinuxcn/vscodium-bin/vscodium-bin.sh
@@ -4,8 +4,8 @@ XDG_CONFIG_HOME=${XDG_CONFIG_HOME:-~/.config}
 
 # Allow users to override command-line options
 if [[ -f $XDG_CONFIG_HOME/codium-flags.conf ]]; then
-   CODE_USER_FLAGS="$(cat $XDG_CONFIG_HOME/codium-flags.conf)"
+    IFS=' ' read -ra CODE_USER_FLAGS < "$XDG_CONFIG_HOME/codium-flags.conf"
 fi
 
 # Launch
-exec /opt/vscodium-bin/bin/codium "$@" $CODE_USER_FLAGS
+exec /opt/vscodium-bin/bin/codium "$@" "${CODE_USER_FLAGS[@]}"
