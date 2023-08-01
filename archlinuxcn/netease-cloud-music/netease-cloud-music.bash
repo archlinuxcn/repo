c6611b1d182d1b39f42ee57f802fb75ba93e4d5c
lilac <lilac@build.archlinuxcn.org>
2023-08-01 15:16:10 +0800

    Processing 4 orphaned packages:
    Removing 3 orphaned packages:
    
    - netease-cloud-music
    - smartgit
    - yandex-browser-beta
    
    Removing maintainers from 1 orphaned packages:
    
    - spotify: depended by blockify (@heavysink)
    
    The following packages are not found:
    
    - clion
    - goland
    
    closes #3373

--- archlinuxcn/netease-cloud-music/netease-cloud-music.bash
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-HERE="$(dirname "$(readlink -f "${0}")")"
-export LD_LIBRARY_PATH="${HERE}"/libs
-export QT_PLUGIN_PATH="${HERE}"/plugins 
-export QT_QPA_PLATFORM_PLUGIN_PATH="${HERE}"/plugins/platforms
-export QT_QPA_PLATFORM=xcb
-export LD_PRELOAD="${HERE}"/libnetease-patch.so
-
-exec "${HERE}"/netease-cloud-music $@
