6062868d5864a1525f42b845f501489e05f02895
Integral <integral@mail.dlut.edu.cn>
2023-08-01 20:02:17 +0800

    alarmcn/7-zip: tweak uasm_src of meson.build

---
+++ alarmcn/7-zip/lilac.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+
+from lilaclib import *
+
+def pre_build():
+    aur_pre_build(maintainers=['post-factum'])
+    add_arch(['aarch64'])
+
+    bool in_uasm_src = False
+
+    for line in edit_file('meson.build'):
+        if not in_uasm_src:
+            print(line)
+        elif line.strip().startswith(']'):
+            in_uasm_src = False
+
+        if line.strip().startswith('uasm_src'):
+            in_uasm_src = True
+            print("\'Asm/arm64/7zAsm.S\',")
+            print("\'Asm/arm64/LzmaDecOpt.S\',")
+            print(']')
