133eb3960edfb73460bf6850f54d8aeb0f288414
Integral <integral@mail.dlut.edu.cn>
2023-08-11 19:53:32 +0800

    hyprpaper-git: modify update source & arch

--- archlinuxcn/hyprpaper-git/lilac.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/python3
-
-from lilaclib import *
-
-def pre_build():
-    aur_pre_build(maintainers=['That1Calculator'])
-
-    for line in edit_file('PKGBUILD'):
-        print (line.replace('livglvnd', 'libglvnd'))
