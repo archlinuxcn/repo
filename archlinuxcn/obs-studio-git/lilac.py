969accab9ef23e95b84351a1495fb4368bd53912
Integral <integral@murena.io>
2023-09-06 11:07:41 +0800

    obs-studio-git: remove lilac.py

--- archlinuxcn/obs-studio-git/lilac.py
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/python3
-
-from lilaclib import *
-
-def pre_build():
-    aur_pre_build(maintainers=['benklett'])
-    add_makedepends(['asio', 'nlohmann-json', 'websocketpp', 'onevpl', 'libdatachannel'])
