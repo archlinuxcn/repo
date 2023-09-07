bc99418f58bd71eeb4dc555a3a0ac6c7bbc623aa
Chih-Hsuan Yen <645432-yan12125@users.noreply.gitlab.com>
2023-09-07 23:54:10 +0800

    android-ndk: add $ANDROID_NDK_ROOT
    This variable seems common enough, as it is requested twice:
    https://aur.archlinux.org/packages/android-ndk#comment-932606 on 2023-09-06 07:46 (UTC)
    https://aur.archlinux.org/packages/android-ndk#comment-640302 on 2018-03-31 16:39 (UTC)

--- archlinuxcn/android-ndk/android-ndk.sh
+++ archlinuxcn/android-ndk/android-ndk.sh
@@ -2,3 +2,5 @@ export PATH="$PATH:/opt/android-ndk"
 export ANDROID_NDK=/opt/android-ndk
 # Some programs such as gradle ask this as well:
 export ANDROID_NDK_HOME=/opt/android-ndk
+# Some other programs like Godot wants this:
+export ANDROID_NDK_ROOT=/opt/android-ndk
