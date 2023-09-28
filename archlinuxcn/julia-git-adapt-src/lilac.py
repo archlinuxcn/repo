5ebe65a76e08018820a0a8e9aeb6de45de85997a
Yichao Yu <yyc1992@gmail.com>
2023-09-28 09:45:17 -0400

    [julia-git-*-src] strip white space

--- archlinuxcn/julia-git-adapt-src/lilac.py
+++ archlinuxcn/julia-git-adapt-src/lilac.py
@@ -4,6 +4,8 @@ from lilaclib import *
 
 def pre_build():
   pkgver, commit = _G.newver.split('@')
+  pkgver = pkgver.strip()
+  commit = commit.strip()
   for line in edit_file('PKGBUILD'):
     if line.startswith('_commit='):
       line = f'_commit={commit}'
