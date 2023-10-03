cbb371be38edd910c362b42390074bae3e508168
Yichao Yu <yyc1992@gmail.com>
2023-10-02 22:21:12 -0400

    [julia-git] support julia version independent packages

--- alarmcn/julia-git/julia-clear-precompile.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-for d in "$1"/*; do
-    if [[ -f "$d"/.archpkg ]]; then
-        # Packaged precompiled cache, do not delete
-        continue
-    fi
-    rm -rf "$d" &> /dev/null
-done
--- alarmcn/julia-git/julia-postinstall.sh
+++ alarmcn/julia-git/julia-postinstall.sh
@@ -4,5 +4,11 @@ if [ -f /usr/lib/julia/.package-precompile-env ]; then
     . /usr/lib/julia/.package-precompile-env
 fi
 
+for d in "$2"/*; do
+    if [[ -d "$d" ]]; then
+        ln -sr "$d" "$1"/
+    fi
+done
+
 exec /usr/bin/julia --startup-file=no \
      /usr/lib/julia/julia-compile-pkgs.jl /usr/share/julia
