70649ea1da049769adc5e50d691c85e12b8d909a
Integral <integral@murena.io>
2023-10-15 12:47:32 +0800

    hyprland-git: add makedepends & modify prepare() by lilac.py

--- archlinuxcn/hyprland-git/lilac.py
+++ archlinuxcn/hyprland-git/lilac.py
@@ -4,4 +4,9 @@ from lilaclib import *
 
 def pre_build():
     aur_pre_build(maintainers=['memchr'])
-    add_depends(['libdisplay-info.so'])
+    add_makedepends(['jq'])
+
+    for line in edit_file('PKGBUILD'):
+        print(line)
+        if line.strip().startswith('popd'):
+            print('pick_mr 3571')
