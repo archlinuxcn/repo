8b76e6ef9c6131d0ad6b5b2f48540c59491cfb38
Integral <integral@murena.io>
2023-10-17 21:25:02 +0800

    hyprland-git: remove redundant fix in pre_build()

--- archlinuxcn/hyprland-git/lilac.py
+++ archlinuxcn/hyprland-git/lilac.py
@@ -5,9 +5,3 @@ from lilaclib import *
 def pre_build():
     aur_pre_build(maintainers=['memchr'])
     add_makedepends(['jq'])
-
-    for line in edit_file('PKGBUILD'):
-        print(line)
-        if line.strip().startswith('cd Hyprland'):
-            print('git config pull.rebase true')
-            print('pick_mr 3571')
