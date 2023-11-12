ab413494136c6587e838d525a4607bae1a33f41e
lilydjwg <lilydjwg@gmail.com>
2023-11-12 19:34:58 +0800

    add telegram-desktop-lily

---
+++ archlinuxcn/telegram-desktop-lily/lilac.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+from lilaclib import *
+
+
+def pre_build():
+    update_pkgver_and_pkgrel(_G.newver.lstrip('v').lstrip())
+
+def post_build():
+    git_add_files('PKGBUILD')
+    git_commit()
+
+if __name__ == '__main__':
+    single_main()
