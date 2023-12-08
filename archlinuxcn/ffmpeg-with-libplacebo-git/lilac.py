ce97ff8ebd5612ac70b39a2fc4063bd4dc6545d6
Coelacanthus <CoelacanthusHex@gmail.com>
2023-12-08 20:52:56 +0800

    ffmpeg-with-libplacebo-git: fix build

--- archlinuxcn/ffmpeg-with-libplacebo-git/lilac.py
+++ archlinuxcn/ffmpeg-with-libplacebo-git/lilac.py
@@ -6,9 +6,9 @@ g = SimpleNamespace()
 
 def pre_build():
   g.files = download_official_pkgbuild('ffmpeg')
-  add_provides('ffmpeg')
-  add_conflicts('ffmpeg')
-  add_depends('libplacebo-git')
+  add_provides(['ffmpeg'])
+  add_conflicts(['ffmpeg'])
+  add_depends(['libplacebo-git'])
   for line in edit_file('PKGBUILD'):
     if line.startswith('pkgname='):
       line = 'pkgname=ffmpeg-with-libplacebo-git'
