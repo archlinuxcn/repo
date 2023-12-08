e74aeecfcec0c63def9a99cf4b4d81207f629bf0
Coelacanthus <CoelacanthusHex@gmail.com>
2023-12-08 13:29:04 +0800

    mpv-git: ffmpeg depends on libplacebo now
    - Disable demos for libplacebo-git to break circular dependency
    - Add ffmpeg-with-libplacebo-git
    
    Signed-off-by: Coelacanthus <CoelacanthusHex@gmail.com>

---
+++ archlinuxcn/ffmpeg-with-libplacebo-git/lilac.py
@@ -0,0 +1,19 @@
+from types import SimpleNamespace
+
+from lilaclib import *
+
+g = SimpleNamespace()
+
+def pre_build():
+  g.files = download_official_pkgbuild('ffmpeg')
+  add_provides('ffmpeg')
+  add_conflicts('ffmpeg')
+  add_depends('libplacebo-git')
+  for line in edit_file('PKGBUILD'):
+    if line.startswith('pkgname='):
+      line = 'pkgname=ffmpeg-with-libplacebo-git'
+    print(line)
+
+def post_build():
+  git_add_files(g.files)
+  git_commit()
