82446a008f57c388b84a4b4c1c8c1d6bcb47fc76
lilydjwg <lilydjwg@gmail.com>
2023-12-08 15:25:29 +0800

    gtk3-lily: remove unused package_* functions
    closes #3553.

--- archlinuxcn/gtk3-lily/lilac.py
+++ archlinuxcn/gtk3-lily/lilac.py
@@ -7,6 +7,10 @@ g = SimpleNamespace()
 def pre_build():
   g.files = download_official_pkgbuild('gtk3')
   run_cmd(['patch', '-Np1', 'PKGBUILD', 'pkgbuild.patch'])
+  for l in edit_file('PKGBUILD'):
+    if l.startswith('package_gtk3-demos'):
+      break
+    print(l)
 
 def post_build():
   git_add_files(g.files)
