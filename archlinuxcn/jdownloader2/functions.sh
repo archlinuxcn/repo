ad4d5a7d07be106002948d34bc5807e7d2abe808
lilac <lilac@build.archlinuxcn.org>
2023-12-30 12:35:55 +0800

    jdownloader2: auto updated to latest-19

--- archlinuxcn/jdownloader2/functions.sh
+++ archlinuxcn/jdownloader2/functions.sh
@@ -1,48 +1,45 @@
 #!/usr/bin/env bash
 
-function changePath(){
-  # check the groups of the current loggedin users
-  groups | grep -q -E '(\s|^)jdownloader(\s|$)' >/dev/null
-  if [ "$?" -eq  0 ] || isRoot ; then
-    export JD_SCOPE="global"
-    echo "[global JDownloader scope]"
-    umask u=rwx,g=rwx,o=rx
-    cd '/opt/JDownloader'
-  else
-    export JD_SCOPE="user"
-    echo "[user JDownloader scope]"
-    mkdir -p "${HOME}/.jd"
-    cd "${HOME}/.jd"
-  fi
+function changePath() {
+	# check the groups of the current loggedin users
+	groups | grep -q -E '(\s|^)jdownloader(\s|$)' >/dev/null
+	if [ "$?" -eq  0 ] || isRoot ; then
+		export JD_SCOPE="global"
+		echo "[global JDownloader scope]"
+		umask u=rwx,g=rwx,o=rx
+		cd '/opt/JDownloader'
+	else
+		export JD_SCOPE="user"
+		echo "[user JDownloader scope]"
+		mkdir -p "${HOME}/.jd"
+		cd "${HOME}/.jd"
+	fi
 }
 
-function changeUser(){
-  if isRoot ; then
-    #restart as user jdownloader
-    echo "changing to user \"jdownloader\""
-    runuser jdownloader -c "/bin/bash $0 $@" -s /bin/bash
-    exit $?
-  fi
-  
+function changeUser() {
+	if isRoot; then
+		# restart as user jdownloader
+		echo "changing to user \"jdownloader\""
+		runuser jdownloader -c "/bin/bash $0 $@" -s /bin/bash
+		exit $?
+	fi
 }
 
-function isRoot(){
-  if [ "$(id -u)" -eq "0" ]; then
-    return 0
-  fi
-  return 2
+function isRoot() {
+	if [ "$(id -u)" -eq "0" ]; then
+		return 0
+	fi
+	return 2
 }
 
-
-function downloadJDownloader(){
-  changePath
-  if [ ! -f "JDownloader.jar" ]; then
-    wget -O JDownloader.jar http://installer.jdownloader.org/JDownloader.jar
-    if [ $? -ne 0 ]; then
-      echo "Cannot download Jdownloader!"
-      exit 2
-    fi
-  fi
+function downloadJDownloader() {
+	changePath
+	if [ ! -f "JDownloader.jar" ]; then
+		if ! curl -o JDownloader.jar https://installer.jdownloader.org/JDownloader.jar; then
+			echo "Cannot download JDownloader!"
+			exit 2
+		fi
+	fi
 }
+
 LOGFILE="JDownloader.service.log"
-
