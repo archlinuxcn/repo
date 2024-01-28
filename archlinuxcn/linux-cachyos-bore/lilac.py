f26dbd9800d24ca34cf2854b5e1631db964ab6f0
Integral <integral@member.fsf.org>
2024-01-28 12:25:07 -0800

    linux-cachyos-bore: fix lilac.py

--- archlinuxcn/linux-cachyos-bore/lilac.py
+++ archlinuxcn/linux-cachyos-bore/lilac.py
@@ -4,9 +4,10 @@ from lilaclib import *
 
 def pre_build():
     aur_pre_build(maintainers=['ptr1337','sir_lucjan'])
-    run_cmd(['updpkgsums'])
 
     for line in edit_file('auto-cpu-optimization.sh'):
         if line.startswith('MARCH=$'):
             line = 'MARCH=CONFIG_GENERIC_CPU3'
         print(line)
+
+    run_cmd(['updpkgsums'])
