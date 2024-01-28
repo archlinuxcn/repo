e15b05d2eb1f7600b0dd47f9a9937b2501d59635
Integral <integral@member.fsf.org>
2024-01-28 11:55:34 -0800

    mutter-performance: use SPDX license identifier

--- archlinuxcn/mutter-performance/lilac.py
+++ archlinuxcn/mutter-performance/lilac.py
@@ -6,4 +6,6 @@ def pre_build():
     aur_pre_build(maintainers=['Terence','Saren','Dawdleming'])
 
     for line in edit_file('PKGBUILD'):
+        if line.startswith('license='):
+            line = "license=('GPL-2.0-or-later')"
         print (line.replace('groups=(gnome)', ''))
