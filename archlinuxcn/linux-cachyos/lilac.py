c3c6bd4a1e2a88d07ddfab183ac7d73e1d579cd3
Integral <integral@member.fsf.org>
2024-01-28 12:24:43 -0800

    linux-cachyos: fix lilac.py

--- archlinuxcn/linux-cachyos/lilac.py
+++ archlinuxcn/linux-cachyos/lilac.py
@@ -4,9 +4,10 @@ from lilaclib import *
 
 def pre_build():
     aur_pre_build(maintainers=['ptr1337','sir_lucjan'])
-    run_cmd(['updpkgsums'])
 
     for line in edit_file('PKGBUILD'):
         if line.startswith('license='):
             line = "license=('GPL-2.0-only')"
         print(line)
+
+    run_cmd(['updpkgsums'])
