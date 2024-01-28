24144549b4e05f3edab340514623f8b6209731ff
Integral <integral@member.fsf.org>
2024-01-28 11:05:37 -0800

    linux-clear-x64-v3: use SPDX license identifier

--- archlinuxcn/linux-clear-x64-v3/lilac.py
+++ archlinuxcn/linux-clear-x64-v3/lilac.py
@@ -10,5 +10,7 @@ def pre_build():
             print('_subarch=38') # Generic-x86-64-v3
         elif line.strip().startswith('pkgbase='):
             print('pkgbase=linux-clear-x64-v3')
+        elif line.strip().startswith('license='):
+            print("license=('GPL-2.0-only')")
         else:
             print(line)
