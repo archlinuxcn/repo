9550f7e11ebeb67b739017bf2e389dd44e0f5765
Integral <integral@murena.io>
2024-02-19 20:44:32 -0800

    hyprland-hidpi-xprop-git: add libhyprlang.so to depends

--- archlinuxcn/hyprland-hidpi-xprop-git/lilac.py
+++ archlinuxcn/hyprland-hidpi-xprop-git/lilac.py
@@ -4,5 +4,5 @@ from lilaclib import *
 
 def pre_build():
     aur_pre_build(maintainers=['Aleksana', 'q234rty'])
-    add_depends(['libdisplay-info.so'])
+    add_depends(['libdisplay-info.so', 'libhyprlang.so'])
     add_replaces(['hyprland-nvidia-hidpi-git'])
