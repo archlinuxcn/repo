23d2aa1928ed2978d97864896dfcf441b04d8f84
oldherl <oldherl@gmail.com>
2024-02-23 01:15:50 +1100

    qt6-base-24hms: same fixes as qt6-base-24h

--- archlinuxcn/qt6-base-24hms/lilac.py
+++ archlinuxcn/qt6-base-24hms/lilac.py
@@ -16,19 +16,29 @@ def pre_build():
     p = line.strip()
     conflict_string = conflict_string + '"' + p + '>$pkgver" '
 
-  prepare = False
+  in_prepare = False
+  in_build_qt6_base = False
   checks = ''
+  variant = '-24hms'
+  variant_sha256 = '6ab571d50f0c17d31ed33bb2ebf6eadf41c18cbf2dd8cb5260b939915a139a28'
+  variant_desc = '24-hour HH:mm:ss notation'
   for line in edit_file('PKGBUILD'):
     if line.startswith('pkgrel='):
-      line = line + '.2'
+      line = line + '.3'
+    elif line.startswith('pkgbase='):
+      line = f"pkgbase=qt6-base{variant}"
+      checks = checks + 'f'
     elif line.startswith('pkgname='):
-      line = 'pkgname=qt6-base-24hms'
-      checks = checks + '1'
-    elif line.startswith('pkgdesc='):
-      line = "pkgdesc='A cross-platform application and UI framework. This package uses 24-hour notation HH:mm:ss in all locales.'"
+      if line == "pkgname=(qt6-base qt6-xcb-private-headers)":
+        line = f"pkgname=(qt6-base{variant})"
+        checks = checks + '1'
+      else:
+        raise ValueError('PKGBUILD pkgname mismatch with preset')
+    elif in_build_qt6_base and line.strip().startswith('pkgdesc='):
+      line = "pkgdesc='A cross-platform application and UI framework. This package uses {variant_desc} in all locales.'"
       checks = checks + 'b'
     elif line.startswith('makedepends=('):
-      line = line.replace('(', "(python 'zstd>=1.5.2-7' ")
+      line = line.replace('(', "(python ")
       checks = checks + '2'
     elif line.startswith('groups=('):
       line = '''
@@ -36,36 +46,43 @@ provides=("qt6-base=$pkgver")
 conflicts=(qt6-base ''' + conflict_string + ")" # remove official groups
       checks = checks + '4'
     elif line.startswith('_pkgfn='):
-      line = line.replace('${pkgname/6-/}', 'qtbase')
+      line = line.replace('${pkgbase/6-/}', 'qtbase')
       checks = checks + '5'
     elif line.startswith('source=('):
-      line = line.replace('=(', '''=(
-      oldherl-24hms.patch
+      line = line.replace('=(', f'''=(
+      oldherl{variant}.patch
       'https://build.archlinuxcn.org/~oldherl/files/cldr/44/core.zip'
       'https://iso639-3.sil.org/sites/iso639-3/files/downloads/iso-639-3.tab'
       ''')
       checks = checks + '6'
     elif line.startswith('sha256sums=('):
-      line = line.replace('=(', '''=(
-      '97ab390edb9b8f452f42138f9dfb2184e5a2bf0a2dddd02c3b6afbc448bd6997'
+      line = line.replace('=(', f'''=(
+      '{variant_sha256}'
       'bd7f70adfe8a999cfa0f5d96145bf0a095b1a099024e4f451fa1afccf6e4aee6'
       '9660ebcab661e7a6bbb194a6c031fb89bea532af4f34fa5d99d653c20d9562cb'
       ''')
       checks = checks + '7'
     elif line.startswith('prepare('):
-      prepare = True
+      in_prepare = True
       checks = checks + '8'
-    elif prepare and line.startswith('}'):
-      line = '''
+    elif in_prepare and line.startswith('}'):
+      line = f'''
   cd $_pkgfn
-  patch -p1 -i ../oldherl-24hms.patch
+  patch -p1 -i ../oldherl{variant}.patch
   cd util/locale_database
   echo "This is slow. It takes about 4 minutes on my desktop."
   ./cldr2qlocalexml.py ../../../ > ./24h.xml
   ./qlocalexml2cpp.py ./24h.xml ../../../iso-639-3.tab ../..
 ''' + line
-      prepare = False
+      in_prepare = False
       checks = checks + '9'
+    elif in_build_qt6_base and line.startswith('}'):
+      in_build_qt6_base = False
+      checks = checks + 'e'
+    elif line.startswith('package_qt6-base('):
+      line = line.replace('(', f'{variant}(')
+      in_build_qt6_base = True
+      checks = checks + 'd'
     elif line.startswith('package_'):
       # other split packages. do not build them.
       line = 'no' + line
@@ -75,7 +92,7 @@ conflicts=(qt6-base ''' + conflict_string + ")" # remove official groups
       line = line.replace('=(', '=(libicui18n.so ')
       checks = checks + 'c'
     print(line)
-  if len(checks) != 10:
+  if len(checks) != 13:
     raise ValueError('PKGBUILD editing not completed. checks=' + checks)
 
 def post_build():
