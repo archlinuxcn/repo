394aefd6d951ae614cb964cec94834ce3a06c4cb
lilydjwg <lilydjwg@gmail.com>
2024-03-18 10:22:53 +0800

    pacman-lily: deprecating

--- archlinuxcn/pacman-lily/lilac.py
+++ archlinuxcn/pacman-lily/lilac.py
@@ -18,7 +18,7 @@ def pre_build():
     elif line.startswith('groups=('):
       continue
     elif line.startswith('source=('):
-      line = line.replace('(', '(pacman.patch\n        pacman-syncdb\n        ')
+      line = 'install=lily.install\n' + line.replace('(', '(pacman.patch\n        pacman-syncdb\n        ')
     elif line.startswith('sha256sums=('):
       line = line.replace('(', '(38cffbcb9d912111549fdf3cf151d6e2af4a35a9af0c347f209b7bea9f1dc09e\n            207d5cee261bba18e650bbd2c249ffd8fe9c1dbd7de6b241d8bf011848faa70b\n            ')
     elif line.startswith('package() '):
