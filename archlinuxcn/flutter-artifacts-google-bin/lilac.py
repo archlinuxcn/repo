cb88b2dbbee16a951d14562291cd3fb30b403acf
Integral <integral@member.fsf.org>
2024-03-26 21:04:43 -0700

    flutter-artifacts-google-bin: update pkgrel

--- archlinuxcn/flutter-artifacts-google-bin/lilac.py
+++ archlinuxcn/flutter-artifacts-google-bin/lilac.py
@@ -8,4 +8,6 @@ def pre_build():
     for line in edit_file('PKGBUILD'):
         if line.startswith('license='):
             line = "license=('BSD-3-Clause')"
+        if line.startswith('pkgrel='):
+            line = "pkgrel=10"
         print(line)
