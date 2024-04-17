47688d1b5de79332a8f6f7f1e7155dc3fc60732e
Integral <integral@member.fsf.org>
2024-04-17 14:57:53 -0700

    linux-cachyos-bore: clean directory before updating from AUR

--- archlinuxcn/linux-cachyos-bore/lilac.py
+++ archlinuxcn/linux-cachyos-bore/lilac.py
@@ -3,11 +3,10 @@
 from lilaclib import *
 
 def pre_build():
+    clean_directory()
     aur_pre_build(maintainers=['ptr1337','sir_lucjan'])
 
     for line in edit_file('auto-cpu-optimization.sh'):
         if line.startswith('MARCH=$'):
             line = 'MARCH=CONFIG_GENERIC_CPU3'
         print(line)
-
-    run_cmd(['updpkgsums'])
