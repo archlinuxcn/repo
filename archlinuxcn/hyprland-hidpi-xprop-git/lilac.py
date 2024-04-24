cd3b408eb8a85fe558fb6543916d27b7462340ef
Integral <integral@member.fsf.org>
2024-04-23 23:23:44 -0700

    hyprland-hidpi-xprop-git: add hyprwayland-scanner to makedepends

--- archlinuxcn/hyprland-hidpi-xprop-git/lilac.py
+++ archlinuxcn/hyprland-hidpi-xprop-git/lilac.py
@@ -5,4 +5,5 @@ from lilaclib import *
 def pre_build():
     aur_pre_build(maintainers=['Aleksana', 'q234rty'])
     add_depends(['libdisplay-info.so', 'libhyprlang.so'])
+    add_makedepends(['hyprwayland-scanner'])
     add_replaces(['hyprland-nvidia-hidpi-git'])
