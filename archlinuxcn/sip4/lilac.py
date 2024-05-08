7a46cc667f244ed1d2962b55aca76d1d76b39c13
Integral <integral@member.fsf.org>
2024-05-08 12:15:00 -0700

    sip4/python-sip4: remove unused provides & replaces

--- archlinuxcn/sip4/lilac.py
+++ archlinuxcn/sip4/lilac.py
@@ -6,5 +6,5 @@ def pre_build():
     aur_pre_build(maintainers=['daizhirui'])
 
     for line in edit_file('PKGBUILD'):
-        if not (line.strip().startswith("replaces=('sip")):
+        if not ((line.strip().startswith("provides=(python-sip4")) or line.strip().startswith("replaces=(python-sip4")):
             print (line)
