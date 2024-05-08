5716daf0d212fdbb79556af2e0f571d5c96178e2
Integral <integral@member.fsf.org>
2024-05-08 12:18:33 -0700

    sip4/python-sip4: clean up provides & replaces

--- archlinuxcn/sip4/lilac.py
+++ archlinuxcn/sip4/lilac.py
@@ -6,5 +6,6 @@ def pre_build():
     aur_pre_build(maintainers=['daizhirui'])
 
     for line in edit_file('PKGBUILD'):
-        if not ((line.strip().startswith("provides=(python-sip4")) or line.strip().startswith("replaces=(python-sip4")):
+        stripped = line.strip()
+        if not (stripped.startswith("provides=(sip4") or stripped.startswith("provides=(python-sip4") or stripped.startswith("replaces=(python-sip4")):
             print (line)
