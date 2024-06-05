cb23eee6292aea09640a1406200d2c208ee41745
lilydjwg <lilydjwg@gmail.com>
2024-06-05 16:57:15 +0800

    nginx-mod-lua: set nginx version on depends

--- archlinuxcn/nginx-mod-lua/lilac.py
+++ archlinuxcn/nginx-mod-lua/lilac.py
@@ -1,7 +1,16 @@
 from lilaclib import *
 
 def pre_build():
+  depends_nginx_re = re.compile(
+    r'''^(depends=.*?)['"]?nginx[^'") ]*['"]?''')
+
+  nginx_ver = _G.newvers[1]
   update_pkgver_and_pkgrel(_G.newver)
+  for line in edit_file('PKGBUILD'):
+    if line.startswith('depends='):
+      line = depends_nginx_re.sub(
+        r'\1"nginx=%s"' % nginx_ver, line)
+    print(line)
 
 def post_build():
   git_pkgbuild_commit()
