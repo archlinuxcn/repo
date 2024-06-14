681a45f6fb289c60c8c2e9887523cbb6d16dbcaf
lilydjwg <lilydjwg@gmail.com>
2024-06-14 11:19:01 +0800

    telegram-desktop-lily: don't proceed if upstream hasn't released the source tarball

--- archlinuxcn/telegram-desktop-lily/lilac.py
+++ archlinuxcn/telegram-desktop-lily/lilac.py
@@ -1,19 +1,27 @@
 import re
 from lilaclib import (
-    update_pkgver_and_pkgrel, edit_file,
-    git_add_files, git_commit,
+  update_pkgver_and_pkgrel, edit_file,
+  git_add_files, git_commit, s,
 )
 
 qt_re = re.compile(r"'qt6-base=[^']+'")
 
+def prepare():
+  pkgver = _G.newver.lstrip('v').lstrip()
+  url = f'https://github.com/telegramdesktop/tdesktop/releases/download/v{pkgver}/tdesktop-{pkgver}-full.tar.gz'
+  r = s.head(url, allow_redirects=False)
+  if r.status_code == 404:
+    return 'upstream source tarball not available'
+
 def pre_build():
-    qt_ver = _G.newvers[2].split('-')[0]
-    update_pkgver_and_pkgrel(_G.newver.lstrip('v').lstrip())
-    for line in edit_file('PKGBUILD'):
-        if 'qt6-base' in line:
-            line = qt_re.sub(f"'qt6-base={qt_ver}'", line)
-        print(line)
+  pkgver = _G.newver.lstrip('v').lstrip()
+  qt_ver = _G.newvers[2].split('-')[0]
+  update_pkgver_and_pkgrel(pkgver)
+  for line in edit_file('PKGBUILD'):
+    if 'qt6-base' in line:
+      line = qt_re.sub(f"'qt6-base={qt_ver}'", line)
+    print(line)
 
 def post_build():
-    git_add_files('PKGBUILD')
-    git_commit()
+  git_add_files('PKGBUILD')
+  git_commit()
