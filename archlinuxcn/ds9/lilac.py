d9ed840ec76acc97835640b8db68b87fd25dd26a
oldherl <oldherl@gmail.com>
2024-06-15 01:18:22 +1000

    ds9: update to 8.6b1

--- archlinuxcn/ds9/lilac.py
+++ archlinuxcn/ds9/lilac.py
@@ -5,9 +5,9 @@ from lilaclib import *
 
 # TODO(oldherl): this package bundles tcl/tk sources
 # Should we remove it and use Arch dependencies some day?
-def pre_build():
-    aur_pre_build(maintainers=['edombek', 'ydallilar'])
-    add_depends(['libxml2', 'libx11', 'desktop-file-utils'])
+#def pre_build():
+#    aur_pre_build(maintainers=['edombek', 'ydallilar'])
+#    add_depends(['libxml2', 'libx11', 'desktop-file-utils'])
 
 #post_build = aur_post_build
 
