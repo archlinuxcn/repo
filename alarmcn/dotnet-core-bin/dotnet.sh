de95f8575bfe4703257d5f9cf92015d54f1ddc45
lilac-alarm <lilac@build.archlinuxcn.org>
2024-06-25 07:31:41 +0800

    ⏰ dotnet-core-bin: auto updated to 8.0.6.sdk302-2

--- alarmcn/dotnet-core-bin/dotnet.sh
+++ alarmcn/dotnet-core-bin/dotnet.sh
@@ -1,6 +1,13 @@
 # Set location for AppHost lookup
 [ -z "$DOTNET_ROOT" ] && export DOTNET_ROOT=/usr/share/dotnet
 
+# Add dotnet directory to PATH, according to docs it must be added, plus VSCode C# Dev Kit doesn't work without this.
+# See https://learn.microsoft.com/en-us/dotnet/core/install/linux-scripted-manual#set-environment-variables-system-wide
+case "$PATH" in
+    *"$DOTNET_ROOT"* ) true ;;
+    * ) PATH="$PATH:$DOTNET_ROOT" ;;
+esac
+
 # Add dotnet tools directory to PATH
 DOTNET_TOOLS_PATH="$HOME/.dotnet/tools"
 case "$PATH" in
