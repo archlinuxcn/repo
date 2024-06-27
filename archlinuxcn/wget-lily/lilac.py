42c98ba9cde29ea34b03aec6f7c93bd00149b081
lilydjwg <lilydjwg@gmail.com>
2024-06-27 14:45:33 +0800

    wget-lily: fix lilac.py

--- archlinuxcn/wget-lily/lilac.py
+++ archlinuxcn/wget-lily/lilac.py
@@ -1,19 +1,13 @@
-# Trimmed lilac.py
-#!/usr/bin/env python3
-
 from types import SimpleNamespace
 
 from lilaclib import *
 
 g = SimpleNamespace()
 
-#build_prefix = 'extra-x86_64'
-
 def pre_build():
   g.files = download_official_pkgbuild('wget')
 
-  prepare = False
-  sumwhich = 0
+  state = 'start'
   for line in edit_file('PKGBUILD'):
     if line.startswith('pkgname='):
       line = 'pkgname=wget-lily'
@@ -21,24 +15,34 @@ def pre_build():
       line = line + '''
 conflicts=('wget')
 provides=("wget=$pkgver")'''
+
     elif line.startswith('prepare('):
-      prepare = True
-    elif prepare and line.startswith('}'):
+      state = 'prepare'
+    elif state == 'prepare' and line.startswith('}'):
       line = '  patch -p1 -i "${srcdir}/wget.patch"\n' + line
-      prepare = False
+      state = ''
+
     elif line.startswith('source=('):
+      state = 'source'
+    elif state == 'source' and line.endswith(')'):
       line = line.replace(')', ' wget.patch)')
+      state = ''
+
     elif line.startswith('sha256sums='):
-      sumwhich = 0
+      state = 'sha256sums'
+    elif state == 'sha256sums' and line.endswith(')'):
+      line = line.replace(')', ' aa28cf8532f6b7cc1fe689eb253b0453102990eb725a6583704ca4f3e665c9b0)')
+      state = ''
+
     elif line.startswith('b2sums='):
-      sumwhich = 1
-    elif "'SKIP')" in line:
-      if sumwhich == 0:
-        line = line.replace(')', ' aa28cf8532f6b7cc1fe689eb253b0453102990eb725a6583704ca4f3e665c9b0)')
-      elif sumwhich == 1:
-        line = line.replace(')', ' c6a292c1be5606c007d1dcd25f02b3760ac98e86f2c1de30b50847b65c5ecfdb5cd89a482ab1e6a5b5978d38057c21d671b72ec45f10f6e59c5596519cecdf2e)')
+      state = 'b2sums'
+    elif state == 'b2sums' and line.endswith(')'):
+      line = line.replace(')', ' c6a292c1be5606c007d1dcd25f02b3760ac98e86f2c1de30b50847b65c5ecfdb5cd89a482ab1e6a5b5978d38057c21d671b72ec45f10f6e59c5596519cecdf2e)')
+      state = ''
+
     if '${pkgname}' in line:
       line = line.replace('${pkgname}', 'wget')
+
     print(line)
 
 def post_build():
