73ceef1fef39f486f5b231fc0cb268e7399e777e
lilac <lilac@build.archlinuxcn.org>
2024-07-16 15:26:23 +0800

    Removing 83 orphaned packages:
    - vim-youcompleteme-git
    - python-pyephem
    - transmission-web-control-git
    - resvg
    - xray-geoip
    - jql
    - cppo
    - flat-remix-gtk
    - i3lock-color
    - rxvt-unicode-better-wheel-scrolling
    - bsnes
    - grub-themes-git
    - firefox-esr-extension-privacybadger
    - vimix-gtk-themes-git
    - okteto
    - neovim-remote
    - ocaml-octavius
    - python-clickgen
    - rofi-themes
    - ocaml-biniou
    - mcrcon
    - lean-community
    - adobe-source-han-mono-fonts
    - ocaml-ppxlib
    - vimix-gtk-themes
    - v2ray-git
    - ibus-libzhuyin
    - xray
    - perl-file-rename
    - rait
    - ttf-comfortaa
    - clash-dashboard-git
    - surface-control
    - rime-cloverpinyin
    - mailspring
    - firefox-esr
    - kodi-standalone-service
    - bigtime
    - browsh
    - rime-symbols
    - courier-unicode
    - vmessping
    - firefox-esr-decentraleyes
    - lightdm-mini-greeter
    - elementary-planner
    - star
    - icecat
    - xray-domain-list-community
    - linux-dctxmei
    - betterlockscreen
    - htpdate
    - vimix-icon-theme
    - vimix-cursors
    - getmail6
    - v2raya-tray-git
    - ocaml-seq
    - bibata-cursor-theme
    - geph4-client
    - ungoogled-chromium
    - ocaml-easy-format
    - nginx-mod-fancyindex
    - linux-ogo
    - xlayoutdisplay
    - gg
    - gnome-shell-pomodoro
    - firefox-extension-tab-stash
    - yacd-meta
    - ebtables
    - cava
    - lazydocker
    - v2raya-git
    - firefox-esr-noscript
    - python-git-versioner
    - flat-remix
    - python-pip-system-certs
    - breeze-adapta-cursor-theme-git
    - mawk
    - ocaml-compiler-libs-repackaged
    - firefox-esr-ublock-origin
    - qbittorrent-enhanced-nox-git
    - flat-remix-gnome
    - chromedriver
    - hath
    
    The following packages are not found:
    
    - python-mathlibtools
    - virtualbox-ext-oracle
    
    closes #3901

--- archlinuxcn/hath/hath.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-exec /usr/bin/java -jar /usr/share/java/hath/hath.jar \
-     --cache-dir=/var/lib/hath/cache \
-     --data-dir=/var/lib/hath/data \
-     --log-dir=/var/lib/hath/log \
-     --temp-dir=/var/lib/hath/temp \
-     "$@"
