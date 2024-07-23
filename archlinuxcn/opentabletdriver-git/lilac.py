0af38d21d38f74830324c2e48c40e34d541d9f5e
Integral <integral@member.fsf.org>
2024-07-23 13:03:28 +0800

    opentabletdriver-git: add new aur maintainer

--- archlinuxcn/opentabletdriver-git/lilac.py
+++ archlinuxcn/opentabletdriver-git/lilac.py
@@ -3,7 +3,7 @@
 from lilaclib import *
 
 def pre_build():
-    aur_pre_build(maintainers=['gonX'])
+    aur_pre_build(maintainers=['gonX', 'jamesbt365'])
     for line in edit_file('PKGBUILD'):
         if line.startswith('license='):
             line = "license=('LGPL-3.0-or-later')"
