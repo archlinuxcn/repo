e3d0fb8d100d8b713e44991771fed5733a48b931
Integral <integral@member.fsf.org>
2024-09-14 12:36:53 +0800

    vcpkg: moved to [extra]

--- archlinuxcn/vcpkg/vcpkg-x86_64.sh
+++
@@ -1,9 +0,0 @@
-# Disable vcpkg telemetry by default.
-if [ -z "$VCPKG_DISABLE_METRICS" ]; then
-    export VCPKG_DISABLE_METRICS="1"
-fi
-
-# Export the correct vcpkg root directory.
-if [ -z "$VCPKG_ROOT" ]; then
-    export VCPKG_ROOT="$HOME/.local/share/vcpkg"
-fi
