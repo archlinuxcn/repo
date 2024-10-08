c1c3e4d6be1f92f071c036555dc9ba32da915f49
Integral <integral@member.fsf.org>
2024-10-08 23:59:48 +0800

    hyprland-hidpi-xprop-git: remove hyprutils-git in lilac.py

--- archlinuxcn/hyprland-hidpi-xprop-git/lilac.py
+++ archlinuxcn/hyprland-hidpi-xprop-git/lilac.py
@@ -6,3 +6,7 @@ def pre_build():
     aur_pre_build(maintainers=['moetayuko', 'q234rty'])
     add_depends(['libdisplay-info.so', 'libhyprlang.so'])
     add_replaces(['hyprland-nvidia-hidpi-git'])
+
+    for line in edit_file('PKGBUILD'):
+        if not line.strip().startswith('hyprutils-git'):
+            print(line)
