f5ca478c38ab76a0e4772e2db279b516017ef52b
Integral <integral@member.fsf.org>
2024-10-19 12:24:43 +0800

    linux-cachyos-bore: clean up package files before pulling from AUR

--- archlinuxcn/linux-cachyos-bore/lilac.py
+++ archlinuxcn/linux-cachyos-bore/lilac.py
@@ -3,7 +3,7 @@
 from lilaclib import *
 
 def pre_build():
-    clean_directory()
+    run_cmd(['cleanup-package-files', 'linux-cachyos-bore', '--real'])
     aur_pre_build(maintainers=['ptr1337','sir_lucjan'])
 
     for line in edit_file('auto-cpu-optimization.sh'):
