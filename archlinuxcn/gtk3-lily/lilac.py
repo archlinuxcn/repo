09df8559484026b4bcedd3e90d7430742e60af32
lilydjwg <lilydjwg@gmail.com>
2024-11-03 13:24:05 +0800

    gtk3-lily: update lilac.py to generate PKGBUILD

--- archlinuxcn/gtk3-lily/lilac.py
+++ archlinuxcn/gtk3-lily/lilac.py
@@ -28,19 +28,21 @@ def pre_build():
     elif line.startswith('source=('):
       state = 'source'
     elif state == 'source' and line.endswith(')'):
-      line = line.replace(')', '  0001-wayland-im-notify-wayland-after-set_cursor_location.patch)')
+      line = line.replace(')', '  0001-wayland-im-notify-wayland-after-set_cursor_location.patch\n  0001-inhibit-hex-check.patch)')
       state = ''
 
     elif line.startswith('b2sums='):
       state = 'b2sums'
     elif state == 'b2sums' and line.endswith(')'):
-      line = line.replace(')', "\n        '2718db7adc07ee2df86b6dacbfb7638217b2cb76c5f5acde4ce9bd7a4617989744326069670b6cc4e5aff59fac410db9b60db7f07dd2a2e36a40b7a3b0d2d8b9')")
+      line = line.replace(')', "\n        '2718db7adc07ee2df86b6dacbfb7638217b2cb76c5f5acde4ce9bd7a4617989744326069670b6cc4e5aff59fac410db9b60db7f07dd2a2e36a40b7a3b0d2d8b9'\n        '805b43b9be928362fc9e5ec1738bc9c069d2f3fc32c6d740aefdbab1f8cbd43a5d11aea8113cf9df613f203f6dd77ac250a1e3a6f2399848de58642f08ac0aac')")
       state = ''
 
     elif line.startswith('prepare('):
       state = 'prepare'
     elif state == 'prepare' and line.startswith('}'):
-      line = '  patch -Np1 -i ../0001-wayland-im-notify-wayland-after-set_cursor_location.patch\n' + line
+      line = ('  patch -Np1 -i ../0001-wayland-im-notify-wayland-after-set_cursor_location.patch\n'
+              '  patch -Np1 -i ../0001-inhibit-hex-check.patch\n'
+              + line)
       state = ''
 
     elif line.startswith('package_gtk3'):
