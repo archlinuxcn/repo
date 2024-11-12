1e953b44024a7795298ac04482766bf026f5862b
Integral <integral@member.fsf.org>
2024-11-12 16:17:31 +0800

    hyprland-hidpi-xprop-git: remove unused depends

--- archlinuxcn/hyprland-hidpi-xprop-git/lilac.py
+++ archlinuxcn/hyprland-hidpi-xprop-git/lilac.py
@@ -4,7 +4,6 @@ from lilaclib import *
 
 def pre_build():
     aur_pre_build(maintainers=["moetayuko", "q234rty"])
-    add_depends(["libdisplay-info.so", "libhyprlang.so"])
     add_replaces(["hyprland-nvidia-hidpi-git"])
 
     for line in edit_file("PKGBUILD"):
