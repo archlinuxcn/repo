9da02f7a1470a24aed06370ec2ac60f13c88bb18
Integral <integral@member.fsf.org>
2024-12-02 11:45:17 +0800

    linux-clear-x64-v3: clean up lilac.py

--- archlinuxcn/linux-clear-x64-v3/lilac.py
+++ archlinuxcn/linux-clear-x64-v3/lilac.py
@@ -10,12 +10,5 @@ def pre_build():
             print('_subarch=42') # Generic-x86-64-v3
         elif line.strip().startswith('pkgbase='):
             print('pkgbase=linux-clear-x64-v3')
-        elif line.strip().startswith('license='):
-            print("license=('GPL-2.0-only')")
-        elif line.strip().startswith('_clr='): # Temporarily fix tag typo
-            print("_clr=3-1472")
-        elif 'more-uarches-for-kernel-' in line.strip(): # Temporarily fix patch filename
-            index = line.find('uarches')
-            print('{}ISA-levels-and-{}'.format(line[:index], line[index:]))
         else:
             print(line)
