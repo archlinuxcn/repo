f3344a731eaba60b5086762ebf04b0413725dd25
lilac <lilac@build.archlinuxcn.org>
2025-01-11 20:22:45 +0800

    google-cloud-cli: auto updated to 505.0.0-2

---
+++ archlinuxcn/google-cloud-cli/google-cloud-cli.sh
@@ -0,0 +1,5 @@
+export CLOUDSDK_ROOT_DIR=/opt/google-cloud-cli
+export CLOUDSDK_PYTHON=/usr/bin/python
+export CLOUDSDK_PYTHON_ARGS='-S -W ignore'
+export PATH=$CLOUDSDK_ROOT_DIR/bin:$PATH
+export GOOGLE_CLOUD_SDK_HOME=$CLOUDSDK_ROOT_DIR
