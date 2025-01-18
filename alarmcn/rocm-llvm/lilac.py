cebf6f343e1c84a77583321e1b6fec88a73832a7
Yichao Yu <yyc1992@gmail.com>
2025-01-17 19:46:02 -0500

    [rocm-llvm] patch llvm

--- alarmcn/rocm-llvm/lilac.py
+++ alarmcn/rocm-llvm/lilac.py
@@ -6,6 +6,15 @@ from lilaclib import *
 
 g = SimpleNamespace()
 
+patch_str = """
+source+=(https://github.com/llvm/llvm-project/commit/078c18de832328f743fb6e8dce728a030c81dc0d.patch)
+prepare() {
+  cd llvm-project
+
+  patch -Np1 --no-backup-if-mismatch < "../078c18de832328f743fb6e8dce728a030c81dc0d.patch"
+}
+"""
+
 def pre_build():
   g.files = download_official_pkgbuild('rocm-llvm')
 
@@ -13,7 +22,10 @@ def pre_build():
     line = line.replace('AMDGPU;NVPTX;X86', 'AMDGPU;NVPTX;AArch64')
     if line.startswith('arch='):
       line = 'arch=(aarch64)'
+    if line.startswith('build()'):
+      print(patch_str)
     print(line)
+  run_cmd(['updpkgsums'])
 
 def post_build():
   git_add_files([f for f in g.files if not f.startswith(".")])
