573d5ed353632593e6c9d71a8d8443794f682136
Integral <integral@member.fsf.org>
2025-01-18 22:15:11 +0800

    linux-clear: add pv to makedepends

--- archlinuxcn/linux-clear/lilac.py
+++ archlinuxcn/linux-clear/lilac.py
@@ -6,3 +6,4 @@ def pre_build():
     aur_pre_build(maintainers=['JeremyStarTM'])
     add_replaces(["linux-clear-x64-v3", "linux-clear-x64-v4"])
     add_provides(["linux-clear-x64-v3", "linux-clear-x64-v4"])
+    add_makedepends(['pv'])
