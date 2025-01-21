8b4993d3e3e8b49d63f3d0845344e8076cb6386b
Roald Clark <roaldclark@gmail.com>
2025-01-21 16:12:19 +0800

    nvidia-open-clear-x64-v3: change NVIDIA-MODULE to nvidia-open
    Signed-off-by: Roald Clark <roaldclark@gmail.com>

--- archlinuxcn/nvidia-open-clear-x64-v3/lilac.py
+++ archlinuxcn/nvidia-open-clear-x64-v3/lilac.py
@@ -8,18 +8,19 @@ from lilaclib import git_pkgbuild_commit
 
 
 def pre_build():
-    depends_clear_re = re.compile(
-        r'''^(\s*depends\+=.*?)['"]?linux-clear-x64-v3[^'") ]*['"]?'''
+    depends_kernel_re = re.compile(
+        r'''^(\s*depends\+=.*?)['"]?\$\{_lkname\}[^'") ]*['"]?'''
     )
-    kernel = _G.newvers[1]
+    kernver = _G.newvers[1]
     update_pkgver_and_pkgrel(_G.newver, updpkgsums=False)
     for line in edit_file('PKGBUILD'):
-        m = depends_clear_re.match(line)
+        m = depends_kernel_re.match(line)
         if m:
-            line = depends_clear_re.sub(
-                r'\1"linux-clear-x64-v3=%s"' % kernel, line
+            line = depends_kernel_re.sub(
+                r'\1"${_lkname}=%s"' % kernver, line
             )
         print(line)
+
 
 def post_build():
     git_pkgbuild_commit()
