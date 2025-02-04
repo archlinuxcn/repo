809c145b17b78e85bb3d4a98c8e79cc5d272ef42
Yichao Yu <yyc1992@gmail.com>
2025-02-04 18:28:49 -0500

    [edk2] add for aarch64

---
+++ alarmcn/edk2/lilac.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python3
+
+from types import SimpleNamespace
+
+from lilaclib import *
+
+g = SimpleNamespace()
+
+def pre_build():
+  g.files = download_official_pkgbuild('edk2')
+
+  in_build = False
+  patched = False
+  for line in edit_file('PKGBUILD'):
+    if line.strip().startswith('make -C BaseTools'):
+      print("""
+  export GCC_BIN="x86_64-linux-gnu-"
+  export GCC_AARCH64_PREFIX=""
+  export GCC_ARM_PREFIX="arm-none-eabi-"
+  export GCC5_BIN="x86_64-linux-gnu-"
+  export GCC5_AARCH64_PREFIX=""
+  export GCC5_ARM_PREFIX="arm-none-eabi-"
+""")
+    elif line.strip() == 'aarch64-linux-gnu-gcc':
+      line = '  x86_64-linux-gnu-gcc'
+    elif line.startswith('arch='):
+      line = 'arch=(aarch64) # Avoid conflicting with the official package in the x86 repo'
+    print(line)
