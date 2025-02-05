c0285b2ef1955f43763179fb251a18d4f7e0b956
everyx <lunt.luo@gmail.com>
2025-02-05 15:28:51 +0800

    rime-lmdg: fix lmdg version check

--- archlinuxcn/rime-lmdg/lilac.py
+++ archlinuxcn/rime-lmdg/lilac.py
@@ -1,11 +1,11 @@
 #!/usr/bin/python3
+
+from email.utils import parsedate_to_datetime
+import time
+
 def pre_build():
-    _release_tag = _G.newver
+    date_object = parsedate_to_datetime(_G.newver)
+    timestamp = int(time.mktime(date_object.timetuple()))
+    pkgver = f"lts.{timestamp}"
 
-    for line in edit_file("PKGBUILD"):
-        if line.startswith("_release_tag="):
-            line = f"_release_tag={_release_tag}"
-        print(line)
-
-    pkgver = _release_tag.lstrip("v").replace("-", "_").lower()
     update_pkgver_and_pkgrel(pkgver)
