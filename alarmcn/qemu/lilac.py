58ad7dad9e30ecde1967266cf51f33f679df1d30
Yichao Yu <yyc1992@gmail.com>
2025-02-04 20:23:42 -0500

    [qemu] add, fix alarm dependencies and add back removed features

---
+++ alarmcn/qemu/lilac.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+
+from types import SimpleNamespace
+
+from lilaclib import *
+
+g = SimpleNamespace()
+
+def pre_build():
+  g.files = download_official_pkgbuild('edk2')
+
+  for line in edit_file('PKGBUILD'):
+    if line.startswith('package_qemu-vmsr-helper() {'):
+      # This is x86 only
+      print(line)
+      print('  pkgdesc="QEMU persistent reservation utility"')
+      print('  return')
+      continue
+    elif line.startswith('arch='):
+      line = 'arch=(aarch64 x86_64)'
+    print(line)
