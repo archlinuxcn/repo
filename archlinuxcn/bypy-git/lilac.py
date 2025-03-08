09ba25dbbe47a3150d4f665741a296880b6a4011
oldherl <oldherl@gmail.com>
2025-03-08 16:45:11 +1100

    bypy-git: fix python3.13
    Not updating from AUR because AUR is still on python3.12

--- archlinuxcn/bypy-git/lilac.py
+++ archlinuxcn/bypy-git/lilac.py
@@ -1,10 +1,10 @@
 #!/usr/bin/env python3
 from lilaclib import *
 
-def pre_build():
-    aur_pre_build(maintainers="petronny")
+#def pre_build():
+#    aur_pre_build(maintainers="petronny")
 
-post_build = aur_post_build
+#post_build = aur_post_build
 
 if __name__ == '__main__':
     single_main(build_prefix)
