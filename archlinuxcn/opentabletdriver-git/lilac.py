3022fdf2602e5faeaadddcd67a326bcfc756fae3
Integral <integral@member.fsf.org>
2025-03-22 15:27:01 +0800

    opentabletdriver-git: downgrade .NET SDK version to 8.0

--- archlinuxcn/opentabletdriver-git/lilac.py
+++ archlinuxcn/opentabletdriver-git/lilac.py
@@ -7,4 +7,6 @@ def pre_build():
     for line in edit_file('PKGBUILD'):
         if line.startswith('license='):
             line = "license=('LGPL-3.0-or-later')"
+        elif line.startswith('makedepends='):
+            line = line.replace("'dotnet-sdk'", "")
         print(line)
