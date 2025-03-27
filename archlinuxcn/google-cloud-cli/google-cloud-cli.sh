1041075621a87e83b885d4a6c93f36f5634991e2
lilac <lilac@build.archlinuxcn.org>
2025-03-27 21:00:39 +0800

    google-cloud-cli: auto updated to 516.0.0-2
    It has been built because:
    * nvchecker detects the following updates: aur(0): 515.0.0-1 -> 516.0.0-2

--- archlinuxcn/google-cloud-cli/google-cloud-cli.sh
+++ archlinuxcn/google-cloud-cli/google-cloud-cli.sh
@@ -1,5 +1,10 @@
 export CLOUDSDK_ROOT_DIR=/opt/google-cloud-cli
-export CLOUDSDK_PYTHON=/usr/bin/python
-export CLOUDSDK_PYTHON_ARGS='-S -W ignore'
-export PATH=$CLOUDSDK_ROOT_DIR/bin:$PATH
 export GOOGLE_CLOUD_SDK_HOME=$CLOUDSDK_ROOT_DIR
+
+#  CLOUDSDK_ROOT_DIR             # installation root dir
+#  CLOUDSDK_PYTHON               # python interpreter path
+#  CLOUDSDK_GSUTIL_PYTHON        # python interpreter path for gsutil
+#  CLOUDSDK_PYTHON_ARGS          # python interpreter arguments
+#  CLOUDSDK_PYTHON_SITEPACKAGES  # use python site packages
+#  CLOUDSDK_BQ_PYTHON            # python interpreter for bq
+#  CLOUDSDK_ENCODING             # python io encoding for gcloud
