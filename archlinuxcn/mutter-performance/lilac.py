0d83fb91a75d90c0c8a4d1e1071e80f11b8f6e7c
Integral <integral@member.fsf.org>
2025-03-30 13:42:19 +0800

    mutter-performance: add epoch & delete license replacement in lilac.py

--- archlinuxcn/mutter-performance/lilac.py
+++ archlinuxcn/mutter-performance/lilac.py
@@ -5,6 +5,6 @@ from lilaclib import *
 def pre_build():
     aur_pre_build(maintainers=['Terence','glorious-yellow'])
     for line in edit_file('PKGBUILD'):
-        if line.startswith('license='):
-            line = "license=('GPL-2.0-or-later')"
-        print (line.replace('groups=(gnome)', ''))
+        if line.startswith('pkgrel='):
+            print("epoch=1")
+        print(line.replace('groups=(gnome)', ''))
