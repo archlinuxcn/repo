096508664c60f6a088e7b21734e2ae2ec9420e41
hoream <hoream@qq.com>
2025-04-01 14:47:47 +0800

    alarm: wayfire-git: fix build

--- alarmcn/wayfire-git/lilac.py
+++
@@ -1,8 +0,0 @@
-from lilaclib import *
-
-def pre_build():
-  aur_pre_build()
-  for line in edit_file('PKGBUILD'):
-    if line.startswith('arch='):
-      line = 'arch=(aarch64 x86_64)'
-    print(line)
