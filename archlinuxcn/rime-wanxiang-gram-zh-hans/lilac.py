4c53cb46e16ce3a7d2672522ffb051a9f0232ae3
everyx <lunt.luo@gmail.com>
2025-05-17 12:44:05 +0800

    rime-wanxiang-gram-zh-hans: fix pkgver update

---
+++ archlinuxcn/rime-wanxiang-gram-zh-hans/lilac.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+
+from email.utils import parsedate_to_datetime
+from zoneinfo import ZoneInfo
+from lilaclib import *
+
+
+def pre_build():
+    dt = parsedate_to_datetime(_G.newvers[0])
+    pkgver = dt.astimezone(ZoneInfo("Asia/Shanghai")).strftime("%Y%m%d")
+
+    update_pkgver_and_pkgrel(pkgver)
