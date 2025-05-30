bddf98b03d6f0db38ec3466262ccc1bd040d46d0
everyx <lunt.luo@gmail.com>
2025-05-30 16:08:57 +0800

    rime-wanxiang-dict-cn-nightly: fix checksum failed

--- archlinuxcn/rime-wanxiang-dict-cn-nightly/lilac.py
+++ archlinuxcn/rime-wanxiang-dict-cn-nightly/lilac.py
@@ -11,3 +11,13 @@ def pre_build():
         if line.startswith('_schema_version='):
             line = f'_schema_version={schema_version}'
         print(line)
+
+    # update checksums for schema
+    update_pkgver_and_pkgrel(schema_version)
+
+    for line in edit_file('PKGBUILD'):
+        if line.startswith('b2sums=('):
+            line = "b2sums=('SKIP'"
+        print(line)
+
+    vcs_update()
