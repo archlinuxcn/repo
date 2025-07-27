2f5320a69bf1bc3068254ca070ab7799616446a2
hoream <hoream@qq.com>
2025-07-27 20:12:38 +0800

    alarmcn: wayfire-plugins-extra-git: move aur_pre_build to lilac.py

--- alarmcn/wayfire-plugins-extra-git/lilac.py
+++ alarmcn/wayfire-plugins-extra-git/lilac.py
@@ -1,6 +1,7 @@
 from lilaclib import *
 
 def pre_build():
+  aur_pre_build(maintainers=['kode54'])
   for line in edit_file('PKGBUILD'):
     if line.startswith('arch='):
       line = 'arch=(aarch64 x86_64)'
