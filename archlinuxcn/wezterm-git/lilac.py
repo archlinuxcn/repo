356884976c10075efabd37f716eca940787e0ab7
yk <yk@archlinuxcn.org>
2025-08-05 01:07:52 +0800

    fix: wezterm-git maintainer

--- archlinuxcn/wezterm-git/lilac.py
+++ archlinuxcn/wezterm-git/lilac.py
@@ -2,7 +2,7 @@
 from lilaclib import *
 
 def pre_build():
-    aur_pre_build(maintainers=['wez', 'gabor_bernat'])
+    aur_pre_build(maintainers=['wez', 'xiota'])
     for line in edit_file('PKGBUILD'):
         # terminfo file conflicts with ncurse
         # https://github.com/archlinuxcn/repo/issues/3838
