b109a5132b395acb81a8cd6a016bc70d63f5e231
yk <yk@archlinuxcn.org>
2025-08-08 17:32:18 +0800

    fix: wezterm-git, use old pkgver()

--- archlinuxcn/wezterm-git/lilac.py
+++ archlinuxcn/wezterm-git/lilac.py
@@ -3,9 +3,19 @@ from lilaclib import *
 
 def pre_build():
     aur_pre_build(maintainers=['wez', 'xiota'])
+
+    pkgver = False
     for line in edit_file('PKGBUILD'):
-        # terminfo file conflicts with ncurse
-        # https://github.com/archlinuxcn/repo/issues/3838
-        if "/usr/share/terminfo/w/wezterm" in line:
+
+        if line.startswith("pkgver()"):
+            print("pkgver() {")
+            print('  cd "$srcdir/wezterm" || exit 1')
+            print('  git -c "core.abbrev=8" show -s "--format=%cd-%h" "--date=format:%Y%m%d-%H%M%S" | tr - .')
+            print('}')
+            pkgver = True
+        if pkgver:
+            if line.strip() == "}":
+                pkgver = False
             line = ""
+
         print(line)
