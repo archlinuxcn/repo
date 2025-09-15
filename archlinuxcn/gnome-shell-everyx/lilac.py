21188346cc11940d9280d1b92fc85dd6b1a9e3d9
everyx <lunt.luo@gmail.com>
2025-09-15 08:55:18 +0800

    gnome-shell-everyx: remove merged patch

--- archlinuxcn/gnome-shell-everyx/lilac.py
+++ archlinuxcn/gnome-shell-everyx/lilac.py
@@ -32,7 +32,6 @@ def pre_build():
       state = "source"
     elif state == "source" and line == ')':
       line = '  "0001-ibus-candidate-popup-support-rime-comment-style.patch"\n' + \
-             '  "0002-messageTray-Fix-race-condition-causing-stuck-notific.patch"\n' + \
              line
       state = "out"
 
@@ -41,7 +40,6 @@ def pre_build():
     elif state == 'b2sums' and line.endswith(')'):
       line = line.replace(')', 
                           "\n        '151a4ad7f50dd2946fe8bef074e79e21dad78591b0624993b41cce64cb0ef0e7766d17f018bec878b105df5ed7d886efc7a0f7b49c13de3c8e8bcfabd78c1114'" + \
-                          "\n        'e54d3c5b5a8db1439287a2f3ebdbcd8bd66d500498b19dda02cb5813fe0a08a15e885cbd2114dc7262159ea253750b7e825915220c02249571e6225fe209c54f'" + \
                           ")")
       state = 'out'
 
