125482f046a67740d8ffbcefa42a8c47e55a4a1c
yk <yk@archlinuxcn.org>
2025-09-16 17:20:55 +0800

    fix: feishin-bin checksum

--- archlinuxcn/feishin-bin/lilac.py
+++ archlinuxcn/feishin-bin/lilac.py
@@ -6,16 +6,4 @@ def pre_build():
     aur_pre_build(maintainers=["exu", "ruahcra"])
 
     for line in edit_file("PKGBUILD"):
-        if line.startswith("package()"):
-            print(
-                """
-_filename=Feishin-linux
-source=("feishin.desktop")
-source_x86_64=("https://github.com/jeffvli/feishin/releases/download/v${pkgver//_/-}/${_filename}-x64.tar.xz")
-source_aarch64=("https://github.com/jeffvli/feishin/releases/download/v${pkgver//_/-}/${_filename}-arm64.tar.xz")
-sha256sums=('ef112b1a9ef80d8bf27f721fdbb12de0a195da4e464dbf27282503ba398bef8d')
-sha256sums_x86_64=('592b403bfd362229ff4e9348ad5d605b94805f660f40895da3cd068dd6df9658')
-sha256sums_aarch64=('52a3a02f5bb52bc6330bc3777661fd0e75a297d6058864187d38e55d40b05522')
-            """
-            )
         print(line)
