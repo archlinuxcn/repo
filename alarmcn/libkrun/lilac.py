6c02050e57a1d9377cbaea0fd4c6981b731d9c81
Yichao Yu <yyc1992@gmail.com>
2025-09-18 15:47:48 -0400

    [libkrun] add

---
+++ alarmcn/libkrun/lilac.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+
+from types import SimpleNamespace
+
+from lilaclib import *
+
+g = SimpleNamespace()
+
+def pre_build():
+  g.files = download_official_pkgbuild('libkrun')
+
+  for line in edit_file('PKGBUILD'):
+    if line.startswith('arch='):
+      line = 'arch=(aarch64 x86_64)'
+    print(line)
+  run_cmd(['updpkgsums'])
+
+def post_build():
+  git_add_files([f for f in g.files if not f.startswith(".")])
+  git_commit()
