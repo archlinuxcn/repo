d2d950d0ae63f0f6c12014e019d2276df3490f41
everyx <lunt.luo@gmail.com>
2025-09-23 09:29:45 +0800

    gnome-shell-everyx: add new patch and fix build for 49.0

--- archlinuxcn/gnome-shell-everyx/lilac.py
+++ archlinuxcn/gnome-shell-everyx/lilac.py
@@ -32,6 +32,7 @@ def pre_build():
       state = "source"
     elif state == "source" and line == ')':
       line = '  "0001-ibus-candidate-popup-support-rime-comment-style.patch"\n' + \
+             '  "0002-theme-Fix-jagged-edges-on-notification-buttons.patch"\n' + \
              line
       state = "out"
 
@@ -39,14 +40,17 @@ def pre_build():
       state = 'b2sums'
     elif state == 'b2sums' and line.endswith(')'):
       line = line.replace(')', 
-                          "\n        '151a4ad7f50dd2946fe8bef074e79e21dad78591b0624993b41cce64cb0ef0e7766d17f018bec878b105df5ed7d886efc7a0f7b49c13de3c8e8bcfabd78c1114'" + \
+                          "\n        '1603004291b8dee5bfa8e2ae17893f21e88f9ee7151c67d1c5963fcdadc5a3e3b88ea6571979822c71e0fd39066823a6b4aa317ea1e8ac2880861c75a5b69549'" + \
+                          "\n        '5098032e61deeead14a596104df1c54569ec3f6ff6218cd313c40972435f508d099fef49fee7e19d8d2b4f5c43bd51a4f6d9237d4bd3544ec06764af620cdf53'" + \
                           ")")
       state = 'out'
 
     elif line.startswith('prepare('):
       state = 'prepare'
     elif state == 'prepare' and line.endswith('}'):
-      line = '  git apply -3 "$srcdir"/*.patch\n' + line
+      line = '  cd $_pkgbase\n' + \
+             '  git apply -3 "$srcdir"/*.patch\n' + \
+             line
       state = 'out'
 
     elif line.startswith('package_gnome-shell()'):
