9c7db0d8630fed037c1a7bcaf4fd16e1bbda56ea
Yichao Yu <yyc1992@gmail.com>
2025-10-21 08:57:07 -0400

    [chromium] add

---
+++ alarmcn/chromium/lilac.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+
+from types import SimpleNamespace
+
+from lilaclib import *
+
+g = SimpleNamespace()
+
+def pre_build():
+  g.files = download_official_pkgbuild('chromium')
+
+  for line in edit_file('PKGBUILD'):
+    if line.startswith('arch='):
+      line = 'arch=(aarch64 x86_64)'
+    print(line)
+
+def post_build():
+  git_add_files([f for f in g.files if not f.startswith(".")])
+  git_commit()
