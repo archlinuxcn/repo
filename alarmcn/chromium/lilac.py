964744821d8c728cc1fc4db5e7c2468e6649676c
Yichao Yu <yyc1992@gmail.com>
2025-10-22 19:20:44 -0400

    [chromium] clean up (remove upstreamed patch)

--- alarmcn/chromium/lilac.py
+++ alarmcn/chromium/lilac.py
@@ -11,22 +11,12 @@ g = SimpleNamespace()
 def pre_build():
   g.files = download_official_pkgbuild('chromium')
 
-  # Fix from upstream, can be removed once there's a new archlinux package built
-  shutil.copyfile('compiler-rt-adjust-paths.patch.fix',
-                  'compiler-rt-adjust-paths.patch')
-
   with open('fetch-chromium-release', 'a') as io:
     print('\nln -sf /usr/bin/node ../chromium-$VERSION/third_party/node/linux/node-linux-x64/bin/', file=io)
 
-  in_prepare = False
   for line in edit_file('PKGBUILD'):
     if line.startswith('arch='):
       line = 'arch=(aarch64 x86_64)'
-    if line.startswith('prepare()'):
-      in_prepare = True
-    if in_prepare and line.startswith('}'):
-      # print('  find third_party/libyuv -type f -exec sed -i -e \'s/__arm_locally_streaming/__attribute__((target("+sme"))) __arm_locally_streaming/\' {} \\;')
-      in_prepare = False
     print(line)
     if line.startswith('build()'):
       print('  CFLAGS="${CFLAGS/-march=*([^ ]) }"')
