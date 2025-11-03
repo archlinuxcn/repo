a261524197d5ec215f9167538babc55fa99788f1
lilydjwg <lilydjwg@gmail.com>
2025-11-03 20:42:06 +0800

    btdu: fix runtime dep

---
+++ alarmcn/btdu/lilac.py
@@ -0,0 +1,9 @@
+from lilaclib import *
+
+def pre_build():
+  aur_pre_build(maintainers=['hehe_uwu'])
+  for l in edit_file('PKGBUILD'):
+    if l.startswith('depends='):
+      # not any d-runtime works; it works only with the one used for building
+      l = l.replace("'d-runtime'", "'liblphobos'")
+    print(l)
