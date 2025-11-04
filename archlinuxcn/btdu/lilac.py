608dfa565128837ea5e0804da5e7fdd4a8e5dff1
lilydjwg <lilydjwg@gmail.com>
2025-11-04 10:23:06 +0800

    btdu: update maintainers

--- archlinuxcn/btdu/lilac.py
+++ archlinuxcn/btdu/lilac.py
@@ -1,7 +1,7 @@
 from lilaclib import *
 
 def pre_build():
-  aur_pre_build(maintainers=['hehe_uwu'])
+  aur_pre_build(maintainers=['CyberShadow'])
   for l in edit_file('PKGBUILD'):
     if l.startswith('depends='):
       # not any d-runtime works; it works only with the one used for building
