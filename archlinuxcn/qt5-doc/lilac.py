95244a5ab62d413ee7a359dce4448781ee321f2f
yk <yk@archlinuxcn.org>
2025-11-12 02:13:52 +0800

    fix: qt5-doc, qt5-webchannel, groups

---
+++ archlinuxcn/qt5-doc/lilac.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+from lilaclib import *
+
+def pre_build():
+    aur_pre_build('qt5-doc', maintainers=['andreas_baumann'])
+    for line in edit_file('PKGBUILD'):
+
+        if line.startswith('groups='):
+            continue
+
+        print(line)
