f12033f4ebbeca4726e4f592e42a769401591c2d
Hu Butui <hot123tea123@gmail.com>
2025-11-14 15:32:39 +0800

    docker-rootless-extras: fix pkgver
    Signed-off-by: Hu Butui <hot123tea123@gmail.com>

--- archlinuxcn/docker-rootless-extras/lilac.py
+++ archlinuxcn/docker-rootless-extras/lilac.py
@@ -4,7 +4,7 @@ from lilaclib import *
 
 
 def pre_build():
-    update_pkgver_and_pkgrel(_G.newver.lstrip("v"))
+    update_pkgver_and_pkgrel(_G.newver.lstrip("docker-v").lstrip("v"))
 
 
 def post_build():
