044d4c9bee3d5af204dd7a7b0eb5386d55fdfa44
Yichao Yu <yyc1992@gmail.com>
2025-11-17 23:06:48 -0500

    [chromium] remove merged patch

--- alarmcn/chromium/lilac.py
+++ alarmcn/chromium/lilac.py
@@ -11,16 +11,10 @@ g = SimpleNamespace()
 def pre_build():
   g.files = download_official_pkgbuild('chromium')
 
-  with open('fetch-chromium-release', 'a') as io:
-    print('\nln -sf /usr/bin/node ../chromium-$VERSION/third_party/node/linux/node-linux-x64/bin/', file=io)
-
   for line in edit_file('PKGBUILD'):
     if line.startswith('arch='):
       line = 'arch=(aarch64 x86_64)'
     print(line)
-    if line.startswith('build()'):
-      print('  CFLAGS="${CFLAGS/-march=*([^ ]) }"')
-      print('  CXXFLAGS="${CXXFLAGS/-march=*([^ ]) }"')
 
   run_protected(["updpkgsums"])
 
