95c29d7f557a35372d44394b8d000ec10278440f
Integral <integral@member.fsf.org>
2026-01-11 16:26:55 +0800

    python-dlib: fix the syntax error in lilac.py

--- archlinuxcn/python-dlib/lilac.py
+++ archlinuxcn/python-dlib/lilac.py
@@ -5,7 +5,7 @@ from lilaclib import *
 def pre_build():
     aur_pre_build(maintainers=['petronny', 'AutoUpdateBot'])
     for line in edit_file('PKGBUILD'):
-        line = line.replace('gcc-14', 'gcc').replace('g++-14', 'g++'))
+        line = line.replace('gcc-14', 'gcc').replace('g++-14', 'g++')
         if '--no DLIB_USE_CUDA' in line:
             line += ' --no USE_SSE4_INSTRUCTIONS --no USE_AVX_INSTRUCTIONS'
 
