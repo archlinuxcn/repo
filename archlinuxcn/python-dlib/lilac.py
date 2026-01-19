38ef10a199d1d2bf8da1539478f31d9155112e95
Integral <integral@member.fsf.org>
2026-01-19 20:58:22 +0800

    python-dlib: remove GCC version patch

--- archlinuxcn/python-dlib/lilac.py
+++ archlinuxcn/python-dlib/lilac.py
@@ -5,7 +5,6 @@ from lilaclib import *
 def pre_build():
     aur_pre_build(maintainers=['zen'])
     for line in edit_file('PKGBUILD'):
-        line = line.replace('gcc-14', 'gcc').replace('g++-14', 'g++')
         if '--no DLIB_USE_CUDA' in line:
             line += ' --no USE_SSE4_INSTRUCTIONS --no USE_AVX_INSTRUCTIONS'
 
