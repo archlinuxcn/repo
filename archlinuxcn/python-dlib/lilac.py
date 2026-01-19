5a177c5a1e574755b6580c317b86df7caa462e55
Integral <integral@member.fsf.org>
2026-01-19 20:56:36 +0800

    python-dlib: update AUR maintainers

--- archlinuxcn/python-dlib/lilac.py
+++ archlinuxcn/python-dlib/lilac.py
@@ -3,7 +3,7 @@
 from lilaclib import *
 
 def pre_build():
-    aur_pre_build(maintainers=['petronny', 'AutoUpdateBot'])
+    aur_pre_build(maintainers=['zen'])
     for line in edit_file('PKGBUILD'):
         line = line.replace('gcc-14', 'gcc').replace('g++-14', 'g++')
         if '--no DLIB_USE_CUDA' in line:
