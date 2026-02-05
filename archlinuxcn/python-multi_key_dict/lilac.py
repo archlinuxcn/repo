7e4ae2cb84a1746c5910391438c4c22beee3fee4
Jax Young <jaxvanyang@gmail.com>
2026-02-05 12:31:25 +0800

    python-multi_key_dict: add LICENSE

--- archlinuxcn/python-multi_key_dict/lilac.py
+++
@@ -1,8 +0,0 @@
-from lilaclib import *
-
-def pre_build():
-    aur_pre_build('python-multi_key_dict', maintainers=['synthead'])
-    for line in edit_file('PKGBUILD'):
-        if line.startswith('arch='):
-            line = 'arch=("any")'
-        print(line)
