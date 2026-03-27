d6d66a0c37213f465eb4ca64210f9f02de7c3599
Integral <integral@member.fsf.org>
2026-03-27 18:01:14 +0800

    goldendict-ng: update lilac.py

--- archlinuxcn/goldendict-ng/lilac.py
+++ archlinuxcn/goldendict-ng/lilac.py
@@ -5,8 +5,8 @@ from lilaclib import *
 def pre_build():
     substrings = _G.newver.split('-Release.')
     for line in edit_file('PKGBUILD'):
-        if line.startswith('_commit_id='):
-            line = f'_commit_id={substrings[1]}'
+        if line.startswith('_commit_hash='):
+            line = f'_commit_hash={substrings[1]}'
         print(line)
 
     update_pkgver_and_pkgrel(substrings[0])
