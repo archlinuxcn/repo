96bb5e43394522d4e781baf4a30c5e761431d328
yk <yk@archlinuxcn.org>
2026-03-29 02:53:50 +0800

    update: enable svg support for emacs packages, #4660

--- archlinuxcn/emacs-native-comp-git/lilac.py
+++ archlinuxcn/emacs-native-comp-git/lilac.py
@@ -12,7 +12,13 @@ def pre_build():
         if line.startswith('replaces='):
             line = ''
 
-        #disable all flags
+        # enable svg
+        # make sure librsvg is present so emacs can build the support
+        # https://github.com/emacs-mirror/emacs/blob/e6c1e87c1e06b44cfffd2f0fd7ee00bb714a1854/INSTALL#L380-L390
+        if line.startswith('depends'):
+            line = line.replace(')', " 'librsvg')")
+
+        # disable all flags
         if '="YES"' in line:
             line = line.replace('="YES"', '=')
 
