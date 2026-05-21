dcc16d41744e7b728c32e454cd908dea616ca16e
oldherl <oldherl@gmail.com>
2026-05-21 22:42:26 +0800

    python2-oldherl: fix provides

--- archlinuxcn/python2-oldherl/lilac.py
+++ archlinuxcn/python2-oldherl/lilac.py
@@ -13,7 +13,7 @@ def pre_build():
     if line.startswith('pkgname='):
       line = 'pkgname=python2-oldherl'
     if line.startswith('pkgdesc='):
-      line = 'pkgdesc="python2 for oldherl packaging only. Do not use"\nprovides=python2'
+      line = 'pkgdesc="python2 for oldherl packaging only. Do not use"\nprovides=("python2")'
     print(line)
 
 def post_build():
