a44687773c7e0206076af1ad3136cf0245ac038f
Winston Wu <winstonwu91@gmail.com>
2026-06-18 11:41:39 +0800

    [wxwidgets-gtk2] Update

--- archlinuxcn/wxwidgets-gtk2/lilac.py
+++ archlinuxcn/wxwidgets-gtk2/lilac.py
@@ -4,7 +4,7 @@ from lilaclib import *
 build_prefix = 'extra-x86_64'
 
 def pre_build():
-  aur_pre_build(maintainers=['petronny'])
+  aur_pre_build(maintainers=['petronny','AutoUpdateBot'])
 
   for line in edit_file('PKGBUILD'):
     if line.lstrip().startswith(('conflicts=', 'provides=')):
