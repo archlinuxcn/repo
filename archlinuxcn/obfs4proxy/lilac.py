1d8a2dd15016ce630098f20b1a3497beaeeb09d7
Rine Amakawa <Misaka13514@gmail.com>
2026-07-22 15:37:56 +0900

    harden aur_pre_build

--- archlinuxcn/obfs4proxy/lilac.py
+++ archlinuxcn/obfs4proxy/lilac.py
@@ -8,7 +8,8 @@
 from lilaclib import *
 
 build_prefix = 'extra-x86_64'
-pre_build = aur_pre_build
+def pre_build():
+    aur_pre_build(maintainers=['aminvakil'])
 post_build = aur_post_build
 
 if __name__ == '__main__':
