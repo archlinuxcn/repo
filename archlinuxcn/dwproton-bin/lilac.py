7eaa6658a04dba705bc5d66a265a6a61ec2892a2
RocketDev <ma2014119@outlook.com>
2026-09-05 17:32:17 +0800

    dwproton-bin: try add missing depends

--- archlinuxcn/dwproton-bin/lilac.py
+++ archlinuxcn/dwproton-bin/lilac.py
@@ -3,8 +3,8 @@ from lilaclib import *
 
 def pre_build():
     aur_pre_build(maintainers=['shira'])
-    for line in edit_file('PKGBUILD'):
-        line: str
-        if line.startswith('depends_x86_64='):
-            print('makedepends=(nvidia-utils)')
-        print(line)
+    # for line in edit_file('PKGBUILD'):
+    #     line: str
+    #     if line.startswith('depends_x86_64='):
+    #         print('makedepends=(nvidia-utils)')
+    #     print(line)
